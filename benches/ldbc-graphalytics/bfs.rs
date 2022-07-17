f153f9e1e0d299d9ffc555cf8fd8b0eb9a70048f
Chase Wilson <me@chasewilson.dev>
2022-07-17 15:20:32 -0500

    Added semijoin

--- benches/ldbc-graphalytics/bfs.rs
+++ benches/ldbc-graphalytics/bfs.rs
@@ -5,60 +5,29 @@
 //! [2.3.1]: https://arxiv.org/pdf/2011.15028v4.pdf#subsection.2.3.1
 //! [A.1]: https://arxiv.org/pdf/2011.15028v4.pdf#section.A.1
 
-use crate::{Weight, DATA_PATH};
-use dbsp::{
-    algebra::HasOne,
-    operator::recursive::RecursiveStreams,
-    time::NestedTimestamp32,
-    trace::{
-        ord::{indexed_zset_batch::OrdIndexedZSetBuilder, zset_batch::OrdZSetBuilder},
-        BatchReader, Builder, Cursor,
-    },
-    zset_set, Circuit, OrdIndexedZSet, OrdZSet, Stream,
-};
-use std::{
-    fs::{self, File},
-    io::{self, BufRead, BufReader, BufWriter, Write},
-    path::Path,
-    process::Command,
-};
+use crate::data::{Distance, DistanceSet, EdgeMap, Node, VertexSet};
+use dbsp::{operator::recursive::RecursiveStreams, time::NestedTimestamp32, Circuit, Stream};
 
-type Node = u64;
-type Distance = u64;
+type Edges<P> = Stream<Circuit<P>, EdgeMap>;
+type Vertices<P> = Stream<Circuit<P>, VertexSet>;
+type Distances<P> = Stream<Circuit<P>, DistanceSet>;
 
-type EdgeMap = OrdIndexedZSet<Node, Node, Weight>;
-type VertexSet = OrdZSet<Node, Weight>;
-type DistanceSet = OrdZSet<(Node, Distance), Weight>;
-
-pub fn bfs<P>(
-    roots: Stream<Circuit<P>, VertexSet>,
-    vertices: Stream<Circuit<P>, VertexSet>,
-    edges: Stream<Circuit<P>, EdgeMap>,
-) -> Stream<Circuit<P>, DistanceSet>
+// TODO: This straight up won't work on the quantities of data required by the
+//       higher scale factor benchmarks, there's simply too much data. We'll
+//       have to process batched data sourced from a custom operator that slowly
+//       loads the input data so we don't OOM
+pub fn bfs<P>(roots: Vertices<P>, vertices: Vertices<P>, edges: Edges<P>) -> Distances<P>
 where
     P: Clone + 'static,
-    Stream<Circuit<Circuit<P>>, DistanceSet>:
-        RecursiveStreams<Circuit<Circuit<P>>, Output = Stream<Circuit<P>, DistanceSet>>,
+    Distances<Circuit<P>>: RecursiveStreams<Circuit<Circuit<P>>, Output = Distances<P>>,
 {
     // Initialize the roots to have a distance of zero
     let root_nodes = roots.index_with_generic::<DistanceSet, _>(|&root| ((root, 0), ()));
 
-    // edges.inspect(|edges| {
-    //     let mut cursor = edges.cursor();
-    //     while cursor.key_valid() {
-    //         let key = *cursor.key();
-    //         while cursor.val_valid() {
-    //             let value = *cursor.val();
-    //             println!("edges: {key}, {value} {:+}", cursor.weight());
-    //             cursor.step_val();
-    //         }
-    //         cursor.step_key();
-    //     }
-    // });
-
     let distances = root_nodes
         .circuit()
-        .recursive(|scope, nodes: Stream<_, DistanceSet>| {
+        // TODO: Can recurse over the indexed version of `nodes` instead of having to re-index it?
+        .recursive(|scope, nodes: Distances<_>| {
             // Import the nodes and edges into the recursive scope
             let root_nodes = root_nodes.delta0(scope);
             let edges = edges.delta0(scope);
@@ -76,15 +45,6 @@ where
                 // is sorted so we can simply select the first element
                 .aggregate_incremental_nested(|&node, distances| (node, *distances[0].0));
 
-            // distances.inspect(|distances: &DistanceSet| {
-            //     let mut cursor = distances.cursor();
-            //     while cursor.key_valid() {
-            //         let key = *cursor.key();
-            //         println!("distances (recursive): {key:?}: {:+}", cursor.weight());
-            //         cursor.step_key();
-            //     }
-            // });
-
             Ok(distances)
         })
         .expect("failed to build dfs recursive scope");
@@ -95,176 +55,14 @@ where
     let unreachable_nodes =
         antijoin(&vertices, &reachable_nodes).map_keys(|&node| (node, i64::MAX as Distance));
 
-    // reachable_nodes.inspect(|distances: &VertexSet| {
-    //     let mut cursor = distances.cursor();
-    //     while cursor.key_valid() {
-    //         let key = *cursor.key();
-    //         println!("reachable_nodes: {key:?}: {:+}", cursor.weight());
-    //         cursor.step_key();
-    //     }
-    // });
-
     distances.plus(&unreachable_nodes)
 }
 
 // FIXME: Replace with a dedicated antijoin
-fn antijoin<P>(
-    vertices: &Stream<Circuit<P>, VertexSet>,
-    reachable_nodes: &Stream<Circuit<P>, VertexSet>,
-) -> Stream<Circuit<P>, VertexSet>
+fn antijoin<P>(vertices: &Vertices<P>, reachable_nodes: &Vertices<P>) -> Vertices<P>
 where
     P: Clone + 'static,
 {
-    let reachable_vertices = vertices.join::<(), _, _, _>(reachable_nodes, |&node, _, _| node);
-    vertices.minus(&reachable_vertices)
-}
-
-// https://arxiv.org/pdf/2011.15028v4.pdf#subsection.3.2.1
-// TODO: example-undirected https://surfdrive.surf.nl/files/index.php/s/enKFbXmUBP2rxgB/download
-pub fn load_datasets(dataset_name: &str, dataset_url: &str) -> DfsData {
-    let data = Path::new(DATA_PATH).canonicalize().unwrap();
-    fs::create_dir_all(&data).expect("failed to create data directory");
-
-    let required_files = [
-        format!("{dataset_name}.v"),
-        format!("{dataset_name}.e"),
-        format!("{dataset_name}-BFS"),
-    ];
-    if !required_files.iter().all(|file| data.join(file).exists()) {
-        let archive = data.join(format!("{dataset_name}.tar.zst"));
-        if !archive.exists() {
-            print!("downloading dataset... ");
-            io::stdout().flush().unwrap();
-
-            let mut contents = ureq::get(dataset_url)
-                .call()
-                .expect("failed to example-directed download data file")
-                .into_reader();
-
-            let mut file = BufWriter::new(
-                File::create(&archive).expect("failed to create example-directed download file"),
-            );
-
-            io::copy(&mut contents, &mut file).expect("failed to write archive to disk");
-
-            println!("done");
-            // Make sure we drop & flush the file before we run tar
-        }
-
-        print!("decompressing dataset... ");
-        io::stdout().flush().unwrap();
-
-        let status = Command::new("zstd")
-            .arg("-d")
-            .arg(archive)
-            .current_dir(&data)
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap();
-
-        if !status.success() {
-            println!("failed");
-            panic!("failed to run zstd on {dataset_name} archive");
-        }
-
-        let status = Command::new("tar")
-            .arg("-xf")
-            .arg(data.join(format!("{dataset_name}.tar")))
-            .current_dir(&data)
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap();
-
-        if !status.success() {
-            println!("failed");
-            panic!("failed to run zstd on {dataset_name} archive");
-        } else {
-            println!("done");
-        }
-    }
-
-    print!("reading dataset... ");
-    let vertices = load(&data, &required_files[0]);
-    let edges = load(&data, &required_files[1]);
-    let results = load(&data, &required_files[2]);
-
-    // TODO: Parse out directed-ness
-    let data = DfsData::new(vertices, edges, results, false);
-    println!("done");
-
-    data
-}
-
-fn load(path: &Path, file: &str) -> BufReader<File> {
-    // TODO: Download if file doesn't exist
-    BufReader::new(
-        File::open(path.join(file))
-            .unwrap_or_else(|error| panic!("failed to load '{file}': {error}")),
-    )
-}
-
-pub struct DfsData {
-    pub roots: VertexSet,
-    pub vertices: VertexSet,
-    pub edges: EdgeMap,
-    pub expected: DistanceSet,
-}
-
-impl DfsData {
-    fn new(
-        vertices_file: BufReader<File>,
-        edges_file: BufReader<File>,
-        results_file: BufReader<File>,
-        directed: bool,
-    ) -> Self {
-        // Our root node is 1
-        // TODO: Parse root node out of .properties file
-        let roots = zset_set! { 2 };
-
-        let mut vertices = OrdZSetBuilder::new(());
-        for line in vertices_file.lines() {
-            let vertex = line
-                .expect("failed to read line from vertex file")
-                .parse::<Node>()
-                .expect("failed to parse vertex");
-            vertices.push((vertex, (), Weight::one()));
-        }
-
-        let mut edges = OrdIndexedZSetBuilder::new(());
-        for line in edges_file.lines() {
-            let line = line.expect("failed to read line from edges file");
-            let mut line = line.splitn(3, ' ');
-
-            let src = line.next().unwrap().parse::<Node>().unwrap();
-            let dest = line.next().unwrap().parse::<Node>().unwrap();
-            // Weight is contained in third split as a float
-
-            edges.push((src, dest, Weight::one()));
-
-            // Add in the reversed edge if the graph isn't directed
-            if !directed {
-                edges.push((dest, src, Weight::one()));
-            }
-        }
-
-        let mut results = OrdZSetBuilder::new(());
-        for line in results_file.lines() {
-            let line = line.expect("failed to read line from results file");
-            let mut line = line.splitn(2, ' ');
-
-            let node = line.next().unwrap().parse::<Node>().unwrap();
-            let distance = line.next().unwrap().parse::<Distance>().unwrap();
-
-            results.push(((node, distance), (), Weight::one()));
-        }
-
-        Self {
-            roots,
-            vertices: vertices.done(),
-            edges: edges.done(),
-            expected: results.done(),
-        }
-    }
+    let reachable_nodes = vertices.semijoin::<(), _, _, _>(reachable_nodes, |&vertex, &()| vertex);
+    vertices.minus(&reachable_nodes)
 }
--- src/operator/mod.rs
+++ src/operator/mod.rs
@@ -1,68 +1,52 @@
 //! Some basic operators.
 
 pub mod adapter;
-pub use adapter::{BinaryOperatorAdapter, UnaryOperatorAdapter};
-
-pub(crate) mod inspect;
-pub use inspect::Inspect;
+pub mod apply2;
+pub mod communication;
+pub mod recursive;
 
 pub(crate) mod apply;
-pub use apply::Apply;
-
-pub mod apply2;
-
-mod plus;
-pub use plus::{Minus, Plus};
-
-mod z1;
-pub use z1::{DelayedFeedback, DelayedNestedFeedback, Z1Nested, Z1};
-
-mod generator;
-pub use generator::{Generator, GeneratorNested};
-
-mod consolidate;
-mod integrate;
-mod trace;
-
-pub mod communication;
-
-mod differentiate;
-
-mod delta0;
-pub use delta0::Delta0;
-
-mod condition;
-pub use condition::Condition;
-
-mod index;
-pub use index::Index;
-
-mod join;
-pub use join::Join;
-
-mod join_range;
-
-mod sum;
-pub use sum::Sum;
-
-mod distinct;
-pub use distinct::Distinct;
-
-mod filter_map;
-pub use filter_map::FilterMap;
+pub(crate) mod inspect;
 
 mod aggregate;
-pub use aggregate::Aggregate;
-
-mod window;
-
+mod condition;
+mod consolidate;
 #[cfg(feature = "with-csv")]
 mod csv;
-
+mod delta0;
+mod differentiate;
+mod distinct;
+mod filter;
+mod filter_map;
+mod generator;
+mod index;
+mod integrate;
+mod join;
+mod map;
 mod neg;
-pub use neg::UnaryMinus;
-
-pub mod recursive;
+mod plus;
+mod semijoin;
+mod sum;
+mod trace;
+mod window;
+mod z1;
 
 #[cfg(feature = "with-csv")]
 pub use self::csv::CsvSource;
+pub use adapter::{BinaryOperatorAdapter, UnaryOperatorAdapter};
+pub use aggregate::Aggregate;
+pub use apply::Apply;
+pub use condition::Condition;
+pub use delta0::Delta0;
+pub use distinct::Distinct;
+pub use filter::FilterKeys;
+pub use filter_map::FilterMapKeys;
+pub use generator::{Generator, GeneratorNested};
+pub use index::Index;
+pub use inspect::Inspect;
+pub use join::Join;
+pub use map::{MapKeys, MapValues};
+pub use neg::UnaryMinus;
+pub use plus::{Minus, Plus};
+pub use sum::Sum;
+pub use z1::{DelayedFeedback, DelayedNestedFeedback, Z1Nested, Z1};
