274e9bc5a5d7b0f2a661b8371bda2062d6336571
Michael Nelson <minelson@vmware.com>
2022-07-18 07:38:50 +1000

    [nexmark] Add supporting functions for the bids generator. (#112)
    Adds the supporting functions for the bids generator as well as adding the `SizedCache` to the NexmarkGenerator allowing us to use the struct's rng and implement the cached function on the struct itself.

--- benches/nexmark/generator/mod.rs
+++ benches/nexmark/generator/mod.rs
@@ -3,9 +3,11 @@
 //! Based on the equivalent [Nexmark Flink generator API](https://github.com/nexmark/nexmark/blob/v0.2.0/nexmark-flink/src/main/java/com/github/nexmark/flink/generator).
 
 use self::config::Config;
+use cached::SizedCache;
 use rand::Rng;
 
 mod auctions;
+mod bids;
 mod config;
 mod people;
 mod price;
@@ -14,4 +16,20 @@ mod strings;
 pub struct NexmarkGenerator<R: Rng> {
     config: Config,
     rng: R,
+    bid_channel_cache: SizedCache<usize, (String, String)>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bids::CHANNELS_NUMBER;
+    use rand::rngs::mock::StepRng;
+
+    pub fn make_test_generator() -> NexmarkGenerator<StepRng> {
+        NexmarkGenerator {
+            config: Config::default(),
+            rng: StepRng::new(0, 1),
+            bid_channel_cache: SizedCache::with_size(CHANNELS_NUMBER),
+        }
+    }
 }
