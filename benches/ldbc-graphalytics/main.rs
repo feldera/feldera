5f6673c19ed1d28410ab8291ebf57fa58aaf5704
Chase Wilson <me@chasewilson.dev>
2022-08-10 11:20:18 -0500

    Moved allocation code

--- benches/ldbc-graphalytics/main.rs
+++ benches/ldbc-graphalytics/main.rs
@@ -40,67 +40,6 @@ use std::{
 #[global_allocator]
 static ALLOC: MiMalloc = MiMalloc;
 
-struct MiMalloc;
-
-impl MiMalloc {
-    /// Reset allocation statistics
-    pub fn reset_stats(&self) {
-        unsafe { mi_stats_reset() };
-    }
-
-    pub fn stats(&self) -> AllocStats {
-        let mut stats = AllocStats::default();
-        unsafe {
-            mi_process_info(
-                &mut stats.elapsed_ms,
-                &mut stats.user_ms,
-                &mut stats.system_ms,
-                &mut stats.current_rss,
-                &mut stats.peak_rss,
-                &mut stats.current_commit,
-                &mut stats.peak_commit,
-                &mut stats.page_faults,
-            );
-        }
-
-        stats
-    }
-}
-
-unsafe impl GlobalAlloc for MiMalloc {
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        mi_malloc_aligned(layout.size(), layout.align()).cast()
-    }
-
-    #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        mi_zalloc_aligned(layout.size(), layout.align()).cast()
-    }
-
-    #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        mi_realloc_aligned(ptr.cast(), new_size, layout.align()).cast()
-    }
-
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        mi_free(ptr.cast());
-    }
-}
-
-#[derive(Debug, Default)]
-struct AllocStats {
-    elapsed_ms: usize,
-    user_ms: usize,
-    system_ms: usize,
-    current_rss: usize,
-    peak_rss: usize,
-    current_commit: usize,
-    peak_commit: usize,
-    page_faults: usize,
-}
-
 enum OutputData {
     None,
     Bfs(DistanceSet<i8>),
@@ -477,4 +416,65 @@ struct Config {
     #[doc(hidden)]
     #[clap(long = "bench", hide = true)]
     __bench: bool,
+}
+
+struct MiMalloc;
+
+impl MiMalloc {
+    /// Reset allocation statistics
+    pub fn reset_stats(&self) {
+        unsafe { mi_stats_reset() };
+    }
+
+    pub fn stats(&self) -> AllocStats {
+        let mut stats = AllocStats::default();
+        unsafe {
+            mi_process_info(
+                &mut stats.elapsed_ms,
+                &mut stats.user_ms,
+                &mut stats.system_ms,
+                &mut stats.current_rss,
+                &mut stats.peak_rss,
+                &mut stats.current_commit,
+                &mut stats.peak_commit,
+                &mut stats.page_faults,
+            );
+        }
+
+        stats
+    }
+}
+
+unsafe impl GlobalAlloc for MiMalloc {
+    #[inline]
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        mi_malloc_aligned(layout.size(), layout.align()).cast()
+    }
+
+    #[inline]
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        mi_zalloc_aligned(layout.size(), layout.align()).cast()
+    }
+
+    #[inline]
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        mi_realloc_aligned(ptr.cast(), new_size, layout.align()).cast()
+    }
+
+    #[inline]
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        mi_free(ptr.cast());
+    }
+}
+
+#[derive(Debug, Default)]
+struct AllocStats {
+    elapsed_ms: usize,
+    user_ms: usize,
+    system_ms: usize,
+    current_rss: usize,
+    peak_rss: usize,
+    current_commit: usize,
+    peak_commit: usize,
+    page_faults: usize,
 }
