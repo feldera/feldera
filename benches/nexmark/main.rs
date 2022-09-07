ff3e9086e32e6faec1f1235826b3aa15055c6d28
Michael Nelson <minelson@vmware.com>
2022-09-07 10:35:16 +1000

    [nexmark] Add query 14 showing custom types, functions and user-defined ranges. (#173)
    * [nexmark] Add query 14 showing custom types, functions and user-defined ranges.
    
    Signed-off-by: Michael Nelson <minelson@vmware.com>

--- benches/nexmark/main.rs
+++ benches/nexmark/main.rs
@@ -20,7 +20,7 @@ use dbsp::{
     nexmark::{
         config::Config as NexmarkConfig,
         model::Event,
-        queries::{q0, q1, q2, q3, q4, q6, q9},
+        queries::{q0, q1, q14, q2, q3, q4, q6, q9},
         NexmarkSource,
     },
     trace::ord::OrdZSet,
@@ -315,7 +315,8 @@ fn main() -> Result<()> {
         ("q3", q3),
         ("q4", q4),
         ("q6", q6),
-        ("q9", q9)
+        ("q9", q9),
+        ("q14", q14)
     );
 
     let ascii_table = create_ascii_table();
