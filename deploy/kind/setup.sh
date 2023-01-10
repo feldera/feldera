c19f06ad678fa50264363e65eeb5e58ce5cef8f7
Lalith Suresh <lsuresh@vmware.com>
2023-01-10 08:02:42 -0800

    Deploy DBSP manager in a kind cluster.
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

---
+++ deploy/kind/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+
+#### Refresh kind
+kind delete cluster
+
+#### Script from https://kind.sigs.k8s.io/docs/user/local-registry/
+#### Sets it up so the kind cluster uses the local docker image registry
+
+set -o errexit
+
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+# create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --image=kindest/node:v1.23.13 --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
+EOF
+
+# connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+#### Install Redpanda helm charts
+helm repo add redpanda https://charts.redpanda.com/
+helm repo update
+helm install redpanda redpanda/redpanda --set statefulset.replicas=1
+
+
+docker tag dbspmanager localhost:5001/dbspmanager && docker push localhost:5001/dbspmanager
