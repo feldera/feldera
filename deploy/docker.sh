515a9074d409c5b4d2cfc70a6fd9b8ec050a7175
Lalith Suresh <lsuresh@vmware.com>
2023-02-06 11:11:29 -0800

    Check if SQL compiler path exists before using it in docker script
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- deploy/docker.sh
+++ deploy/docker.sh
@@ -6,6 +6,11 @@ DEFAULT_SQL_COMPILER_PATH="${THIS_DIR}/../../sql-to-dbsp-compiler"
 SQL_COMPILER="${1:-$DEFAULT_SQL_COMPILER_PATH}"
 DBSP_PATH="${THIS_DIR}/../"
 
+if [ ! -d "$SQL_COMPILER" ]; then
+    echo "Could not find SQL compiler source (https://github.com/vmware/sql-to-dbsp-compiler) at path $SQL_COMPILER."
+    exit 1
+fi
+
 (cd ${DBSP_PATH} && (git ls-files | tar -cvf ${THIS_DIR}/dbsp_files.tar -T -))
 (cd ${SQL_COMPILER} && (git ls-files | tar -cvf ${THIS_DIR}/sql_compiler_files.tar -T -))
 
