5019a70dfec96d45b0e84cde5895be8a9d869ded
Leonid Ryzhyk <ryzhyk@gmail.com>
2023-02-18 00:21:55 -0800

    Parse manager config from file or CLI.
    The manager can read the entire configuration from either command line
    or config file.

--- scripts/start_manager.sh
+++ scripts/start_manager.sh
@@ -16,26 +16,16 @@ WORKING_DIR=$(cd "$(dirname "${1}")" && pwd -P)/$(basename "${1}")
 DEFAULT_BIND_ADDRESS="127.0.0.1"
 BIND_ADDRESS="${2:-$DEFAULT_BIND_ADDRESS}"
 
-mkdir -p "${WORKING_DIR}"
-
 # Kill manager. pkill doesn't handle process names >15 characters.
 pkill -9 dbsp_pipeline_
 
 set -e
 
-# Start pipeline manager.
-
-manager_config="
-    port: 8080
-    bind_address:  \"${BIND_ADDRESS}\"
-    logfile: \"${WORKING_DIR}/manager.log\"
-    working_directory: \"${WORKING_DIR}\"
-    sql_compiler_home: \"${SQL_COMPILER_DIR}\"
-    dbsp_override_path: \"${ROOT_DIR}\"
-    with_prometheus: false
-    debug: false"
-
-printf "$manager_config" > "${WORKING_DIR}/manager.yaml"
-
 cd "${MANAGER_DIR}" && ~/.cargo/bin/cargo build --release
-cd "${MANAGER_DIR}" && ~/.cargo/bin/cargo run --release -- --static-html=static --config-file="${WORKING_DIR}/manager.yaml"
+cd "${MANAGER_DIR}" && ~/.cargo/bin/cargo run --release -- \
+    --bind-address="${BIND_ADDRESS}" \
+    --working-directory="${WORKING_DIR}" \
+    --sql-compiler-home="${SQL_COMPILER_DIR}" \
+    --dbsp-override-path="${ROOT_DIR}" \
+    --static-html=static \
+    --unix-daemon
