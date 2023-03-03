77774455cae66c68875c25f6b36ee032c9da2ccf
Leonid Ryzhyk <ryzhyk@gmail.com>
2023-03-03 10:46:39 -0800

    Make SQL compiler a git submodule.
    - Add SQL-to-DBSP compiler as a submodule to the repo.
    - Change Dockerfile to update the submodule before building
      the SQL compiler.
    - Remove the no longer needed docker.sh.
    - Improve the `start_manager.sh` script:
      - Update and build the SQL compiler submodule before running the
        manager.
      - Use ~/.dbsp as default working directory -- this way the script can
        be run without arguments.
    - Change pipeline manager makefile to build the compiler
      before running end-to-end tests.
    
    With these changes, one should be able to fork the repo and then start
    the service with one command (`./scripts/start_manager.sh`).

--- scripts/start_manager.sh
+++ scripts/start_manager.sh
@@ -5,14 +5,23 @@ ROOT_DIR="${THIS_DIR}/.."
 SQL_COMPILER_DIR="${ROOT_DIR}/sql-to-dbsp-compiler"
 MANAGER_DIR="${ROOT_DIR}/crates/pipeline_manager"
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage '$0 <working_directory_path> <bind address (optional)>'"
-    exit 1
+# if [ "$#" -lt 1 ]; then
+#    echo "Usage '$0 <working_directory_path> <bind address (optional)>'"
+#    exit 1
+# fi
+
+if [[ ! -d ${SQL_COMPILER_DIR}/.git || -z $(cd ${SQL_COMPILER_DIR} && git branch --show-current) ]]
+then
+    git submodule update --init
 fi
+cd "${SQL_COMPILER_DIR}/SQL-compiler" && mvn -DskipTests package
+
+
+WORKING_DIR="${1:-${HOME}/.dbsp}"
 
 # This is the most portable way to get an absolute path since
 # 'realpath' is not available on MacOS by default.
-WORKING_DIR=$(cd "$(dirname "${1}")" && pwd -P)/$(basename "${1}")
+WORKING_DIR_ABS=$(cd "$(dirname "${WORKING_DIR}")" && pwd -P)/$(basename "${WORKING_DIR}")
 DEFAULT_BIND_ADDRESS="127.0.0.1"
 BIND_ADDRESS="${2:-$DEFAULT_BIND_ADDRESS}"
 
@@ -24,7 +33,7 @@ set -e
 cd "${MANAGER_DIR}" && ~/.cargo/bin/cargo build --release
 cd "${MANAGER_DIR}" && ~/.cargo/bin/cargo run --release -- \
     --bind-address="${BIND_ADDRESS}" \
-    --working-directory="${WORKING_DIR}" \
+    --working-directory="${WORKING_DIR_ABS}" \
     --sql-compiler-home="${SQL_COMPILER_DIR}" \
     --dbsp-override-path="${ROOT_DIR}" \
     --static-html=static \
