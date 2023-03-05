67490d893f1bfe1ebd882f06fd4f4bb75e6861b4
Leonid Ryzhyk <ryzhyk@gmail.com>
2023-03-04 17:57:00 -0800

    Use dockerized Redpanda for local demos.
    Start Redpanda container before starting local demo.

--- demo/docker_demo.sh
+++ demo/docker_demo.sh
@@ -4,4 +4,6 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -e
 
+export REDPANDA_BROKERS=localhost:19092
+
 DBSP_MANAGER="localhost:8085" ${THIS_DIR}/create_demo_projects.sh
