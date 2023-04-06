d9b49136cd9e26fde5ad440fe6cfb70f7f090bbf
Lalith Suresh <lsuresh@vmware.com>
2023-04-06 09:01:24 -0700

    Create 3 kind workers to not have to create tolerations on pods
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- deploy/kind/setup.sh
+++ deploy/kind/setup.sh
@@ -22,6 +22,24 @@ fi
 cat <<EOF | kind create cluster --image=kindest/node:v1.23.13 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+    kubeadmConfigPatches:
+    - |
+      kind: InitConfiguration
+      nodeRegistration:
+        kubeletExtraArgs:
+          node-labels: "ingress-ready=true"
+    extraPortMappings:
+    - containerPort: 80
+      hostPort: 80
+      protocol: TCP
+    - containerPort: 443
+      hostPort: 443
+      protocol: TCP
+  - role: worker
+  - role: worker
+  - role: worker
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
