320dfa05150e250dbed398b59382dc5d7593e842
Gerd Zellweger <mail@gerdzellweger.com>
2023-04-09 16:36:10 -0700

    Rerouting / to home.

---
+++ web-ui/src/components/spinner/index.tsx
@@ -0,0 +1,22 @@
+import Box, { BoxProps } from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const LoadingScreen = ({ sx }: { sx?: BoxProps['sx'] }) => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        ...sx
+      }}
+    >
+      <img src='/images/dbsp-primary-main.svg' alt='Logo' width='35%' height='35%' />
+      <CircularProgress disableShrink sx={{ mt: 6 }} />
+    </Box>
+  )
+}
+
+export default LoadingScreen
