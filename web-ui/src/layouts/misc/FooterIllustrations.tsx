45da86840dd4c0d3da8794c5b409fa51f0f30c7f
Gerd Zellweger <mail@gerdzellweger.com>
2023-04-24 19:05:12 -0700

    No trees or people images.

--- web-ui/src/layouts/misc/FooterIllustrations.tsx
+++ web-ui/src/layouts/misc/FooterIllustrations.tsx
@@ -1,15 +1,10 @@
-import { Fragment, ReactNode } from 'react'
+import { Fragment } from 'react'
 import Image from 'next/image'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { styled, useTheme } from '@mui/material/styles'
 
-import Tree2 from 'public/images/pages/tree-2.png'
 import MiscMaskLight from 'public/images/pages/misc-mask-light.png'
 import MiscMaskDark from 'public/images/pages/misc-mask-dark.png'
-
-interface FooterIllustrationsProp {
-  image?: ReactNode
-}
 
 // Styled Components
 const MaskImg = styled(Image)(() => ({
@@ -19,18 +14,8 @@ const MaskImg = styled(Image)(() => ({
   position: 'absolute'
 }))
 
-const TreeImg = styled(Image)(({ theme }) => ({
-  left: '2.25rem',
-  bottom: '4.25rem',
-  position: 'absolute',
-  [theme.breakpoints.down('lg')]: {
-    left: 0,
-    bottom: 0
-  }
-}))
 
-const FooterIllustrations = (props: FooterIllustrationsProp) => {
-  const { image } = props
+const FooterIllustrations = () => {
   const theme = useTheme()
 
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
@@ -39,7 +24,6 @@ const FooterIllustrations = (props: FooterIllustrationsProp) => {
   if (!hidden) {
     return (
       <Fragment>
-        {image || <TreeImg alt='tree' src={Tree2} />}
         <MaskImg alt='mask' src={mask} />
       </Fragment>
     )
--- web-ui/src/pages/404.tsx
+++ web-ui/src/pages/404.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
@@ -8,8 +7,6 @@ import Box, { BoxProps } from '@mui/material/Box'
 import BlankLayout from 'src/@core/layouts/BlankLayout'
 import FooterIllustrations from 'src/layouts/misc/FooterIllustrations'
 
-import Img404 from 'public/images/pages/404.png'
-import Tree from 'public/images/pages/tree.png'
 
 const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   [theme.breakpoints.down('md')]: {
@@ -17,46 +14,22 @@ const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   }
 }))
 
-const Img = styled(Image)(({ theme }) => ({
-  marginBottom: theme.spacing(10),
-  [theme.breakpoints.down('lg')]: {
-    height: 450,
-    marginTop: theme.spacing(10)
-  },
-  [theme.breakpoints.down('md')]: {
-    height: 400
-  },
-  [theme.breakpoints.up('lg')]: {
-    marginTop: theme.spacing(13)
-  }
-}))
-
-const TreeIllustration = styled(Image)(({ theme }) => ({
-  left: 0,
-  bottom: '5rem',
-  position: 'absolute',
-  [theme.breakpoints.down('lg')]: {
-    bottom: 0
-  }
-}))
-
 const Error404 = () => {
   return (
     <Box className='content-center'>
       <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <BoxWrapper>
+        <BoxWrapper sx={{ p: 10 }}>
           <Typography variant='h1'>404</Typography>
           <Typography variant='h5' sx={{ mb: 1, fontSize: '1.5rem !important' }}>
             Page Not Found ⚠️
           </Typography>
           <Typography variant='body2'>We couldn&prime;t find the page you are looking for.</Typography>
         </BoxWrapper>
-        <Img height='487' alt='error-illustration' src={Img404} />
         <Button component={Link} href='/' variant='contained' sx={{ px: 5.5 }}>
           Back to Home
         </Button>
       </Box>
-      <FooterIllustrations image={<TreeIllustration alt='tree' src={Tree} />} />
+      <FooterIllustrations />
     </Box>
   )
 }
