822ab0e39732d19b0d7097c3bf2543dc9eab5090
Gerd Zellweger <mail@gerdzellweger.com>
2023-04-24 19:05:12 -0700

    No trees or people images.

--- web-ui/src/pages/401.tsx
+++ web-ui/src/pages/401.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
@@ -8,25 +7,9 @@ import Box, { BoxProps } from '@mui/material/Box'
 import BlankLayout from 'src/@core/layouts/BlankLayout'
 import FooterIllustrations from 'src/layouts/misc/FooterIllustrations'
 
-import Img401 from 'public/images/pages/401.png'
-
 const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   [theme.breakpoints.down('md')]: {
     width: '90vw'
-  }
-}))
-
-const Img = styled(Image)(({ theme }) => ({
-  marginBottom: theme.spacing(10),
-  [theme.breakpoints.down('lg')]: {
-    height: 450,
-    marginTop: theme.spacing(10)
-  },
-  [theme.breakpoints.down('md')]: {
-    height: 400
-  },
-  [theme.breakpoints.up('lg')]: {
-    marginTop: theme.spacing(13)
   }
 }))
 
@@ -34,14 +17,13 @@ const Error401 = () => {
   return (
     <Box className='content-center'>
       <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <BoxWrapper>
+        <BoxWrapper sx={{ p: 10 }}>
           <Typography variant='h1'>401</Typography>
           <Typography variant='h5' sx={{ mb: 1, fontSize: '1.5rem !important' }}>
             You are not authorized! 🔐
           </Typography>
           <Typography variant='body2'>You don&prime;t have permission to access this page.</Typography>
         </BoxWrapper>
-        <Img height='487' alt='error-illustration' src={Img401} />
         <Button component={Link} href='/' variant='contained' sx={{ px: 5.5 }}>
           Back to Home
         </Button>
--- web-ui/src/pages/500.tsx
+++ web-ui/src/pages/500.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
@@ -8,35 +7,9 @@ import Box, { BoxProps } from '@mui/material/Box'
 import BlankLayout from 'src/@core/layouts/BlankLayout'
 import FooterIllustrations from 'src/layouts/misc/FooterIllustrations'
 
-import Img500 from 'public/images/pages/404.png'
-import Tree3 from 'public/images/pages/tree-3.png'
-
 const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   [theme.breakpoints.down('md')]: {
     width: '90vw'
-  }
-}))
-
-const Img = styled(Image)(({ theme }) => ({
-  marginBottom: theme.spacing(10),
-  [theme.breakpoints.down('lg')]: {
-    height: 450,
-    marginTop: theme.spacing(10)
-  },
-  [theme.breakpoints.down('md')]: {
-    height: 400
-  },
-  [theme.breakpoints.up('lg')]: {
-    marginTop: theme.spacing(13)
-  }
-}))
-
-const TreeIllustration = styled(Image)(({ theme }) => ({
-  left: 0,
-  bottom: '5rem',
-  position: 'absolute',
-  [theme.breakpoints.down('lg')]: {
-    bottom: 0
   }
 }))
 
@@ -44,19 +17,18 @@ const Error500 = () => {
   return (
     <Box className='content-center'>
       <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <BoxWrapper>
+        <BoxWrapper sx={{ p: 10 }}>
           <Typography variant='h1'>500</Typography>
           <Typography variant='h5' sx={{ mb: 1, fontSize: '1.5rem !important' }}>
             Internal server error 👨🏻‍💻
           </Typography>
           <Typography variant='body2'>Oops, something went wrong on the server!</Typography>
         </BoxWrapper>
-        <Img height='487' alt='error-illustration' src={Img500} />
         <Button component={Link} href='/' variant='contained' sx={{ px: 5.5 }}>
           Back to Home
         </Button>
       </Box>
-      <FooterIllustrations image={<TreeIllustration alt='tree' src={Tree3} />} />
+      <FooterIllustrations />
     </Box>
   )
 }
