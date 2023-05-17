feldera/feldera

commit bd2517f2874c0ca4b76ae82d2eb02bd4dcb85001

Improve documentation for the Rust API.
Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/dbsp/src/algebra/zset/zset_macro.rs
@@ -1,8 +1,20 @@
+#[cfg(doc)]
+use crate::zset_set;
+
 /// Create an indexed Z-set with specified elements.
 ///
 /// This macro is used in unit tests to create reference inputs and outputs.
 /// It generates an indexed Z-set of type
 /// [`OrdIndexedZSet`](crate::trace::ord::OrdIndexedZSet)s.
+///
+/// # Example
+///
+/// The following macro invocation yields an indexed Z-set with `(key, value,
+/// weight)` tuples `(1, 3, -1), (1, 2, 1), (2, 2, 1)`:
+///
+/// ```
+/// indexed_zset! {1 => {3 => -1, 2 => 1}, 2 => {2 => 1}}
+/// ```
 #[macro_export]
 macro_rules! indexed_zset {
     ( $($key:expr => { $($value:expr => $weight:expr),* }),* $(,)?) => {{
@@ -17,6 +29,18 @@ macro_rules! indexed_zset {
 ///
 /// This macro is used in unit tests to create reference inputs and outputs.
 /// It generates a Z-set of type [`OrdZSet`](crate::trace::ord::OrdZSet)s.
+///
+/// If all the elements in the Z-set will have weight 1, [`zset_set!`] is a
+/// little easier.
+///
+/// # Example
+///
+/// The following macro invocation yields an Z-set with `(key, weight)` tuples
+/// `(1, -1), (2, 1)`:
+///
+/// ```
+/// zset! {1 => -1, 2 => 1}
+/// ```
 #[macro_export]
 macro_rules! zset {
     ( $( $key:expr => $weight:expr ),* $(,)?) => {{
@@ -32,6 +56,15 @@ macro_rules! zset {
 ///
 /// This macro is used in unit tests to create reference inputs and outputs.
 /// It generates a Z-set of type [`OrdZSet`](crate::trace::ord::OrdZSet)s.
+///
+/// # Example
+///
+/// The following macro invocation yields an Z-set with `(key, weight)` tuples
+/// `(1, 1), (2, 1), (-3, 1)`:
+///
+/// ```
+/// zset_set! {1, 2, -3}
+/// ```
 #[macro_export]
 macro_rules! zset_set {
     ( $( $key:expr ),* $(,)?) => {{

crates/dbsp/src/algebra/checked_int.rs
@@ -6,8 +6,9 @@ use std::{
     ops::{Add, AddAssign, Neg},
 };
 
-/// Ring on numeric values that panics on overflow
-/// Computes exactly like any signed numeric value, but panics on overflow
+/// Ring on numeric values that panics on overflow.
+///
+/// Computes exactly like any signed numeric value, but panics on overflow.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 #[repr(transparent)]
 pub struct CheckedInt<T> {

crates/dbsp/src/algebra/floats.rs
@@ -15,7 +15,7 @@ use serde::{Deserialize, Serialize};
 macro_rules! float {
     ($($outer:ident($inner:ident)),* $(,)?) => {
         $(
-            #[doc = concat!("A wrapper around [`", stringify!($inner), "`] that allows using it as a DBSP weight")]
+            #[doc = concat!("A wrapper around [`", stringify!($inner), "`] that allows using it in a DBSP stream.")]
             #[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, SizeOf)]
             #[repr(transparent)]
             #[size_of(skip_all)]

crates/dbsp/src/algebra/zset/mod.rs
@@ -1,13 +1,55 @@
 #[macro_use]
 mod zset_macro;
 
+#[cfg(doc)]
+use crate::trace::{ord::OrdIndexedZSet, ord::OrdZSet, BatchReader};
 use crate::{
     algebra::{GroupValue, HasOne, HasZero, ZRingValue},
     trace::{cursor::Cursor, Batch, Builder},
     NumEntries,
 };
 
-/// An indexed Z-set maps arbitrary keys to Z-set values.
+/// A set of weighted key-value pairs.
+///
+/// An indexed Z-set is a set of `(key, value)` pairs.  **Pairs must
+/// be unique**, but keys need not be.
+///
+/// Each pair has a weight drawn from a ring, ordinarily the ring of
+/// integers ℤ (hence the name "Z-set").  Weights are often
+/// interpreted as the number of times that the pair appears in the
+/// set, and for this reason **a weight of 0 is disallowed** and must
+/// not appear in an indexed Z-set.  Negative weights are allowed,
+/// however, because of an important secondary interpretation as an
+/// "update" or "delta" to be added to some other Z-set: for this use,
+/// a positive weight represents adding copies of a pair and a
+/// negative weight represents removing them.
+///
+/// `IndexedZSet` has supertrait [`Batch`], which has supertrait
+/// [`BatchReader`].  These supertraits have all of the interesting
+/// related type definitions:
+///
+///  * The `Key` associated type, which is the type of `key`.
+///
+///  * The `Val` associated type, which is the type of `value`.
+///
+///  * The `R` associated type, which is the type of `weight`.  The client
+///    specifies this type.  `i32` and `i64` are common choices.
+///
+/// The "index" in `IndexedZSet` refers to how it contains key-value
+/// pairs: an `IndexedZSet` is often regarded as mapping from keys to
+/// values.  For a simpler Z-set, without the index, use `()` for
+/// `Val`, and indeed the [`ZSet`] trait simply contrains
+/// `IndexedZSet` with `Val = ()`.
+///
+/// `IndexedZSet` has no requirements for implementors beyond its
+/// supertraits, so it has no substantive implementations, only an
+/// empty blanket implementation for all types that satisfy its
+/// supertraits.  If DBSP client code needs to create its own
+/// `IndexedZSet`s, the [`OrdIndexedZSet`] and [`OrdZSet`] types are
+/// likely suitable.  But it is somewhat unusual for a client to need
+/// to do this outside of test code, in which the [`indexed_zset!`],
+/// [`zset!`], and [`zset_set!`] macros may be useful for creating
+/// (indexed) Z-sets with specified elements.
 pub trait IndexedZSet: Batch<Time = ()> + GroupValue + NumEntries {
     /// Returns an indexed Z-set that contains all elements with positive
     /// weights from `self` with weights set to 1.
@@ -100,11 +142,12 @@ where
 
 /// The Z-set trait.
 ///
-/// A Z-set is a set where each element has a weight.
-/// Weights belong to some ring.
+/// A Z-set is a set of unique keys, each associated with a nonzero weight.
+/// A `ZSet` is merely an `IndexedZSet` with its value type set to `()`.
 pub trait ZSet: IndexedZSet<Val = ()> {
-    /// Count of elements, where each count is multiplied by its weight.
-    /// Notice that the result could be 0 for non-empty sets.
+    /// Sum of the weights of the elements in the Z-set.  Weights can
+    /// be negative, so the result can be zero even if the Z-set is
+    /// nonempty.
     fn weighted_count(&self) -> Self::R;
 }
 

crates/dbsp/src/circuit/dbsp_handle.rs
@@ -11,17 +11,28 @@ use std::{
     time::Instant,
 };
 
+#[cfg(doc)]
+use crate::circuit::circuit_builder::Stream;
+
 impl Runtime {
     /// Instantiate a circuit in a multithreaded runtime.
     ///
     /// Creates a multithreaded runtime with `nworkers` worker threads and
-    /// instantiates identical circuits in each worker, using `constructor`
-    /// closure. Returns a [`DBSPHandle`] that can be used to control
-    /// the circuit and a user-defined value returned by the constructor.
-    /// This value typically contains one or more input handles used to
-    /// push data to the circuit at runtime (see
-    /// [`RootCircuit::add_input_stream`], [`RootCircuit::add_input_zset`], and
-    /// related methods).
+    /// instantiates an identical circuit in each worker, by calling
+    /// `constructor` once per worker.  `init_circuit` passes each call to
+    /// `constructor` a new [`RootCircuit`], in which it should create input
+    /// operators by calling [`RootCircuit::add_input_zset`] and related
+    /// methods.  Each of these calls returns an input handle and a `Stream`.
+    /// The `constructor` can call
+    /// [`Stream`] methods to do
+    /// computation, each of which yields further `Stream`s.  It can also use
+    /// [`Stream::output`] to obtain an output handle.
+    ///
+    /// Returns a [`DBSPHandle`] that the caller can use to control the circuit
+    /// and a user-defined value returned by the constructor.  The
+    /// `constructor` should use the latter to return the input and output
+    /// handles it obtains, because these allow the caller to feed input into
+    /// the circuit and read output from the circuit.
     ///
     /// To ensure that the multithreaded runtime has identical input/output
     /// behavior to a single-threaded circuit, the `constructor` closure
@@ -30,6 +41,8 @@ impl Runtime {
     /// operators in the same order.  This ensures that operators that shard
     /// their inputs across workers, e.g.,
     /// [`Stream::join`](`crate::Stream::join`), work correctly.
+    /// The closure should return the same value in each worker thread; this
+    /// function returns one of these values arbitrarily.
     ///
     /// TODO: Document other requirements.  Not all operators are currently
     /// thread-safe.

crates/dbsp/src/circuit/mod.rs
@@ -1,9 +1,17 @@
 //! Synchronous circuits over streams.
 //!
 //! A circuit consists of [operators](`operator_traits::Operator`) connected by
-//! [streams](`circuit_builder::Stream`). At every clock cycle, each operator in
-//! the circuit is triggered, consuming a single value from each of its input
-//! streams and emitting a single value to the output stream.
+//! [streams](`circuit_builder::Stream`). At every clock cycle, each operator
+//! consumes a single value from each of its input streams and emits a single
+//! value to the output stream.
+//!
+//! Use `RootCircuit::build` to create and populate an circuit that executes in
+//! the calling thread, or `Runtime::init_circuit` to create a multi-circuit,
+//! multi-worker threaded runtime.  These functions return a [`CircuitHandle`]
+//! or [`DBSPHandle`], respectively, that control the circuits' execution,
+//! plus, when used in the recommended way, additional input handles for
+//! feeding data into the circuits and output handles for obtaining their
+//! output.
 
 mod activations;
 mod dbsp_handle;

crates/dbsp/src/circuit/schedule/mod.rs
@@ -48,6 +48,10 @@ impl Display for Error {
 /// A scheduler defines the order in which nodes in a circuit are evaluated at
 /// runtime.
 ///
+/// Schedulers are per-[`Circuit`], not per-[`Runtime`](crate::Runtime).  They
+/// decide the order in which a step through the circuit evaluates operators.
+/// They do not affect process or thread scheduling.
+///
 /// A valid schedule evaluates each node exactly once, after all of its upstream
 /// nodes have been evaluated.  Note that this works for circuits with logical
 /// cycles, as all such cycles must contain a strict operator, which maps into a

commit ce095021ba433ca1df3f550e0d2f821eadf225af

Fix `cargo test --doc` failures.
Signed-off-by: Ben Pfaff <blp@feldera.com>
Fixes: bd2517f2874c ("Improve documentation for the Rust API.")

crates/dbsp/src/algebra/zset/zset_macro.rs
@@ -9,11 +9,17 @@ use crate::zset_set;
 ///
 /// # Example
 ///
-/// The following macro invocation yields an indexed Z-set with `(key, value,
-/// weight)` tuples `(1, 3, -1), (1, 2, 1), (2, 2, 1)`:
+/// The following example shows how to construct an indexed Z-set with `(key,
+/// value, weight)` tuples `(1, 3, -1), (1, 2, 1), (2, 2, 1)`:
 ///
 /// ```
-/// indexed_zset! {1 => {3 => -1, 2 => 1}, 2 => {2 => 1}}
+/// use dbsp::{indexed_zset, IndexedZSet};
+///
+/// let zset = indexed_zset! {1 => {3 => -1, 2 => 1}, 2 => {2 => 1}};
+/// assert_eq!(
+///     zset.iter().collect::<Vec<_>>(),
+///     vec![(1, 2, 1), (1, 3, -1), (2, 2, 1)]
+/// );
 /// ```
 #[macro_export]
 macro_rules! indexed_zset {
@@ -34,11 +40,17 @@ macro_rules! indexed_zset {
 ///
 /// # Example
 ///
-/// The following macro invocation yields an Z-set with `(key, weight)` tuples
-/// `(1, -1), (2, 1)`:
+/// The following example shows how to construct a Z-set with `(key, weight)`
+/// tuples `(1, -1), (2, 1)`:
 ///
 /// ```
-/// zset! {1 => -1, 2 => 1}
+/// use dbsp::{zset, IndexedZSet};
+///
+/// let zset = zset! {1 => -1, 2 => 1};
+/// assert_eq!(
+///     zset.iter().collect::<Vec<_>>(),
+///     vec![(1, (), -1), (2, (), 1)]
+/// );
 /// ```
 #[macro_export]
 macro_rules! zset {
@@ -58,11 +70,17 @@ macro_rules! zset {
 ///
 /// # Example
 ///
-/// The following macro invocation yields an Z-set with `(key, weight)` tuples
-/// `(1, 1), (2, 1), (-3, 1)`:
+/// The following examples shows how to construct a Z-set with `(key, weight)`
+/// tuples `(1, 1), (2, 1), (-3, 1)`:
 ///
 /// ```
-/// zset_set! {1, 2, -3}
+/// use dbsp::{zset_set, IndexedZSet};
+///
+/// let zset = zset_set! {1, 2, -3};
+/// assert_eq!(
+///     zset.iter().collect::<Vec<_>>(),
+///     vec![(-3, (), 1), (1, (), 1), (2, (), 1)]
+/// );
 /// ```
 #[macro_export]
 macro_rules! zset_set {

crates/dbsp/src/circuit/circuit_builder.rs
@@ -245,7 +245,7 @@ impl<D> StreamValue<D> {
 /// It's common to pass explicit type arguments to the functions that
 /// create input streams, e.g.:
 ///
-/// ```
+/// ```ignore
 /// circuit.add_input_indexed_zset::<KeyType, ValueType, WeightType>()
 /// ```
 ///
@@ -456,7 +456,7 @@ impl<D> StreamValue<D> {
 /// indexed Z-set with a unit key `()` before aggregating, then map again to
 /// remove the index if necessary, e.g.:
 ///
-/// ```
+/// ```ignore
 /// let max_auction_count = auction_counts
 ///     .map_index(|(_auction, count)| ((), *count))
 ///     .aggregate(Max)