feldera/dbsp

Force line-by-line processing from egress API
Otherwise it would sometimes try to parse invalid JSON
if the socket ends up returning something that ends
in the middle of the line.

Also change the details button in the pipeline table
to only appear if the pipeline has a committed revision.

Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

web-ui/src/streaming/introspection/IntrospectionTable.tsx
@@ -5,7 +5,7 @@ import { DataGridPro, GridColumns, useGridApiRef } from '@mui/x-data-grid-pro'
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import { PipelineDescr, PipelineStatus, OpenAPI, PipelineRevision } from 'src/types/manager'
-import { parse } from 'csv-parse'
+import useTableUpdater from './hooks/useTableUpdater'
 
 export type IntrospectionTableProps = {
   pipelineDescr: PipelineDescr | undefined
@@ -47,6 +47,7 @@ export const IntrospectionTable = ({ pipelineDescr, name }: IntrospectionTablePr
     }
   }, [pipelineRevisionQuery.isLoading, pipelineRevisionQuery.isError, pipelineRevisionQuery.data, setHeaders, name])
 
+  const tableUpdater = useTableUpdater()
   // Stream changes from backend and update the table
   useEffect(() => {
     if (
@@ -56,64 +57,12 @@ export const IntrospectionTable = ({ pipelineDescr, name }: IntrospectionTablePr
       headers !== undefined &&
       apiRef.current
     ) {
-      const watchStream = async function (url: string) {
-        const response = await fetch(url)
-        const reader = response.body?.getReader()
-        const decoder = new TextDecoder('utf-8')
-
-        while (true) {
-          const value = await reader?.read().then(function (result) {
-            return decoder.decode(result.value)
-          })
-          const obj = JSON.parse(value || '{}')
-          parse(
-            obj.text_data,
-            {
-              delimiter: ','
-            },
-            (error, result: string[][]) => {
-              if (error) {
-                console.error(error)
-              }
-
-              const newRows: any[] = result.map(row => {
-                const genId = row[0]
-                const weight = row[row.length - 1]
-                const fields = row.slice(0, row.length - 1)
-
-                const newRow = { genId, weight: parseInt(weight) } as any
-                headers.forEach((col, i) => {
-                  if (col.field !== 'genId' && col.field !== 'weight') {
-                    newRow[col.field] = fields[i - 1]
-                  }
-                })
-
-                return newRow
-              })
-
-              apiRef.current?.updateRows(
-                newRows
-                  .map(row => {
-                    const curRow = apiRef.current.getRow(row.genId)
-                    if (curRow !== null && curRow.weight + row.weight == 0) {
-                      return row
-                    } else if (curRow == null && row.weight < 0) {
-                      return null
-                    } else {
-                      return { ...row, weight: row.weight + (curRow?.weight || 0) }
-                    }
-                  })
-                  .filter(x => x !== null)
-              )
-            }
-          )
-        }
-      }
-
-      const url = OpenAPI.BASE + '/v0/pipelines/' + pipelineDescr.pipeline_id + '/egress/' + name + '?format=csv'
-      watchStream(url)
+      const url = new URL(
+        OpenAPI.BASE + '/v0/pipelines/' + pipelineDescr.pipeline_id + '/egress/' + name + '?format=csv'
+      )
+      tableUpdater(url, apiRef, headers)
     }
-  }, [pipelineDescr, name, apiRef, headers])
+  }, [pipelineDescr, name, apiRef, headers, tableUpdater])
 
   return (
     <Card>

feldera/feldera

web-ui/src/streaming/introspection/IntrospectionTable.tsx
@@ -5,7 +5,7 @@ import { DataGridPro, GridColumns, useGridApiRef } from '@mui/x-data-grid-pro'
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import { Pipeline, PipelineStatus, OpenAPI, PipelineRevision } from 'src/types/manager'
-import { parse } from 'csv-parse'
+import useTableUpdater from './hooks/useTableUpdater'
 
 export type IntrospectionTableProps = {
   pipeline: Pipeline | undefined
@@ -47,6 +47,7 @@ export const IntrospectionTable = ({ pipeline, name }: IntrospectionTableProps) 
     }
   }, [pipelineRevisionQuery.isLoading, pipelineRevisionQuery.isError, pipelineRevisionQuery.data, setHeaders, name])
 
+  const tableUpdater = useTableUpdater()
   // Stream changes from backend and update the table
   useEffect(() => {
     if (
@@ -56,64 +57,12 @@ export const IntrospectionTable = ({ pipeline, name }: IntrospectionTableProps) 
       headers !== undefined &&
       apiRef.current
     ) {
-      const watchStream = async function (url: string) {
-        const response = await fetch(url)
-        const reader = response.body?.getReader()
-        const decoder = new TextDecoder('utf-8')
-
-        while (true) {
-          const value = await reader?.read().then(function (result) {
-            return decoder.decode(result.value)
-          })
-          const obj = JSON.parse(value || '{}')
-          parse(
-            obj.text_data,
-            {
-              delimiter: ','
-            },
-            (error, result: string[][]) => {
-              if (error) {
-                console.error(error)
-              }
-
-              const newRows: any[] = result.map(row => {
-                const genId = row[0]
-                const weight = row[row.length - 1]
-                const fields = row.slice(0, row.length - 1)
-
-                const newRow = { genId, weight: parseInt(weight) } as any
-                headers.forEach((col, i) => {
-                  if (col.field !== 'genId' && col.field !== 'weight') {
-                    newRow[col.field] = fields[i - 1]
-                  }
-                })
-
-                return newRow
-              })
-
-              apiRef.current?.updateRows(
-                newRows
-                  .map(row => {
-                    const curRow = apiRef.current.getRow(row.genId)
-                    if (curRow !== null && curRow.weight + row.weight == 0) {
-                      return row
-                    } else if (curRow == null && row.weight < 0) {
-                      return null
-                    } else {
-                      return { ...row, weight: row.weight + (curRow?.weight || 0) }
-                    }
-                  })
-                  .filter(x => x !== null)
-              )
-            }
-          )
-        }
-      }
-
-      const url = OpenAPI.BASE + '/pipelines/' + pipeline.descriptor.pipeline_id + '/egress/' + name + '?format=csv'
-      watchStream(url)
+      const url = new URL(
+        OpenAPI.BASE + '/pipelines/' + pipeline.descriptor.pipeline_id + '/egress/' + name + '?format=csv'
+      )
+      tableUpdater(url, apiRef, headers)
     }
-  }, [pipeline, name, apiRef, headers])
+  }, [pipeline, name, apiRef, headers, tableUpdater])
 
   return (
     <Card>