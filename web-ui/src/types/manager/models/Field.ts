5a784ad879be547fee2225f16b7e2f21d048c054
Gerd Zellweger <mail@gerdzellweger.com>
2023-07-28 15:14:39 -0700

    Update typescript bindings.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- web-ui/src/types/manager/models/Field.ts
+++ web-ui/src/types/manager/models/Field.ts
@@ -2,11 +2,14 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { ColumnType } from './ColumnType'
+
 /**
  * A SQL field.
+ *
+ * Matches the Calcite JSON format.
  */
 export type Field = {
+  columntype: ColumnType
   name: string
-  nullable: boolean
-  type: string
 }
