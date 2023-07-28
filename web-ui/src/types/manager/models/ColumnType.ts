1d4f7c0cafe620301ee20490bd14075df1bd7e94
Gerd Zellweger <mail@gerdzellweger.com>
2023-07-28 15:14:39 -0700

    Update typescript bindings.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

---
+++ web-ui/src/types/manager/models/ColumnType.ts
@@ -0,0 +1,37 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * A SQL column type description.
+ *
+ * Matches the Calcite JSON format.
+ */
+export type ColumnType = {
+  component?: ColumnType | null
+  /**
+   * Does the type accept NULL values?
+   */
+  nullable: boolean
+  /**
+   * Precision of the type.
+   *
+   * # Examples
+   * - `VARCHAR` sets precision to `-1`.
+   * - `VARCHAR(255)` sets precision to `255`.
+   * - `BIGINT`, `DATE`, `FLOAT`, `DOUBLE`, `GEOMETRY`, etc. sets precision to None
+   * - `TIME`, `TIMESTAMP` set precision to `0`.
+   */
+  precision?: number | null
+  /**
+   * The scale of the type.
+   *
+   * # Example
+   * - `DECIMAL(1,2)` sets scale to `2`.
+   */
+  scale?: number | null
+  /**
+   * Identifier for the type (e.g., `VARCHAR`, `BIGINT`, `ARRAY` etc.)
+   */
+  type: string
+}
--- web-ui/src/types/manager/models/Relation.ts
+++ web-ui/src/types/manager/models/Relation.ts
@@ -6,6 +6,8 @@ import type { Field } from './Field'
 
 /**
  * A SQL table or view. It has a name and a list of fields.
+ *
+ * Matches the Calcite JSON format.
  */
 export type Relation = {
   fields: Array<Field>
