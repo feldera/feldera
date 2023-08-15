99a9ab5d491b4352995069f6f36c61a4dd3c8a0d
George <bulakh.96@gmail.com>
2023-08-15 18:35:24 +0000

    Yarn format
    Signed-off-by: George <bulakh.96@gmail.com>

--- web-console/src/lib/components/home/WelcomeCard.tsx
+++ web-console/src/lib/components/home/WelcomeCard.tsx
@@ -1,11 +1,17 @@
-import { Button, Card, Grid, Typography, Box } from "@mui/material";
+import { Button, Card, Grid, Typography, Box } from '@mui/material'
 
-export const WelcomeCard = () => <Card sx={{p: 4, display: 'flex', flexDirection: 'column', gap: 8}}>
-   <Typography variant='h4' sx={{ textAlign: 'center' }}>
+export const WelcomeCard = () => (
+  <Card sx={{ p: 4, display: 'flex', flexDirection: 'column', gap: 8 }}>
+    <Typography variant='h4' sx={{ textAlign: 'center' }}>
       Welcome to Feldera platofrm!
-   </Typography>
-   <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-      <Button href='https://docs.feldera.io/docs/tour/' target="_blank" rel="noreferrer">Take the tour</Button>
-      <Button href='https://docs.feldera.io/docs/demos/' target="_blank" rel="noreferrer">Follow the demos</Button>
-   </Box>
-</Card>
+    </Typography>
+    <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      <Button href='https://docs.feldera.io/docs/tour/' target='_blank' rel='noreferrer'>
+        Take the tour
+      </Button>
+      <Button href='https://docs.feldera.io/docs/demos/' target='_blank' rel='noreferrer'>
+        Follow the demos
+      </Button>
+    </Box>
+  </Card>
+)
