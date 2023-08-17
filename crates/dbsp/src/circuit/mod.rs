fb4cb4dd7837f7b3474db6d7add92b1ee262fce0
Ben Pfaff <blp@feldera.com>
2023-08-17 08:47:49 -0700

    dbsp: Add demo-level support for distribution.
    Until now, DBSP has only supported computation in a single process on a
    single host.  This commit starts to add support for distribution across
    multiple hosts.  `crates/dbsp/examples/dist/README.md` explains what's
    here so far, which is just a demo.  Production support for distribution
    and fault tolerance will come later.
    
    For instructions on how to run the demo, read
    `crates/dbsp/examples/dist/README.md`.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/circuit/mod.rs
+++ crates/dbsp/src/circuit/mod.rs
@@ -32,7 +32,7 @@ pub use circuit_builder::{
     ChildCircuit, Circuit, CircuitHandle, ExportId, ExportStream, FeedbackConnector, GlobalNodeId,
     NodeId, OwnershipPreference, RootCircuit, Scope, Stream, WithClock,
 };
-pub use dbsp_handle::DBSPHandle;
+pub use dbsp_handle::{DBSPHandle, Host, IntoLayout, Layout};
 pub use runtime::{Error as RuntimeError, LocalStore, LocalStoreMarker, Runtime, RuntimeHandle};
 
 pub use schedule::Error as SchedulerError;
