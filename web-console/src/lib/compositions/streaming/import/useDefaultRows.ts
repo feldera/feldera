29cffd737af6293d1c1141e2f77c2d913717e6b4
Gerd Zellweger <mail@gerdzellweger.com>
2023-08-18 02:22:33 +0200

    Insert rows clears rows again.
    Fixes #507.
    
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

---
+++ web-console/src/lib/compositions/streaming/import/useDefaultRows.ts
@@ -0,0 +1,53 @@
+// Generates rows and inserts them into a table.
+
+import { getValueParser, Row } from '$lib/functions/ddl'
+import { ColumnType, Field, Relation } from '$lib/services/manager'
+import { Dispatch, MutableRefObject, SetStateAction, useCallback } from 'react'
+
+import { GridApi } from '@mui/x-data-grid-pro'
+import { match } from 'ts-pattern'
+import dayjs from 'dayjs'
+
+export const getDefaultValue = (columntype: ColumnType) =>
+  match(columntype)
+    .with({ type: 'BOOLEAN' }, () => false)
+    .with({ type: 'TINYINT' }, { type: 'SMALLINT' }, { type: 'INTEGER' }, { type: 'BIGINT' }, () => 0)
+    .with({ type: 'DECIMAL' }, { type: 'FLOAT' }, { type: 'DOUBLE' }, () => 0.0)
+    .with({ type: 'VARCHAR' }, { type: 'CHAR' }, () => '')
+    .with({ type: 'TIME' }, () => dayjs(new Date()))
+    .with({ type: 'TIMESTAMP' }, () => dayjs(new Date()))
+    .with({ type: 'DATE' }, () => dayjs(new Date()))
+    .with({ type: 'GEOMETRY' }, () => 'st_point(0.0, 0.0)')
+    .with({ type: 'ARRAY' }, () => '[]')
+    .otherwise(() => '')
+
+function useDefaultRows(
+  apiRef: MutableRefObject<GridApi>,
+  setRows: Dispatch<SetStateAction<Row[]>>,
+  relation: Relation
+) {
+  const insertRows = useCallback(
+    (rowCount: number) => {
+      if (relation) {
+        const newRows: Row[] = []
+        const curRowCount = apiRef.current?.getRowsCount()
+
+        for (let i = 0; i < rowCount; i++) {
+          const row: Row = { genId: curRowCount + i, weight: 1, record: {} }
+          relation.fields.forEach((field: Field) => {
+            const valueParser = getValueParser(field.columntype)
+            row.record[field.name] = valueParser(getDefaultValue(field.columntype))
+          })
+          newRows.push(row)
+        }
+
+        setRows(prevRows => [...prevRows, ...newRows])
+      }
+    },
+    [apiRef, setRows, relation]
+  )
+
+  return insertRows
+}
+
+export default useDefaultRows
