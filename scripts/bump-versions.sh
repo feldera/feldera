e62a71f46d58b4022041c8f3062cf16ac2d6d451
Lalith Suresh <lalith@feldera.com>
2023-08-23 08:53:50 -0700

    build: tie monorepo release version to pipeline-manager version
    Signed-off-by: Lalith Suresh <lalith@feldera.com>

--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -3,7 +3,7 @@
 set -ex
 
 #
-# To use this function, make sure you have maven installed, and run `cargo install cargo-edit` first
+# Run `cargo install cargo-edit` once before you use this function
 #
 # When pushing commits created by this script, don't forget to use the --follow-tags argument to `git push`
 #
@@ -14,31 +14,22 @@ release() {
     fi
 
     if [ $1 = "major" ]; then
-        cargo set-version --bump major
+        cargo set-version --bump major -p pipeline-manager
     fi
 
     if [ $1 = "minor" ]; then
-        cargo set-version --bump minor
+        cargo set-version --bump minor -p pipeline-manager
     fi
 
     if [ $1 = "patch" ]; then
-        cargo set-version --bump patch
+        cargo set-version --bump patch -p pipeline-manager
     fi
 
     version=`cargo run --bin=api-server -- -V | awk '{print $NF}'`
 
-    cd ../sql-to-dbsp-compiler/SQL-compiler
-
-    mvn versions:set -DnewVersion=$version
-
     # Commit the new version
     git commit -am "release: bump project version to $version" -s
     git tag -a v$version -m "v$version"
-
-    # Start next development cycle. Bump the SQL compiler to a
-    # SNAPSHOT release for the next patch version.
-    mvn release:update-versions --batch-mode
-    git commit -am "release: start post-$version release cycle" -s
 }
 
 release "$@"
