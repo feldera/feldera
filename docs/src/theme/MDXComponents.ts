4d7d5749853e883dff5374604f14409e04cb122d
Gerd Zellweger <mail@gerdzellweger.com>
2023-08-23 07:11:41 +0200

    Remove old docs website.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- docs/src/theme/MDXComponents.ts
+++
@@ -1,8 +0,0 @@
-import MDXComponents from '@theme-original/MDXComponents'
-import { Icon } from '@iconify/react'
-
-export default {
-  // Re-use the default mapping
-  ...MDXComponents,
-  icon: Icon // Make the iconify Icon component available in MDX as <icon />.
-}
