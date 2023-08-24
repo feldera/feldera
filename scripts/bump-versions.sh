cc9f76f41592922a5bb1ba200f0a087a35a00dc8
Lalith Suresh <lalith@feldera.com>
2023-08-24 15:51:23 -0700

    docker: make the feldera version configurable
    Also adapts the release script to update the container
    image version in docker-compose.yml, and uses cargo
    metadata to find the current pipeline-manager package
    version.
    
    Signed-off-by: Lalith Suresh <lalith@feldera.com>

--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -3,7 +3,7 @@
 set -ex
 
 #
-# Run `cargo install cargo-edit` once before you use this function
+# Install cargo-edit (via cargo install cargo-edit) and jq before you use this function
 #
 # When pushing commits created by this script, don't forget to use the --follow-tags argument to `git push`
 #
@@ -18,16 +18,31 @@ release() {
         exit 1
     fi
 
+    if ! (jq --version) >/dev/null 2>&1; then
+        echo >&2 "$0: jq not installed"
+        exit 1
+    fi
+
+    old_version=`cargo metadata --no-deps | jq -r '.packages[]|select(.name == "pipeline-manager")|.version'`
     case $1 in
         major|minor|patch) cargo set-version --bump $1 -p pipeline-manager ;;
         *) echo >&2 "Argument must be 'major' or 'minor' or 'patch'"; exit 1 ;;
     esac
+    new_version=`cargo metadata --no-deps | jq -r '.packages[]|select(.name == "pipeline-manager")|.version'`
 
-    version=`cargo run --bin=api-server -- -V | awk '{print $NF}'`
+    # Patch the docker-compose.yml file with the new version
+    sed "s/\:\-v${old_version}/\:\-v${new_version}/g" ../deploy/docker-compose.yml > /tmp/docker-compose-bumped.yml
+    mv /tmp/docker-compose-bumped.yml ../deploy/docker-compose.yml
 
     # Commit the new version
-    git commit -am "release: bump project version to $version" -s
-    git tag -a v$version -m "v$version"
+    tag="v$new_version"
+    if git rev-parse "$tag" >/dev/null 2>&1; then
+        echo "Error: tag $tag already exists. Aborting";
+        exit 1;
+    fi
+    git commit -am "release: bump project version to $new_version" -s
+    git tag -a v$new_version -m "v$new_version"
+
 }
 
 release "$@"
