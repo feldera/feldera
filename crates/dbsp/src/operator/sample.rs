1f9938b7f334cc8a26d0a4cd2859acaf16837f38
Leonid Ryzhyk <leonid@feldera.com>
2023-10-02 11:21:50 -0700

    `Catalog::register_input_map()`
    Added a method to register input streams created using `add_input_map`
    in the catalog.  Such a stream represents a table with a unique or
    primary key and upsert modification semantics.  Entries are deleted by
    key and updated or modified by value (inserting a value whose key
    already exists in the collection removes the existing value associated
    with this key).
    
    In addition to the input stream, the caller must provide a function to
    extract keys from values.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/dbsp/src/operator/sample.rs
+++ crates/dbsp/src/operator/sample.rs
@@ -1,12 +1,12 @@
 //! Compute random samples of data.
 
 use crate::{
-    algebra::{HasOne, ZRingValue},
+    algebra::{HasOne, HasZero, ZRingValue},
     circuit::{
         operator_traits::{BinaryOperator, Operator},
         Scope,
     },
-    trace::{Batch, BatchReader, Builder},
+    trace::{Batch, BatchReader, Builder, Cursor},
     Circuit, DBData, DBWeight, OrdZSet, RootCircuit, Stream,
 };
 use rand::thread_rng;
@@ -76,6 +76,37 @@ where
         })
     }
 
+
+
+    /// Generates a uniform random sample of (key,value) pairs from `self`,
+    /// assuming that `self` contains exactly one value per key.
+    ///
+    /// Equivalent to `self.map(|(k, v)| (k, v)).stream_sample_keys()`,
+    /// but is more efficient.
+    #[allow(clippy::type_complexity)]
+    pub fn stream_sample_unique_key_vals(
+        &self,
+        sample_size: &Stream<RootCircuit, usize>,
+    ) -> Stream<RootCircuit, OrdZSet<(B::Key, B::Val), B::R>> {
+        self.circuit().region("stream_sample_unique_key_vals", || {
+            let stream = self.try_sharded_version();
+
+            // Sample each worker using `SampleUniqueKeyVals`.
+            let local_output = self.circuit().add_binary_operator(
+                SampleUniqueKeyVals::new(),
+                &stream,
+                sample_size,
+            );
+
+            // Sample results from all workers using `SampleKeys`.
+            self.circuit().add_binary_operator(
+                SampleKeys::new(),
+                &local_output.gather(0),
+                sample_size,
+            )
+        })
+    }
+
     /// Generates a subset of keys that partition the set of all keys in `self`
     /// into `num_quantiles + 1` approximately equal-size quantiles.
     ///
@@ -127,6 +158,39 @@ where
                 }
             },
         )
+    }
+
+    /// Generates a subset of (key, value) pairs that partition the set of all
+    /// tuples in `self` `num_quantiles + 1` approximately equal-size quantiles,
+    /// assuming that `self` contains exactly one value per key.
+    ///
+    /// Equivalent to `self.map(|(k, v)| (k, v)).stream_unique_key_val_quantiles()`,
+    /// but is more efficient.
+    #[allow(clippy::type_complexity)]
+    pub fn stream_unique_key_val_quantiles(
+        &self,
+        num_quantiles: &Stream<RootCircuit, usize>,
+    ) -> Stream<RootCircuit, OrdZSet<(B::Key, B::Val), B::R>> {
+        let sample_size = num_quantiles.apply(|num| num * num);
+
+        self.stream_sample_unique_key_vals(&sample_size)
+            .apply2_owned(num_quantiles, |sample, num_quantiles| {
+                let num_quantiles = min(*num_quantiles, MAX_QUANTILES);
+
+                let sample_size = sample.key_count();
+
+                if sample_size <= num_quantiles {
+                    sample
+                } else {
+                    let mut builder =
+                        <<OrdZSet<_, _> as Batch>::Builder>::with_capacity((), num_quantiles);
+                    for i in 0..num_quantiles {
+                        let key = sample.layer.keys()[(i * sample_size) / num_quantiles].clone();
+                        builder.push((key, HasOne::one()));
+                    }
+                    builder.done()
+                }
+            })
     }
 }
 
@@ -185,6 +249,78 @@ where
     }
 }
 
+struct SampleUniqueKeyVals<T>
+where
+    T: BatchReader,
+{
+    _phantom: PhantomData<T>,
+}
+
+impl<T> SampleUniqueKeyVals<T>
+where
+    T: BatchReader,
+{
+    fn new() -> Self {
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Operator for SampleUniqueKeyVals<T>
+where
+    T: BatchReader + 'static,
+{
+    fn name(&self) -> Cow<'static, str> {
+        Cow::from("SampleUniqueKeyVals")
+    }
+    fn fixedpoint(&self, _scope: Scope) -> bool {
+        true
+    }
+}
+
+impl<T> BinaryOperator<T, usize, OrdZSet<(T::Key, T::Val), T::R>> for SampleUniqueKeyVals<T>
+where
+    T: BatchReader<Time = ()>,
+    T::Key: DBData,
+    T::Val: DBData,
+    T::R: DBWeight + ZRingValue,
+{
+    fn eval(&mut self, input_trace: &T, &sample_size: &usize) -> OrdZSet<(T::Key, T::Val), T::R> {
+        let sample_size = min(sample_size, MAX_SAMPLE_SIZE);
+
+        if sample_size != 0 {
+            let mut sample = Vec::with_capacity(sample_size);
+            input_trace.sample_keys(&mut thread_rng(), sample_size, &mut sample);
+
+            let mut cursor = input_trace.cursor();
+            let mut sample_with_vals = Vec::with_capacity(sample_size);
+
+            for key in sample.into_iter() {
+                cursor.seek_key(&key);
+                debug_assert!(cursor.key_valid());
+                debug_assert_eq!(cursor.key(), &key);
+                while cursor.val_valid() {
+                    if !cursor.weight().is_zero() {
+                        sample_with_vals.push((key, cursor.val().clone()));
+                        break;
+                    }
+                    cursor.step_val();
+                }
+            }
+
+            let mut builder =
+                <<OrdZSet<_, _> as Batch>::Builder>::with_capacity((), sample_with_vals.len());
+            for key in sample_with_vals.into_iter() {
+                builder.push((key, HasOne::one()));
+            }
+            builder.done()
+        } else {
+            <OrdZSet<_, _>>::empty(())
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::{
@@ -193,7 +329,8 @@ mod test {
             test_batch::{batch_to_tuples, TestBatch},
             BatchReader, Trace,
         },
-        CollectionHandle, InputHandle, OrdZSet, OutputHandle, RootCircuit, Runtime,
+        CollectionHandle, InputHandle, OrdIndexedZSet, OrdZSet, OutputHandle, RootCircuit, Runtime,
+        UpsertHandle,
     };
     use anyhow::Result as AnyResult;
     use std::collections::BTreeSet;
@@ -226,6 +363,41 @@ mod test {
             input_handle,
             sample_handle,
             quantile_handle,
+        ))
+    }
+
+    fn test_unique_key_circuit(
+        circuit: &mut RootCircuit,
+    ) -> AnyResult<(
+        InputHandle<usize>,
+        UpsertHandle<i32, Option<i32>>,
+        OutputHandle<OrdZSet<(i32, i32), i32>>,
+        OutputHandle<OrdZSet<(i32, i32), i32>>,
+        OutputHandle<OrdIndexedZSet<i32, i32, i32>>,
+    )> {
+        let (sample_size_stream, sample_size_handle) = circuit.add_input_stream::<usize>();
+        let (input_stream, input_handle) = circuit.add_input_map::<i32, i32, i32>();
+
+        let sample_handle = input_stream
+            .shard()
+            .integrate_trace()
+            .stream_sample_unique_key_vals(&sample_size_stream)
+            .output();
+
+        let quantile_handle = input_stream
+            .shard()
+            .integrate_trace()
+            .stream_unique_key_val_quantiles(&sample_size_stream)
+            .output();
+
+        let trace_handle = input_stream.integrate().output();
+
+        Ok((
+            sample_size_handle,
+            input_handle,
+            sample_handle,
+            quantile_handle,
+            trace_handle,
         ))
     }
 
@@ -305,5 +477,63 @@ mod test {
                 }
             }
         }
+
+        #[test]
+        fn sample_unique_keys_proptest(trace in input_trace(100, 5, 200, 20)) {
+
+            let (mut dbsp, (sample_size_handle, input_handle, output_sample_handle, output_quantile_handle, output_trace_handle)) =
+                Runtime::init_circuit(4, test_unique_key_circuit).unwrap();
+
+            for (batch, sample_size) in trace.into_iter() {
+
+                for (k, v, r) in batch.into_iter() {
+                    if r > 0 {
+                        input_handle.push(k, Some(v));
+                    } else if r < 0 {
+                        input_handle.push(k, None);
+                    }
+                }
+
+                sample_size_handle.set_for_all(sample_size);
+
+                dbsp.step().unwrap();
+
+                let output_sample = output_sample_handle.consolidate();
+                let output_quantile = output_quantile_handle.consolidate();
+                let output_trace = output_trace_handle.consolidate();
+
+                // Validation.
+
+                // Collect all keys in the trace.
+                let batch = TestBatch::from_data(&batch_to_tuples(&output_trace));
+
+                let mut all_keys = Vec::new();
+                let mut cursor = batch.cursor();
+                while cursor.key_valid() {
+                    all_keys.push((*cursor.key(), *cursor.val()));
+                    cursor.step_key();
+                }
+                let all_keys_set = all_keys.iter().cloned().collect::<BTreeSet<_>>();
+
+                assert!(output_sample.key_count() <= all_keys_set.len());
+                assert!(output_sample.key_count() <= sample_size);
+
+                let mut cursor = output_sample.cursor();
+                while cursor.key_valid() {
+                    assert!(all_keys_set.contains(cursor.key()));
+                    cursor.step_key();
+                }
+
+                assert!(output_quantile.key_count() <= all_keys_set.len());
+                assert!(output_quantile.key_count() <= sample_size);
+
+                let mut cursor = output_quantile.cursor();
+                while cursor.key_valid() {
+                    assert!(all_keys_set.contains(cursor.key()));
+                    cursor.step_key();
+                }
+            }
+        }
+
     }
 }
