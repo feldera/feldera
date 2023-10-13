2de33199ee66ce76f7ef559c419a43a0582f8127
George <bulakh.96@gmail.com>
2023-10-13 12:49:39 +0000

    Hotfix: WebConsole not loading when auth is not configured
    Signed-off-by: George <bulakh.96@gmail.com>

--- web-console/src/lib/components/layouts/AuthProvider.tsx
+++ web-console/src/lib/components/layouts/AuthProvider.tsx
@@ -4,36 +4,35 @@ import { authContext, useAuthStore } from '$lib/compositions/auth/useAuth'
 import { OpenAPI } from '$lib/services/manager'
 import { PipelineManagerQuery } from '$lib/services/pipelineManagerQuery'
 import { ReactNode } from 'react'
+import { isEmptyObject } from 'src/lib/functions/common/object'
 import { match, P } from 'ts-pattern'
 
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { useQuery } from '@tanstack/react-query'
 
 export const AuthenticationProvider = (props: { children: ReactNode }) => {
-  const { data: auth, isLoading } = useQuery(PipelineManagerQuery.getAuthConfig())
+  const { data: auth } = useQuery(PipelineManagerQuery.getAuthConfig())
   const { auth: authState, setAuth } = useAuthStore()
 
   if (typeof authState === 'object') {
     OpenAPI.TOKEN = authState.bearer
   }
-  if (isLoading) {
+  if (!auth) {
     return <></>
   }
 
-  if (auth && typeof authState !== 'object' && authState !== 'Unauthenticated') {
+  if (!isEmptyObject(auth) && typeof authState !== 'object' && authState !== 'Unauthenticated') {
     setAuth('Unauthenticated')
     return <></>
   }
-  if (!auth && typeof authState !== 'object' && authState !== 'NoAuth') {
+  if (isEmptyObject(auth) && typeof authState !== 'object' && authState !== 'NoAuth') {
     setAuth('NoAuth')
     return <></>
   }
 
   return (
     <authContext.Provider value={authState}>
-      {' '}
       {match(auth)
-        .with(undefined, () => props.children)
         .with({ AwsCognito: P._ }, () => {
           return props.children
         })
