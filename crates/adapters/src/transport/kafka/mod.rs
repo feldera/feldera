6d297d5f1df78f9335d549eb388aa6e135b051b4
Ben Pfaff <blp@feldera.com>
2023-10-27 12:20:09 -0700

    dbsp_adapters: Capture log messages during kafka unit tests.
    This makes the unit test output easier to read and attributes log messages
    to particular tests.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/adapters/src/transport/kafka/mod.rs
+++ crates/adapters/src/transport/kafka/mod.rs
@@ -6,6 +6,8 @@ use rdkafka::{
     error::KafkaError,
     types::RDKafkaErrorCode,
 };
+#[cfg(test)]
+use std::sync::Mutex;
 
 mod input;
 mod output;
@@ -60,5 +62,105 @@ where
             }
         }
         None | Some(_) => (false, AnyError::from(e)),
+    }
+}
+
+/// Captures and redirect log messages during tests.
+///
+/// The Rust unit test framework captures log messages during tests and, in some
+/// cases, prints them later associated with the particular test.  It does this
+/// OK in most cases, but one of the cases it does not handle is output from
+/// threads created by anything other than `std::thread` primitives.  This
+/// includes the thread created by librdkafka.  If we log anything from a
+/// context callback, then it goes directly to stderr, bypassing the unit test
+/// framework.  This makes it hard to read the unit test output and hard to
+/// attribute messages to particular tests.
+///
+/// `DeferredLogging` provides a way to capture log messages emitted by the unit
+/// tests.  A context can instantiate `DeferredLogging` and use
+/// `DeferredLogging::with_deferred_logging` to wrap calls to librdkafka
+/// functions that are likely to emit log messages.  This can be specific calls
+/// instead of every call, since we know what message the unit tests actually
+/// provoke.
+///
+/// When tests are disabled, this framework compiles to nothing.
+#[cfg(test)]
+pub(crate) struct DeferredLogging(Mutex<Option<Vec<(RDKafkaLogLevel, String, String)>>>);
+
+#[cfg(test)]
+impl DeferredLogging {
+    pub fn new() -> Self {
+        Self(Mutex::new(None))
+    }
+
+    /// Calls `f`, capturing log messages that occur during the call, and then
+    /// logging them in the current thread.
+    pub fn with_deferred_logging<F, R>(&self, f: F) -> R
+    where
+        F: Fn() -> R,
+    {
+        *self.0.lock().unwrap() = Some(Vec::new());
+        let r = f();
+        for (level, fac, message) in self.0.lock().unwrap().take().unwrap().drain(..) {
+            log::info!("{level:?} {fac} {message}");
+        }
+        r
+    }
+
+    /// Logs the message in the usual way, or captures it for later logging if
+    /// we're running inside `Self::with_deferred_logging`.
+    pub fn log(&self, level: RDKafkaLogLevel, fac: &str, log_message: &str) {
+        if let Some(ref mut deferred_logging) = self.0.lock().unwrap().as_mut() {
+            deferred_logging.push((level, fac.into(), log_message.into()))
+        } else {
+            Self::default_log(level, fac, log_message)
+        }
+    }
+}
+
+#[cfg(not(test))]
+pub(crate) struct DeferredLogging;
+
+#[cfg(not(test))]
+impl DeferredLogging {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn with_deferred_logging<F, R>(&self, f: F) -> R
+    where
+        F: Fn() -> R,
+    {
+        f()
+    }
+
+    pub fn log(&self, level: RDKafkaLogLevel, fac: &str, log_message: &str) {
+        Self::default_log(level, fac, log_message)
+    }
+}
+
+impl DeferredLogging {
+    // This is a copy of the default implementation of [`ClientContext::log`].
+    fn default_log(level: RDKafkaLogLevel, fac: &str, log_message: &str) {
+        match level {
+            RDKafkaLogLevel::Emerg
+            | RDKafkaLogLevel::Alert
+            | RDKafkaLogLevel::Critical
+            | RDKafkaLogLevel::Error => {
+                log::error!(target: "librdkafka", "librdkafka: {} {}", fac, log_message)
+            }
+            RDKafkaLogLevel::Warning => {
+                log::warn!(target: "librdkafka", "librdkafka: {} {}", fac, log_message)
+            }
+            RDKafkaLogLevel::Notice => {
+                log::info!(target: "librdkafka", "librdkafka: {} {}", fac, log_message)
+            }
+            RDKafkaLogLevel::Info => {
+                log::info!(target: "librdkafka", "librdkafka: {} {}", fac, log_message)
+            }
+            RDKafkaLogLevel::Debug => {
+                log::debug!(target: "librdkafka", "librdkafka: {} {}", fac, log_message)
+            }
+        }
     }
 }
