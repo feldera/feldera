9af4121efbe7841f8cf34bc8e6bfa5d0d099759d
Ben Pfaff <blp@feldera.com>
2023-10-30 14:03:38 -0700

    dbsp_adapters: Generalize `KafkaResources::create_topics` to delete topics.
    Some of the upcoming tests for durable Kafka will want to make sure that
    some topics don't exist (i.e. are deleted) at the beginning of the test.
    This slightly generalizes `create_topics` to support that.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/adapters/src/test/kafka.rs
+++ crates/adapters/src/test/kafka.rs
@@ -37,28 +37,59 @@ pub struct KafkaResources {
 // Checks `consumer` to make sure that all of the topics in `topics` are
 // accessible and have the specified number of partitions.
 fn check_topics<C: ClientContext>(consumer: &Client<C>, topics: &[(&str, i32)]) -> AnyResult<()> {
+    // Retrieve the list of all topics and then filter it.  If the broker is
+    // configured to automatically create topics, then asking for each of them
+    // individually will create them, which is no good for topics we want to
+    // delete.
+    let metadata = consumer.fetch_metadata(None, Duration::from_secs(1))?;
+
     for &(topic, partitions) in topics.iter() {
-        let m = consumer
-            .fetch_metadata(Some(topic), Duration::from_secs(1))
-            .map_err(|e| anyhow!("topic {topic}: {e}"))?;
-        let cur_partitions = m
+        let cur_partitions = metadata
             .topics()
-            .get(0)
-            .map(|topic| topic.partitions().len() as i32)
-            .unwrap_or_default();
+            .iter()
+            .find(|m| m.name() == topic)
+            .map(|mt| mt.partitions().len())
+            .unwrap_or(0) as i32;
         if cur_partitions != partitions {
-            bail!("{topic} only has {cur_partitions} partitions, waiting for {partitions}");
+            bail!("{topic} has {cur_partitions} partitions, waiting for {partitions}");
         }
-        consumer
-            .fetch_watermarks(topic, 0, Duration::from_secs(1))
-            .map_err(|e| anyhow!("topic {topic}: {e}"))?;
+        if partitions > 0 {
+            consumer
+                .fetch_watermarks(topic, 0, Duration::from_secs(1))
+                .map_err(|e| anyhow!("topic {topic}: {e}"))?;
+        }
     }
     Ok(())
+}
+
+fn wait_for_completion<C: ClientContext>(admin_client: &AdminClient<C>, topics: &[(&str, i32)]) {
+    let topic_names = topics
+        .iter()
+        .map(|(topic_name, _partitions)| &**topic_name)
+        .collect::<Vec<_>>();
+
+    let start = Instant::now();
+    let mut backoff = 100;
+    let mut n_retries = 0;
+    while let Err(err) = check_topics(admin_client.inner(), topics) {
+        info!("KafkaResources::create_topics {topic_names:?}: unable to connect to newly created topics, retrying: {err}");
+        if start.elapsed() > MAX_TOPIC_PROBE_TIMEOUT {
+            panic!("KafkaResources::create_topics {topic_names:?}: unable to connect to newly created topics, giving up after {}ms: {err}", MAX_TOPIC_PROBE_TIMEOUT.as_millis());
+        }
+        sleep(Duration::from_millis(backoff));
+        backoff = 1000.min(backoff * 2);
+        n_retries += 1;
+    }
+    if n_retries > 0 {
+        info!("KafkaResources::create_topics {topic_names:?}: success after {n_retries} tries");
+    }
 }
 
 /// An object that creates Kafka topics on startup and deletes them
 /// on drop.  Helps make sure that test runs don't leave garbage behind.
 impl KafkaResources {
+    /// `(topic, n_partitions)` creates a topic with `n_partitions` partitions.
+    /// If `n_partitions` is 0 then the topic is deleted instead.
     pub fn create_topics(topics: &[(&str, i32)]) -> Self {
         // Kafka does not handle topic deletion and creation consistently when
         // multiple operations are performed in parallel, so serialize calls to
@@ -74,40 +105,32 @@ impl KafkaResources {
             .set_log_level(RDKafkaLogLevel::Debug);
         let admin_client = AdminClient::from_config(&admin_config).unwrap();
 
-        let new_topics = topics
-            .iter()
-            .map(|(topic_name, partitions)| {
-                NewTopic::new(topic_name, *partitions, TopicReplication::Fixed(1))
-            })
-            .collect::<Vec<_>>();
-        let topic_names = topics
-            .iter()
-            .map(|(topic_name, _partitions)| &**topic_name)
-            .collect::<Vec<_>>();
-
         // Delete topics if they exist from previous failed runs that crashed before
         // cleaning up.  Otherwise, it may take a long time to re-join a
         // group whose members are dead, plus the old topics may contain
         // messages that will mess up our tests.
-        let _ = block_on(admin_client.delete_topics(&topic_names, &AdminOptions::new()));
+        //
+        // We wait for deletion to finish before creating topics, because Kafka
+        // doesn't seem to always do what we want if we tell it to re-create a
+        // topic before it's been fully deleted.
+        let delete_topics = topics
+            .iter()
+            .map(|(topic_name, _partitions)| &**topic_name)
+            .collect::<Vec<_>>();
+        let _ = block_on(admin_client.delete_topics(&delete_topics, &AdminOptions::new()));
+        let deleted_topics: Vec<_> = topics.iter().map(|(name, _)| (*name, 0)).collect();
+        wait_for_completion(&admin_client, &deleted_topics[..]);
 
+        // Now create the topics and wait for the creations to complete.
+        let new_topics = topics
+            .iter()
+            .filter_map(|(topic_name, partitions)| {
+                (*partitions > 0)
+                    .then(|| NewTopic::new(topic_name, *partitions, TopicReplication::Fixed(1)))
+            })
+            .collect::<Vec<_>>();
         block_on(admin_client.create_topics(&new_topics, &AdminOptions::new())).unwrap();
-
-        let start = Instant::now();
-        let mut backoff = 100;
-        let mut n_retries = 0;
-        while let Err(err) = check_topics(admin_client.inner(), topics) {
-            info!("KafkaResources::create_topics {topic_names:?}: unable to connect to newly created topics, retrying: {err}");
-            if start.elapsed() > MAX_TOPIC_PROBE_TIMEOUT {
-                panic!("KafkaResources::create_topics {topic_names:?}: unable to connect to newly created topics, giving up after {}ms: {err}", MAX_TOPIC_PROBE_TIMEOUT.as_millis());
-            }
-            sleep(Duration::from_millis(backoff));
-            backoff = 1000.min(backoff * 2);
-            n_retries += 1;
-        }
-        if n_retries > 0 {
-            info!("KafkaResources::create_topics {topic_names:?}: success after {n_retries} tries");
-        }
+        wait_for_completion(&admin_client, topics);
 
         Self {
             admin_client,
