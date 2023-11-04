0bfb9d80f5e57373fb9245d334f39efef48d413f
Leonid Ryzhyk <leonid@feldera.com>
2023-11-04 11:29:49 -0700

    TypedLeafCursor: Implement reverse iteration
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/dbsp/src/trace/layers/erased/erased_leaf/cursor.rs
+++ crates/dbsp/src/trace/layers/erased/erased_leaf/cursor.rs
@@ -19,7 +19,7 @@ where
     K: Ord + Clone,
     R: Clone,
 {
-    current: usize,
+    pos: isize,
     storage: &'a ErasedLeaf,
     bounds: (usize, usize),
     __type: PhantomData<(K, R)>,
@@ -27,12 +27,12 @@ where
 
 impl<'a, K, R> TypedLayerCursor<'a, K, R>
 where
-    K: Ord + Clone,
-    R: Clone,
+    K: Ord + Clone + 'static,
+    R: Clone + 'static,
 {
     pub const fn new(pos: usize, storage: &'a TypedLayer<K, R>, bounds: (usize, usize)) -> Self {
         Self {
-            current: pos,
+            pos: pos as isize,
             storage: &storage.layer,
             bounds,
             __type: PhantomData,
@@ -51,25 +51,29 @@ where
     where
         P: Fn(&K) -> bool,
     {
-        self.current += advance_erased(
-            self.storage.keys.range(self.current..self.bounds.1),
-            self.storage.key_size(),
-            |x| unsafe { predicate(&*(x as *const K)) },
-        );
+        if self.valid() {
+            self.pos += advance_erased(
+                self.storage.keys.range(self.pos as usize..self.bounds.1),
+                self.storage.key_size(),
+                |x| unsafe { predicate(&*(x as *const K)) },
+            ) as isize;
+        }
     }
 
     pub fn current_key(&self) -> &'a K
     where
         K: 'static,
     {
-        self.storage.keys.index_as(self.current)
+        debug_assert!(self.pos >= 0);
+        self.storage.keys.index_as(self.pos as usize)
     }
 
     pub fn current_diff(&self) -> &'a R
     where
         R: 'static,
     {
-        self.storage.diffs.index_as(self.current)
+        debug_assert!(self.pos >= 0);
+        self.storage.diffs.index_as(self.pos as usize)
     }
 }
 
@@ -91,8 +95,8 @@ where
     }
 
     fn item(&self) -> Self::Item<'s> {
-        if self.current >= self.storage.keys.len() {
-            cursor_position_oob(self.current, self.storage.keys.len());
+        if self.pos as usize >= self.storage.keys.len() || self.pos < 0 {
+            cursor_position_oob(self.pos, self.storage.keys.len());
         }
 
         (self.current_key(), self.current_diff())
@@ -101,41 +105,47 @@ where
     fn values(&self) {}
 
     fn step(&mut self) {
-        self.current += 1;
+        self.pos += 1;
 
         if !self.valid() {
-            self.current = self.bounds.1;
+            self.pos = self.bounds.1 as isize;
         }
     }
 
     fn seek(&mut self, key: &Self::Key) {
         let key = key as *const K as *const u8;
-        self.current += advance_erased(
-            self.storage.keys.range(self.current..self.bounds.1),
-            self.storage.key_size(),
-            |x| unsafe { (self.storage.keys.vtable().common.lt)(x, key) },
-        );
+        if self.valid() {
+            self.pos += advance_erased(
+                self.storage.keys.range(self.pos as usize..self.bounds.1),
+                self.storage.key_size(),
+                |x| unsafe { (self.storage.keys.vtable().common.lt)(x, key) },
+            ) as isize;
+        }
     }
 
     fn valid(&self) -> bool {
-        self.current < self.bounds.1
+        self.pos >= self.bounds.0 as isize && self.pos < self.bounds.1 as isize
     }
 
     fn rewind(&mut self) {
-        self.current = self.bounds.0;
+        self.pos = self.bounds.0 as isize;
     }
 
     fn position(&self) -> usize {
-        self.current
+        self.pos as usize
     }
 
     fn reposition(&mut self, lower: usize, upper: usize) {
-        self.current = lower;
+        self.pos = lower as isize;
         self.bounds = (lower, upper);
     }
 
     fn step_reverse(&mut self) {
-        todo!()
+        self.pos -= 1;
+
+        if self.pos < self.bounds.0 as isize {
+            self.pos = self.bounds.0 as isize - 1;
+        }
     }
 
     fn seek_reverse(&mut self, _key: &Self::Key) {
@@ -143,7 +153,7 @@ where
     }
 
     fn fast_forward(&mut self) {
-        todo!()
+        self.pos = self.bounds.1 as isize - 1;
     }
 }
 
