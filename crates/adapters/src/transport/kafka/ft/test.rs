feldera/feldera

commit e6391827d8cf75b3b82d8ab6fb45fe31bbaeca9d

dbsp_adapters: Unify `kafka` and `durable_kafka` transports.
This makes both of these transports available under the name `kafka`.
It does not try to unify their code, because there isn't a lot in common,
especially on the input side.  As an experiment before I took this
approach, I did try to unify the output side, but it just ended up making
a mess by ending up with a bunch of `if` statements that made the code
harder to read.

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/adapters/src/transport/kafka/ft/test.rs
@@ -96,12 +96,12 @@ outputs:
     test_output:
         stream: test_output1
         transport:
-            name: durable_kafka
+            name: kafka
             config:
-                kafka_options:
-                    bootstrap.servers: localhost:11111
+                bootstrap.servers: localhost:11111
                 topic: durable_end_to_end_test_output_topic
                 max_inflight_messages: 0
+                fault_tolerance: {}
         format:
             name: csv
 "#;
@@ -151,21 +151,23 @@ inputs:
     test_input1:
         stream: test_input1
         transport:
-            name: durable_kafka
+            name: kafka
             config:
                 topics: ["{input_topic}"]
                 log_level: debug
+                fault_tolerance: {{}}
         format:
             name: csv
 outputs:
     test_output2:
         stream: test_output1
         transport:
-            name: durable_kafka
+            name: kafka
             config:
                 topic: {output_topic}
                 max_inflight_messages: 0
                 message.max.bytes: "{message_max_bytes}"
+                fault_tolerance: {{}}
         format:
             name: {format}
             config:
@@ -230,11 +232,12 @@ fn test_empty_input() {
     let mut _kafka_resources =
         KafkaResources::create_topics(&[("empty", 1), ("empty_input-index", 0)]);
 
-    let transport = <dyn InputTransport>::get_transport("durable_kafka").unwrap();
+    let transport = <dyn InputTransport>::get_transport("kafka").unwrap();
 
     let config_str = r#"
 topics: [empty]
 log_level: debug
+fault_tolerance: {{}}
 "#
     .to_string();
 
@@ -304,11 +307,12 @@ fn test_input() {
         r#"
 topics: [{topic}]
 log_level: debug
-max_step_messages: 5
+fault_tolerance:
+    max_step_messages: 5
 "#
     );
 
-    let transport = <dyn InputTransport>::get_transport("durable_kafka").unwrap();
+    let transport = <dyn InputTransport>::get_transport("kafka").unwrap();
 
     let endpoint = transport
         .new_endpoint(&serde_yaml::from_str(&config_str).unwrap())
@@ -551,15 +555,16 @@ fn kafka_output_test(
     // Create topics.
     let _kafka_resources = KafkaResources::create_topics(&[(&output_topic, 1)]);
 
-    let transport = <dyn OutputTransport>::get_transport("durable_kafka").unwrap();
+    let transport = <dyn OutputTransport>::get_transport("kafka").unwrap();
 
     let config_str = format!(
         r#"
 stream: test_output1
 transport:
-    name: durable_kafka
+    name: kafka
     config:
         topic: {output_topic}
+        fault_tolerance: {{}}
 format:
     name: csv
 "#
@@ -582,14 +587,15 @@ format:
 }
 
 fn _test() {
-    let transport = <dyn OutputTransport>::get_transport("durable_kafka").unwrap();
+    let transport = <dyn OutputTransport>::get_transport("kafka").unwrap();
 
     let config_str = r#"
 stream: my_test_stream
 transport:
-    name: durable_kafka
+    name: kafka
     config:
         topic: my_topic
+        fault_tolerance: {{}}
 format:
     name: csv
 "#;
@@ -630,12 +636,12 @@ fn test_durable_kafka_input(data: Vec<Vec<TestStruct>>, topic1: &str, topic2: &s
         r#"
 stream: test_input
 transport:
-    name: durable_kafka
+    name: kafka
     config:
-        kafka_options:
-            bootstrap.servers: localhost:11111
+        bootstrap.servers: localhost:11111
         topics: ["{topic1}", "{topic2}"]
         log_level: debug
+        fault_tolerance: {{}}
 format:
     name: csv
 "#
@@ -656,10 +662,11 @@ format:
     let config_str = r#"
 stream: test_input
 transport:
-    name: durable_kafka
+    name: kafka
     config:
         topics: ["this_topic_does_not_exist"]
         log_level: debug
+        fault_tolerance: {}
 format:
     name: csv
 "#;
@@ -678,10 +685,11 @@ format:
         r#"
 stream: test_input
 transport:
-    name: durable_kafka
+    name: kafka
     config:
         topics: [{topic1}, {topic2}]
         log_level: debug
+        fault_tolerance: {{}}
 format:
     name: csv
 "#

crates/adapters/src/transport/kafka/ft/input.rs
@@ -1,12 +1,12 @@
 use super::{count_partitions_in_topic, Ctp, DataConsumerContext, ErrorHandler, POLL_TIMEOUT};
-use crate::transport::durable_kafka::check_fatal_errors;
+use crate::transport::kafka::ft::check_fatal_errors;
 use crate::transport::{InputReader, Step};
-use crate::{InputConsumer, InputEndpoint, InputTransport};
+use crate::{InputConsumer, InputEndpoint};
 use anyhow::{anyhow, bail, Context, Error as AnyError, Result as AnyResult};
 use crossbeam::sync::{Parker, Unparker};
 use futures::executor::block_on;
 use log::{debug, error, info, warn};
-use pipeline_types::transport::durable_kafka::KafkaDurableInputConfig;
+use pipeline_types::transport::kafka::KafkaInputConfig;
 use rdkafka::admin::{AdminClient, AdminOptions, NewTopic};
 use rdkafka::config::{FromClientConfig, FromClientConfigAndContext};
 use rdkafka::consumer::base_consumer::PartitionQueue;
@@ -18,7 +18,6 @@ use rdkafka::{consumer::Consumer, producer::Producer, Message, Offset};
 use rdkafka::{ClientContext, TopicPartitionList};
 use serde::ser::SerializeTuple;
 use serde::{Deserialize, Serialize, Serializer};
-use serde_yaml::Value as YamlValue;
 use std::cmp::{max, Ordering};
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
@@ -27,7 +26,6 @@ use std::sync::atomic::{AtomicBool, Ordering as AtomicOrdering};
 use std::thread::{self};
 use std::time::Duration;
 use std::{
-    borrow::Cow,
     error::Error,
     fmt::Result as FmtResult,
     ops::Range,
@@ -36,31 +34,6 @@ use std::{
 };
 
 use super::CommonConfig;
-
-/// [`InputTransport`] implementation that durably reads data from one or more
-/// Kafka topics.
-///
-/// This input transport is only available if the crate is configured with
-/// `with-kafka` feature.
-///
-/// The input transport factory gives this transport the name `durable_kafka`.
-pub struct KafkaDurableInputTransport;
-
-impl InputTransport for KafkaDurableInputTransport {
-    fn name(&self) -> Cow<'static, str> {
-        Cow::Borrowed("durable_kafka")
-    }
-
-    /// Creates a new [`InputEndpoint`] for reading from Kafka topics,
-    /// interpreting `config` as a [`KafkaDurableInputConfig`].
-    ///
-    /// See [`InputTransport::new_endpoint()`] for more information.
-    fn new_endpoint(&self, config: &YamlValue) -> AnyResult<Box<dyn InputEndpoint>> {
-        let config = KafkaDurableInputConfig::deserialize(config)?;
-        let ep = Endpoint::new(config)?;
-        Ok(Box::new(ep))
-    }
-}
 
 /// Validated version of [`KafkaDurableInputConfig`], converted into a
 /// convenient form for internal use.
@@ -85,15 +58,21 @@ struct Config {
     max_step_messages: i64,
 }
 
-impl TryFrom<KafkaDurableInputConfig> for Config {
+impl TryFrom<KafkaInputConfig> for Config {
     type Error = AnyError;
 
-    fn try_from(source: KafkaDurableInputConfig) -> Result<Self, Self::Error> {
-        let max_step_bytes = source.max_step_bytes.unwrap_or(u64::MAX).max(1);
-        let max_step_messages = source.max_step_messages.unwrap_or(u64::MAX).max(1);
+    fn try_from(source: KafkaInputConfig) -> Result<Self, Self::Error> {
+        let ft = source.fault_tolerance.as_ref().unwrap();
+        let max_step_bytes = ft.max_step_bytes.unwrap_or(u64::MAX).max(1);
+        let max_step_messages = ft.max_step_messages.unwrap_or(u64::MAX).max(1);
 
-        let common = CommonConfig::try_from(&source.common)?;
-        let index_suffix = match source.index_suffix {
+        let common = CommonConfig::new(
+            &source.kafka_options,
+            &ft.consumer_options,
+            &ft.producer_options,
+            source.log_level,
+        )?;
+        let index_suffix = match ft.index_suffix {
             Some(ref s) => s.clone(),
             None => "_input-index".into(),
         };
@@ -106,7 +85,7 @@ impl TryFrom<KafkaDurableInputConfig> for Config {
             common,
             data_topics: source.topics.clone(),
             index_suffix,
-            create_missing_index: source.create_missing_index.unwrap_or(true),
+            create_missing_index: ft.create_missing_index.unwrap_or(true),
             max_step_bytes: max_step_bytes.try_into().unwrap_or(usize::MAX),
             max_step_messages: max_step_messages.try_into().unwrap_or(i64::MAX),
         })
@@ -114,10 +93,10 @@ impl TryFrom<KafkaDurableInputConfig> for Config {
 }
 
 /// Durable Kafka input endpoint.
-struct Endpoint(Arc<Config>);
+pub struct Endpoint(Arc<Config>);
 
 impl Endpoint {
-    fn new(config: KafkaDurableInputConfig) -> AnyResult<Self> {
+    pub fn new(config: KafkaInputConfig) -> AnyResult<Self> {
         Ok(Self(Arc::new(config.try_into()?)))
     }
 }

crates/adapters/src/transport/kafka/ft/mod.rs
@@ -17,9 +17,7 @@ mod output;
 use crate::transport::kafka::refine_kafka_error;
 use anyhow::{anyhow, bail, Context, Error as AnyError, Result as AnyResult};
 use log::{debug, error, info, warn};
-use pipeline_types::transport::{
-    durable_kafka::CommonConfigSchema, kafka::default_redpanda_server,
-};
+use pipeline_types::transport::kafka::{default_redpanda_server, KafkaLogLevel};
 use rdkafka::{
     client::Client as KafkaClient,
     config::RDKafkaLogLevel,
@@ -42,10 +40,10 @@ use std::{
 };
 use uuid::Uuid;
 
-pub use input::KafkaDurableInputTransport;
-pub use output::KafkaDurableOutputTransport;
+pub use input::Endpoint as KafkaFtInputEndpoint;
+pub use output::KafkaOutputEndpoint as KafkaFtOutputEndpoint;
 
-use super::kafka::{rdkafka_loglevel_from, DeferredLogging};
+use super::{rdkafka_loglevel_from, DeferredLogging};
 
 #[cfg(test)]
 pub mod test;
@@ -85,14 +83,14 @@ fn set_option_if_missing<'a>(
 ///
 /// `config_name` is used only in the returned error message, if any.
 fn kafka_config(
-    common: &CommonConfigSchema,
+    kafka_options: &BTreeMap<String, String>,
     type_specific_options: &BTreeMap<String, String>,
+    log_level: Option<KafkaLogLevel>,
     overrides: &[(&str, &str)],
     add_group_id: bool,
     config_name: &str,
 ) -> AnyResult<ClientConfig> {
-    let mut settings: BTreeMap<&str, &str> = common
-        .kafka_options
+    let mut settings: BTreeMap<&str, &str> = kafka_options
         .iter()
         .chain(type_specific_options.iter())
         .map(|(o, v)| (o.as_str(), v.as_str()))
@@ -120,7 +118,7 @@ fn kafka_config(
         .iter()
         .map(|(&o, &v)| (String::from(o), String::from(v)))
         .collect();
-    if let Some(log_level) = common.log_level {
+    if let Some(log_level) = log_level {
         config.set_log_level(rdkafka_loglevel_from(log_level));
     }
 
@@ -144,10 +142,13 @@ struct CommonConfig {
     admin_config: ClientConfig,
 }
 
-impl TryFrom<&CommonConfigSchema> for CommonConfig {
-    type Error = AnyError;
-
-    fn try_from(source: &CommonConfigSchema) -> Result<Self, Self::Error> {
+impl CommonConfig {
+    fn new(
+        kafka_options: &BTreeMap<String, String>,
+        consumer_options: &BTreeMap<String, String>,
+        producer_options: &BTreeMap<String, String>,
+        log_level: Option<KafkaLogLevel>,
+    ) -> AnyResult<Self> {
         const CONSUMER_SETTINGS: &[(&str, &str)] = &[
             ("enable.auto.commit", "false"),
             ("enable.auto.offset.store", "false"),
@@ -157,8 +158,9 @@ impl TryFrom<&CommonConfigSchema> for CommonConfig {
             ("fetch.min.bytes", "1"),
         ];
         let mut seekable_consumer_config = kafka_config(
-            source,
-            &source.consumer_options,
+            kafka_options,
+            consumer_options,
+            log_level,
             CONSUMER_SETTINGS,
             true,
             "consumer",
@@ -177,15 +179,23 @@ impl TryFrom<&CommonConfigSchema> for CommonConfig {
             ("linger.ms", "0"),
         ];
         let mut producer_config = kafka_config(
-            source,
-            &source.producer_options,
+            kafka_options,
+            producer_options,
+            log_level,
             PRODUCER_SETTINGS,
             true,
             "producer",
         )?;
         producer_config.remove("group.id");
 
-        let admin_config = kafka_config(source, &BTreeMap::new(), &[], false, "admin")?;
+        let admin_config = kafka_config(
+            kafka_options,
+            &BTreeMap::new(),
+            log_level,
+            &[],
+            false,
+            "admin",
+        )?;
 
         Ok(Self {
             seekable_consumer_config,

crates/adapters/src/transport/kafka/ft/output.rs
@@ -1,10 +1,10 @@
 use crate::{
     transport::{kafka::DeferredLogging, Step},
-    AsyncErrorCallback, OutputEndpoint, OutputEndpointConfig, OutputTransport,
+    AsyncErrorCallback, OutputEndpoint,
 };
 use anyhow::{anyhow, bail, Context, Error as AnyError, Result as AnyResult};
 use log::{debug, info, warn};
-use pipeline_types::transport::durable_kafka::KafkaDurableOutputConfig;
+use pipeline_types::transport::kafka::KafkaOutputConfig;
 use rdkafka::{
     config::FromClientConfigAndContext,
     consumer::BaseConsumer,
@@ -15,7 +15,6 @@ use rdkafka::{
 };
 use serde::{Deserialize, Serialize};
 use std::{
-    borrow::Cow,
     cmp::max,
     sync::{Condvar, Mutex, RwLock},
     time::Duration,
@@ -29,33 +28,6 @@ const DEFAULT_MAX_MESSAGE_SIZE: usize = 1_000_000;
 /// plus this overhead must not exceed `message.max.bytes`.
 // This value was established empirically.
 const MAX_MESSAGE_OVERHEAD: usize = 64;
-
-/// [`OutputTransport`] implementation that durably writes data to a Kafka
-/// topic.
-///
-/// This output transport is only available if the crate is configured with
-/// `with-kafka` feature.
-///
-/// The output transport factory gives this transport the name `durable_kafka`.
-pub struct KafkaDurableOutputTransport;
-
-impl OutputTransport for KafkaDurableOutputTransport {
-    fn name(&self) -> Cow<'static, str> {
-        Cow::Borrowed("durable_kafka")
-    }
-
-    /// Creates a new [`OutputEndpoint`] fpor writing to a Kafka topic,
-    /// interpreting `config` as a [`KafkaDurableOutputConfig`].
-    ///
-    /// See [`OutputTransport::new_endpoint()`] for more information.
-    fn new_endpoint(&self, config: &OutputEndpointConfig) -> AnyResult<Box<dyn OutputEndpoint>> {
-        let config =
-            KafkaDurableOutputConfig::deserialize(&config.connector_config.transport.config)?;
-        let ep = KafkaOutputEndpoint::new(config)?;
-
-        Ok(Box::new(ep))
-    }
-}
 
 /// State of the `KafkaOutputEndpoint`.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -102,7 +74,7 @@ impl OutputPosition {
     }
 }
 
-struct KafkaOutputEndpoint {
+pub struct KafkaOutputEndpoint {
     kafka_producer: ThreadedProducer<DataProducerContext>,
     topic: String,
     next_partition: usize,
@@ -113,8 +85,14 @@ struct KafkaOutputEndpoint {
 }
 
 impl KafkaOutputEndpoint {
-    fn new(config: KafkaDurableOutputConfig) -> AnyResult<Self> {
-        let mut common: CommonConfig = (&config.common).try_into()?;
+    pub fn new(config: KafkaOutputConfig) -> AnyResult<Self> {
+        let ft = config.fault_tolerance.as_ref().unwrap();
+        let mut common = CommonConfig::new(
+            &config.kafka_options,
+            &ft.consumer_options,
+            &ft.producer_options,
+            config.log_level,
+        )?;
         common
             .producer_config
             .set("transactional.id", &config.topic);
@@ -148,8 +126,9 @@ impl KafkaOutputEndpoint {
             .context()
             .deferred_logging
             .with_deferred_logging(|| {
-                kafka_producer
-                    .init_transactions(Duration::from_secs(config.initialization_timeout_secs))
+                kafka_producer.init_transactions(Duration::from_secs(
+                    config.initialization_timeout_secs.into(),
+                ))
             })?;
 
         // Read the number of partitions and the next step number.  We do this

crates/adapters/src/transport/kafka/mod.rs
@@ -1,35 +1,80 @@
-use anyhow::Error as AnyError;
-use pipeline_types::transport::kafka::KafkaLogLevel;
+use anyhow::{Error as AnyError, Result as AnyResult};
+use pipeline_types::{
+    config::OutputEndpointConfig,
+    transport::kafka::{KafkaInputConfig, KafkaLogLevel, KafkaOutputConfig},
+};
 use rdkafka::{
     client::{Client as KafkaClient, ClientContext},
     config::RDKafkaLogLevel,
     error::KafkaError,
     types::RDKafkaErrorCode,
 };
+use serde::Deserialize;
+use serde_yaml::Value as YamlValue;
+use std::borrow::Cow;
 #[cfg(test)]
 use std::sync::Mutex;
 
-mod input;
-mod output;
+use crate::{InputEndpoint, InputTransport, OutputEndpoint, OutputTransport};
 
-#[cfg(test)]
-pub mod test;
+use ft::{KafkaFtInputEndpoint, KafkaFtOutputEndpoint};
+use nonft::{KafkaInputEndpoint, KafkaOutputEndpoint};
 
-pub use input::KafkaInputTransport;
-pub use output::KafkaOutputTransport;
+mod ft;
+mod nonft;
 
-// fn kafka_loglevel_from(level: RDKafkaLogLevel) -> KafkaLogLevel {
-//     match level {
-//         RDKafkaLogLevel::Emerg => KafkaLogLevel::Emerg,
-//         RDKafkaLogLevel::Alert => KafkaLogLevel::Alert,
-//         RDKafkaLogLevel::Critical => KafkaLogLevel::Critical,
-//         RDKafkaLogLevel::Error => KafkaLogLevel::Error,
-//         RDKafkaLogLevel::Warning => KafkaLogLevel::Warning,
-//         RDKafkaLogLevel::Notice => KafkaLogLevel::Notice,
-//         RDKafkaLogLevel::Info => KafkaLogLevel::Info,
-//         RDKafkaLogLevel::Debug => KafkaLogLevel::Debug,
-//     }
-// }
+/// [`InputTransport`] implementation that reads data from one or more
+/// Kafka topics.
+///
+/// This input transport is only available if the crate is configured with
+/// `with-kafka` feature.
+///
+/// The input transport factory gives this transport the name `kafka`.
+pub struct KafkaInputTransport;
+
+impl InputTransport for KafkaInputTransport {
+    fn name(&self) -> Cow<'static, str> {
+        Cow::Borrowed("kafka")
+    }
+
+    /// Creates a new [`InputEndpoint`] for reading from Kafka topics,
+    /// interpreting `config` as a [`KafkaInputConfig`].
+    ///
+    /// See [`InputTransport::new_endpoint()`] for more information.
+    fn new_endpoint(&self, config: &YamlValue) -> AnyResult<Box<dyn InputEndpoint>> {
+        let config = KafkaInputConfig::deserialize(config)?;
+        match config.fault_tolerance {
+            None => Ok(Box::new(KafkaInputEndpoint::new(config)?)),
+            Some(_) => Ok(Box::new(KafkaFtInputEndpoint::new(config)?)),
+        }
+    }
+}
+
+/// [`OutputTransport`] implementation that writes data to a Kafka topic.
+///
+/// This output transport is only available if the crate is configured with
+/// `with-kafka` feature.
+///
+/// The output transport factory gives this transport the name `kafka`.
+pub struct KafkaOutputTransport;
+
+impl OutputTransport for KafkaOutputTransport {
+    fn name(&self) -> Cow<'static, str> {
+        Cow::Borrowed("kafka")
+    }
+
+    /// Creates a new [`OutputEndpoint`] fpor writing to a Kafka topic,
+    /// interpreting `config` as a [`KafkaOutputConfig`].
+    ///
+    /// See [`OutputTransport::new_endpoint()`] for more information.
+    fn new_endpoint(&self, config: &OutputEndpointConfig) -> AnyResult<Box<dyn OutputEndpoint>> {
+        let config = KafkaOutputConfig::deserialize(&config.connector_config.transport.config)?;
+        match config.fault_tolerance {
+            None => Ok(Box::new(KafkaOutputEndpoint::new(config)?)),
+            Some(_) => Ok(Box::new(KafkaFtOutputEndpoint::new(config)?)),
+        }
+    }
+}
 
 pub(crate) fn rdkafka_loglevel_from(level: KafkaLogLevel) -> RDKafkaLogLevel {
     match level {

crates/adapters/src/transport/kafka/nonft/input.rs
@@ -1,7 +1,10 @@
-use super::{refine_kafka_error, DeferredLogging};
 use crate::{
-    transport::{kafka::rdkafka_loglevel_from, secret_resolver::MaybeSecret, InputReader, Step},
-    InputConsumer, InputEndpoint, InputTransport, PipelineState,
+    transport::{
+        kafka::{rdkafka_loglevel_from, refine_kafka_error, DeferredLogging},
+        secret_resolver::MaybeSecret,
+        InputReader, Step,
+    },
+    InputConsumer, InputEndpoint, PipelineState,
 };
 use anyhow::{anyhow, bail, Error as AnyError, Result as AnyResult};
 use crossbeam::queue::ArrayQueue;
@@ -15,10 +18,7 @@ use rdkafka::{
     error::{KafkaError, KafkaResult},
     ClientConfig, ClientContext, Message,
 };
-use serde::Deserialize;
-use serde_yaml::Value as YamlValue;
 use std::{
-    borrow::Cow,
     sync::{
         atomic::{AtomicU32, Ordering},
         Arc, Mutex, Weak,
@@ -33,36 +33,12 @@ const POLL_TIMEOUT: Duration = Duration::from_millis(100);
 // to the worker thread.
 const ERROR_BUFFER_SIZE: usize = 1000;
 
-/// [`InputTransport`] implementation that reads data from one or more
-/// Kafka topics.
-///
-/// This input transport is only available if the crate is configured with
-/// `with-kafka` feature.
-///
-/// The input transport factory gives this transport the name `kafka`.
-pub struct KafkaInputTransport;
-
-impl InputTransport for KafkaInputTransport {
-    fn name(&self) -> Cow<'static, str> {
-        Cow::Borrowed("kafka")
-    }
-
-    /// Creates a new [`InputEndpoint`] for reading from Kafka topics,
-    /// interpreting `config` as a [`KafkaInputConfig`].
-    ///
-    /// See [`InputTransport::new_endpoint()`] for more information.
-    fn new_endpoint(&self, config: &YamlValue) -> AnyResult<Box<dyn InputEndpoint>> {
-        let config = KafkaInputConfig::deserialize(config)?;
-        Ok(Box::new(KafkaInputEndpoint::new(config)?))
-    }
-}
-
-struct KafkaInputEndpoint {
+pub struct KafkaInputEndpoint {
     config: Arc<KafkaInputConfig>,
 }
 
 impl KafkaInputEndpoint {
-    fn new(mut config: KafkaInputConfig) -> AnyResult<KafkaInputEndpoint> {
+    pub fn new(mut config: KafkaInputConfig) -> AnyResult<KafkaInputEndpoint> {
         config.validate()?;
         Ok(KafkaInputEndpoint {
             config: Arc::new(config),

crates/adapters/src/transport/kafka/nonft/mod.rs
@@ -1,0 +1,7 @@
+mod input;
+mod output;
+#[cfg(test)]
+pub mod test;
+
+pub use input::KafkaInputEndpoint;
+pub use output::KafkaOutputEndpoint;

commit e09a14a5746d7e74ac4dcc9e43a66bfe5796bbc4

dbsp_adapters: Fix config string in ignored test.
I didn't spot this in testing the previous commit because it was ignored.

Signed-off-by: Ben Pfaff <blp@feldera.com>
Fixes: e6391827d8cf ("dbsp_adapters: Unify `kafka` and `durable_kafka` transports.")

crates/adapters/src/transport/kafka/ft/test.rs
@@ -237,7 +237,7 @@ fn test_empty_input() {
     let config_str = r#"
 topics: [empty]
 log_level: debug
-fault_tolerance: {{}}
+fault_tolerance: {}
 "#
     .to_string();
 