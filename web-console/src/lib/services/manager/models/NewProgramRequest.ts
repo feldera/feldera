1755cdb11c2830ac774633020a4cb17ab6e5cfd9
George <bulakh.96@gmail.com>
2023-11-20 15:50:03 +0000

    Generate Pipeline Manager OpenAPI schema v0.3.2
    Signed-off-by: George <bulakh.96@gmail.com>

--- web-console/src/lib/services/manager/models/NewProgramRequest.ts
+++ web-console/src/lib/services/manager/models/NewProgramRequest.ts
@@ -17,6 +17,10 @@ export type NewProgramRequest = {
    */
   description: string
   /**
+   * Compile the program in JIT mode.
+   */
+  jit_mode?: boolean
+  /**
    * Program name.
    */
   name: string
