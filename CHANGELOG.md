1349b44ba5800cade44a106f4081f251e3e01e36
Lalith Suresh <lalith@feldera.com>
2023-11-21 10:57:07 -0800

    release: bump project version to 0.4.0
    Signed-off-by: Lalith Suresh <lalith@feldera.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.4.0] - 2023-11-21
+
 
 ### Fixed
 
--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -23,29 +23,39 @@ release() {
         exit 1
     fi
 
+    # Update pipeline manager version
     old_version=`cargo metadata --no-deps | jq -r '.packages[]|select(.name == "pipeline-manager")|.version'`
     case $1 in
         major|minor|patch) cargo set-version --bump $1 -p pipeline-manager ;;
         *) echo >&2 "Argument must be 'major' or 'minor' or 'patch'"; exit 1 ;;
     esac
+
+    # Update python API version
     new_version=`cargo metadata --no-deps | jq -r '.packages[]|select(.name == "pipeline-manager")|.version'`
     (cd .. && cargo make --cwd crates/pipeline_manager openapi_python)
 
+    # Update changelog
+    release_date=`date +"%Y-%m-%d"`
+    sed -i.backup "s/\[Unreleased\]/\[Unreleased\]\n\n\#\# \[$new_version\] - $release_date/" ../CHANGELOG.md
+
     # Commit the new version
-    tag="v$new_version"
-    if git rev-parse "$tag" >/dev/null 2>&1; then
-        echo "Error: tag $tag already exists. Aborting";
+    release_branch="release-v$new_version"
+    if git rev-parse "$release_branch" >/dev/null 2>&1; then
+        echo "Error: branch $release_branch already exists. Aborting";
         exit 1;
     fi
+    git checkout -b $release_branch
     git commit -am "release: bump project version to $new_version" -s
-    git tag -a v$new_version -m "v$new_version"
 
     # Patch the docker-compose.yml file with the new version. Check this
     # change in only after we confirm the new containers are available.
-    sed "s/\:\-${old_version}/\:\-${new_version}/g" ../deploy/docker-compose.yml > /tmp/docker-compose-bumped.yml
-    mv /tmp/docker-compose-bumped.yml ../deploy/docker-compose.yml
-    sed "s/\:\-${old_version}/\:\-${new_version}/g" ../deploy/docker-compose-debezium.yml > /tmp/docker-compose-bumped.yml
-    mv /tmp/docker-compose-bumped.yml ../deploy/docker-compose-debezium.yml
+    sed -i.backup "s/\:\-${old_version}/\:\-${new_version}/g" ../deploy/docker-compose.yml
+    sed -i.backup "s/\:\-${old_version}/\:\-${new_version}/g" ../deploy/docker-compose-debezium.yml
+
+    # Patch the last stable pipeline manager version in the Earthlfile
+    sed -i.backup "s/FELDERA_VERSION=${old_version}/FELDERA_VERSION=${new_version}/g" ../Earthfile
+    sed -i.backup "s/pipeline-manager\:${old_version}/pipeline-manager\:${new_version}/g" ../Earthfile
+    sed -i.backup "s/demo-container\:${old_version}/demo-container\:${new_version}/g" ../Earthfile
 }
 
 release "$@"
