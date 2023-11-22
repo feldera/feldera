237cac3b13d0ce3ef4d1bb8e06a80a4bc7a1cdcf
George <bulakh.96@gmail.com>
2023-11-22 18:16:21 +0000

    Add Playwright to WebConsole
    Signed-off-by: George <bulakh.96@gmail.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,4 @@
 # Build dbsp python library
 cd ./crates/pipeline_manager && cargo make openapi_python
+# Install Playwright system dependencies
+cd ../../web-console && yarn playwright install-deps
