509b47a749785eb8be1f6b39db3a23461a9ff0c6
George <bulakh.96@gmail.com>
2023-12-04 21:21:51 +0000

    Remove runtime dependency of Monaco Editor
    This change forces all and only required files of monaco-editor to statically bundle in the deployment
    
    Signed-off-by: George <bulakh.96@gmail.com>

--- web-console/src/app/(root)/(authenticated)/layout.tsx
+++ web-console/src/app/(root)/(authenticated)/layout.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { useAuth } from '$lib/compositions/auth/useAuth'
+import * as monaco from 'monaco-editor'
 import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
+
+import { loader } from '@monaco-editor/react'
 
 export default (props: { children: ReactNode }) => {
   const { auth } = useAuth()
@@ -12,3 +15,7 @@ export default (props: { children: ReactNode }) => {
 
   return props.children
 }
+
+// Configure monaco-editor to statically bundle required resources
+// https://www.npmjs.com/package/@monaco-editor/react#use-monaco-editor-as-an-npm-package
+loader.config({ monaco })
