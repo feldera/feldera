8a70b2c7430f17961ba62bc788099b07ddaa4f44
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2023-12-05 00:42:19 +0545

    Refactor and add tests for safe_add
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/SafeAddTests.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/SafeAddTests.java
@@ -1,50 +1,79 @@
 package org.dbsp.sqlCompiler.compiler.sql.simple;
 import org.dbsp.sqlCompiler.compiler.sql.SqlIoTest;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SafeAddTests extends SqlIoTest {
     @Test
-    public void testSimpleInt() {
-        this.q("SELECT SAFE_ADD(2, 3) as 'result';\n" +
+    public void testSafeAdd() {
+        this.qs(
+                "SELECT SAFE_ADD(2, 3) as 'result';\n" +
                 " result \n" +
                 "--------\n" +
-                "5"
+                "5\n" +
+                "(1 row)\n" +
+                "\n" +
+                "SELECT SAFE_ADD(9223372036854775807, 2) as 'result';\n" +
+                " result \n" +
+                "--------\n" +
+                " \n" +
+                "(1 row)\n" +
+                "\n" +
+                "SELECT SAFE_ADD(-9223372036854775807, -2) as 'result';\n" +
+                " result \n" +
+                "--------\n" +
+                " \n" +
+                "(1 row)\n" +
+                "\n" +
+                "SELECT SAFE_ADD(9.9, 1.0) as 'result';\n" +
+                " result \n" +
+                "--------\n" +
+                "10.9\n" +
+                "(1 row)\n" +
+                "\n" +
+                "SELECT SAFE_ADD(9.9, 1) as 'result';\n" +
+                " result \n" +
+                "--------\n" +
+                "10.9\n" +
+                "(1 row)\n" +
+                "\n" +
+                "SELECT SAFE_ADD(CAST(125 AS TINYINT), CAST(2 AS TINYINT)) as 'result';\n" +
+                " result \n" +
+                "--------\n" +
+                " 127\n" +
+                "(1 row)\n" +
+                "\n"
         );
     }
 
     @Test
-    public void testOverflowInt() {
-        this.q("SELECT SAFE_ADD(9223372036854775807, 2) as 'result';\n" +
+    @Ignore
+    public void testSafeAddWithCasts() {
+        this.q(
+        "SELECT SAFE_ADD(CAST(32767 AS SMALLINT), CAST(2 AS SMALLINT)) as 'result';\n" +
                 " result \n" +
                 "--------\n" +
-                " "
+                " \n"
         );
     }
 
     @Test
-    public void testUnderflowInt() {
-        this.q("SELECT SAFE_ADD(-9223372036854775807, -2) as 'result';\n" +
+    @Ignore
+    public void testSafeAddWithNestedSelectCast() {
+        this.q("SELECT SAFE_ADD((SELECT CAST(2 AS SMALLINT)), (SELECT CAST(4 AS SMALLINT))) as 'result';\n" +
                 " result \n" +
                 "--------\n" +
-                " "
+                " 6\n"
         );
     }
 
     @Test
-    public void testDecimal() {
-        this.q("SELECT SAFE_ADD(9.9, 1.0) as 'result';\n" +
+    @Ignore
+    public void testSafeAddDecimalOverflow() {
+        this.q("SELECT SAFE_ADD(9, cast(9.999999999999999999e75 as DECIMAL(38, 19))) as 'result';\n" +
                 " result \n" +
                 "--------\n" +
-                "10.9"
-        );
-    }
-
-    @Test
-    public void testDecimalInt() {
-        this.q("SELECT SAFE_ADD(9.9, 1) as 'result';\n" +
-                " result \n" +
-                "--------\n" +
-                "10.9"
+                " \n"
         );
     }
 }
