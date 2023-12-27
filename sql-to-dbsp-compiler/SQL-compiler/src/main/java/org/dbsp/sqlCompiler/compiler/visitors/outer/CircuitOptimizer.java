ef06559a3e5c0bf8916a728acfa0f2a223a44737
Mihai Budiu <mbudiu@feldera.com>
2023-12-26 18:09:53 -0800

    Remove 'debug' flag
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/visitors/outer/CircuitOptimizer.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/visitors/outer/CircuitOptimizer.java
@@ -51,7 +51,7 @@ public class CircuitOptimizer implements ICompilerComponent {
     @SuppressWarnings("ConstantValue")
     CircuitTransform monotonicity() {
         return circuit -> {
-            final boolean debug = true;
+            final boolean debug = false;
 
             if (debug)
                 ToDotVisitor.toDot(compiler, "original.jpg", true, "jpg", circuit);
