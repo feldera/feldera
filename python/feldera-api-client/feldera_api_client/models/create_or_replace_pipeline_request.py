e6bf70019ee7cd88ac0e4b6b4ff751c7a81acfd3
Lalith Suresh <lalith@feldera.com>
2024-01-11 08:59:19 -0800

    pipeline-manager: update OpenAPI bindings
    Signed-off-by: Lalith Suresh <lalith@feldera.com>

---
+++ python/feldera-api-client/feldera_api_client/models/create_or_replace_pipeline_request.py
@@ -0,0 +1,89 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.attached_connector import AttachedConnector
+    from ..models.runtime_config import RuntimeConfig
+
+
+T = TypeVar("T", bound="CreateOrReplacePipelineRequest")
+
+
+@define
+class CreateOrReplacePipelineRequest:
+    """Request to create or replace an existing pipeline.
+
+    Attributes:
+        config (RuntimeConfig): Global pipeline configuration settings. This is the publicly
+            exposed type for users to configure pipelines.
+        description (str): Pipeline description.
+        connectors (Union[Unset, None, List['AttachedConnector']]): Attached connectors.
+        program_name (Union[Unset, None, str]): Name of the program to create a pipeline for.
+    """
+
+    config: "RuntimeConfig"
+    description: str
+    connectors: Union[Unset, None, List["AttachedConnector"]] = UNSET
+    program_name: Union[Unset, None, str] = UNSET
+
+    def to_dict(self) -> Dict[str, Any]:
+        config = self.config.to_dict()
+
+        description = self.description
+        connectors: Union[Unset, None, List[Dict[str, Any]]] = UNSET
+        if not isinstance(self.connectors, Unset):
+            if self.connectors is None:
+                connectors = None
+            else:
+                connectors = []
+                for connectors_item_data in self.connectors:
+                    connectors_item = connectors_item_data.to_dict()
+
+                    connectors.append(connectors_item)
+
+        program_name = self.program_name
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(
+            {
+                "config": config,
+                "description": description,
+            }
+        )
+        if connectors is not UNSET:
+            field_dict["connectors"] = connectors
+        if program_name is not UNSET:
+            field_dict["program_name"] = program_name
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        from ..models.attached_connector import AttachedConnector
+        from ..models.runtime_config import RuntimeConfig
+
+        d = src_dict.copy()
+        config = RuntimeConfig.from_dict(d.pop("config"))
+
+        description = d.pop("description")
+
+        connectors = []
+        _connectors = d.pop("connectors", UNSET)
+        for connectors_item_data in _connectors or []:
+            connectors_item = AttachedConnector.from_dict(connectors_item_data)
+
+            connectors.append(connectors_item)
+
+        program_name = d.pop("program_name", UNSET)
+
+        create_or_replace_pipeline_request = cls(
+            config=config,
+            description=description,
+            connectors=connectors,
+            program_name=program_name,
+        )
+
+        return create_or_replace_pipeline_request
