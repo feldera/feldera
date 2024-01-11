5257e59dd6a495c19fab3cdb788247718952182e
Leonid Ryzhyk <leonid@feldera.com>
2024-01-11 00:31:36 -0800

    Bump Rust dependencies flagged by dependabot.
    See #1181, #1212.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- python/feldera-api-client/feldera_api_client/models/new_pipeline_request.py
+++ python/feldera-api-client/feldera_api_client/models/new_pipeline_request.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
 
-from attrs import define, field
+from attrs import define
 
 from ..types import UNSET, Unset
 
@@ -30,7 +30,6 @@ class NewPipelineRequest:
     name: str
     connectors: Union[Unset, None, List["AttachedConnector"]] = UNSET
     program_name: Union[Unset, None, str] = UNSET
-    additional_properties: Dict[str, Any] = field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
         config = self.config.to_dict()
@@ -51,7 +50,6 @@ class NewPipelineRequest:
         program_name = self.program_name
 
         field_dict: Dict[str, Any] = {}
-        field_dict.update(self.additional_properties)
         field_dict.update(
             {
                 "config": config,
@@ -95,21 +93,4 @@ class NewPipelineRequest:
             program_name=program_name,
         )
 
-        new_pipeline_request.additional_properties = d
         return new_pipeline_request
-
-    @property
-    def additional_keys(self) -> List[str]:
-        return list(self.additional_properties.keys())
-
-    def __getitem__(self, key: str) -> Any:
-        return self.additional_properties[key]
-
-    def __setitem__(self, key: str, value: Any) -> None:
-        self.additional_properties[key] = value
-
-    def __delitem__(self, key: str) -> None:
-        del self.additional_properties[key]
-
-    def __contains__(self, key: str) -> bool:
-        return key in self.additional_properties
