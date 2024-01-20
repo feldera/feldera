36c314e5bf32149c361edf37d723987b0b39e3c5
Mihai Budiu <mbudiu@gmail.com>
2024-01-19 16:16:23 -0800

    [SQL] Add TPC-DS sources (currently test is disabled) (#1283)
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

---
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/suites/TpcDsTest.java
@@ -0,0 +1,25 @@
+package org.dbsp.sqlCompiler.compiler.sql.suites;
+
+import org.dbsp.sqlCompiler.compiler.CompilerOptions;
+import org.dbsp.sqlCompiler.compiler.DBSPCompiler;
+import org.dbsp.sqlCompiler.compiler.TestUtil;
+import org.dbsp.sqlCompiler.compiler.sql.BaseSQLTests;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class TpcDsTest extends BaseSQLTests {
+    @Test @Ignore("https://github.com/feldera/feldera/issues/1281")
+    public void compileTpcds() throws IOException {
+        String tpch = TestUtil.readStringFromResourceFile("tpcds.sql");
+        CompilerOptions options = this.testOptions(true, true);
+        DBSPCompiler compiler = new DBSPCompiler(options);
+        options.languageOptions.ignoreOrderBy = true;
+        options.languageOptions.outputsAreSets = true;
+        compiler.compileStatements(tpch);
+        System.err.println(compiler.messages);
+        compiler.throwIfErrorsOccurred();
+        this.addRustTestCase("tpcds", compiler, getCircuit(compiler));
+    }
+}
