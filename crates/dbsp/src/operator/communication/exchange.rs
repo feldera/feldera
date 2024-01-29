feldera/feldera

commit de643bf89bc1a18f1130c31f7f7ec54be1179b23

Use storage backends in layer files and adapt dbsp to the changes.
This modifies `feldera_storage::file` from using `File` directly to using
`feldera_storage::backend`, and then adapts the `dbsp` code that used
`feldera_storage::file` to the new interface.

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/feldera-storage/src/file/mod.rs
@@ -155,12 +155,14 @@ pub type Deserializer = Infallible;
 
 #[cfg(test)]
 mod test {
-    use std::fmt::Debug;
+    use std::{fmt::Debug, rc::Rc};
 
-    use tempfile::tempfile;
+    use crate::backend::{
+        DefaultBackend, StorageControl, StorageExecutor, StorageRead, StorageWrite,
+    };
 
     use super::{
-        reader::{ColumnSpec, Reader, RowGroup},
+        reader::{ColumnSpec, RowGroup},
         writer::{Parameters, Writer1, Writer2},
         Rkyv,
     };
@@ -184,13 +186,14 @@ mod test {
         fn aux1(row0: usize, row1: usize) -> Self::A1;
     }
 
-    fn test_find<K, A, N, T>(
-        row_group: &RowGroup<K, A, N, T>,
+    fn test_find<S, K, A, N, T>(
+        row_group: &RowGroup<S, K, A, N, T>,
         before: &K,
         key: &K,
         after: &K,
         aux: A,
     ) where
+        S: StorageRead + StorageExecutor,
         K: Rkyv + Debug + Ord + Eq + Clone,
         A: Rkyv + Debug + Eq + Clone,
         T: ColumnSpec,
@@ -228,8 +231,9 @@ mod test {
         assert_eq!(unsafe { cursor.item() }, Some((key.clone(), aux.clone())));
     }
 
-    fn test_out_of_range<K, A, N, T>(row_group: &RowGroup<K, A, N, T>, before: &K, after: &K)
+    fn test_out_of_range<S, K, A, N, T>(row_group: &RowGroup<S, K, A, N, T>, before: &K, after: &K)
     where
+        S: StorageRead + StorageExecutor,
         K: Rkyv + Debug + Ord + Eq + Clone,
         A: Rkyv + Debug + Eq + Clone,
         T: ColumnSpec,
@@ -252,12 +256,13 @@ mod test {
     }
 
     #[allow(clippy::len_zero)]
-    fn test_cursor_helper<K, A, N, T>(
-        rows: &RowGroup<K, A, N, T>,
+    fn test_cursor_helper<S, K, A, N, T>(
+        rows: &RowGroup<S, K, A, N, T>,
         offset: u64,
         n: usize,
         expected: impl Fn(usize) -> (K, K, K, A),
     ) where
+        S: StorageRead + StorageExecutor,
         K: Rkyv + Debug + Ord + Eq + Clone,
         A: Rkyv + Debug + Eq + Clone,
         T: ColumnSpec,
@@ -323,11 +328,12 @@ mod test {
         }
     }
 
-    fn test_cursor<K, A, N, T>(
-        rows: &RowGroup<K, A, N, T>,
+    fn test_cursor<S, K, A, N, T>(
+        rows: &RowGroup<S, K, A, N, T>,
         n: usize,
         expected: impl Fn(usize) -> (K, K, K, A),
     ) where
+        S: StorageRead + StorageExecutor,
         K: Rkyv + Debug + Ord + Eq + Clone,
         A: Rkyv + Debug + Eq + Clone,
         T: ColumnSpec,
@@ -343,8 +349,12 @@ mod test {
         })
     }
 
-    fn test_two_columns<T: TwoColumns>(parameters: Parameters) {
-        let mut layer_file = Writer2::new(tempfile().unwrap(), parameters).unwrap();
+    fn test_two_columns<S, T>(storage: &Rc<S>, parameters: Parameters)
+    where
+        S: StorageControl + StorageRead + StorageWrite + StorageExecutor,
+        T: TwoColumns,
+    {
+        let mut layer_file = Writer2::new(storage, parameters).unwrap();
         let n0 = T::n0();
         for row0 in 0..n0 {
             for row1 in 0..T::n1(row0) {
@@ -376,7 +386,10 @@ mod test {
         }
     }
 
-    fn test_2_columns_helper(parameters: Parameters) {
+    fn test_2_columns_helper<S>(storage: &Rc<S>, parameters: Parameters)
+    where
+        S: StorageControl + StorageRead + StorageWrite + StorageExecutor,
+    {
         struct TwoInts;
         impl TwoColumns for TwoInts {
             type K0 = i32;
@@ -413,41 +426,54 @@ mod test {
             }
         }
 
-        test_two_columns::<TwoInts>(parameters);
+        test_two_columns::<_, TwoInts>(storage, parameters);
     }
 
     #[test]
     fn test_2_columns() {
-        test_2_columns_helper(Parameters::default());
+        test_2_columns_helper(&DefaultBackend::default_for_thread(), Parameters::default());
+    }
+
+    #[cfg(feature = "glommio")]
+    #[test]
+    fn test_2_columns_glommio() {
+        use crate::backend::glommio_impl::GlommioBackend;
+
+        test_2_columns_helper(&GlommioBackend::default_for_thread(), Parameters::default());
     }
 
     #[test]
     fn test_2_columns_max_branch_2() {
-        test_2_columns_helper(Parameters::with_max_branch(2));
+        test_2_columns_helper(
+            &DefaultBackend::default_for_thread(),
+            Parameters::with_max_branch(2),
+        );
     }
 
-    fn test_one_column<K, A>(
+    fn test_one_column<S, K, A>(
+        storage: &Rc<S>,
         n: usize,
         expected: impl Fn(usize) -> (K, K, K, A),
         parameters: Parameters,
     ) where
+        S: StorageControl + StorageRead + StorageWrite + StorageExecutor,
         K: Rkyv + Debug + Ord + Eq + Clone,
         A: Rkyv + Debug + Eq + Clone,
     {
-        let mut layer_file = Writer1::new(tempfile().unwrap(), parameters).unwrap();
+        let mut writer = Writer1::new(storage, parameters).unwrap();
         for row in 0..n {
             let (_before, key, _after, aux) = expected(row);
-            layer_file.write0((&key, &aux)).unwrap();
+            writer.write0((&key, &aux)).unwrap();
         }
-        let file = layer_file.close().unwrap();
 
-        let reader = Reader::<(K, A, ())>::new(file).unwrap();
+        let reader = writer.into_reader().unwrap();
         assert_eq!(reader.rows().len(), n as u64);
         test_cursor(&reader.rows(), n, expected);
     }
 
     fn test_i64_helper(parameters: Parameters) {
         test_one_column(
+            &DefaultBackend::default_for_thread(),
             1000,
             |row| (row * 2, row * 2 + 1, row * 2 + 2, ()),
             parameters,
@@ -481,6 +507,7 @@ mod test {
         }
 
         test_one_column(
+            &DefaultBackend::default_for_thread(),
             1000,
             |row| (f(row * 2), f(row * 2 + 1), f(row * 2 + 2), ()),
             Parameters::default(),
@@ -490,6 +517,7 @@ mod test {
     #[test]
     fn test_tuple() {
         test_one_column(
+            &DefaultBackend::default_for_thread(),
             1000,
             |row| ((row, 0), (row, 1), (row, 2), row),
             Parameters::default(),
@@ -502,6 +530,7 @@ mod test {
             (0..row as i64).collect()
         }
         test_one_column(
+            &DefaultBackend::default_for_thread(),
             500,
             |row| (v(row * 2), v(row * 2 + 1), v(row * 2 + 2), ()),
             Parameters::default(),

crates/dbsp/src/trace/layers/file/column_layer/cursor.rs
@@ -5,7 +5,10 @@ use std::{
 
 use feldera_storage::file::reader::Cursor as FileCursor;
 
-use crate::{trace::layers::Cursor, DBData, DBWeight};
+use crate::{
+    trace::{layers::Cursor, ord::file::StorageBackend},
+    DBData, DBWeight,
+};
 
 use super::FileColumnLayer;
 
@@ -18,7 +21,7 @@ where
 {
     storage: &'s FileColumnLayer<K, R>,
     item: Option<(K, R)>,
-    cursor: FileCursor<'s, K, R, (), (K, R, ())>,
+    cursor: FileCursor<'s, StorageBackend, K, R, (), (K, R, ())>,
 }
 
 impl<'s, K, R> FileColumnLayerCursor<'s, K, R>

crates/dbsp/src/trace/layers/file/column_layer/mod.rs
@@ -16,6 +16,7 @@ use std::{
 
 use crate::algebra::{AddAssignByRef, AddByRef, NegByRef};
 use crate::trace::layers::{Builder, Trie, TupleBuilder};
+use crate::trace::ord::file::StorageBackend;
 use crate::{DBData, DBWeight, NumEntries};
 
 pub use self::builders::FileColumnLayerBuilder;
@@ -24,7 +25,7 @@ pub use consumer::{FileColumnLayerConsumer, FileColumnLayerValues};
 
 #[derive(Clone)]
 pub struct FileColumnLayer<K, R> {
-    file: Reader<(K, R, ())>,
+    file: Reader<StorageBackend, (K, R, ())>,
     lower_bound: usize,
 }
 
@@ -43,7 +44,7 @@ where
 
     pub fn empty() -> Self {
         Self {
-            file: Reader::empty().unwrap(),
+            file: Reader::empty(&StorageBackend::default_for_thread()).unwrap(),
             lower_bound: 0,
         }
     }

crates/dbsp/src/trace/layers/file/ordered/mod.rs
@@ -6,17 +6,18 @@ use feldera_storage::file::{
     writer::{Parameters, Writer2},
 };
 use rand::{seq::index::sample, Rng};
-use tempfile::tempfile;
 
 use crate::{
     algebra::{AddAssignByRef, AddByRef, NegByRef},
-    trace::layers::{Builder, Cursor, MergeBuilder, Trie, TupleBuilder},
+    trace::{
+        layers::{Builder, Cursor, MergeBuilder, Trie, TupleBuilder},
+        ord::file::StorageBackend,
+    },
     DBData, DBWeight, NumEntries,
 };
 use std::{
     cmp::{min, Ordering},
     fmt::Debug,
-    fs::File,
     ops::{Add, AddAssign, Neg, Range},
 };
 
@@ -27,7 +28,8 @@ where
     V: 'static,
     R: 'static,
 {
-    file: Reader<(K, (), (V, R, ()))>,
+    #[allow(clippy::type_complexity)]
+    file: Reader<StorageBackend, (K, (), (V, R, ()))>,
     lower_bound: usize,
 }
 
@@ -83,7 +85,7 @@ where
 {
     pub fn empty() -> Self {
         Self {
-            file: Reader::empty().unwrap(),
+            file: Reader::empty(&StorageBackend::default_for_thread()).unwrap(),
             lower_bound: 0,
         }
     }
@@ -204,7 +206,7 @@ where
     }
 }
 
-pub struct FileOrderedMergeBuilder<K, V, R>(Writer2<File, K, (), V, R>)
+pub struct FileOrderedMergeBuilder<K, V, R>(Writer2<StorageBackend, K, (), V, R>)
 where
     K: DBData,
     V: DBData,
@@ -382,7 +384,7 @@ where
     }
 
     fn with_key_capacity(_capacity: usize) -> Self {
-        Self(Writer2::new(tempfile().unwrap(), Parameters::default()).unwrap())
+        Self(Writer2::new(&StorageBackend::default_for_thread(), Parameters::default()).unwrap())
     }
 
     fn reserve(&mut self, _additional: usize) {}
@@ -424,7 +426,7 @@ where
     V: DBData,
     R: DBWeight,
 {
-    writer: Writer2<File, K, (), V, R>,
+    writer: Writer2<StorageBackend, K, (), V, R>,
     key: Option<K>,
 }
 
@@ -462,7 +464,8 @@ where
 
     fn new() -> Self {
         Self {
-            writer: Writer2::new(tempfile().unwrap(), Parameters::default()).unwrap(),
+            writer: Writer2::new(&StorageBackend::default_for_thread(), Parameters::default())
+                .unwrap(),
             key: None,
         }
     }
@@ -500,7 +503,7 @@ where
     storage: &'s FileOrderedLayer<K, V, R>,
     key: Option<K>,
     #[allow(clippy::type_complexity)]
-    cursor: FileCursor<'s, K, (), (V, R, ()), (K, (), (V, R, ()))>,
+    cursor: FileCursor<'s, StorageBackend, K, (), (V, R, ()), (K, (), (V, R, ()))>,
 }
 
 impl<'s, K, V, R> FileOrderedCursor<'s, K, V, R>
@@ -543,7 +546,7 @@ where
 
     fn move_cursor(
         &mut self,
-        f: impl FnOnce(&mut FileCursor<'s, K, (), (V, R, ()), (K, (), (V, R, ()))>),
+        f: impl FnOnce(&mut FileCursor<'s, StorageBackend, K, (), (V, R, ()), (K, (), (V, R, ()))>),
     ) {
         f(&mut self.cursor);
         self.key = unsafe { self.cursor.key() };
@@ -648,7 +651,7 @@ where
 {
     item: Option<(V, R)>,
     #[allow(clippy::type_complexity)]
-    cursor: FileCursor<'s, V, R, (), (K, (), (V, R, ()))>,
+    cursor: FileCursor<'s, StorageBackend, V, R, (), (K, (), (V, R, ()))>,
 }
 
 impl<'s, K, V, R> FileOrderedValueCursor<'s, K, V, R>
@@ -658,7 +661,9 @@ where
     R: DBWeight,
 {
     #[allow(clippy::type_complexity)]
-    pub fn new(cursor: &FileCursor<'s, K, (), (V, R, ()), (K, (), (V, R, ()))>) -> Self {
+    pub fn new(
+        cursor: &FileCursor<'s, StorageBackend, K, (), (V, R, ()), (K, (), (V, R, ()))>,
+    ) -> Self {
         let cursor = cursor.next_column().unwrap().first().unwrap();
         let item = unsafe { cursor.item() };
         Self { cursor, item }
@@ -683,7 +688,10 @@ where
         item
     }
 
-    fn move_cursor(&mut self, f: impl Fn(&mut FileCursor<'s, V, R, (), (K, (), (V, R, ()))>)) {
+    fn move_cursor(
+        &mut self,
+        f: impl Fn(&mut FileCursor<'s, StorageBackend, V, R, (), (K, (), (V, R, ()))>),
+    ) {
         f(&mut self.cursor);
         self.item = unsafe { self.cursor.item() };
     }

crates/dbsp/src/trace/ord/file/mod.rs
@@ -8,6 +8,8 @@ pub use indexed_zset_batch::FileIndexedZSet;
 pub use key_batch::FileKeyBatch;
 pub use zset_batch::FileZSet;
 
+pub type StorageBackend = feldera_storage::backend::DefaultBackend;
+
 use crate::trace::Spine;
 
 /// A trace implementation for empty values using a spine of ordered lists.

crates/dbsp/Cargo.toml
@@ -63,7 +63,6 @@ log = "0.4.20"
 paste = "1"
 derive_more = { version = "0.99.17", features = ["add", "not", "from"] }
 feldera-storage = { path = "../feldera-storage" }
-tempfile = "3.8.1"
 ouroboros = "0.18.1"
 
 [dev-dependencies]

crates/dbsp/src/trace/layers/file/column_layer/builders.rs
@@ -1,6 +1,9 @@
 use crate::{
-    trace::layers::{
-        file::column_layer::FileColumnLayer, Builder, Cursor, MergeBuilder, Trie, TupleBuilder,
+    trace::{
+        layers::{
+            file::column_layer::FileColumnLayer, Builder, Cursor, MergeBuilder, Trie, TupleBuilder,
+        },
+        ord::file::StorageBackend,
     },
     DBData, DBWeight,
 };
@@ -9,11 +12,10 @@ use feldera_storage::file::{
     writer::{Parameters, Writer1},
 };
 use size_of::SizeOf;
-use std::{cmp::Ordering, fs::File};
-use tempfile::tempfile;
+use std::cmp::Ordering;
 
 /// Implements [`MergeBuilder`] and [`TupleBuilder`] for [`FileColumnLayer`].
-pub struct FileColumnLayerBuilder<K, R>(Writer1<File, K, R>)
+pub struct FileColumnLayerBuilder<K, R>(Writer1<StorageBackend, K, R>)
 where
     K: DBData,
     R: DBWeight;
@@ -31,7 +33,11 @@ where
 
     fn done(self) -> Self::Trie {
         FileColumnLayer {
-            file: Reader::new(self.0.close().unwrap()).unwrap(),
+            file: Reader::new(
+                &StorageBackend::default_for_thread(),
+                self.0.close().unwrap(),
+            )
+            .unwrap(),
             lower_bound: 0,
         }
     }
@@ -48,7 +54,7 @@ where
     }
 
     fn with_key_capacity(_capacity: usize) -> Self {
-        Self(Writer1::new(tempfile().unwrap(), Parameters::default()).unwrap())
+        Self(Writer1::new(&StorageBackend::default_for_thread(), Parameters::default()).unwrap())
     }
 
     fn reserve(&mut self, _additional: usize) {}
@@ -135,7 +141,7 @@ where
     type Item = (K, R);
 
     fn new() -> Self {
-        Self(Writer1::new(tempfile().unwrap(), Parameters::default()).unwrap())
+        Self(Writer1::new(&StorageBackend::default_for_thread(), Parameters::default()).unwrap())
     }
 
     fn with_capacity(_capacity: usize) -> Self {

commit 70fc662b86c26dce35c2ad34f020080fc2e09ac8

dbsp: Fix build with `--features=persistence`.
The problem was that the persistence code was using `rkyv::to_bytes`,
which only works with one particular serializer that we're no longer using.
This commit adds an equivalent for our serializer and switches the
persistence code (and one other caller) to use it.

Thanks to @gz for reporting the problem.

Signed-off-by: Ben Pfaff <blp@feldera.com>
Fixes: de643bf89bc1 ("Use storage backends in layer files and adapt dbsp to the changes.")

crates/dbsp/src/operator/communication/exchange.rs
@@ -12,14 +12,13 @@ use crate::{
         Host, LocalStoreMarker, OwnershipPreference, Runtime, Scope,
     },
     circuit_cache_key,
-    trace::{unaligned_deserialize, Rkyv, Serializer},
+    trace::{unaligned_deserialize, Rkyv},
 };
 
 use crossbeam_utils::CachePadded;
-use feldera_storage::buffer_cache::FBuf;
+use feldera_storage::file::to_bytes;
 use futures::{future, prelude::*};
 use once_cell::sync::{Lazy, OnceCell};
-use rkyv::{ser::Serializer as _, Fallible, Serialize};
 use std::{
     borrow::Cow,
     collections::HashMap,
@@ -93,15 +92,6 @@ impl ExchangeService for ExchangeServer {
 // Maps from a range of worker IDs to the RPC client used to contact those
 // workers.  Only worker IDs for remote workers appear in the map.
 struct Clients(Vec<(Host, TokioOnceCell<ExchangeServiceClient>)>);
-
-pub fn rkyv_serialize<T>(value: &T) -> Result<FBuf, <Serializer as Fallible>::Error>
-where
-    T: Serialize<Serializer>,
-{
-    let mut serializer = Serializer::default();
-    serializer.serialize_value(value)?;
-    Ok(serializer.into_serializer().into_inner())
-}
 
 impl Clients {
     fn new(runtime: &Runtime) -> Clients {
@@ -522,7 +512,7 @@ where
                                     .unwrap()
                                     .take()
                                     .unwrap();
-                                rkyv_serialize(&item).unwrap().into_vec()
+                                to_bytes(&item).unwrap().into_vec()
                             })
                             .collect()
                     })

crates/dbsp/src/trace/persistent/cursor.rs
@@ -5,7 +5,7 @@
 
 use std::sync::Arc;
 
-use rkyv::to_bytes;
+use feldera_storage::file::to_bytes;
 use rocksdb::{BoundColumnFamily, DBRawIterator};
 
 use super::trace::PersistedValue;

crates/dbsp/src/trace/persistent/trace.rs
@@ -5,9 +5,10 @@ use std::fmt::{Debug, Formatter};
 use std::marker::PhantomData;
 use std::sync::Arc;
 
+use feldera_storage::file::to_bytes;
 use rand::Rng;
 use rkyv::ser::Serializer;
-use rkyv::{to_bytes, Archive, Archived, Deserialize, Fallible, Serialize};
+use rkyv::{Archive, Archived, Deserialize, Fallible, Serialize};
 use rocksdb::compaction_filter::Decision;
 use rocksdb::{BoundColumnFamily, MergeOperands, Options, WriteBatch};
 use size_of::SizeOf;

crates/feldera-storage/src/file/mod.rs
@@ -72,10 +72,13 @@
 #[cfg(doc)]
 use crc32c::crc32c;
 use rkyv::{
-    ser::serializers::{
-        AllocScratch, CompositeSerializer, FallbackScratch, HeapScratch, SharedSerializeMap,
+    ser::{
+        serializers::{
+            AllocScratch, CompositeSerializer, FallbackScratch, HeapScratch, SharedSerializeMap,
+        },
+        Serializer as _,
     },
-    Archive, Archived, Deserialize, Infallible, Serialize,
+    Archive, Archived, Deserialize, Fallible, Infallible, Serialize,
 };
 
 use crate::buffer_cache::{FBuf, FBufSerializer};
@@ -152,6 +155,19 @@ pub type Serializer = CompositeSerializer<
 
 /// The particular [`rkyv`] deserializer that we use.
 pub type Deserializer = Infallible;
+
+/// Serializes the given value and returns the resulting bytes.
+///
+/// This is like [`rkyv::to_bytes`], but that function only works with one
+/// particular serializer whereas this function works with our [`Serializer`].
+pub fn to_bytes<T>(value: &T) -> Result<FBuf, <Serializer as Fallible>::Error>
+where
+    T: Serialize<Serializer>,
+{
+    let mut serializer = Serializer::default();
+    serializer.serialize_value(value)?;
+    Ok(serializer.into_serializer().into_inner())
+}
 
 #[cfg(test)]
 mod test {