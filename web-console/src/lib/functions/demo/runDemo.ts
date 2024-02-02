f96e5e7104becd182a658cc962697ddded773e9b
George <bulakh.96@gmail.com>
2024-02-02 16:47:45 +0000

    Add alert when conflicting entities are present before demo setup
    Signed-off-by: George <bulakh.96@gmail.com>

--- web-console/src/lib/functions/demo/runDemo.ts
+++ web-console/src/lib/functions/demo/runDemo.ts
@@ -1,6 +1,5 @@
 import type { DemoSetup } from '$lib/types/demo'
 import {
-  ApiError,
   ConnectorDescr,
   ConnectorsService,
   Pipeline,
@@ -10,32 +9,69 @@ import {
 } from '$lib/services/manager'
 import { match } from 'ts-pattern'
 
-export const runDemoSetup = async function* ({ prefix, steps }: DemoSetup) {
-  const entityTotal = steps.reduce((acc, step) => acc + step.entities.length, 0)
-  let entityNumber = 0
-  if (steps.length) {
-    yield { description: steps[0].name, ratio: 0 }
-  }
-  for (const step of steps) {
-    for (const entity of step.entities) {
-      await match(entity)
-        .with({ type: 'program' }, e => ProgramsService.createOrReplaceProgram(prefix + e.name, e.body).then())
-        .with({ type: 'connector' }, e => ConnectorsService.createOrReplaceConnector(prefix + e.name, e.body).then())
-        .with({ type: 'pipeline' }, e =>
-          PipelinesService.createOrReplacePipeline(prefix + e.name, {
-            ...e.body,
-            program_name: e.body.program_name ? prefix + e.body.program_name : e.body.program_name,
-            connectors: e.body.connectors
-              ? e.body.connectors.map(connector => ({
-                  ...connector,
-                  connector_name: prefix + connector.connector_name
-                }))
-              : e.body.connectors
-          }).then()
-        )
-        .exhaustive()
-      ++entityNumber
-      yield { description: step.name, ratio: entityNumber / entityTotal }
+const relatedEntities = async ({ prefix, steps }: DemoSetup) => {
+  const partOfTheDemo = (target: Pipeline | ProgramDescr | ConnectorDescr) =>
+    (name => !!steps.find(step => step.entities.find(entity => prefix + entity.name === name)))(
+      'name' in target ? target.name : target.descriptor.name
+    )
+  const [relatedPipelines, relatedPrograms, relatedConnectors] = await Promise.all([
+    PipelinesService.listPipelines().then(es =>
+      es.filter(e => e.descriptor.name.startsWith(prefix)).filter(partOfTheDemo)
+    ),
+    ProgramsService.getPrograms().then(es => es.filter(e => e.name.startsWith(prefix)).filter(partOfTheDemo)),
+    ConnectorsService.listConnectors().then(es => es.filter(e => e.name.startsWith(prefix)).filter(partOfTheDemo))
+  ])
+  return { relatedPipelines, relatedPrograms, relatedConnectors }
+}
+
+export const runDemoSetup = async ({ prefix, steps }: DemoSetup) => {
+  const { relatedPipelines, relatedPrograms, relatedConnectors } = await relatedEntities({ prefix, steps })
+  return {
+    entities: steps.flatMap(step =>
+      step.entities.map(e =>
+        match(e)
+          .with({ type: 'program' }, e => ({ ...e, exists: !!relatedPrograms.find(r => r.name === prefix + e.name) }))
+          .with({ type: 'connector' }, e => ({
+            ...e,
+            exists: !!relatedConnectors.find(r => r.name === prefix + e.name)
+          }))
+          .with({ type: 'pipeline' }, e => ({
+            ...e,
+            exists: !!relatedPipelines.find(r => r.descriptor.name === prefix + e.name)
+          }))
+          .exhaustive()
+      )
+    ),
+    setup: async function* () {
+      const entityTotal = steps.reduce((acc, step) => acc + step.entities.length, 0)
+      let entityNumber = 0
+      if (steps.length) {
+        yield { description: steps[0].name, ratio: 0 }
+      }
+      for (const step of steps) {
+        for (const entity of step.entities) {
+          await match(entity)
+            .with({ type: 'program' }, e => ProgramsService.createOrReplaceProgram(prefix + e.name, e.body).then())
+            .with({ type: 'connector' }, e =>
+              ConnectorsService.createOrReplaceConnector(prefix + e.name, e.body).then()
+            )
+            .with({ type: 'pipeline' }, e =>
+              PipelinesService.createOrReplacePipeline(prefix + e.name, {
+                ...e.body,
+                program_name: e.body.program_name ? prefix + e.body.program_name : e.body.program_name,
+                connectors: e.body.connectors
+                  ? e.body.connectors.map(connector => ({
+                      ...connector,
+                      connector_name: prefix + connector.connector_name
+                    }))
+                  : e.body.connectors
+              }).then()
+            )
+            .exhaustive()
+          ++entityNumber
+          yield { description: step.name, ratio: entityNumber / entityTotal }
+        }
+      }
     }
   }
 }
@@ -47,39 +83,7 @@ const cleanupDescription = {
 }
 
 export const runDemoCleanup = async ({ prefix, steps }: DemoSetup) => {
-  const partOfTheDemo = (target: Pipeline | ProgramDescr | ConnectorDescr) =>
-    (name => steps.find(step => step.entities.find(entity => entity.name === name)))(
-      'name' in target ? target.name : target.descriptor.name
-    )
-  const [relatedPipelines, relatedPrograms, relatedConnectors] = await Promise.all([
-    PipelinesService.listPipelines().then(
-      es => es.filter(e => e.descriptor.name.startsWith(prefix)).filter(partOfTheDemo),
-      e => {
-        if (e instanceof ApiError && (e.body as any).error_code === 'UnknownPipelineName') {
-          return []
-        }
-        throw e
-      }
-    ),
-    ProgramsService.getPrograms().then(
-      es => es.filter(e => e.name.startsWith(prefix)).filter(partOfTheDemo),
-      e => {
-        if (e instanceof ApiError && (e.body as any).error_code === 'UnknownProgramName') {
-          return []
-        }
-        throw e
-      }
-    ),
-    ConnectorsService.listConnectors().then(
-      es => es.filter(e => e.name.startsWith(prefix)).filter(partOfTheDemo),
-      e => {
-        if (e instanceof ApiError && (e.body as any).error_code === 'UnknownConnectorName') {
-          return []
-        }
-        throw e
-      }
-    )
-  ])
+  const { relatedPipelines, relatedPrograms, relatedConnectors } = await relatedEntities({ prefix, steps })
   return {
     relatedPipelines,
     relatedPrograms,
