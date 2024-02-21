d7897d2adeea835ff69f50d465c2cdb547bac883
Simon Kassing <simon.kassing@feldera.com>
2024-02-21 14:39:48 +0100

    pipeline-manager: integration test: longer compilation timeout
    Signed-off-by: Simon Kassing <simon.kassing@feldera.com>

--- crates/pipeline_manager/src/integration_test.rs
+++ crates/pipeline_manager/src/integration_test.rs
@@ -443,7 +443,7 @@ impl TestConfig {
         loop {
             println!("Waiting for compilation");
             std::thread::sleep(time::Duration::from_secs(1));
-            if now.elapsed().as_secs() > 200 {
+            if now.elapsed().as_secs() > 400 {
                 panic!("Compilation timeout");
             }
             let mut resp = self.get(format!("/v0/programs/{program_name}")).await;
