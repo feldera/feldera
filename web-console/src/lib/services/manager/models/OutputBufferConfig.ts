5af34fec4deab90ce12fb6183abfe32d02f5aeec
Ben Pfaff <blp@feldera.com>
2024-04-08 14:51:06 -0700

    web-console: Update OpenAPI and explain how to fix it repeatably.
    `yarn generate-openapi` generates some code with bugs in it.  This commit
    regenerates the OpenAPI with those bugs fixes and adds instructions on how
    to do that again.
    
    Thanks to @Karakatiza666 for explaining the fix.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- web-console/src/lib/services/manager/models/OutputBufferConfig.ts
+++ web-console/src/lib/services/manager/models/OutputBufferConfig.ts
@@ -16,19 +16,19 @@ export type OutputBufferConfig = {
    * small files every few milliseconds.
    *
    * To achieve such input/output decoupling, users can enable output buffering by
-   * setting the `enable_buffer` flag to `true`.  When buffering is enabled, output
+   * setting the `enable_output_buffer` flag to `true`.  When buffering is enabled, output
    * updates produced by the pipeline are consolidated in an internal buffer and are
    * pushed to the output transport when one of several conditions is satisfied:
    *
-   * * data has been accumulated in the buffer for more than `max_buffer_time_millis`
+   * * data has been accumulated in the buffer for more than `max_output_buffer_time_millis`
    * milliseconds.
-   * * buffer size exceeds `max_buffered_records` records.
+   * * buffer size exceeds `max_output_buffer_size_records` records.
    *
    * This flag is `false` by default.
    */
-  enable_buffer?: boolean
+  enable_output_buffer?: boolean
   /**
-   * Maximum number of output updates to be kept in the buffer.
+   * Maximum number of updates to be kept in the output buffer.
    *
    * This parameter bounds the maximal size of the buffer.
    * Note that the size of the buffer is not always equal to the
@@ -38,20 +38,20 @@ export type OutputBufferConfig = {
    * By default, the buffer can grow indefinitely until one of
    * the other output conditions is satisfied.
    *
-   * NOTE: this configuration option requires the `enable_buffer` flag
+   * NOTE: this configuration option requires the `enable_output_buffer` flag
    * to be set.
    */
-  max_buffer_size_records?: number
+  max_output_buffer_size_records?: number
   /**
-   * Maximum time in milliseconds data is kept in the buffer.
+   * Maximum time in milliseconds data is kept in the output buffer.
    *
    * By default, data is kept in the buffer indefinitely until one of
    * the other output conditions is satisfied.  When this option is
    * set the buffer will be flushed at most every
-   * `max_buffer_time_millis` milliseconds.
+   * `max_output_buffer_time_millis` milliseconds.
    *
-   * NOTE: this configuration option requires the `enable_buffer` flag
+   * NOTE: this configuration option requires the `enable_output_buffer` flag
    * to be set.
    */
-  max_buffer_time_millis?: number
+  max_output_buffer_time_millis?: number
 }
