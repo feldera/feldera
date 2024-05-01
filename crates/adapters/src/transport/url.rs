feldera/feldera

commit 269682d7537bc724331f88deb2828a80316fa69f

dbsp_adapters: Check that waits don't time out in tests.
Generally, waits should not time out; that's a bug.  This changes `wait`
from returning an `Option` to returning a `Result`, so that callers have
to choose what to do with the result, and then changes most of the callers
to `unwrap()` the result.

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/adapters/src/transport/url.rs
@@ -365,7 +365,7 @@ bar,false,-10
 
         // Unpause the endpoint, wait for the data to appear at the output.
         endpoint.start(0).unwrap();
-        wait(|| n_recs(&zset) == test_data.len(), DEFAULT_TIMEOUT_MS);
+        wait(|| n_recs(&zset) == test_data.len(), DEFAULT_TIMEOUT_MS).unwrap();
         for (i, upd) in zset.state().flushed.iter().enumerate() {
             assert_eq!(upd.unwrap_insert(), &test_data[i]);
         }
@@ -438,7 +438,7 @@ bar,false,-10
         // on busy CI systems it often seems to take longer, so be generous.
         let timeout_ms = 2000;
         let n1_time = wait(|| n_recs(&zset) >= 10, timeout_ms)
-            .unwrap_or_else(|| panic!("only {} records after {timeout_ms} ms", n_recs(&zset)));
+            .unwrap_or_else(|()| panic!("only {} records after {timeout_ms} ms", n_recs(&zset)));
         let n1 = n_recs(&zset);
         println!("{n1} records took {n1_time} ms to arrive");
 
@@ -462,7 +462,7 @@ bar,false,-10
 
         // Wait for the first 50 records to arrive.
         let n4_time = wait(|| n_recs(&zset) >= 50, 350)
-            .unwrap_or_else(|| panic!("only {} records after 350 ms", n_recs(&zset)));
+            .unwrap_or_else(|()| panic!("only {} records after 350 ms", n_recs(&zset)));
         let n4 = n_recs(&zset);
         println!("{} records took {n4_time} ms longer to arrive", n4 - n3);
 
@@ -500,7 +500,7 @@ bar,false,-10
         // Within 600 ms more, though, we should get all 100 records, but fudge
         // it to 1000 ms.
         let n6_time = wait(|| n_recs(&zset) >= 100, 1000)
-            .unwrap_or_else(|| panic!("only {} records after 1000 ms", n_recs(&zset)));
+            .unwrap_or_else(|()| panic!("only {} records after 1000 ms", n_recs(&zset)));
         let n6 = n_recs(&zset);
         println!("{} more records took {n6_time} ms to arrive", n6 - n5);
 

crates/adapters/src/format/parquet/test.rs
@@ -96,7 +96,8 @@ format:
     wait(
         || zset.state().flushed.len() == test_data.len(),
         DEFAULT_TIMEOUT_MS,
-    );
+    )
+    .unwrap();
 
     // Make sure the input data matches original test structs
     for (i, upd) in zset.state().flushed.iter().enumerate() {

crates/adapters/src/integrated/delta_table/output.rs
@@ -586,7 +586,7 @@ outputs:
 
         controller.start();
 
-        wait(|| controller.status().pipeline_complete(), 20_000);
+        wait(|| controller.status().pipeline_complete(), 20_000).unwrap();
 
         if verify {
             let parquet_files =

crates/adapters/src/test/kafka.rs
@@ -333,7 +333,8 @@ impl BufferConsumer {
                 n == num_records
             },
             DEFAULT_TIMEOUT_MS,
-        );
+        )
+        .unwrap();
         //println!("{num_records} records received: {:?}",
         // received_data.lock().unwrap().iter().map(|r| r.id).collect::<Vec<_>>());
 

crates/adapters/src/test/mod.rs
@@ -61,8 +61,8 @@ impl Log for TestLogger {
 
 /// Wait for `predicate` to become `true`.
 ///
-/// Returns the number of milliseconds elapsed or `None` on timeout.
-pub fn wait<P>(mut predicate: P, timeout_ms: u128) -> Option<u128>
+/// Returns the number of milliseconds elapsed or `Err(())` on timeout.
+pub fn wait<P>(mut predicate: P, timeout_ms: u128) -> Result<u128, ()>
 where
     P: FnMut() -> bool,
 {
@@ -70,12 +70,12 @@ where
 
     while !predicate() {
         if start.elapsed().as_millis() >= timeout_ms {
-            return None;
+            return Err(());
         }
         sleep(Duration::from_millis(10));
     }
 
-    Some(start.elapsed().as_millis())
+    Ok(start.elapsed().as_millis())
 }
 
 /// Build an input pipeline that allows testing a parser

crates/adapters/src/transport/file.rs
@@ -270,7 +270,8 @@ format:
         wait(
             || zset.state().flushed.len() == test_data.len(),
             DEFAULT_TIMEOUT_MS,
-        );
+        )
+        .unwrap();
         for (i, upd) in zset.state().flushed.iter().enumerate() {
             assert_eq!(upd.unwrap_insert(), &test_data[i]);
         }
@@ -329,7 +330,8 @@ format:
             wait(
                 || zset.state().flushed.len() == test_data.len(),
                 DEFAULT_TIMEOUT_MS,
-            );
+            )
+            .unwrap();
             for (i, upd) in zset.state().flushed.iter().enumerate() {
                 assert_eq!(upd.unwrap_insert(), &test_data[i]);
             }

crates/adapters/src/transport/kafka/ft/test.rs
@@ -32,7 +32,8 @@ fn wait_for_output_ordered(zset: &MockDeZSet<TestStruct, TestStruct>, data: &[Ve
     wait(
         || zset.state().flushed.len() == num_records,
         DEFAULT_TIMEOUT_MS,
-    );
+    )
+    .unwrap();
 
     for (i, val) in data.iter().flat_map(|data| data.iter()).enumerate() {
         assert_eq!(zset.state().flushed[i].unwrap_insert(), val);
@@ -46,7 +47,8 @@ fn wait_for_output_unordered(zset: &MockDeZSet<TestStruct, TestStruct>, data: &[
     wait(
         || zset.state().flushed.len() == num_records,
         DEFAULT_TIMEOUT_MS,
-    );
+    )
+    .unwrap();
 
     let mut data_sorted = data
         .iter()

crates/adapters/src/transport/kafka/nonft/test.rs
@@ -26,7 +26,8 @@ fn wait_for_output_ordered(zset: &MockDeZSet<TestStruct, TestStruct>, data: &[Ve
     wait(
         || zset.state().flushed.len() == num_records,
         DEFAULT_TIMEOUT_MS,
-    );
+    )
+    .unwrap();
 
     for (i, val) in data.iter().flat_map(|data| data.iter()).enumerate() {
         assert_eq!(zset.state().flushed[i].unwrap_insert(), val);
@@ -40,7 +41,8 @@ fn wait_for_output_unordered(zset: &MockDeZSet<TestStruct, TestStruct>, data: &[
     wait(
         || zset.state().flushed.len() == num_records,
         DEFAULT_TIMEOUT_MS,
-    );
+    )
+    .unwrap();
 
     let mut data_sorted = data
         .iter()

commit 7126196bd94ba73323b6dbf8563244a0e3cd04fb

dbsp_adapters: Add another `unwrap()` to wait calls in tests.
I missed this and it wasn't caught by CI at the time.

Fixes: 269682d7537b ("dbsp_adapters: Check that waits don't time out in tests.")
Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/adapters/src/transport/url.rs
@@ -392,7 +392,8 @@ bar,false,-10
         wait(
             || consumer.state().endpoint_error.is_some(),
             DEFAULT_TIMEOUT_MS,
-        );
+        )
+        .unwrap();
         Ok(())
     }
 