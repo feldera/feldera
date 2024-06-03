cc270ddec25083e1f5b9a8794c4a5ea7d0d5aee4
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2024-06-03 19:06:56 +0545

    py: raise error if dataframe has no column names
    Fixes: #1782
    
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- python/feldera/_helpers.py
+++ python/feldera/_helpers.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
 
+def ensure_dataframe_has_columns(df: pd.DataFrame):
+    """
+    Ensures that the DataFrame has column names set.
+    """
+
+    if [v for v in range(df.shape[1])] == list(df.columns):
+        raise ValueError(
+            """
+            DataFrame has no column names set.
+            Input DataFrame must have column names set and they must be consistent with the columns in the input table.
+            """
+        )
+
+
 def dataframe_from_response(buffer: list[list[dict]]):
     """
     Converts the response from Feldera to a pandas DataFrame.
