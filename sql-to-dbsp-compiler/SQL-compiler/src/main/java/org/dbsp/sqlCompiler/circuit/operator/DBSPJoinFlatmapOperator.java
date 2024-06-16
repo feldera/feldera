e5c18e52b33914a20c37f463780bd760acf2fe4a
Mihai Budiu <mbudiu@feldera.com>
2024-06-15 17:36:28 -0700

    [SQL] Use join_flatmap when possible
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

---
+++ sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/circuit/operator/DBSPJoinFlatmapOperator.java
@@ -0,0 +1,52 @@
+package org.dbsp.sqlCompiler.circuit.operator;
+
+import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
+import org.dbsp.sqlCompiler.compiler.visitors.VisitDecision;
+import org.dbsp.sqlCompiler.compiler.visitors.outer.CircuitVisitor;
+import org.dbsp.sqlCompiler.ir.expression.DBSPExpression;
+import org.dbsp.sqlCompiler.ir.type.DBSPType;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeZSet;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+
+/** We use join_flatmap to implement a join followed by a filter.
+ * The function returns None for dropping a value, and Some(result)
+ * for keeping a result. */
+public final class DBSPJoinFlatmapOperator extends DBSPOperator {
+    public DBSPJoinFlatmapOperator(
+            CalciteObject node, DBSPTypeZSet outputType,
+            DBSPExpression function, boolean isMultiset,
+            DBSPOperator left, DBSPOperator right) {
+        super(node, "join_flatmap", function, outputType, isMultiset);
+        this.addInput(left);
+        this.addInput(right);
+    }
+
+    @Override
+    public DBSPOperator withFunction(@Nullable DBSPExpression expression, DBSPType outputType) {
+        return new DBSPJoinFlatmapOperator(
+                this.getNode(), outputType.to(DBSPTypeZSet.class),
+                Objects.requireNonNull(expression),
+                this.isMultiset, this.inputs.get(0), this.inputs.get(1));
+    }
+
+    @Override
+    public DBSPOperator withInputs(List<DBSPOperator> newInputs, boolean force) {
+        if (force || this.inputsDiffer(newInputs))
+            return new DBSPJoinFlatmapOperator(
+                    this.getNode(), this.getOutputZSetType(),
+                    this.getFunction(), this.isMultiset, newInputs.get(0), newInputs.get(1));
+        return this;
+    }
+
+    @Override
+    public void accept(CircuitVisitor visitor) {
+        visitor.push(this);
+        VisitDecision decision = visitor.preorder(this);
+        if (!decision.stop())
+            visitor.postorder(this);
+        visitor.pop(this);
+    }
+}
