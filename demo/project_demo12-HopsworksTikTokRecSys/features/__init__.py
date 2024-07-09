e9966ebd64a54d4b7cf404425aea724881740a36
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2024-07-09 19:17:04 +0545

    demo: SQL: int id & pivot on aggregated data only
    - The simulator now generates integer ids for the user, video and
      interaction.
    - The SQL query has been updated to only perform pivot on the aggregated
      data. This should be a lot more efficient than the previous query.
        - The spikey nature of the pipeline throughput has gone away.
          Locally now I get ~300k/s.
        - The memory usage also seems to be reduced.
    - The aggregates are now rolling aggregates of 1 HOUR instead of 7 DAYs.
    
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

---
+++ demo/project_demo12-HopsworksTikTokRecSys/features/__init__.py
@@ -0,0 +1,7 @@
+# This module is borrowed from the Hopsworks TikTok RecSys Demo
+
+# Original Repository: https://github.com/davitbzh/tiktok-recsys
+# Original Source: https://github.com/davitbzh/tiktok-recsys/blob/main/python/Jupyter/features
+
+# Modifications made:
+# - The fields video_id, user_id, interactions_id now generate integers instead of strings
--- demo/project_demo12-HopsworksTikTokRecSys/features/users.py
+++ demo/project_demo12-HopsworksTikTokRecSys/features/users.py
@@ -1,3 +1,7 @@
+# This code is borrowed from the Hopsworks TikTok RecSys Demo
+
+# Original Source: https://github.com/davitbzh/tiktok-recsys/blob/main/python/Jupyter/features/users.py
+
 from mimesis import Generic
 from mimesis.locales import Locale
 import random
@@ -30,7 +34,7 @@ def generate_users(num_users: int, historical=False) -> List[Dict[str, str]]:
 
             # Generate each user's details
         user = {
-            'user_id': generic.person.identifier(mask='@@###@'),  # Unique user identifier
+            'user_id': int(generic.person.identifier(mask='#####')),  # Unique user identifier
             'gender': generic.person.gender(),                    # Randomly generated gender
             'age': random.randint(12, 90),                        # Randomly generated age between 12 and 90
             'country': generic.address.country(),                 # Randomly generated country name
