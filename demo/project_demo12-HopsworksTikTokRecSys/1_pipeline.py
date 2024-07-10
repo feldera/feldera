3cfa736fdeb32dcae1877a6a323136a6a62e360a
Leonid Ryzhyk <leonid@feldera.com>
2024-07-10 12:09:35 -0700

    demo12: switch to rolling aggregates only.
    * Use rolling aggregates with different time windows insteaf of hopping
      windows
    
    * Increase the number of DBSP workers and Kafka polling threads
    
    * Increase dataset size to 50M records (takes a while to generate)
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- demo/project_demo12-HopsworksTikTokRecSys/1_pipeline.py
+++ demo/project_demo12-HopsworksTikTokRecSys/1_pipeline.py
@@ -1,9 +1,10 @@
 from feldera import FelderaClient, SQLContext, SQLSchema
 from feldera.formats import JSONFormat, JSONUpdateFormat
 import config
+import time
 
 client = FelderaClient("http://localhost:8080")
-sql = SQLContext("mil", client)
+sql = SQLContext("mil", client, workers=16)
 
 sql.register_table("interactions",
     SQLSchema({
@@ -19,70 +20,62 @@ sql.register_table("interactions",
     })
 )
 
-sql.register_local_view("video_agg", """
+sql.register_view("video_agg", """
     SELECT
         video_id,
         interaction_type,
-        count(*) OVER week as interaction_len,
-        avg(watch_time) OVER week as average_watch_time,
+        count(*) OVER hour as interaction_len_h,
+        count(*) OVER day as interaction_len_d,
+        count(*) OVER week as interaction_len_w,
+        avg(watch_time) OVER hour as average_watch_time_h,
+        avg(watch_time) OVER day as average_watch_time_d,
+        avg(watch_time) OVER week as average_watch_time_w,
         interaction_date as hour_start
     FROM interactions
     WINDOW
-        week AS (PARTITION BY video_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW)
+        hour AS (PARTITION BY video_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW),
+        day AS (PARTITION BY video_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' DAY PRECEDING AND CURRENT ROW),
+        week AS (PARTITION BY video_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '7' DAY PRECEDING AND CURRENT ROW)
 """)
 
-sql.register_local_view("user_agg", """
+sql.register_view("user_agg", """
     SELECT
         user_id,
         interaction_type,
-        count(*) OVER week as interaction_len,
-        avg(watch_time) OVER week as average_watch_time,
+        count(*) OVER hour as interaction_len_h,
+        count(*) OVER day as interaction_len_d,
+        count(*) OVER week as interaction_len_w,
+        avg(watch_time) OVER hour as average_watch_time_h,
+        avg(watch_time) OVER day as average_watch_time_d,
+        avg(watch_time) OVER week as average_watch_time_w,
         interaction_date as hour_start
     FROM interactions
     WINDOW
-        week AS (PARTITION BY user_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW)
+        hour AS (PARTITION BY user_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' HOUR PRECEDING AND CURRENT ROW),
+        day AS (PARTITION BY user_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '1' DAY PRECEDING AND CURRENT ROW),
+        week AS (PARTITION BY user_id ORDER BY interaction_date RANGE BETWEEN INTERVAL '7' DAY PRECEDING AND CURRENT ROW)
 """)
 
-sql.register_materialized_view("video_agg_1h", """
-    SELECT * FROM video_agg
-    PIVOT (
-        SUM(interaction_len) FOR interaction_type IN (
-            'like' as "like",
-            'dislike' as "dislike",
-            'view' as "view",
-            'comment' as "comment",
-            'share' as "share",
-            'skip' as "skip"
-        )
-    )
-""")
-
-sql.register_materialized_view("user_agg_1h", """
-    SELECT * FROM user_agg
-    PIVOT (
-        SUM(interaction_len) FOR interaction_type IN (
-            'like' as "like",
-            'dislike' as "dislike",
-            'view' as "view",
-            'comment' as "comment",
-            'share' as "share",
-            'skip' as "skip"
-        )
-    )
-""")
 
 in_fmt = JSONFormat().with_array(False).with_update_format(JSONUpdateFormat.Raw)
 sql.connect_source_kafka("interactions", "kafka_conn_in_interactions", {
    "topics": [config.KAFKA_TOPIC_NAME],
     "bootstrap.servers": config.KAFKA_SERVER_FROM_PIPELINE,
     "auto.offset.reset": "earliest",
+    "poller_threads": 8,
 }, in_fmt)
 
 print("Starting Feldera Pipeline")
 sql.start()
 print("Pipeline started")
 
-sql.wait_for_idle()
-print("Found pipeline to be idle, shutting down...")
+start_time = time.time()
+
+sql.wait_for_idle(idle_interval_s = 1)
+
+end_time = time.time()
+elapsed = end_time - start_time
+
+print(f"Pipeline finished in {elapsed}, shutting down...")
 
 sql.shutdown()
