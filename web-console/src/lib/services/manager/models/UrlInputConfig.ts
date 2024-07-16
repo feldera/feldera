f5da0319627c872c51792c4fc9e027f3a229efe8
Ben Pfaff <blp@feldera.com>
2024-07-16 15:55:17 -0700

    dbsp_adapters: Wait a bit before disconnecting on pause in `url` adapter.
    Until now, the `url` adapter has disconnected from the server immediately
    if the input adapter was paused, and then reconnected later if it was
    restarted.  If the pause is due to a backlog filling up, this is likely
    only a short pause, and incurs the cost of churning the connection to the
    server, for no gain given that the connection will be resumed later.
    
    This commit adds a delay before disconnecting, solving the problem.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- web-console/src/lib/services/manager/models/UrlInputConfig.ts
+++ web-console/src/lib/services/manager/models/UrlInputConfig.ts
@@ -11,4 +11,14 @@ export type UrlInputConfig = {
    * URL.
    */
   path: string
+  /**
+   * Timeout before disconnection when paused.
+   *
+   * If the pipeline is paused, or if the input adapter reads data faster
+   * than the pipeline can process it, then the controller will pause the
+   * input adapter. If the input adapter stays paused longer than this
+   * timeout, it will drop the network connection to the server. It will
+   * automatically reconnect when the input adapter starts running again.
+   */
+  pause_timeout?: number
 }
