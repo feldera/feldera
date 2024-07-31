09737e655eec9343b56008fc453fb581546005f0
Simon Kassing <simon.kassing@feldera.com>
2024-07-31 15:57:21 +0200

    Error module public
    Signed-off-by: Simon Kassing <simon.kassing@feldera.com>

--- crates/pipeline_manager/src/lib.rs
+++ crates/pipeline_manager/src/lib.rs
@@ -1,5 +1,4 @@
 mod auth;
-mod error;
 #[cfg(test)]
 #[cfg(feature = "integration-test")]
 mod integration_test;
@@ -10,6 +9,7 @@ pub mod config;
 pub mod db;
 pub mod db_notifier;
 pub mod demo;
+pub mod error;
 pub mod local_runner;
 pub mod logging;
 pub mod metrics;
