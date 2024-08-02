ad1f8dae1099e8dde3c888176578d4d0bc2ceb68
Ben Pfaff <blp@feldera.com>
2024-08-02 16:44:15 -0700

    demo: Convert project_demo00-SecOps to new API.
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- demo/project_demo00-SecOps/run.py
+++ demo/project_demo00-SecOps/run.py
@@ -12,6 +12,7 @@ import time
 import requests
 import argparse
 import subprocess
+import json
 from shutil import which
 from plumbum.cmd import rpk
 
@@ -30,156 +31,66 @@ def main():
                         help="Kafka URL from pipeline")
     parser.add_argument("--registry-url-for-connector", required=False, default="http://redpanda:8081",
                         help="Schema registry URL from pipeline")
-    parser.add_argument('--delete-extra', default=False, action=argparse.BooleanOptionalAction, help='delete other programs, pipelines, and connectors (default: --no-delete-extra)')
 
     args = parser.parse_args()
-    prepare_feldera(args.api_url, args.kafka_url_for_connector, args.registry_url_for_connector, args.delete_extra)
+    prepare_feldera(args.api_url, args.kafka_url_for_connector, args.registry_url_for_connector)
     prepare_redpanda_start_simulator("0" if args.prepare_args is None else args.prepare_args).wait()
 
 
-PROGRAM_NAME = "sec-ops-program"
 PIPELINE_NAME = "sec-ops-pipeline"
-CONNECTORS = [
-    ("secops_pipeline", 'PIPELINE', ["secops_pipeline"], True),
-    ("secops_pipeline_sources", 'PIPELINE_SOURCES', ["secops_pipeline_sources"], True),
-    ("secops_artifact", 'ARTIFACT', ["secops_artifact"], True),
-    ("secops_vulnerability", 'VULNERABILITY', ["secops_vulnerability"], True),
-    ("secops_cluster", 'K8SCLUSTER', ["secops_cluster"], True),
-    ("secops_k8sobject", 'K8SOBJECT', ["secops_k8sobject"], True),
-    ("secops_vulnerability_stats", 'K8SCLUSTER_VULNERABILITY_STATS', "secops_vulnerability_stats", False),
-]
 
+def make_connector(topic, pipeline_to_redpanda_server, input=True, group_id=None):
+    if input:
+        name = "kafka_input"
+        config = {"topics": [topic], "auto.offset.reset": "earliest"}
+        if group_id is not None:
+            config["group.id"] = "secops_pipeline_sources"
+            config["enable.auto.commit"] = "true"
+            config["enable.auto.offset.store"] = "true"
+    else:
+        name = "kafka_output"
+        config = {"topic": topic}
+    config["bootstrap.servers"] = pipeline_to_redpanda_server
 
-def wait_for_status(api_url, pipeline_name, status):
-    start = time.time()
-    while requests.get(f"{api_url}/v0/pipelines/{pipeline_name}").json()["state"]["current_status"] != status:
-        time.sleep(.1)
-    return time.time() - start
-
-def list_names(api_url, entity):
-    return set([entity["name"] for entity in requests.get(f"{api_url}/v0/{entity}").json()])
-
-def list_programs(api_url):
-    return list_names(api_url, "programs")
-
-def list_pipelines(api_url):
-    return set([pipeline["descriptor"]["name"] for pipeline in requests.get(f"{api_url}/v0/pipelines").json()])
-
-def list_connectors(api_url):
-    return list_names(api_url, "connectors")
-
-def stop_pipeline(api_url, pipeline_name, wait):
-    requests.post(f"{api_url}/v0/pipelines/{pipeline_name}/shutdown").raise_for_status()
-    if wait:
-        return wait_for_status(api_url, pipeline_name, "Shutdown")
-
-def start_pipeline(api_url, pipeline_name, wait):
-    requests.post(f"{api_url}/v0/pipelines/{pipeline_name}/start").raise_for_status()
-    if wait:
-        return wait_for_status(api_url, pipeline_name, "Running")
-
-def delete_pipeline(api_url, pipeline_name):
-    requests.delete(f"{api_url}/v0/pipelines/{pipeline_name}").raise_for_status()
-
-def delete_connector(api_url, connector_name):
-    requests.delete(f"{api_url}/v0/connectors/{connector_name}").raise_for_status()
-
-def delete_program(api_url, program_name):
-    requests.delete(f"{api_url}/v0/programs/{program_name}").raise_for_status()
-
-
-def prepare_feldera(api_url, pipeline_to_redpanda_server, pipeline_to_schema_registry, delete_extra):
-    if delete_extra:
-        for pipeline in list_pipelines(api_url):
-            stop_pipeline(api_url, pipeline, False)
-        for pipeline in list_pipelines(api_url):
-            stop_pipeline(api_url, pipeline, True)
-        for pipeline in list_pipelines(api_url) - set([PIPELINE_NAME]):
-            delete_pipeline(api_url, pipeline)
-        for program in list_programs(api_url) - set([PROGRAM_NAME]):
-            delete_program(api_url, program)
-        for connector in list_connectors(api_url) - set([c[0] for c in CONNECTORS]):
-            delete_connector(api_url, connector)
-
-    # Create program
-    program_sql = open(PROJECT_SQL).read()
-    response = requests.put(f"{api_url}/v0/programs/{PROGRAM_NAME}", json={
-        "description": "",
-        "code": program_sql
-    })
-    response.raise_for_status()
-    program_version = response.json()["version"]
-
-    # Compile program
-    print(f"Compiling program {PROGRAM_NAME} (version: {program_version})...")
-    requests.post(f"{api_url}/v0/programs/{PROGRAM_NAME}/compile", json={"version": program_version}).raise_for_status()
-    while True:
-        status = requests.get(f"{api_url}/v0/programs/{PROGRAM_NAME}").json()["status"]
-        print(f"Program status: {status}")
-        if status == "Success":
-            break
-        elif status != "Pending" and status != "CompilingRust" and status != "CompilingSql":
-            raise RuntimeError(f"Failed program compilation with status {status}")
-        time.sleep(5)
-
-    # Connectors
-    connectors = []
-    for (connector_name, stream, topic_topics, is_input) in CONNECTORS:
-        requests.put(f"{api_url}/v0/connectors/{connector_name}", json={
-            "description": "",
+    return [{
+        "format": {
+            "name": "json",
             "config": {
-                "format": {
-                    "name": "json",
-                    "config": {
-                        "update_format": "insert_delete"
-                    }
-                },
-                "transport": {
-                    "name": "kafka_" + ("input" if is_input else "output"),
-                    "config": {
-                        "bootstrap.servers": pipeline_to_redpanda_server,
-                        "topic": topic_topics
-                    } if not is_input else (
-                        {
-                            "bootstrap.servers": pipeline_to_redpanda_server,
-                            "topics": topic_topics,
-                            "auto.offset.reset": "earliest",
-                            "group.id": "secops_pipeline_sources",
-                            "enable.auto.commit": "true",
-                            "enable.auto.offset.store": "true",
-                        }
-                        if stream == "PIPELINE_SOURCES" else
-                        {
-                            "bootstrap.servers": pipeline_to_redpanda_server,
-                            "topics": topic_topics,
-                            "auto.offset.reset": "earliest"
-                        }
-                    )
-                }
+                "update_format": "insert_delete"
             }
-        })
-        connectors.append({
-            "connector_name": connector_name,
-            "is_input": is_input,
-            "name": connector_name,
-            "relation_name": stream
-        })
+        },
+        "transport": {
+            "name": name,
+            "config": config
+        }
+    }]
 
-    schema = """{
-            "type": "record",
-            "name": "k8scluster_vulnerability_stats",
-            "fields": [
-                { "name": "k8scluster_id", "type": "long" },
-                { "name": "k8scluster_name", "type": "string" },
-                { "name": "total_vulnerabilities", "type": "long" },
-                { "name": "most_severe_vulnerability", "type": ["null","int"] }
-            ]
-        }"""
+def build_sql(pipeline_to_redpanda_server, pipeline_to_schema_registry):
+    subst = {}
+    for topic in ("secops_pipeline",
+                  "secops_artifact",
+                  "secops_vulnerability",
+                  "secops_cluster",
+                  "secops_k8sobject"):
+        subst[topic] = make_connector(topic, pipeline_to_redpanda_server)
 
-    if pipeline_to_schema_registry:
-        requests.put(f"{api_url}/v0/connectors/secops_vulnerability_stats_avro", json={
-            "description": "",
-            "config": {
+    for topic in ('secops_pipeline_sources',):
+        subst[topic] = make_connector(topic, pipeline_to_redpanda_server, group_id=topic)
+
+    for topic in ("secops_vulnerability_stats",):
+        connector = make_connector(topic, pipeline_to_redpanda_server, input=False)
+        if pipeline_to_schema_registry:
+            schema = """{
+                    "type": "record",
+                    "name": "k8scluster_vulnerability_stats",
+                    "fields": [
+                        { "name": "k8scluster_id", "type": "long" },
+                        { "name": "k8scluster_name", "type": "string" },
+                        { "name": "total_vulnerabilities", "type": "long" },
+                        { "name": "most_severe_vulnerability", "type": ["null","int"] }
+                    ]
+                }"""
+            connector += [{
                 "format": {
                     "name": "avro",
                     "config": {
@@ -196,22 +107,206 @@ def prepare_feldera(api_url, pipeline_to_redpanda_server, pipeline_to_schema_reg
                                     {"key": "header2", "value": list(b'byte array')}]
                     }
                 }
-            }
-        })
-        connectors.append({
-            "connector_name": "secops_vulnerability_stats_avro",
-            "is_input": False,
-            "name": "secops_vulnerability_stats_avro",
-            "relation_name": "k8scluster_vulnerability_stats"
-        })
+            }]
+        subst[topic] = connector
 
+    for key in subst.keys():
+        subst[key] = json.dumps(subst[key], indent=4)
+
+
+    return """-- CI/CD pipeline.
+create table pipeline (
+    pipeline_id bigint not null primary key,
+    create_date timestamp not null,
+    createdby_user_id bigint not null,
+    update_date timestamp,
+    updatedby_user_id bigint
+) WITH ('connectors' = '{secops_pipeline}');
+
+-- Git commits used by each pipeline.
+create table pipeline_sources (
+    git_commit_id bigint not null,
+    pipeline_id bigint not null foreign key references pipeline(pipeline_id)
+) WITH ('connectors' = '{secops_pipeline_sources}');
+
+-- Binary artifact created by a CI pipeline.
+create table artifact (
+    artifact_id bigint not null primary key,
+    artifact_uri varchar not null,
+    create_date timestamp not null,
+    createdby_user_id bigint not null,
+    checksum varchar not null,
+    checksum_type varchar not null,
+    artifact_size_in_bytes bigint not null,
+    artifact_type varchar not null,
+    builtby_pipeline_id bigint not null foreign key references pipeline(pipeline_id),
+    parent_artifact_id bigint foreign key references artifact(artifact_id)
+) WITH ('connectors' = '{secops_artifact}');
+
+-- Vulnerabilities discovered in source code.
+create table vulnerability (
+    vulnerability_id bigint not null,
+    discovery_date timestamp not null,
+    discovered_by_user_id bigint not null,
+    discovered_in bigint not null,
+    update_date timestamp,
+    updated_by_user_id bigint,
+    checksum varchar not null,
+    checksum_type varchar not null,
+    vulnerability_reference_id varchar not null,
+    severity int,
+    priority varchar
+)
+  -- Instruct Feldera to store the snapshot of the table, allowing the
+  -- user to browse it via the UI or API.
+  with ('materialized' = 'true', 'connectors' = '{secops_vulnerability}');
+
+-- K8s clusters.
+create table k8scluster (
+    k8scluster_id bigint not null primary key,
+    k8s_uri varchar not null,
+    name varchar not null,
+    k8s_service_provider varchar not null
+) WITH ('connectors' = '{secops_cluster}');
+
+
+-- Deployed k8s objects.
+create table k8sobject (
+    k8sobject_id bigint not null,
+    artifact_id bigint not null foreign key references artifact(artifact_id),
+    create_date timestamp not null,
+    createdby_user_id bigint not null,
+    update_date timestamp,
+    updatedby_user_id bigint,
+    checksum varchar not null,
+    checksum_type varchar not null,
+    deployed_id bigint not null foreign key references k8scluster(k8scluster_id),
+    deployment_type varchar not null,
+    k8snamespace varchar not null
+) WITH ('connectors' = '{secops_k8sobject}');
+
+-- Vulnerabilities that affect each pipeline.
+create view pipeline_vulnerability (
+    pipeline_id,
+    vulnerability_id
+) as
+    SELECT pipeline_sources.pipeline_id as pipeline_id, vulnerability.vulnerability_id as vulnerability_id FROM
+    pipeline_sources
+    INNER JOIN
+    vulnerability
+    ON pipeline_sources.git_commit_id = vulnerability.discovered_in;
+
+-- Vulnerabilities that affect each artifact.
+create view artifact_vulnerability (
+    artifact_id,
+    vulnerability_id
+) as
+    SELECT artifact.artifact_id as artifact_id, pipeline_vulnerability.vulnerability_id as vulnerability_id FROM
+    artifact
+    INNER JOIN
+    pipeline_vulnerability
+    ON artifact.builtby_pipeline_id = pipeline_vulnerability.pipeline_id;
+
+-- Vulnerabilities in the artifact or any of its children.
+create view transitive_artifact_vulnerability(
+    artifact_id,
+    via_artifact_id,
+    vulnerability_id
+) as
+    SELECT artifact_id, artifact_id as via_artifact_id, vulnerability_id from artifact_vulnerability
+    UNION
+    (
+        SELECT
+            artifact.parent_artifact_id as artifact_id,
+            artifact.artifact_id as via_artifact_id,
+            artifact_vulnerability.vulnerability_id as vulnerability_id FROM
+        artifact
+        INNER JOIN
+        artifact_vulnerability
+        ON artifact.artifact_id = artifact_vulnerability.artifact_id
+        WHERE artifact.parent_artifact_id IS NOT NULL
+    );
+
+-- Vulnerabilities that affect each k8s object.
+create view k8sobject_vulnerability (
+    k8sobject_id,
+    vulnerability_id
+) as
+    SELECT k8sobject.k8sobject_id, transitive_artifact_vulnerability.vulnerability_id FROM
+    k8sobject
+    INNER JOIN
+    transitive_artifact_vulnerability
+    ON k8sobject.artifact_id = transitive_artifact_vulnerability.artifact_id;
+
+-- Vulnerabilities that affect each k8s cluster.
+create view k8scluster_vulnerability (
+    k8scluster_id,
+    vulnerability_id
+) as
+    SELECT
+        k8sobject.deployed_id as k8scluster_id,
+        k8sobject_vulnerability.vulnerability_id FROM
+    k8sobject_vulnerability
+    INNER JOIN
+    k8sobject
+    ON k8sobject_vulnerability.k8sobject_id = k8sobject.k8sobject_id;
+
+-- Per-cluster statistics:
+-- * Number of vulnerabilities.
+-- * Most severe vulnerability.
+create materialized view k8scluster_vulnerability_stats (
+    k8scluster_id,
+    k8scluster_name,
+    total_vulnerabilities,
+    most_severe_vulnerability
+) WITH ('connectors' = '{secops_vulnerability_stats}') AS
+    SELECT
+        cluster_id,
+        k8scluster.name as k8scluster_name,
+        total_vulnerabilities,
+        most_severe_vulnerability
+    FROM
+    (
+        SELECT
+            cluster_id,
+            COUNT(*) as total_vulnerabilities,
+            MAX(severity) as most_severe_vulnerability
+        FROM
+        (
+            SELECT k8scluster_vulnerability.k8scluster_id as cluster_id, vulnerability.vulnerability_id, vulnerability.severity FROM
+            k8scluster_vulnerability
+            INNER JOIN
+            vulnerability
+            ON k8scluster_vulnerability.vulnerability_id = vulnerability.vulnerability_id
+        )
+        GROUP BY cluster_id
+    )
+    INNER JOIN
+    k8scluster
+    ON k8scluster.k8scluster_id = cluster_id;
+""".format(**subst)
+
+
+def prepare_feldera(api_url, pipeline_to_redpanda_server, pipeline_to_schema_registry):
     # Create pipeline
     requests.put(f"{api_url}/v0/pipelines/{PIPELINE_NAME}", json={
-        "description": "",
-        "config": {"workers": 8},
-        "program_name": PROGRAM_NAME,
-        "connectors": connectors,
+        "name": PIPELINE_NAME,
+        "description": "Developer security operations demo",
+        "runtime_config": {"workers": 8},
+        "program_config": {},
+        "program_code": build_sql(pipeline_to_redpanda_server, pipeline_to_schema_registry),
     }).raise_for_status()
+
+    # Compile program
+    print("Compiling program ...")
+    while True:
+        status = requests.get(f"{api_url}/v0/pipelines/{PIPELINE_NAME}").json()["program_status"]
+        print(f"Program status: {status}")
+        if status == "Success":
+            break
+        elif status != "Pending" and status != "CompilingRust" and status != "CompilingSql":
+            raise RuntimeError(f"Failed program compilation with status {status}")
+        time.sleep(2)
 
 
 def prepare_redpanda_start_simulator(num_pipelines, quiet=False):
