9dac16d6544f191ee595db319ae07af61a19f40c
Lalith Suresh <lalith@feldera.com>
2024-08-01 21:06:07 -0700

    examples: adapt S3 example to use new API and delete stale README
    Signed-off-by: Lalith Suresh <lalith@feldera.com>

--- demo/project_demo09-S3/program.sql
+++ demo/project_demo09-S3/program.sql
@@ -1,5 +1,24 @@
-create table test_table(
-    id integer not null primary key
+CREATE TABLE test_table(
+    id INTEGER NOT NULL PRIMARY KEY
+) WITH (
+    'connectors' = '[{
+        "format": { "name": "csv", },
+        "transport": {
+            "name": "s3_input",
+            "config": {
+                "credentials": {
+                    "type": "AccessKey",
+                    "aws_access_key_id": <S3_ACCESS_KEY>,
+                    "aws_secret_access_key": <S3_SECRET_KEY>,
+                },
+                "bucket_name": "feldera-connector-test-bucket",
+                "region": "us-west-1",
+                "read_strategy": {
+                    "type": "Prefix",
+                    "prefix": "foo",
+                }
+            }
+    }}]'
 );
 
-create view test_view as select * from test_table;
+CREATE VIEW test_view AS SELECT * FROM test_table;
