efdbae43c036dc7ce2314a14c2aefa119a39e054
Gerd Zellweger <mail@gerdzellweger.com>
2024-08-02 22:25:12 -0700

    Adjust HopsworkTiktok demo.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- demo/project_demo12-HopsworksTikTokRecSys/1_pipeline.py
+++ demo/project_demo12-HopsworksTikTokRecSys/1_pipeline.py
@@ -1,31 +1,36 @@
-from feldera import FelderaClient, SQLContext, SQLSchema
+from feldera import FelderaClient, SQLContext
 from feldera.formats import JSONFormat, JSONUpdateFormat
 import config
 from sql_program import generate_program
 import time
 
+
 client = FelderaClient("http://localhost:8080")
-sql = SQLContext("mil", client, workers=10, storage = False)
+sql = SQLContext("mil", client, workers=10, storage=False)
 
-generate_program(sql)
+code = generate_program(
+    {
+        "name": "kafka_input",
+        "config": {
+            "topics": [config.KAFKA_TOPIC_NAME],
+            "bootstrap.servers": config.KAFKA_SERVER_FROM_PIPELINE,
+            "auto.offset.reset": "earliest",
+            "poller_threads": 12,
+        },
+    },
+    JSONFormat().with_array(False).with_update_format(JSONUpdateFormat.Raw).to_dict(),
+)
 
-# sql.foreach_chunk("user_agg", lambda df, chunk : print(df))
-
-in_fmt = JSONFormat().with_array(False).with_update_format(JSONUpdateFormat.Raw)
-sql.connect_source_kafka("interactions", "kafka_conn_in_interactions", {
-   "topics": [config.KAFKA_TOPIC_NAME],
-    "bootstrap.servers": config.KAFKA_SERVER_FROM_PIPELINE,
-    "auto.offset.reset": "earliest",
-    "poller_threads": 12,
-}, in_fmt)
+sql.sql(code)
 
 print("Starting Feldera Pipeline")
 sql.start()
 print("Pipeline started")
+# sql.foreach_chunk("user_agg", lambda df, chunk: print(df))
 
 start_time = time.time()
 
-sql.wait_for_idle(idle_interval_s = 1)
+sql.wait_for_idle(idle_interval_s=1)
 
 end_time = time.time()
 elapsed = end_time - start_time
--- demo/project_demo12-HopsworksTikTokRecSys/test.py
+++ demo/project_demo12-HopsworksTikTokRecSys/test.py
@@ -1,18 +1,21 @@
 from datetime import datetime
-from feldera.formats import JSONFormat
+from feldera.formats import JSONFormat, JSONUpdateFormat
 from sql_program import generate_program
 from feldera import SQLContext, FelderaClient
 import pandas as pd
 from typing import Dict, Any, List
 
+
 def process_input(sql: SQLContext, data: List[Dict[str, Any]]):
     sql.input_pandas("interactions", pd.DataFrame(data))
     sql.wait_for_completion(shutdown=False)
 
-client = FelderaClient("http://localhost:8080")
-sql = SQLContext("tiktok_test", client, workers=10, storage = False)
 
-generate_program(sql)
+client = FelderaClient("http://localhost:8080")
+sql = SQLContext("tiktok_test", client, workers=10, storage=False)
+
+code = generate_program(None, None)
+sql.sql(code)
 
 # sql.foreach_chunk("user_agg", lambda df, chunk : print(df))
 
@@ -21,82 +24,115 @@ sql.start()
 print("Pipeline started")
 
 hvideo_agg = sql.listen("video_agg")
-huser_agg  = sql.listen("user_agg")
+huser_agg = sql.listen("user_agg")
 
-process_input(sql, [
-    {
-        "interaction_id": 1,
-        "user_id": 1,
-        "video_id": 1,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 10,
-        "interaction_date": datetime.strptime("2024-07-11 01:00:00", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    },
-    {
-        "interaction_id": 2,
-        "user_id": 1,
-        "video_id": 2,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 20,
-        "interaction_date": datetime.strptime("2024-07-11 01:00:00", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    }
-])
+process_input(
+    sql,
+    [
+        {
+            "interaction_id": 1,
+            "user_id": 1,
+            "video_id": 1,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 10,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 01:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+        {
+            "interaction_id": 2,
+            "user_id": 1,
+            "video_id": 2,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 20,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 01:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+    ],
+)
 
-process_input(sql, [
-    {
-        "interaction_id": 3,
-        "user_id": 1,
-        "video_id": 1,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 10,
-        "interaction_date": datetime.strptime("2024-07-11 01:00:10", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    },
-    {
-        "interaction_id": 4,
-        "user_id": 1,
-        "video_id": 2,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 20,
-        "interaction_date": datetime.strptime("2024-07-11 01:00:20", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    }
-])
+process_input(
+    sql,
+    [
+        {
+            "interaction_id": 3,
+            "user_id": 1,
+            "video_id": 1,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 10,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 01:00:10", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+        {
+            "interaction_id": 4,
+            "user_id": 1,
+            "video_id": 2,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 20,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 01:00:20", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+    ],
+)
 
-process_input(sql, [
-    {
-        "interaction_id": 5,
-        "user_id": 1,
-        "video_id": 1,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 70,
-        "interaction_date": datetime.strptime("2024-07-11 03:00:10", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    },
-    {
-        "interaction_id": 6,
-        "user_id": 1,
-        "video_id": 2,
-        "category_id": 100,
-        "interaction_type": "like",
-        "watch_time": 70,
-        "interaction_date": datetime.strptime("2024-07-11 03:00:20", "%Y-%m-%d %H:%M:%S"),
-        "previous_interaction_date": datetime.strptime("2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
-    }
-])
+process_input(
+    sql,
+    [
+        {
+            "interaction_id": 5,
+            "user_id": 1,
+            "video_id": 1,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 70,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 03:00:10", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+        {
+            "interaction_id": 6,
+            "user_id": 1,
+            "video_id": 2,
+            "category_id": 100,
+            "interaction_type": "like",
+            "watch_time": 70,
+            "interaction_date": datetime.strptime(
+                "2024-07-11 03:00:20", "%Y-%m-%d %H:%M:%S"
+            ),
+            "previous_interaction_date": datetime.strptime(
+                "2024-07-11 00:00:00", "%Y-%m-%d %H:%M:%S"
+            ),
+            "interaction_month": datetime.strptime("2024-07-01", "%Y-%m-%d"),
+        },
+    ],
+)
 
 print("video_agg")
 print(hvideo_agg.to_pandas())
