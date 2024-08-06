6343623a8de9474655ce355049ed579a22f0e408
Karakatiza666 <bulakh.96@gmail.com>
2024-08-06 13:44:25 +0000

    Fix login with Google after logout
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/components/pipelines/editor/ChangeStream.svelte
+++ web-console/src/lib/components/pipelines/editor/ChangeStream.svelte
@@ -76,14 +76,15 @@
 <div class="flex-1">
   <VList data={changes} let:item getKey={(d, i) => i} bind:this={ref}>
     <div
-      class={`even:bg-surface-100-900 whitespace-nowrap pl-2 before:inline-block before:w-2 even:!bg-opacity-30 ` +
+      class={`whitespace-nowrap pl-2 before:inline-block before:w-2 even:!bg-opacity-30 even:bg-surface-100-900 ` +
         ('insert' in item
           ? "shadow-[inset_26px_0px_0px_0px_rgba(0,255,0,0.3)] before:content-['+']"
           : 'delete' in item
             ? "shadow-[inset_26px_0px_0px_0px_rgba(255,0,0,0.3)] before:pl-[1px] before:content-['-']"
-            : '')}>
-      <span class="inline-block w-64 overflow-clip overflow-ellipsis pl-4"
-        >{item.relationName}</span>
+            : '')}
+    >
+      <span class="inline-block w-64 overflow-clip overflow-ellipsis pl-4">{item.relationName}</span
+      >
       <span class="">{JSONbig.stringify((item as any).insert ?? (item as any).delete)}</span>
     </div>
   </VList>
--- web-console/src/routes/(authenticated)/+layout.ts
+++ web-console/src/routes/(authenticated)/+layout.ts
@@ -4,8 +4,8 @@ import { getPipelines } from '$lib/services/pipelineManager'
 export const load = async ({ parent }) => {
   const data = await parent()
   if (typeof data.auth === 'object' && 'login' in data.auth) {
-    await data.auth.login()
-    error(401)
+    data.auth.login()
+    await new Promise(() => {}) // Await indefinitely to avoid loading the page - until redirected to auth page
   }
   const pipelines = await getPipelines()
   return { ...data, preloaded: { pipelines } }
