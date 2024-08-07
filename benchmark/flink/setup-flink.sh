cad6d7edf2655ff0f54343eb0e2a3eb251ecb3ca
Ben Pfaff <blp@feldera.com>
2024-08-07 09:15:19 -0700

    benchmark: Suppress transfer progress messages when building Flink.
    This should reduce the log size substantially.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- benchmark/flink/setup-flink.sh
+++ benchmark/flink/setup-flink.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 rm -rf nexmark nexmark-flink
 git clone https://github.com/blp/nexmark.git
-(cd nexmark/nexmark-flink && ./build.sh)
+(cd nexmark/nexmark-flink && MVN='mvn --no-transfer-progress' ./build.sh)
 tar xzf nexmark/nexmark-flink/nexmark-flink.tgz
