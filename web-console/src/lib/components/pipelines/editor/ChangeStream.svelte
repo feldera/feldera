6fc7ebeb2c5f0afa803bd3680cc7f08a5a2ef4de
Karakatiza666 <bulakh.96@gmail.com>
2024-08-13 11:28:42 +0000

    Fix default change stream behavior not following list end
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/components/pipelines/editor/ChangeStream.svelte
+++ web-console/src/lib/components/pipelines/editor/ChangeStream.svelte
@@ -2,11 +2,7 @@
 </script>
 
 <script lang="ts">
-  import { accumulateChanges } from '$lib/functions/pipelines/changeStream'
-
-  import { relationEggressStream } from '$lib/services/pipelineManager'
   import type { XgressRecord } from '$lib/types/pipelineManager'
-  import type BigNumber from 'bignumber.js'
   import JSONbig from 'true-json-bigint'
 
   import { VList } from 'virtua/svelte'
@@ -14,36 +10,12 @@
   let {
     changes
   }: {
-    // changes: { relationName: string; type: 'insert' | 'delete'; record: XgressRecord }[]
     changes: ({ relationName: string } & ({ insert: XgressRecord } | { delete: XgressRecord }))[]
   } = $props()
 
-  // $effect(() => {
-  //   // Initialize row array when pipelineName changes
-  //   rows[pipelineName] ??= []
-  // })
-
-  // $effect(() => {
-  //   const handle = relationEggressStream(pipelineName, relationName).then((stream) => {
-  //     if (!stream) {
-  //       return undefined
-  //     }
-  //     const reader = stream.getReader()
-  //     accumulateChanges(reader, pushChanges)
-  //     return () => reader.cancel('not_needed')
-  //   })
-  //   return () => {
-  //     handle.then((cancel) => cancel?.())
-  //   }
-  // })
-
-  // const bufferSize = 100
-  // const pushChanges = (changes: Record<'insert' | 'delete', XgressRecord>[]) => {
-  //   rows[pipelineName].splice(Math.max(bufferSize - changes.length, 0))
-  //   rows[pipelineName].unshift(...changes.slice(-bufferSize).reverse())
-  // }
   let ref: VList<any>
   let len = $derived(changes.length)
+  let lastScrollOffset = $state(0) // NOTE: Part of a workaround, see below
   {
     // Keep scroll position at the bottom of the list when its length increases if it's there already
     let lastScrollSize = $state(0)
@@ -58,6 +30,16 @@
       const curScroll = Math.round(ref.getScrollOffset() + ref.getViewportSize())
       if (curScroll === lastScrollSize) {
         ref.scrollTo(ref.getScrollSize())
+        lastScrollOffset = ref.getScrollOffset()
+      } else if (lastScrollSize > curScroll && lastScrollOffset === 0) {
+        // NOTE: There is a bug with incorrectly reporting scroll position in a virtual list.
+        // When the virtual list is scrolled all the way to the bottom,
+        // the first time list length is increased after the list is already longer than the visible height,
+        // the following does not hold (but it should):
+        // Math.round(ref.getScrollOffset()) + Math.round(ref.getViewportSize()) === lastScrollSize
+        // We need this special case and to update `lastScrollOffset` as a hack to fix this behavior
+        ref.scrollTo(ref.getScrollSize())
+        lastScrollOffset = ref.getScrollOffset()
       }
       lastScrollSize = Math.round(ref.getScrollSize())
     })
@@ -76,27 +58,15 @@
 <div class="flex-1">
   <VList data={changes} let:item getKey={(d, i) => i} bind:this={ref}>
     <div
-      class={`whitespace-nowrap pl-2 before:inline-block before:w-2 even:!bg-opacity-30 even:bg-surface-100-900 ` +
+      class={`even:bg-surface-100-900 whitespace-nowrap pl-2 before:inline-block before:w-2 even:!bg-opacity-30 ` +
         ('insert' in item
           ? "shadow-[inset_26px_0px_0px_0px_rgba(0,255,0,0.3)] before:content-['+']"
           : 'delete' in item
             ? "shadow-[inset_26px_0px_0px_0px_rgba(255,0,0,0.3)] before:pl-[1px] before:content-['-']"
-            : '')}
-    >
-      <span class="inline-block w-64 overflow-clip overflow-ellipsis pl-4">{item.relationName}</span
-      >
+            : '')}>
+      <span class="inline-block w-64 overflow-clip overflow-ellipsis pl-4"
+        >{item.relationName}</span>
       <span class="">{JSONbig.stringify((item as any).insert ?? (item as any).delete)}</span>
     </div>
   </VList>
-
-  <!-- <div class="h-full overflow-auto">
-    {#each changes as item}
-      <div
-        class={'even:bg-surface-100-900 border-l-4 pl-2 even:!bg-opacity-30 ' +
-          (item.type === 'insert' ? '  border-green-500 ' : 'border-red-500')}>
-        <span class="w-64 overflow-hidden overflow-ellipsis">{item.pipelineName}</span>
-        {JSONbig.stringify(item.record)}
-      </div>
-    {/each}
-  </div> -->
 </div>
