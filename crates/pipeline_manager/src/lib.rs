46f7f832e0204f539c0f57490608205f96f1ef9e
Leonid Ryzhyk <leonid@feldera.com>
2024-08-25 10:29:29 -0700

    Set default crypto provider.
    The rustls dependency, which pipeline manager now inherits from the
    `metrics-exporter-prometheus` crate, requires setting a default crypto
    provider for the process.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/pipeline_manager/src/lib.rs
+++ crates/pipeline_manager/src/lib.rs
@@ -1,3 +1,5 @@
+use rustls::crypto::CryptoProvider;
+
 mod auth;
 #[cfg(test)]
 #[cfg(feature = "integration-test")]
@@ -17,3 +19,15 @@ pub mod pipeline_automata;
 pub mod probe;
 pub mod retries;
 pub mod runner;
+
+/// Some dependencies of this crate use the `rustls` library. This library has two features
+/// `ring` and `aws-lc-rs`. When both are enabled, the library requires a process-wide default
+/// crypto provider to be configured. While no single dependency enables both these features,
+/// Rust's feature unification may end up enabling both of them, depending on the exact rustls
+/// dependency versions in use.
+///
+/// Bottom line: this function must be called in the `main` function in this crate, as well as
+/// in all tests that exercise libraries that use `rustls` internally.
+pub fn ensure_default_crypto_provider() {
+    let _ = CryptoProvider::install_default(rustls::crypto::aws_lc_rs::default_provider());
+}
