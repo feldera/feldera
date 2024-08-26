7fda39dfe11a98b7a8ac8474bfc32dd1814eefcd
rivudhk <rivudhkr@gmail.com>
2024-08-26 21:57:26 +0545

    Aggregate tests for integer types
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/sql_aggregates/test_average.py
+++ python/tests/sql_aggregates/test_average.py
@@ -11,17 +11,16 @@ class TestAggregatesBase(unittest.TestCase):
         sql = f'''CREATE TABLE {table_name}(
                     id INT, c1 TINYINT, c2 TINYINT NOT NULL, c3 INT2, c4 INT2 NOT NULL, c5 INT, c6 INT NOT NULL,c7 BIGINT,c8 BIGINT NOT NULL);''' + view_query
         pipeline = PipelineBuilder(TEST_CLIENT, f'{pipeline_name}', sql=sql).create_or_replace()
-        out = pipeline.listen('avg_vieqw')
-
+        out = pipeline.listen('avg_view')
         pipeline.start()
         pipeline.input_json(table_name, self.data, update_format="insert_delete")
         pipeline.wait_for_completion(True)
-
         out_data = out.to_dict()
         print(out_data)
         for datum in expected_data:
             datum.update({"insert_delete": 1})
         assert expected_data == out_data
+        pipeline.delete()
         
     def add_data(self, new_data, delete: bool = False):
         key = "delete" if delete else "insert"
@@ -42,7 +41,6 @@ class Avg(TestAggregatesBase):
                          FROM {table_name}'''
         self.execute_query(pipeline_name, expected_data, table_name, view_query)
 
-@unittest.skip("https://github.com/feldera/feldera/issues/2142")
 class Avg_Groupby(TestAggregatesBase):
     def test_avg_groupby(self):
         pipeline_name = "test_avg_groupby"
@@ -54,7 +52,6 @@ class Avg_Groupby(TestAggregatesBase):
         expected_data = [
             {"id": 0, "c1": 1, "c2": 11, "c3": 16, "c4": 22, "c5": 5, "c6": 33, "c7": 70, "c8": 44},
             {"id" : 1, "c1": 11, "c2": 22, "c3" : None, "c4" : 45, "c5" : 51, "c6" : 61, "c7":72, "c8": 88}]
-        # in id: 0, returns c3 : 16.0 && in id: 1, returns c3: nan
         table_name = "avg_groupby"
         view_query = f'''CREATE VIEW avg_view AS SELECT
                             id,AVG(c1) AS c1, AVG(c2) AS c2, AVG(c3) AS c3, AVG(c4) AS c4, AVG(c5) AS c5, AVG(c6) AS c6, AVG(c7) AS c7, AVG(c8) AS c8
@@ -99,7 +96,7 @@ class Avg_Distinct_Groupby(TestAggregatesBase):
 
 class Avg_Where(TestAggregatesBase):
     def test_avg_where0(self):
-        pipeline_name ="test_avg_where"
+        pipeline_name ="test_avg_where0"
         # validated using postgres
         new_data = [
             {"c1": None, "c2": 2, "c3": 30, "c4": 4, "c5": None, "c6": 60, "c7": 70, "c8": 8},
@@ -107,7 +104,7 @@ class Avg_Where(TestAggregatesBase):
             {"c1": 14, "c2": 21, "c3": 44, "c4": 32, "c5": 34, "c6": 90, "c7": 29, "c8": 67}]
         self.add_data(new_data)
         expected_data = [{"c1": 14, "c2": 21, "c3" : 44, "c4" : 32, "c5" : 34, "c6" : 90, "c7": 29, "c8": 67}]
-        table_name = "avg_where"
+        table_name = "avg_where0"
         view_query = f'''CREATE VIEW avg_view AS SELECT
                             AVG(c1) AS c1, AVG(c2) AS c2, AVG(c3) AS c3, AVG(c4) AS c4, AVG(c5) AS c5, AVG(c6) AS c6, AVG(c7) AS c7, AVG(c8) AS c8
                         FROM {table_name}
@@ -115,7 +112,6 @@ class Avg_Where(TestAggregatesBase):
                             c1 is NOT NULl AND c3 is NOT NULL AND c5 is NOT NULL AND c7 is NOT NULL;'''   
         self.execute_query(pipeline_name, expected_data, table_name, view_query)
     
-    @unittest.skip("https://github.com/feldera/feldera/issues/2142")
     def test_avg_where1(self):
         pipeline_name ="test_avg_where1"
         # checked manually
@@ -124,7 +120,6 @@ class Avg_Where(TestAggregatesBase):
             {"c1": 11, "c2": 22, "c3": None, "c4": 45, "c5": 51, "c6": 6, "c7": 72, "c8": 88},
             {"c1": 14, "c2": 21, "c3": 44, "c4": 32, "c5": 34, "c6": 90, "c7": 29, "c8": 67}]
         self.add_data(new_data)
-        # in row: 1, returns c3 : 1nan && in row : 2, returns c3: 44.0
         expected_data = [
             {"c1": 11, "c2": 22, "c3": None, "c4": 45, "c5": 51, "c6": 6, "c7": 72, "c8": 88},
             {"c1": 14, "c2": 21, "c3": 44, "c4": 32, "c5": 34, "c6": 90, "c7": 29, "c8": 67}]
@@ -158,7 +153,6 @@ class Avg_Where_Groupby(TestAggregatesBase):
                         GROUP BY id;'''   
         self.execute_query(pipeline_name, expected_data, table_name, view_query) 
         
-    @unittest.skip("https://github.com/feldera/feldera/issues/2142")
     def test_avg_where_groupby1(self):
         pipeline_name ="test_avg_where_groupby1"
         # checked manually
@@ -168,7 +162,6 @@ class Avg_Where_Groupby(TestAggregatesBase):
             {"id": 1,"c1": None, "c2": 2, "c3": 30, "c4": 4, "c5": None, "c6": 60, "c7": 70, "c8": 8},
             {"id" :1,"c1": 14, "c2": 21, "c3": 44, "c4": 32, "c5": 34, "c6": 90, "c7": 29, "c8": 67}]
         self.add_data(new_data)
-        # in id: 0, returns c3 : nan && in id: 1, returns c3: 44.0
         expected_data = [
             {"id": 0,"avg_c1": 11, "avg_c2": 22, "avg_c3": None, "avg_c4": 45, "avg_c5": 51, "avg_c6": 6, "avg_c7": 72, "avg_c8": 88},
             {"id" :1,"avg_c1": 14, "avg_c2": 21, "avg_c3": 44, "avg_c4": 32, "avg_c5": 34, "avg_c6": 90, "avg_c7": 29, "avg_c8": 67}]
@@ -176,7 +169,7 @@ class Avg_Where_Groupby(TestAggregatesBase):
         view_query = f'''CREATE VIEW avg_view AS
                          WITH avg_val AS (
                             SELECT
-                                FLOOR(AVG(c1)) AS avg_c1, FLOOR(AVG(c2)) AS avg_c2, FLOOR(AVG(c3)) AS avg_c3, FLOOR(AVG(c4)) AS avg_c4, FLOOR(AVG(c5)) AS avg_c5, FLOOR(AVG(c6)) AS avg_c6, FLOOR(AVG(c7)) AS avg_c7, FLOOR(AVG(c8)) AS avg_c8
+                                AVG(c1) AS avg_c1, AVG(c2) AS avg_c2, AVG(c3) AS avg_c3, AVG(c4) AS avg_c4, AVG(c5) AS avg_c5, AVG(c6) AS avg_c6, AVG(c7) AS avg_c7, AVG(c8) AS avg_c8
                             FROM {table_name})
                             SELECT
                                 t.id, AVG(t.c1) AS avg_c1, AVG(t.c2) AS avg_c2, AVG(t.c3) AS avg_c3, AVG(t.c4) AS avg_c4,  AVG(t.c5) AS avg_c5, AVG(t.c6) AS avg_c6, AVG(t.c7) AS avg_c7, AVG(t.c8) AS avg_c8
@@ -185,18 +178,6 @@ class Avg_Where_Groupby(TestAggregatesBase):
                             GROUP BY t.id;'''   
                          
         self.execute_query(pipeline_name, expected_data, table_name, view_query) 
-        
-class Avg_Fail0(TestAggregatesBase):
-    # checks for assertion fail on incorrect expected output
-    def test_avg_fail0(self):
-        pipeline_name ="test_avg_fail0"
-        expected_data = [{"c1": 0, "c2": 0, "c3" : 0, "c4" : 4, "c5" : 5, "c6" : 6,	"c7":7, "c8": 8}] 
-        table_name = "avg_fail0"
-        view_query = f'''CREATE VIEW avg_view AS SELECT
-                            AVG(c1) AS c1,AVG(c2) AS c2,AVG(c3) AS c3,AVG(c4) AS c4,AVG(c5) AS c5,AVG(c6) AS c6,AVG(c7) AS c7,AVG(c8) AS c8
-                        FROM {table_name}'''  
-        with self.assertRaises(AssertionError):              
-            self.execute_query(pipeline_name, expected_data,  table_name, view_query)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ python/tests/sql_aggregates/test_every.py
@@ -0,0 +1,70 @@
+import unittest
+from feldera import PipelineBuilder, Pipeline
+from tests import TEST_CLIENT
+
+class TestAggregatesBase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = [{"insert":{"id": 0, "c1": 5, "c2": 2, "c3": None, "c4": 4, "c5": 5, "c6": 6, "c7": None, "c8": 8}},
+                    {"insert":{"id": 1,"c1": 4, "c2": 3, "c3": 4, "c4": 6, "c5": 2, "c6": 3, "c7": 4, "c8": 2}},
+                    {"insert":{"id" :0 ,"c1": 4, "c2": 2, "c3": 30, "c4": 14, "c5": None, "c6": 60, "c7": 70, "c8": 18}},
+                    {"insert":{"id": 1,"c1": 5, "c2": 3, "c3": None, "c4": 9, "c5": 51, "c6": 6, "c7": 72, "c8": 2}}]       
+        return super().setUp()
+      
+    def execute_query(self, pipeline_name, expected_data, table_name, view_query):
+        sql = f'''CREATE TABLE {table_name}(
+                    id INT NOT NULL, c1 TINYINT, c2 TINYINT NOT NULL, c3 INT2, c4 INT2 NOT NULL, c5 INT, c6 INT NOT NULL,c7 BIGINT,c8 BIGINT NOT NULL);''' + view_query
+        pipeline = PipelineBuilder(TEST_CLIENT, f'{pipeline_name}', sql=sql).create_or_replace()
+        out = pipeline.listen('every_view')
+        pipeline.start()
+        pipeline.input_json(table_name, self.data, update_format="insert_delete")
+        pipeline.wait_for_completion(True)
+        out_data = out.to_dict()
+        print(out_data)
+        for datum in expected_data:
+            datum.update({"insert_delete": 1})
+        assert expected_data == out_data
+        pipeline.delete()
+        
+    def add_data(self, new_data, delete: bool = False):
+        key = "delete" if delete else "insert"
+        for datum in new_data:
+            self.data.append({key: datum})
+            
+class Every(TestAggregatesBase):
+    def test_every_value(self):
+        pipeline_name = "test_every"
+        # validated using postgres
+        expected_data = [{'every': True}]
+        table_name = "every"
+        view_query = f'''CREATE VIEW every_view AS SELECT EVERY(c4>3) AS every FROM {table_name}'''
+        self.execute_query(pipeline_name, expected_data, table_name, view_query)
+
+class Every_Groupby(TestAggregatesBase):
+    def test_every_groupby(self):
+        pipeline_name = "test_every_groupby"
+        # validated using postgres
+        expected_data = [{'every': True}, {'every': True}]
+        table_name = "every_groupby"
+        view_query = f'''CREATE VIEW every_view AS SELECT EVERY(c4>3) AS every FROM {table_name} GROUP BY id;'''
+        self.execute_query(pipeline_name, expected_data, table_name, view_query)
+        
+class Every_Where(TestAggregatesBase):
+    def test_every_where(self):
+        pipeline_name = "test_every_where"
+        # validated using postgres
+        expected_data = [{'every': False}]
+        table_name = "every_where"
+        view_query = f'''CREATE VIEW every_view AS SELECT EVERY(c4=4) AS every FROM {table_name} WHERE c6=6;'''
+        self.execute_query(pipeline_name, expected_data, table_name, view_query)
+        
+class Every_Where_Groupby(TestAggregatesBase):
+    def test_every_where_groupby(self):
+        pipeline_name = "test_every_where_groupby"
+        # validated using postgres
+        expected_data = [{'every': False}, {'every': True}]
+        table_name = "every_where_groupby"
+        view_query = f'''CREATE VIEW every_view AS SELECT EVERY(c4>4) AS every FROM {table_name} WHERE c6>3 GROUP BY id;'''
+        self.execute_query(pipeline_name, expected_data, table_name, view_query)
+        
+if __name__ == '__main__':
+    unittest.main()  
