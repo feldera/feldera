2ffa0bf156f99af3f0f651c664ed67359c12aabf
Gerd Zellweger <mail@gerdzellweger.com>
2024-08-28 10:10:01 -0700

    Fix benchmark CI.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- benchmark/feldera-sql/benchmarks/tpc-h/generate.bash
+++ benchmark/feldera-sql/benchmarks/tpc-h/generate.bash
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 
 if [ ! -d "data-large" ]; then
-	git clone git@github.com:dbtoaster/dbtoaster-experiments-data.git
+	git clone https://github.com/dbtoaster/dbtoaster-experiments-data.git
 	mv dbtoaster-experiments-data/tpch/big data-large
 	mv dbtoaster-experiments-data/tpch/standard data-medium
 	rm -rf dbtoaster-experiments-data
