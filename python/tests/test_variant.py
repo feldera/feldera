3e4d8a0a7042aa1296bc5c3db93ff8a0b6c0f090
Leonid Ryzhyk <leonid@feldera.com>
2024-09-12 22:38:30 -0700

    [SQL] End-to-end test for VARIANT input and output.
    Test input/outup
    
    - Feed JSON data as string columns, convert it into VARIANT using
      PARSE_JSON
    - Output as VARIANT
    - Extract strongly typed columns from VARIANT
    - Convert VARIANT back to JSON strings.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- python/tests/test_variant.py
+++ python/tests/test_variant.py
@@ -10,27 +10,41 @@ from decimal import Decimal
 class TestVariant(unittest.TestCase):
     def test_local(self):
         sql = f"""
+-- Ingest JSON as string; output it as VARIANT.
 CREATE TABLE json_table (json VARCHAR);
-CREATE TABLE variant_table(val VARIANT);
+CREATE VIEW json_view AS SELECT PARSE_JSON(json) AS json FROM json_table;
+CREATE VIEW json_string_view AS SELECT UNPARSE_JSON(json) AS json FROM json_view;
 
-CREATE LOCAL VIEW tmp AS SELECT PARSE_JSON(json) AS json FROM json_table;
-
-CREATE VIEW average AS SELECT
+CREATE VIEW average_view AS SELECT
 CAST(json['name'] AS VARCHAR) as name,
 ((CAST(json['scores'][1] AS DECIMAL(8, 2)) + CAST(json['scores'][2] AS DECIMAL(8, 2))) / 2) as average
-FROM tmp;
+FROM json_view;
 
-CREATE VIEW typed_view AS SELECT
+-- Ingest JSON as variant; extract strongly types columns from it.
+CREATE TABLE variant_table(val VARIANT);
+
+CREATE MATERIALIZED VIEW typed_view AS SELECT
     CAST(val['name'] AS VARCHAR) as name,
     CAST(val['scores'] AS DECIMAL ARRAY) as scores
 FROM variant_table;
-
-
         """
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
         pipeline = PipelineBuilder(TEST_CLIENT, name="test_variant", sql=sql).create_or_replace()
-        df_grades = pd.read_csv(dir_path + '/grades-json.csv')
+
+        input_strings = [
+            {"json":"{\"name\":\"Bob\",\"scores\":[8,10]}"},
+            {"json":"{\"name\":\"Dunce\",\"scores\":[3,4]}"},
+            {"json":"{\"name\":\"John\",\"scores\":[9,10]}"}
+        ]
+
+        input_json = [
+            {"val": {"name": "Bob", "scores": [8, 10]}},
+            {"val": {"name": "Dunce", "scores": [3, 4]}},
+            {"val": {"name": "John", "scores": [9, 10]}}
+        ]
+
+        expected_strings = [j | {"insert_delete": 1} for j in input_strings]
 
         expected_average = [
             { "name": "Bob", "average": Decimal(9) },
@@ -40,30 +54,47 @@ FROM variant_table;
         for datum in expected_average:
             datum.update({"insert_delete": 1})
 
-        average_out = pipeline.listen("average")
+        expected_typed = [
+            { "name": "Bob", "scores": [8, 10] },
+            { "name": "Dunce", "scores": [3, 4] },
+            { "name": "John", "scores": [9, 10] }
+        ]
+        for datum in expected_typed:
+            datum.update({"insert_delete": 1})
+
+        expected_variant = [
+            {"json": { "name": "Bob", "scores": [8, 10] }},
+            {"json": { "name": "Dunce", "scores": [3, 4] }},
+            {"json": { "name": "John", "scores": [9, 10] }}
+        ]
+        for datum in expected_variant:
+            datum.update({"insert_delete": 1})
+
+        variant_out = pipeline.listen("json_view")
+        json_out = pipeline.listen("json_string_view")
+        average_out = pipeline.listen("average_view")
         typed_out = pipeline.listen("typed_view")
 
         pipeline.start()
 
-        pipeline.input_pandas('json_table', df_grades)
+        # Feed JSON as strings, receive output from `average_view` and `json_view`
+        pipeline.input_json('json_table', input_strings)
+
+        pipeline.wait_for_completion(False)
+
+        assert expected_average == average_out.to_dict()
+        assert expected_variant == variant_out.to_dict()
+
+        assert expected_strings == json_out.to_dict()
+
+        # Feed VARIANT, read strongly typed columns. Since output colums have the same
+        # shape as inputs, output and input should be identical.
+        pipeline.input_json('variant_table', input_json)
+        pipeline.wait_for_completion(False)
+
+        assert expected_typed == typed_out.to_dict()
+
         pipeline.wait_for_completion(True)
-        out_data = average_out.to_dict()
-
-        assert expected_average == out_data
-
-        pipeline.input_pandas('variant_table', df_grades)
-        pipeline.wait_for_completion(True)
-        out_data = typed_out.to_dict()
-
-        expected_typed = [
-            { "name": "Bob", "average": [Decimal(8), Decimal(10)] },
-            { "name": "Dunce", "average": [Decimal(9), Decimal(10)] },
-            { "name": "John", "average": [Decimal(3), Decimal(4)] }
-        ]
-        for datum in expected_average:
-            datum.update({"insert_delete": 1})
-
-        assert expected_typed == out_data
 
         pipeline.delete()
 
