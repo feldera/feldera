b33c869e752b6a9a44a8464f1ff79364841bcdc8
Mihai Budiu <mbudiu@feldera.com>
2024-09-26 16:32:17 -0700

    [SQL] Run sqllogictests by rotation, some every day
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/slt/src/main/java/org/dbsp/sqllogictest/Main.java
+++ sql-to-dbsp-compiler/slt/src/main/java/org/dbsp/sqllogictest/Main.java
@@ -32,15 +32,51 @@ import org.dbsp.sqllogictest.executors.DbspJdbcExecutor;
 import org.dbsp.util.Linq;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 /** Execute some or all of SqlLogicTest tests. */
 public class Main {
-    public static final String rustDirectory = "./temp/src/";
+    static final String rustDirectory = "./temp/src/";
     public static final String testFileName = "lib";
+
+    public static String getAbsoluteRustDirectory() {
+        String wd = System.getProperty("user.dir");
+        return wd + "/" + rustDirectory;
+    }
+
+    /** Executes a few test files based on the day of the month */
+    public static void rotateTests() throws IOException, ClassNotFoundException {
+        Set<String> tests = net.hydromatic.sqllogictest.Main.getTestList();
+        // Number of days required to run all tests
+        final int rotationPeriodInDays = 365;
+        int testsPerDay = (tests.size() + rotationPeriodInDays - 1) / rotationPeriodInDays;
+        LocalDate date = LocalDate.now();
+        final int day = date.getDayOfYear() % rotationPeriodInDays;
+        final int firstTest = testsPerDay * (day - 1);
+        final int lastTest = firstTest + testsPerDay;
+
+        List<String> list = new ArrayList<>(tests);
+        list.sort(String::compareTo);
+        List<String> toRun = list.subList(firstTest, lastTest);
+        String[] args = new String[] { "-v", "-x", "-inc", "-e", "hybrid" };
+
+        String wd = System.getProperty("user.dir");
+        File directory = new File(wd + "/..").getAbsoluteFile();
+        System.setProperty("user.dir", directory.getAbsolutePath());
+        wd = System.getProperty("user.dir");
+        System.out.println("working directory is " + wd);
+
+        String[] argv = new String[args.length + toRun.size()];
+        System.arraycopy(args, 0, argv, 0, args.length);
+        for (int i = 0; i < toRun.size(); i++)
+            argv[i + args.length] = toRun.get(i);
+        main(argv);
+    }
 
     @SuppressWarnings("SpellCheckingInspection")
     public static void main(String[] argv) throws IOException, ClassNotFoundException {
