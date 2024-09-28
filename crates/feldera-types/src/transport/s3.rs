3199cabf32ec2541048fadbe8693a450552dfc7e
Leonid Ryzhyk <leonid@feldera.com>
2024-09-28 14:15:52 -0700

    [adapters] Simplify S3 connector config.
    * Simplify and flatten S3 config properties to be more inline with other
      connectors, so:
    
      ```
      credentials:
        type: AccessKey
        aws_access_key_id: FAKE_ACCESS_KEY
        aws_secret_access_key: FAKE_SECRET
      bucket_name: test-bucket
      region: us-west-1
      read_strategy:
        type: Prefix
        prefix: ''
      consume_strategy:
        type: Fragment
      ```
    
      becomes
    
      ```
      aws_access_key_id: FAKE_ACCESS_KEY
      aws_secret_access_key: FAKE_SECRET
      bucket_name: test-bucket
      region: us-west-1
      prefix: ''
      streaming: true
      ```
    
    * Better configuration error messages
    
    * Change the default behavior from fragments (now called 'streaming') to objects.
      This is a safe default that works for all input formats.  Besides S3 objects are
      typically small (10s of MBs), so always pre-loading the entire object
      isn't necessarily a problem even for streaming formats.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/feldera-types/src/transport/s3.rs
+++ crates/feldera-types/src/transport/s3.rs
@@ -4,56 +4,37 @@ use utoipa::ToSchema;
 /// Configuration for reading data from AWS S3.
 #[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
 pub struct S3InputConfig {
-    /// Credentials to authenticate against AWS
-    pub credentials: AwsCredentials,
-    /// AWS region
+    /// AWS Access Key id. This property must be specified unless `no_sign_request` is set to `true`.
+    pub aws_access_key_id: Option<String>,
+
+    /// Secret Access Key. This property must be specified unless `no_sign_request` is set to `true`.
+    pub aws_secret_access_key: Option<String>,
+
+    /// Do not sign requests. This is equivalent to the `--no-sign-request` flag in the AWS CLI.
+    #[serde(default)]
+    pub no_sign_request: bool,
+
+    /// Read a single object specified by a key.
+    pub key: Option<String>,
+
+    /// Read all objects whose keys match a prefix. Set to an empty string to read all objects in the bucket.
+    pub prefix: Option<String>,
+
+    /// AWS region.
     pub region: String,
-    /// S3 bucket name to access
+
+    /// S3 bucket name to access.
     pub bucket_name: String,
-    /// Strategy that determines which objects to
-    /// read from the bucket
-    pub read_strategy: ReadStrategy,
-    /// Streaming vs chunked reads
-    #[serde(default = "default_consume_strategy")]
-    pub consume_strategy: ConsumeStrategy,
-}
 
-fn default_consume_strategy() -> ConsumeStrategy {
-    ConsumeStrategy::Fragment
-}
-
-/// Configuration to authenticate against AWS
-#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
-#[serde(tag = "type")]
-pub enum AwsCredentials {
-    /// Do not sign requests. This is equivalent to
-    /// the `--no-sign-request` flag in the AWS CLI
-    NoSignRequest,
-    /// Authenticate using a long-lived AWS access key and secret
-    AccessKey {
-        aws_access_key_id: String,
-        aws_secret_access_key: String,
-    },
-}
-
-/// Strategy that determines which objects to read from a given bucket
-#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
-#[serde(tag = "type")]
-pub enum ReadStrategy {
-    /// Read a single object specified by a key
-    SingleKey { key: String },
-    /// Read all objects whose keys match a prefix
-    Prefix { prefix: String },
-}
-
-/// Strategy to feed a fetched object into an InputConsumer.
-#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
-#[serde(tag = "type")]
-pub enum ConsumeStrategy {
-    /// Write the object as a series of fragments (see
-    /// InputConsumer::input_fragment).
-    Fragment,
-    /// Write the entire object at once. Appropriate for formats like Parquet
-    /// that cannot be streamed.
-    Object,
+    /// Determines how the connector ingests an individual S3 object. When `true`,
+    /// the connector pushes the object to the pipeline chunk-by-chunk, so that the
+    /// pipeline can parse and process initial chunks of the object before the entire
+    /// object has been retrieved. This mode is suitable for streaming formats such as
+    /// newline-delimited JSON. When `false`, the connector buffers the entire object
+    /// in memory and pushes it to the pipeline as a single chunk.  Appropriate for
+    /// formats like Parquet that cannot be streamed.
+    ///
+    /// The default value is `false`.
+    #[serde(default)]
+    pub streaming: bool,
 }
