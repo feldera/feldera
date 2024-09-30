8581445cd8b4cc3702d7048b79beef2435c30b9a
Ben Pfaff <blp@feldera.com>
2024-09-30 15:54:57 -0700

    feldera-types: Allow default value for `StorageConfig::cache`.
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- web-console/src/lib/services/manager/schemas.gen.ts
+++ web-console/src/lib/services/manager/schemas.gen.ts
@@ -2601,7 +2601,7 @@ export const $StorageCacheConfig = {
 export const $StorageConfig = {
   type: 'object',
   description: 'Configuration for persistent storage in a [`PipelineConfig`].',
-  required: ['path', 'cache'],
+  required: ['path'],
   properties: {
     cache: {
       $ref: '#/components/schemas/StorageCacheConfig'
