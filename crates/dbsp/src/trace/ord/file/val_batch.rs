49cdd8ccf3d45796af111e716b7bb0aa14d749fc
Ben Pfaff <blp@feldera.com>
2024-10-03 14:09:22 -0700

    Upgrade to Rust 1.81.
    It seems that nightly has changed how it formats some code we use, so this
    also runs `cargo +nightly fmt` to make it pass.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/trace/ord/file/val_batch.rs
+++ crates/dbsp/src/trace/ord/file/val_batch.rs
@@ -990,7 +990,8 @@ where
     T: Timestamp,
     R: WeightTrait + ?Sized,
 {
-    type TimeDiffCursor<'a> = FileValTimeDiffCursor<T, R>
+    type TimeDiffCursor<'a>
+        = FileValTimeDiffCursor<T, R>
     where
         Self: 'a;
 
