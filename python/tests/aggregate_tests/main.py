0d0c6b3eff9e754f46dcbc4a7cc9709e3996906d
rivudhk <rivudhkr@gmail.com>
2024-10-12 03:29:01 +0545

    py: varchar type tests for MIN aggregate
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/main.py
+++ python/tests/aggregate_tests/main.py
@@ -30,6 +30,7 @@ from tests.aggregate_tests.test_stddev_pop import *  # noqa: F403
 from tests.aggregate_tests.test_stddev_samp import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_arr_agg import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_max import *  # noqa: F403
+from tests.aggregate_tests.test_varchar_min import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_table import *  # noqa: F403
 
 
