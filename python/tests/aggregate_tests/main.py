1580f3c0ca36d85cc27e1b14273f63e0beea51f0
rivudhk <rivudhkr@gmail.com>
2024-10-16 01:52:11 +0545

    py: varchar type tests for ARG_MAX aggregate
    Signed-off-by: rivudhk <rivudhkr@gmail.com>
    
    # Conflicts:
    #	python/tests/aggregate_tests/main.py

--- python/tests/aggregate_tests/main.py
+++ python/tests/aggregate_tests/main.py
@@ -28,6 +28,7 @@ from tests.aggregate_tests.test_min import *  # noqa: F403
 from tests.aggregate_tests.test_some import *  # noqa: F403
 from tests.aggregate_tests.test_stddev_pop import *  # noqa: F403
 from tests.aggregate_tests.test_stddev_samp import *  # noqa: F403
+from tests.aggregate_tests.test_varchar_argmax import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_argmin import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_arr_agg import *  # noqa: F403
 from tests.aggregate_tests.test_varchar_max import *  # noqa: F403
