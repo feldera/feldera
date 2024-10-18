04874eba621cb681a98f0a55fc1d6979fe29a007
Ben Pfaff <blp@feldera.com>
2024-10-18 12:04:56 -0700

    dbsp: Rewrite dirlock code to hold OS lock for the whole run.
    Until now, the directory locking code has taken the OS lock briefly while
    it writes the file.  After that, it relied on a pid liveness check to see
    whether the directory is still locked.
    
    This worked OK on the assumption that pids are rarely reused.  However, in
    a cloud environment, the pid from one run to the next is often the same
    (for example, in the Kubernetes test environment I was just working with,
    it was always pid 9).  This meant that if the pod crashed and restarted
    with the previous storage, the new instance would treat the directory as
    locked.
    
    This fixes the problem by holding the OS-based lock for the entire time
    that we need the directory to be locked.  On a crash, when the pod comes
    back up again, the OS lock was automatically dropped.
    
    This switches the locking implementation to use one similar to that in
    Open vSwitch, where it has been used successfully for many years.  See
    https://github.com/openvswitch/ovs/blob/main/lib/lockfile.c and
    https://github.com/openvswitch/ovs/blob/main/lib/lockfile.h for the
    OVS implementation.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/storage/dirlock/test.rs
+++ crates/dbsp/src/storage/dirlock/test.rs
@@ -1,37 +1,64 @@
 use super::LockedDirectory;
-use crate::storage::backend::StorageError::StorageLocked;
-use std::fs::File;
-use std::io::Read;
-use sysinfo::Pid;
+use crate::storage::backend::StorageError::{self};
+use std::{fs, path::Path, process};
+
+fn cant_relock(path: &Path) {
+    assert_eq!(
+        LockedDirectory::new(path).unwrap_err(),
+        StorageError::StorageLocked(process::id(), path.to_path_buf(),)
+    );
+}
 
 #[test]
 fn test_pidlock_lifecycle() {
     let temp_dir = tempfile::tempdir().unwrap();
-    let pidfile = LockedDirectory::new(temp_dir.path()).unwrap();
-    let pidfile_path = temp_dir.path().join(LockedDirectory::LOCKFILE_NAME);
-    assert!(pidfile_path.exists());
+    let temp_path = temp_dir.path();
+    let pidfile_path = temp_path.join(LockedDirectory::LOCKFILE_NAME);
 
-    let mut file = File::open(&pidfile_path).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    assert_eq!(contents, format!("{}\n", std::process::id()));
+    for _ in 0..2 {
+        // Lock directory.
+        let _pidfile = LockedDirectory::new(temp_path).unwrap();
+        assert!(pidfile_path.exists());
 
-    drop(pidfile);
-    assert!(!pidfile_path.exists());
+        // Can't re-lock same directory.
+        //
+        // Because of `fcntl` weird behavior on file close, we try this twice in
+        // case trying to lock the second time actually unlocks it for the third
+        // try.
+        for _ in 0..2 {
+            cant_relock(temp_path);
+        }
+
+        // Drop the lock with the end of the block.
+    }
 }
 
 #[test]
-fn test_pidlock_locks() {
+fn test_multiple_locks() {
     let temp_dir = tempfile::tempdir().unwrap();
-    let pidfile = LockedDirectory::new(temp_dir.path()).unwrap();
-    let r = LockedDirectory::with_pid(temp_dir.path(), Pid::from(0));
-    assert_eq!(
-        r.unwrap_err(),
-        StorageLocked(
-            std::process::id(),
-            temp_dir.path().join(LockedDirectory::LOCKFILE_NAME)
-        )
-    );
-    drop(pidfile);
-    LockedDirectory::with_pid(temp_dir.path(), Pid::from(0)).expect("other PID can take over");
+    let temp_path = temp_dir.path();
+
+    let a_path = temp_path.join("a");
+    fs::create_dir(&a_path).unwrap();
+
+    let b_path = temp_path.join("b");
+    fs::create_dir(&b_path).unwrap();
+
+    let c_path = temp_path.join("c");
+    fs::create_dir(&c_path).unwrap();
+
+    let _a = LockedDirectory::new(&a_path).unwrap();
+    let _b = LockedDirectory::new(&b_path).unwrap();
+    let _c = LockedDirectory::new(&c_path).unwrap();
+
+    drop(_a);
+    let _a = LockedDirectory::new(&a_path).unwrap();
+    cant_relock(&a_path);
+    drop(_a);
+
+    drop(_b);
+    let _a = LockedDirectory::new(&a_path).unwrap();
+
+    drop(_c);
+    drop(_a);
 }
