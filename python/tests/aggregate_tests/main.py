f2936df9c4bf17af3c3282a1a25a12fdfb80a833
rivudhk <rivudhkr@gmail.com>
2024-10-23 01:40:42 +0545

    py: varchar(n) type tests for MAX aggregate
    Signed-off-by: rivudhk <rivudhkr@gmail.com>
    
    # Conflicts:
    #	python/tests/aggregate_tests/main.py

--- python/tests/aggregate_tests/main.py
+++ python/tests/aggregate_tests/main.py
@@ -42,6 +42,7 @@ from tests.aggregate_tests.test_varcharn_arragg import *  # noqa: F403
 from tests.aggregate_tests.test_varcharn_count import *  # noqa: F403
 from tests.aggregate_tests.test_varcharn_count_col import *  # noqa: F403
 from tests.aggregate_tests.test_varcharn_min import *  # noqa: F403
+from tests.aggregate_tests.test_varcharn_max import *  # noqa: F403
 
 
 def register_tests_in_module(module, ta: TstAccumulator):
