537b47d37ff42bfbad3d9b58feababd102beb8df
Leonid Ryzhyk <leonid@feldera.com>
2024-10-23 20:30:25 -0700

    Remove the hello-world demo.
    The demo/hello-world example has not been maintained and does not really
    serve any purpose anymore (pre-packaged examples and tutorials make for
    better hello-world examples).
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- demo/hello-world/combiner.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE messages (id INTEGER, message STRING);
-CREATE TABLE records (id INTEGER, message STRING);
-
-CREATE VIEW matches AS
-SELECT messages.message || ' ' || records.message AS combined
-FROM messages
-JOIN records
-    ON messages.id = records.id;
