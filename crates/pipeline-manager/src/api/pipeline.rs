a688271e2b8827491acffd27ec750bbb412f2c61
Ben Pfaff <blp@feldera.com>
2024-10-29 17:07:11 -0700

    pipeline-manager: Improve description of Bad Request for checkpointing.
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/pipeline-manager/src/api/pipeline.rs
+++ crates/pipeline-manager/src/api/pipeline.rs
@@ -697,7 +697,7 @@ pub(crate) async fn get_pipeline_circuit_profile(
             , body = ErrorResponse
             , example = json!(examples::error_unknown_pipeline())),
         (status = BAD_REQUEST
-            , description = "Pipeline is not running or paused"
+            , description = "Pipeline is not running or paused, or fault tolerance is not enabled for this pipeline"
             , body = ErrorResponse
             , example = json!(examples::error_pipeline_not_running_or_paused()))
     ),
