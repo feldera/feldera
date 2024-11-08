c611cf6b832e2f650a09c490b8dd6570e7970cfb
Simon Kassing <simon.kassing@feldera.com>
2024-11-08 13:03:01 +0100

    demo: compile and run all packaged demos in a test
    This tests that that the packaged demos are able to compile and at least
    run shortly (2 seconds).
    
    Fixes the `udf-tutorial` demo by correcting the UDF Rust filename.
    
    Signed-off-by: Simon Kassing <simon.kassing@feldera.com>

---
+++ demo/all-packaged/run.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+# Run locally with:
+#   python3 demo/all-packaged/run.py --api-url http://localhost:8080
+
+import time
+import requests
+import argparse
+from feldera import FelderaClient, PipelineBuilder
+from feldera.enums import PipelineStatus
+
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api-url", required=True, help="Feldera API URL (e.g., http://localhost:8080)")
+    args = parser.parse_args()
+
+    # Create Feldera client using the API URL
+    api_url = args.api_url
+    print(f"Feldera API URL: {api_url}")
+    client = FelderaClient(api_url)
+
+    # Retrieve and run all packaged demos
+    demos = requests.get(f"{api_url}/v0/config/demos").json()
+    print(f"Total {len(demos)} packages demos were found and will be run")
+    for demo in demos:
+        print(f"Running packaged demo: {demo['name']}...")
+        pipeline = PipelineBuilder(
+            client,
+            demo["name"],
+            demo["program_code"],
+            udf_rust=demo["udf_rust"],
+            udf_toml=demo["udf_toml"]
+        ).create_or_replace()
+        pipeline.start()
+        time.sleep(2)
+        status = pipeline.status()
+        assert status == PipelineStatus.RUNNING, f"FAIL: demo {demo['name']}: expected pipeline to be RUNNING but instead is {status}"
+        pipeline.shutdown()
+        print(f"PASS: demo {demo['name']}")
+
+
+# Main entry point
+if __name__ == "__main__":
+    main()
