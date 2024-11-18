b94468288f67d9c5a666503d7c2f02bab46c7ab8
rivudhk <rivudhkr@gmail.com>
2024-11-19 03:32:54 +0545

    py: INTERVAL type tests for COUNT and MAX aggregates
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/test_interval_count.py
+++ python/tests/aggregate_tests/test_interval_count.py
@@ -7,7 +7,7 @@ class aggtst_interval_count(TstView):
         self.data = [{"count": 5}]
         self.sql = """CREATE MATERIALIZED VIEW interval_count AS SELECT
                       COUNT(*) AS count
-                      FROM atbl_interval"""
+                      FROM atbl_interval_seconds"""
 
 
 class aggtst_interval_count_groupby(TstView):
@@ -16,24 +16,24 @@ class aggtst_interval_count_groupby(TstView):
         self.data = [{"id": 0, "count": 2}, {"id": 1, "count": 3}]
         self.sql = """CREATE MATERIALIZED VIEW interval_count_gby AS SELECT
                       id, COUNT(*) AS count
-                      FROM atbl_interval
+                      FROM atbl_interval_seconds
                       GROUP BY id"""
 
 
 class aggtst_interval_count_where(TstView):
     def __init__(self):
         # checked manually
-        self.data = [{"count": 3}]
+        self.data = [{"count": 2}]
         self.sql = """CREATE MATERIALIZED VIEW interval_count_where AS SELECT
                       COUNT(*) FILTER(WHERE c2_minus_c1 > c1_minus_c2) AS count
-                      FROM atbl_interval"""
+                      FROM atbl_interval_seconds"""
 
 
 class aggtst_interval_count_where_groupby(TstView):
     def __init__(self):
         # checked manually
-        self.data = [{"id": 0, "count": 1}, {"id": 1, "count": 2}]
+        self.data = [{"id": 0, "count": 1}, {"id": 1, "count": 1}]
         self.sql = """CREATE MATERIALIZED VIEW interval_count_where_gby AS SELECT
                       id, COUNT(*) FILTER(WHERE c2_minus_c1 > c1_minus_c2) AS count
-                      FROM atbl_interval
+                      FROM atbl_interval_seconds
                       GROUP BY id"""
