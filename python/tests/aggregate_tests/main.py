b769f9551417cf68aca0da7b729bb1517d0f5f08
rivudhk <rivudhkr@gmail.com>
2024-11-25 01:05:32 +0545

    py: VARBINARY type tests for ARG_MAX and ARG_MIN aggregates
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/main.py
+++ python/tests/aggregate_tests/main.py
@@ -98,6 +98,8 @@ from tests.aggregate_tests.test_timestamp_max import *  # noqa: F403
 from tests.aggregate_tests.test_timestamp_min import *  # noqa: F403
 from tests.aggregate_tests.test_timestamp_some import *  # noqa: F403
 from tests.aggregate_tests.test_timestamp_tbl import *  # noqa: F403
+from tests.aggregate_tests.test_varbinary_arg_min import *  # noqa: F403
+from tests.aggregate_tests.test_varbinary_arg_max import *  # noqa: F403
 from tests.aggregate_tests.test_varbinary_arr_agg import *  # noqa: F403
 from tests.aggregate_tests.test_varbinary_count_col import *  # noqa: F403
 from tests.aggregate_tests.test_varbinary_count import *  # noqa: F403
