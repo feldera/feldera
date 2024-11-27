0f8beb54c592263ea3085c09eeea564c859d2150
rivudhk <rivudhkr@gmail.com>
2024-11-27 20:23:12 +0545

    py: ARRAY type tests for ARRAY_AGG aggregate
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

---
+++ python/tests/aggregate_tests/test_array_tbl.py
@@ -0,0 +1,17 @@
+from tests.aggregate_tests.aggtst_base import TstTable
+
+
+class aggtst_array_tbl(TstTable):
+    """Define the table used by the array tests"""
+
+    def __init__(self):
+        self.sql = """CREATE TABLE array_tbl(
+                      id INT, 
+                      c1 INT ARRAY NOT NULL, 
+                      c2 INT ARRAY)"""
+        self.data = [
+            {"id": 0, "c1": [12, 22], "c2": None},
+            {"id": 0, "c1": [23, 56, 16], "c2": [55, 66, 77]},
+            {"id": 1, "c1": [23, 56, 16], "c2": [99]},
+            {"id": 1, "c1": [49], "c2": [32, 34, 22, 12]},
+        ]
