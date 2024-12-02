af644683456fef90849a331d8013e415fd2e94f8
rivudhk <rivudhkr@gmail.com>
2024-12-03 01:48:09 +0545

    py: ARRAY type tests for COUNT, EVERY and SOME aggregates
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/test_array_arr_agg.py
+++ python/tests/aggregate_tests/test_array_arr_agg.py
@@ -3,15 +3,16 @@ from .aggtst_base import TstView
 
 class aggtst_array_arr_agg_value(TstView):
     def __init__(self):
-        # Validated on Postgres
+        # checked manually
         self.data = [
             {
                 "c1": [[12, 22], [23, 56, 16], [23, 56, 16], [49]],
                 "c2": [None, [55, 66, None], [99], [32, 34, 22, 12]],
+                'c3': [[{'a': 5, 'b': 66}], [{'c': 2}], None, [{'x': 1}]]
             }
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg AS SELECT
-                      ARRAY_AGG(c1) AS c1, ARRAY_AGG(c2) AS c2
+                      ARRAY_AGG(c1) AS c1, ARRAY_AGG(c2) AS c2, ARRAY_AGG(c3) AS c3
                       FROM array_tbl"""
 
 
@@ -19,11 +20,11 @@ class aggtst_array_arr_agg_gby(TstView):
     def __init__(self):
         # checked manually
         self.data = [
-            {"id": 0, "c1": [[12, 22], [23, 56, 16]], "c2": [None, [55, 66, None]]},
-            {"id": 1, "c1": [[23, 56, 16], [49]], "c2": [[99], [32, 34, 22, 12]]},
+            {"id": 0, "c1": [[12, 22], [23, 56, 16]], "c2": [None, [55, 66, None]], 'c3': [[{'a': 5, 'b': 66}], [{'c': 2}]]},
+            {"id": 1, "c1": [[23, 56, 16], [49]], "c2": [[99], [32, 34, 22, 12]], 'c3': [None, [{'x': 1}]]},
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg_gby AS SELECT
-                      id, ARRAY_AGG(c1) AS c1, ARRAY_AGG(c2) AS c2
+                      id, ARRAY_AGG(c1) AS c1, ARRAY_AGG(c2) AS c2, ARRAY_AGG(c3) AS c3
                       FROM array_tbl
                       GROUP BY id"""
 
@@ -33,12 +34,13 @@ class aggtst_array_arr_agg_distinct(TstView):
         # checked manually
         self.data = [
             {
-                "c1": [[12, 22], [23, 56, 16], [49]],
-                "c2": [None, [32, 34, 22, 12], [55, 66, None], [99]],
+                'c1': [[12, 22], [23, 56, 16], [49]],
+                'c2': [None, [32, 34, 22, 12], [55, 66, None], [99]],
+                'c3': [None, [{'a': 5, 'b': 66}], [{'c': 2}], [{'x': 1}]]
             }
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg_distinct AS SELECT
-                      ARRAY_AGG(DISTINCT c1) AS c1, ARRAY_AGG(DISTINCT c2) AS c2
+                      ARRAY_AGG(DISTINCT c1) AS c1, ARRAY_AGG(DISTINCT c2) AS c2, ARRAY_AGG(DISTINCT c3) AS c3
                       FROM array_tbl"""
 
 
@@ -46,11 +48,11 @@ class aggtst_array_arr_agg_distinct_gby(TstView):
     def __init__(self):
         # checked manually
         self.data = [
-            {"id": 0, "c1": [[12, 22], [23, 56, 16]], "c2": [None, [55, 66, None]]},
-            {"id": 1, "c1": [[23, 56, 16], [49]], "c2": [[32, 34, 22, 12], [99]]},
+            {"id": 0, "c1": [[12, 22], [23, 56, 16]], "c2": [None, [55, 66, None]], 'c3': [[{'a': 5, 'b': 66}], [{'c': 2}]]},
+            {'id': 1, 'c1': [[23, 56, 16], [49]], 'c2': [[32, 34, 22, 12], [99]], 'c3': [None, [{'x': 1}]]}
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg_distinct_gby AS SELECT
-                      id, ARRAY_AGG(DISTINCT c1) AS c1, ARRAY_AGG(DISTINCT c2) AS c2
+                      id, ARRAY_AGG(DISTINCT c1) AS c1, ARRAY_AGG(DISTINCT c2) AS c2, ARRAY_AGG(DISTINCT c3) AS c3
                       FROM array_tbl
                       GROUP BY id"""
 
@@ -59,10 +61,10 @@ class aggtst_array_arr_agg_where(TstView):
     def __init__(self):
         # checked manually
         self.data = [
-            {"f_c1": [[23, 56, 16], [23, 56, 16]], "f_c2": [[55, 66, None], [99]]}
+            {"f_c1": [[23, 56, 16], [23, 56, 16]], "f_c2": [[55, 66, None], [99]], 'f_c3': [[{'c': 2}], None]}
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg_where AS SELECT
-                      ARRAY_AGG(c1) FILTER(WHERE c1 < c2) AS f_c1, ARRAY_AGG(c2) FILTER(WHERE c1 < c2) AS f_c2
+                      ARRAY_AGG(c1) FILTER(WHERE c1 < c2) AS f_c1, ARRAY_AGG(c2) FILTER(WHERE c1 < c2) AS f_c2, ARRAY_AGG(c3) FILTER(WHERE c1 < c2) AS f_c3
                       FROM array_tbl"""
 
 
@@ -70,10 +72,10 @@ class aggtst_array_arr_agg_where_gby(TstView):
     def __init__(self):
         # checked manually
         self.data = [
-            {"id": 0, "f_c1": [[23, 56, 16]], "f_c2": [[55, 66, None]]},
-            {"id": 1, "f_c1": [[23, 56, 16]], "f_c2": [[99]]},
+            {"id": 0, "f_c1": [[23, 56, 16]], "f_c2": [[55, 66, None]], 'f_c3': [[{'c': 2}]]},
+            {"id": 1, "f_c1": [[23, 56, 16]], "f_c2": [[99]], 'f_c3': [None]},
         ]
         self.sql = """CREATE MATERIALIZED VIEW array_arr_agg_where_gby AS SELECT
-                      id, ARRAY_AGG(c1) FILTER(WHERE c1 < c2) AS f_c1, ARRAY_AGG(c2) FILTER(WHERE c1 < c2) AS f_c2
+                      id, ARRAY_AGG(c1) FILTER(WHERE c1 < c2) AS f_c1, ARRAY_AGG(c2) FILTER(WHERE c1 < c2) AS f_c2, ARRAY_AGG(c3) FILTER(WHERE c1 < c2) AS f_c3
                       FROM array_tbl
                       GROUP BY id"""
--- python/tests/aggregate_tests/test_array_tbl.py
+++ python/tests/aggregate_tests/test_array_tbl.py
@@ -8,10 +8,11 @@ class aggtst_array_tbl(TstTable):
         self.sql = """CREATE TABLE array_tbl(
                       id INT,
                       c1 INT ARRAY NOT NULL,
-                      c2 INT ARRAY)"""
+                      c2 INT ARRAY,
+                      c3 MAP<VARCHAR, INT> ARRAY)"""
         self.data = [
-            {"id": 0, "c1": [12, 22], "c2": None},
-            {"id": 0, "c1": [23, 56, 16], "c2": [55, 66, None]},
-            {"id": 1, "c1": [23, 56, 16], "c2": [99]},
-            {"id": 1, "c1": [49], "c2": [32, 34, 22, 12]},
+            {"id": 0, "c1": [12, 22], "c2": None, "c3": [{"a": 5, "b": 66}]},
+            {"id": 0, "c1": [23, 56, 16], "c2": [55, 66, None], "c3": [{"c": 2}]},
+            {"id": 1, "c1": [23, 56, 16], "c2": [99], "c3": None},
+            {"id": 1, "c1": [49], "c2": [32, 34, 22, 12], "c3": [{"x": 1}]},
         ]
