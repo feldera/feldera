489e092001e54f5e566ad8e10d5ebfc027d84d67
Ben Pfaff <blp@feldera.com>
2024-12-11 13:14:46 -0800

    benchmark: Use CSV delimiter feature for TPC-H instead of rewriting input.
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- benchmark/feldera-sql/benchmarks/tpc-h/generate.bash
+++ benchmark/feldera-sql/benchmarks/tpc-h/generate.bash
@@ -1,28 +1,12 @@
-#!/bin/bash
-
-set -e
+#!/bin/sh -e
 
 echo "running tpc-h/generate.bash"
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pushd ${THIS_DIR}
+cd "${THIS_DIR}"
 
-rewrite_csv() {
-    for i in `ls *.csv`; do
-        csvformat -d"|" $i >x && mv x ${i}
-    done
-}
-
-if [[ ! -d "data-large" ]]; then
+if test ! -d "data-large"; then
     git clone --quiet https://github.com/dbtoaster/dbtoaster-experiments-data.git
     mv dbtoaster-experiments-data/tpch/big data-large
-    cd data-large
-    rewrite_csv
-    cd ..
     mv dbtoaster-experiments-data/tpch/standard data-medium
-    cd data-medium
-    rewrite_csv
-    cd ..
     rm -rf dbtoaster-experiments-data
 fi
-
-popd
