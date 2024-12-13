a1e1f14c02904e135c21807f51c5a1c1d6a5e297
rivudhk <rivudhkr@gmail.com>
2024-12-13 21:33:25 +0545

    py: ROW type tests for MAX and MIN aggregates
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

---
+++ python/tests/aggregate_tests/test_row_min.py
@@ -0,0 +1,69 @@
+from .aggtst_base import TstView
+
+
+class aggtst_row_min_value(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [{'c1': {'expr$0': 2, 'expr$1': 'elo', 'expr$2': 'ciao'}}]
+        self.sql = """CREATE MATERIALIZED VIEW row_min AS SELECT
+                      MIN(ROW(c1, c2, c3)) AS c1
+                      FROM row_tbl"""
+
+
+class aggtst_row_min_gby(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [
+            {'id': 0, 'c1': {'expr$0': 3, 'expr$1': 'ola', 'expr$2': 'ciao'}},
+            {'id': 1, 'c1': {'expr$0': 2, 'expr$1': 'elo', 'expr$2': 'ciao'}}
+        ]
+        self.sql = """CREATE MATERIALIZED VIEW row_min_gby AS SELECT
+                      id, 
+                      MIN(ROW(c1, c2, c3)) AS c1
+                      FROM row_tbl
+                      GROUP BY id"""
+
+
+class aggtst_row_min_distinct(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [{'c1': {'expr$0': 2, 'expr$1': 'elo', 'expr$2': 'ciao'}}]
+        self.sql = """CREATE MATERIALIZED VIEW row_min_distinct AS SELECT
+                      MIN(DISTINCT ROW(c1, c2, c3)) AS c1
+                      FROM row_tbl"""
+
+
+class aggtst_row_min_distinct_gby(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [
+            {'id': 0, 'c1': {'expr$0': 3, 'expr$1': 'ola', 'expr$2': 'ciao'}},
+            {'id': 1, 'c1': {'expr$0': 2, 'expr$1': 'elo', 'expr$2': 'ciao'}}
+        ]
+        self.sql = """CREATE MATERIALIZED VIEW row_min_distinct_gby AS SELECT
+                      id,
+                      MIN(DISTINCT ROW(c1, c2, c3)) AS c1
+                      FROM row_tbl
+                      GROUP BY id"""
+
+
+class aggtst_row_min_where(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [{'c1': {'expr$0': 7, 'expr$1': 'hi', 'expr$2': 'hiya'}}]
+        self.sql = """CREATE MATERIALIZED VIEW row_min_where AS SELECT
+                      MIN(ROW(c1, c2, c3)) FILTER(WHERE c2 < c3) AS c1
+                      FROM row_tbl"""
+
+
+class aggtst_row_min_where_gby(TstView):
+    def __init__(self):
+        # checked manually
+        self.data = [
+            {'id': 0, 'c1': None},
+            {'id': 1, 'c1': {'expr$0': 7, 'expr$1': 'hi', 'expr$2': 'hiya'}}
+        ]
+        self.sql = """CREATE MATERIALIZED VIEW row_min_where_gby AS SELECT
+                      id, MIN(ROW(c1, c2, c3)) FILTER(WHERE c2 < c3) AS c1
+                      FROM row_tbl
+                      GROUP BY id"""
