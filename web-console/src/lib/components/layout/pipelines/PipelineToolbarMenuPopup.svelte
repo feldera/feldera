fbc7087d7bc4d2c0c9f5bc4b48c91ea2f78ce964
Karakatiza666 <bulakh.96@gmail.com>
2024-12-17 18:26:03 +0000

    [WebConsole] Add custom error page when backend becomes unavailable
    Fix book icon visibility on home page in dark mode
    
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/components/layout/pipelines/PipelineToolbarMenuPopup.svelte
+++ web-console/src/lib/components/layout/pipelines/PipelineToolbarMenuPopup.svelte
@@ -1,41 +1,11 @@
 <script lang="ts">
-  import { PaneGroup, Pane, PaneResizer } from 'paneforge'
-  import InteractionsPanel from '$lib/components/pipelines/editor/InteractionsPanel.svelte'
-  import DeploymentStatus from '$lib/components/pipelines/list/DeploymentStatus.svelte'
-  import IconLayputPanelRight from '$assets/icons/generic/layout-panel-right.svg?component'
-  import PipelineActions from '$lib/components/pipelines/list/Actions.svelte'
   import { base } from '$app/paths'
-  import {
-    extractProgramErrors,
-    programErrorReport,
-    programErrorsPerFile
-  } from '$lib/compositions/health/systemErrors'
-  import { extractErrorMarkers, felderaCompilerMarkerSource } from '$lib/functions/pipelines/monaco'
-  import {
-    postPipelineAction,
-    type ExtendedPipeline,
-    type Pipeline,
-    type PipelineAction,
-    type PipelineThumb
-  } from '$lib/services/pipelineManager'
-  import { isPipelineIdle } from '$lib/functions/pipelines/status'
-  import { nonNull } from '$lib/functions/common/function'
-  import { useUpdatePipelineList } from '$lib/compositions/pipelines/usePipelineList.svelte'
-  import { usePipelineActionCallbacks } from '$lib/compositions/pipelines/usePipelineActionCallbacks.svelte'
-  import { useAggregatePipelineStats } from '$lib/compositions/useAggregatePipelineStats.svelte'
-  import CodeEditor from '$lib/components/pipelines/editor/CodeEditor.svelte'
-  import ProgramStatus from '$lib/components/pipelines/editor/ProgramStatus.svelte'
-  import PipelineBreadcrumbs from '$lib/components/layout/PipelineBreadcrumbs.svelte'
-  import PipelineStatus from '$lib/components/pipelines/list/PipelineStatus.svelte'
-  import TabAdHocQuery from '$lib/components/pipelines/editor/TabAdHocQuery.svelte'
-  import { useLocalStorage } from '$lib/compositions/localStore.svelte'
-  import AppHeader from '$lib/components/layout/AppHeader.svelte'
+  import { type ExtendedPipeline, type Pipeline } from '$lib/services/pipelineManager'
   import { goto } from '$app/navigation'
   import { deletePipeline as _deletePipeline } from '$lib/services/pipelineManager'
   import { useGlobalDialog } from '$lib/compositions/useGlobalDialog.svelte'
   import Popup from '$lib/components/common/Popup.svelte'
   import { fade } from 'svelte/transition'
-  import { Switch } from '@skeletonlabs/skeleton-svelte'
   import DeleteDialog, { deleteDialogProps } from '$lib/components/dialogs/DeleteDialog.svelte'
   import JSONDialog from '$lib/components/dialogs/JSONDialog.svelte'
   import JSONbig from 'true-json-bigint'
---
+++ web-console/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from '$app/state'
+  import AppHeader from '$lib/components/layout/AppHeader.svelte'
+</script>
+
+<div class="flex h-full w-full max-w-[3200px] flex-col place-self-center">
+  <AppHeader></AppHeader>
+
+  <div class="pt:16 p-8 text-center sm:pt-32">
+    <div
+      class="h1 inline-block bg-gradient-to-br from-fuchsia-300 via-amber-50 to-orange-300 bg-clip-text text-[160px] text-transparent sm:text-[300px] dark:from-fuchsia-700 dark:via-amber-950 dark:to-orange-700"
+    >
+      {page.status}
+    </div>
+    <div class="h3">
+      {page.error?.message === 'Not Found'
+        ? 'This page is not a part of your Feldera instance'
+        : page.error?.message}
+    </div>
+  </div>
+</div>
