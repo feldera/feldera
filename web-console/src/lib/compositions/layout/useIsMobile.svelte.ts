682e08a2f022447cb21aec7cc7855b6b555f4104
Karakatiza666 <bulakh.96@gmail.com>
2024-12-18 13:59:53 +0000

    [WebConsole] In pipeline edit layout header, keep pipeline status inline with the name for as small width as possible
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
+++ web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
@@ -2,19 +2,19 @@ import { listen } from 'svelte-mq-store'
 import { MediaQuery, Store } from 'runed'
 
 export const useIsTablet = () => {
-  return new Store(listen('(max-width: 1280px)'))
+  return new Store(listen('not (min-width: 1280px)'))
   // const isTablet = new MediaQuery('(max-width: 1280px)')
 }
 
 export const useIsMobile = () => {
-  return new Store(listen('(max-width: 640px)'))
+  return new Store(listen('not (min-width: 640px)'))
   // const isMobile = new MediaQuery('(max-width: 640px)')
 }
 
 export const useIsScreenMd = () => {
-  return new Store(listen('(max-width: 768px)'))
+  return new Store(listen('not (min-width: 768px)'))
 }
 
 export const useIsScreenLg = () => {
-  return new Store(listen('(max-width: 1024px)'))
+  return new Store(listen('not (min-width: 1024px)'))
 }
