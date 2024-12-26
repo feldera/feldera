423092ca3197170f1c4130f16ce3854559f7ee47
rivudhk <rivudhkr@gmail.com>
2024-12-26 23:28:21 +0545

    py: add tests returning empty for ROW and MAP types
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/test_empty_set.py
+++ python/tests/aggregate_tests/test_empty_set.py
@@ -1,6 +1,7 @@
 from tests.aggregate_tests.aggtst_base import TstView
 
 
+# Array
 class aggtst_count_emp_test(TstView):
     def __init__(self):
         self.data = [{"count": 0}]
@@ -78,6 +79,158 @@ class aggtst_arr_agg_emp_test(TstView):
                       WHERE FALSE"""
 
 
+# ROW
+class aggtst_count_col_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"count_col": 0}]
+        self.sql = """CREATE MATERIALIZED VIEW count_col_emp_row AS SELECT
+                      COUNT(DISTINCT ROW(c1, c2, c3)) AS count_col
+                      FROM row_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_max_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"max": None}]
+        self.sql = """CREATE MATERIALIZED VIEW max_emp_row AS SELECT
+                      MAX(ROW(c1, c2, c3)) FILTER(WHERE c1 IS NULL) AS max
+                      FROM row_tbl"""
+
+
+class aggtst_min_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"min": None}]
+        self.sql = """CREATE MATERIALIZED VIEW min_emp_row AS SELECT
+                      MIN(ROW(c1, c2, c3)) AS min
+                      FROM row_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_argmin_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"argmin": None}]
+        self.sql = """CREATE MATERIALIZED VIEW argmin_emp_test_row AS SELECT
+                      ARG_MIN(ROW(c1, c2, c3), c2) AS argmin
+                      FROM row_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_argmax_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"argmax": None}]
+        self.sql = """CREATE MATERIALIZED VIEW argmax_emp_test_row AS SELECT
+                      ARG_MAX(ROW(c1, c2, c3), c2) AS argmax
+                      FROM row_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_arrgg_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"arragg": []}]
+        self.sql = """CREATE MATERIALIZED VIEW arrgg_emp_test_row AS SELECT
+                      ARRAY_AGG(ROW(c2, c3)) AS arragg
+                      FROM row_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_some_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"sme": None}]
+        self.sql = """CREATE MATERIALIZED VIEW some_emp_row AS SELECT
+                      SOME(c1 > c2) AS sme
+                      FROM row_tbl"""
+
+
+class aggtst_every_emp_test_row(TstView):
+    def __init__(self):
+        self.data = [{"evry": None}]
+        self.sql = """CREATE MATERIALIZED VIEW every_emp_row AS SELECT
+                      EVERY(c1 > c2) AS evry
+                      FROM row_tbl"""
+
+
+# Map
+class aggtst_count_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"count": 0}]
+        self.sql = """CREATE MATERIALIZED VIEW count_emp_map AS SELECT
+                      COUNT (*) AS count
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_count_col_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"count_col": 0}]
+        self.sql = """CREATE MATERIALIZED VIEW count_col_emp_map AS SELECT
+                      COUNT(c1) AS count_col
+                      FROM map_tbl
+                      WHERE c1 IS NULL"""
+
+
+class aggtst_max_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"max": None}]
+        self.sql = """CREATE MATERIALIZED VIEW max_emp_map AS SELECT
+                      MAX(c1) FILTER(WHERE c1 IS NULL) AS max
+                      FROM map_tbl"""
+
+
+class aggtst_min_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"min": None}]
+        self.sql = """CREATE MATERIALIZED VIEW min_emp_map AS SELECT
+                      MIN(c1) AS min
+                      FROM map_tbl
+                      WHERE c1 = c2"""
+
+
+class aggtst_argmin_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"argmin": None}]
+        self.sql = """CREATE MATERIALIZED VIEW argmin_emp_test_map AS SELECT
+                      ARG_MIN(c1, c2) AS argmin
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_argmax_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"argmax": None}]
+        self.sql = """CREATE MATERIALIZED VIEW argmax_emp_test_map AS SELECT
+                      ARG_MAX(c2, c1) AS argmax
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_arrgg_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"arragg": []}]
+        self.sql = """CREATE MATERIALIZED VIEW arrgg_emp_test_map AS SELECT
+                      ARRAY_AGG(c1) AS arragg
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_some_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"sme": None}]
+        self.sql = """CREATE MATERIALIZED VIEW some_emp_map AS SELECT
+                      SOME(c1 > c2) AS sme
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+class aggtst_every_emp_test_map(TstView):
+    def __init__(self):
+        self.data = [{"evry": None}]
+        self.sql = """CREATE MATERIALIZED VIEW every_emp_map AS SELECT
+                      EVERY(c1 > c2) AS evry
+                      FROM map_tbl
+                      WHERE FALSE"""
+
+
+# Int
 class aggtst_avg_emp_test(TstView):
     def __init__(self):
         self.data = [{"c1_avg": None, "c2_avg": None}]
