023ebc2a403fd67ca969d5521012296d57d5b8b9
Karakatiza666 <bulakh.96@gmail.com>
2025-01-07 11:41:15 +0000

    [WebConsole] Add 'book a demo' to pipeline edit page
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/components/other/BookADemo.svelte
+++ web-console/src/lib/components/other/BookADemo.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-  let { class: _class }: { class?: string } = $props()
+  import type { Snippet } from 'svelte'
+
+  let {
+    class: _class,
+    children,
+    icon = defaultIcon
+  }: { class?: string; children?: Snippet; icon?: Snippet } = $props()
 </script>
 
+{#snippet defaultIcon()}
+  <span class="fd fd-rocket text-[20px]"></span>
+{/snippet}
+
 <a
-  class="btn preset-filled-primary-500 {_class}"
+  class="btn {_class}"
   href="https://calendly.com/d/cqnj-p63-mbq/feldera-demo"
   target="_blank"
   rel="noreferrer"
 >
-  <span class="fd fd-message-circle-question text-[20px]"></span>
-  Book a demo
+  {@render icon()}
+  {@render children?.()}
 </a>
