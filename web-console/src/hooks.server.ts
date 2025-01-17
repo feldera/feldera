0aa62ffd0c0042cb7d0ce118ed0d2fcd3fe90440
Karakatiza666 <bulakh.96@gmail.com>
2025-01-17 18:24:28 +0000

    [WebConsole] Improve font preloading
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/hooks.server.ts
+++ web-console/src/hooks.server.ts
@@ -4,6 +4,8 @@ import { svelteAttr } from 'svelte-attr'
 import '$lib/compositions/setupHttpClient'
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const response = resolve(event)
+  const response = resolve(event, {
+    preload: ({ type }) => type === 'js' || type === 'css' || type === 'font'
+  })
   return svelteAttr(response)
 }
