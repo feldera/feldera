7f37fcc9680d73358b9c443517108eb8fb7b7e1e
Mihai Budiu <mbudiu@feldera.com>
2025-01-20 14:24:11 -0800

    [SQL] Use 'cargo check' instead of 'cargo test' for tests that have no data
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/TestListener.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/TestListener.java
@@ -1,13 +1,24 @@
 package org.dbsp.sqlCompiler.compiler;
 
 import org.dbsp.sqlCompiler.compiler.sql.tools.BaseSQLTests;
+import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.RunListener;
 
 /** Invoked when all tests are finished. */
 public class TestListener extends RunListener {
+    long startTime = 0;
+
+    @Override
+    public void testRunStarted(Description description) {
+        this.startTime = System.currentTimeMillis();
+    }
+
     @Override
     public void testRunFinished(Result result) {
-        System.out.println("Executed " + BaseSQLTests.testsExecuted + " Rust tests");
+        long end = System.currentTimeMillis();
+        System.out.println("Executed " + BaseSQLTests.testsExecuted +
+                ", checked " + BaseSQLTests.testsChecked + " Rust tests in " +
+                (end - this.startTime)/1000 + "s");
     }
 }
