ba2d18d6d6412df907fe07597109241b5d8c173a
Karakatiza666 <bulakh.96@gmail.com>
2025-01-23 13:34:18 +0000

    [WebConsole] Fix stderr not displayed on some system errors
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/functions/common/array.ts
+++ web-console/src/lib/functions/common/array.ts
@@ -1,3 +1,5 @@
+import { nonNull } from '$lib/functions/common/function'
+
 /**
  * Group elements into two based on a binary predicate
  * @param arr
@@ -292,3 +294,5 @@ export function binarySearchMax(arr: number[], target: number): number {
 
   return result
 }
+
+export const singleton = <T>(value: T | null | undefined) => (nonNull(value) ? [value] : [])
