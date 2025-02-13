9be1d38d93674de6f36dc37b5b6248eb926bcadc
Karakatiza666 <bulakh.96@gmail.com>
2025-02-13 12:41:54 +0000

    [WebConsole] display total records statistics nicer
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

---
+++ web-console/src/lib/compositions/common/useInterval.svelte.ts
@@ -0,0 +1,19 @@
+export const useInterval = <T>(f: () => T, durationMs: number, offsetMs?: number) => {
+  let state = $state(f())
+  let interval: Timer
+  $effect.pre(() => {
+    if (offsetMs) {
+      setTimeout(() => (interval = setInterval(() => (state = f()), durationMs)), offsetMs)
+    } else {
+      interval = setInterval(() => (state = f()), durationMs)
+    }
+    return () => {
+      clearInterval(interval)
+    }
+  })
+  return {
+    get current() {
+      return state
+    }
+  }
+}
