cc5987d9ee27d0918c9c9080e58869d72ee34df3
Lalith Suresh <lalith@feldera.com>
2025-02-14 12:44:21 -0800

    Revert "[WebConsole] Avoid occasional Invalid Memory Access error in CI builds by pre-building icons"
    This reverts commit ba46ebe6c4513d29c1fe220e7807ef2b9cec411f.

--- web-console/vite.config.ts
+++ web-console/vite.config.ts
@@ -3,9 +3,31 @@ import { defineConfig, type UserConfig } from 'vite'
 import svg from '@poppanator/sveltekit-svg'
 import { sveltekit } from '@sveltejs/kit/vite'
 
+import viteSvgToWebfont from 'vite-svg-2-webfont'
+import { resolve } from 'path'
+
 export default defineConfig(async () => {
   return {
-    plugins: [sveltekit(), svg()],
+    plugins: [
+      sveltekit(),
+      svg(),
+      viteSvgToWebfont({
+        context: resolve(__dirname, 'src/assets/icons/feldera-material-icons'),
+        fontName: 'FelderaMaterialIconsFont',
+        baseSelector: '.fd',
+        classPrefix: 'fd-',
+        moduleId: 'feldera-material-icons-webfont.css',
+        cssFontsUrl: '/'
+      }),
+      viteSvgToWebfont({
+        context: resolve(__dirname, 'src/assets/icons/generic'),
+        fontName: 'FelderaGenericIconsFont',
+        baseSelector: '.gc',
+        classPrefix: 'gc-',
+        moduleId: 'generic-icons-webfont.css',
+        cssFontsUrl: '/'
+      })
+    ],
     build: {
       minify: 'esbuild'
     },
