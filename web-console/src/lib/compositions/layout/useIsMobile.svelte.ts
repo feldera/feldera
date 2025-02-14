d7d317686507f57bf05d700862d6a20d35595aec
Lalith Suresh <lalith@feldera.com>
2025-02-14 12:44:22 -0800

    Revert "[WebConsole] Downgrade Bun version to fix CI build getting stuck"
    This reverts commit 6ab0abd75315034cbecf426bd5aa0abfbbcf1a17.

--- web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
+++ web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
@@ -1,21 +1,24 @@
-import { MediaQuery } from 'svelte/reactivity'
+import { listen } from 'svelte-mq-store'
+import { MediaQuery, Store } from 'runed'
 
 export const useIsTablet = () => {
-  return new MediaQuery('not (min-width: 1280px)')
+  return new Store(listen('not (min-width: 1280px)'))
+  // const isTablet = new MediaQuery('(max-width: 1280px)')
 }
 
 export const useIsMobile = () => {
-  return new MediaQuery('not (min-width: 640px)')
+  return new Store(listen('not (min-width: 640px)'))
+  // const isMobile = new MediaQuery('(max-width: 640px)')
 }
 
 export const useIsScreenMd = () => {
-  return new MediaQuery('not (min-width: 768px)')
+  return new Store(listen('not (min-width: 768px)'))
 }
 
 export const useIsScreenLg = () => {
-  return new MediaQuery('not (min-width: 1024px)')
+  return new Store(listen('not (min-width: 1024px)'))
 }
 
 export const useIsScreenXl = () => {
-  return new MediaQuery('not (min-width: 1280px)')
+  return new Store(listen('not (min-width: 1280px)'))
 }
