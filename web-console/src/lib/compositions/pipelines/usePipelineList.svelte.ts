5364d62ef575f47154aa0b35a1efa3a7de0de579
Lalith Suresh <lalith@feldera.com>
2025-02-14 12:44:25 -0800

    Revert "[WebConsole] Propagate most client-side errors as toast notifications"
    This reverts commit 484abe95e501e99da4bc046ce20829ea14e89d39.

--- web-console/src/lib/compositions/pipelines/usePipelineList.svelte.ts
+++ web-console/src/lib/compositions/pipelines/usePipelineList.svelte.ts
@@ -1,6 +1,5 @@
 import { getPipelines, type PipelineThumb } from '$lib/services/pipelineManager'
 import { onMount } from 'svelte'
-import { useToast } from '$lib/compositions/useToastNotification'
 
 let pipelines = $state<PipelineThumb[] | undefined>(undefined)
 const reload = async () => {
@@ -18,19 +17,18 @@ export const useUpdatePipelineList = () => {
 /**
  * Start calling an action in a loop with an interval while waiting for the previous invocation to resolve
  * Does not perform the action on the initial call
- * Does not handle rejection
  * @returns A function to cancel the action loop
  */
 const closedIntervalAction = (action: () => Promise<void>, periodMs: number) => {
   let timeout: NodeJS.Timeout | undefined
-  let onTimeoutReject: () => void
+  let onReject: () => void
   let t1 = Date.now()
   setTimeout(async () => {
     do {
       await new Promise((resolve, reject) => {
         const t2 = Date.now()
         timeout = setTimeout(resolve, Math.max(Math.min(periodMs - t2 + t1, periodMs), 0))
-        onTimeoutReject = reject
+        onReject = reject
       })
       t1 = Date.now()
       await action()
@@ -39,14 +37,13 @@ const closedIntervalAction = (action: () => Promise<void>, periodMs: number) => 
   return () => {
     clearTimeout(timeout)
     timeout = undefined
-    onTimeoutReject()
+    onReject()
   }
 }
 
 export const useRefreshPipelineList = () => {
-  const { toastError } = useToast()
   onMount(() => {
-    return closedIntervalAction(() => reload().catch(toastError), 2000)
+    return closedIntervalAction(reload, 2000)
   })
 }
 
