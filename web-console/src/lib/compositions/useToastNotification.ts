4e6dbcf52437fb287d5b2544bcf604ff886cf953
Karakatiza666 <bulakh.96@gmail.com>
2025-02-20 09:24:07 +0000

    Groundwork for displaying client-side errors as toast notifications
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

---
+++ web-console/src/lib/compositions/useToastNotification.ts
@@ -0,0 +1,16 @@
+import { getContext } from 'svelte'
+import { type ToastContext } from '@skeletonlabs/skeleton-svelte'
+
+export const useToast = () => {
+  const toast: ToastContext = getContext('toast')
+  return {
+    toastError(e: Error) {
+      // toast.create({
+      //   title: (e.cause as any)?.name,
+      //   description: e.message,
+      //   type: 'error',
+      //   duration: 15000
+      // })
+    }
+  }
+}
--- web-console/src/routes/(system)/(authenticated)/+error.svelte
+++ web-console/src/routes/(system)/(authenticated)/+error.svelte
@@ -8,5 +8,5 @@
 <div class="pt:16 flex flex-col gap-4 p-8 text-center sm:pt-32">
   <div class="h3 sm:h1">Unable to reach the Feldera service</div>
   <div class="h4 sm:h3">Please make sure the Feldera service is running and reload the page.</div>
-  <div class="h5">Error message: {page.error?.message}</div>
+  <div class="">Error message: {page.error?.message}</div>
 </div>
