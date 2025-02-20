b47f59c01f1bb1c7cf3c3ecb5300e0001ec10599
Karakatiza666 <bulakh.96@gmail.com>
2025-02-20 09:43:50 +0000

    [WebConsole] Update dependencies, remove stray files
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
+++ web-console/src/lib/compositions/layout/useIsMobile.svelte.ts
@@ -1,24 +1,21 @@
-import { listen } from 'svelte-mq-store'
-import { MediaQuery, Store } from 'runed'
+import { MediaQuery } from 'svelte/reactivity'
 
 export const useIsTablet = () => {
-  return new Store(listen('not (min-width: 1280px)'))
-  // const isTablet = new MediaQuery('(max-width: 1280px)')
+  return new MediaQuery('not (min-width: 1280px)')
 }
 
 export const useIsMobile = () => {
-  return new Store(listen('not (min-width: 640px)'))
-  // const isMobile = new MediaQuery('(max-width: 640px)')
+  return new MediaQuery('not (min-width: 640px)')
 }
 
 export const useIsScreenMd = () => {
-  return new Store(listen('not (min-width: 768px)'))
+  return new MediaQuery('not (min-width: 768px)')
 }
 
 export const useIsScreenLg = () => {
-  return new Store(listen('not (min-width: 1024px)'))
+  return new MediaQuery('not (min-width: 1024px)')
 }
 
 export const useIsScreenXl = () => {
-  return new Store(listen('not (min-width: 1280px)'))
+  return new MediaQuery('not (min-width: 1280px)')
 }
