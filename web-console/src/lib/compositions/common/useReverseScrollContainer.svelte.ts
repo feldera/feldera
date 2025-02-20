4e3c9a5273657eb8fd711e16b72974f1a3322e1b
Karakatiza666 <bulakh.96@gmail.com>
2025-02-20 14:55:36 +0000

    [WebConsole] Unify stick-to-bottom behavior implementations used in different components
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- web-console/src/lib/compositions/common/useReverseScrollContainer.svelte.ts
+++ web-console/src/lib/compositions/common/useReverseScrollContainer.svelte.ts
@@ -1,6 +1,15 @@
+import { Debounced } from 'runed'
 import type { Action } from 'svelte/action'
 
-export const useReverseScrollContainer = () => {
+export const useReverseScrollContainer = (
+  params:
+    | {
+        observeContentElement: (node: HTMLElement) => Element
+      }
+    | {
+        observeContentSize: () => number
+      }
+) => {
   let ref = <HTMLDivElement>undefined!
   let stickToBottom = $state(true)
 
@@ -8,14 +17,15 @@ export const useReverseScrollContainer = () => {
     if (!ref) {
       return
     }
+
     ref.scrollTo({
       top:
         ref.scrollHeight -
-        (Math.round(ref.scrollHeight - ref.clientHeight) <= 0 ? ref.clientHeight : 0)
+        (Math.round(ref.scrollHeight - ref.clientHeight) >= 0 ? ref.clientHeight : 0)
     })
   }
 
-  let lastHeight: number
+  let lastSize: number = 0
 
   const onResize = (change: 'expanded' | 'contracted') => {
     if (stickToBottom) {
@@ -23,47 +33,71 @@ export const useReverseScrollContainer = () => {
     }
   }
 
-  const resizeObserver = new ResizeObserver((entries) => {
-    const entry = entries[0]
-    if (!entry) {
-      return
-    }
-    const newHeight = entry.contentRect.height
-    if (newHeight !== lastHeight) {
-      onResize(newHeight > lastHeight ? 'expanded' : 'contracted')
-      lastHeight = newHeight
-    }
-  })
-
   const onscroll = () => {
     if (!ref || !(ref instanceof HTMLDivElement)) {
       return
     }
-
     stickToBottom = Math.round(ref.scrollTop - ref.scrollHeight + ref.clientHeight + 1) >= 0 // + 1 pixel is added to account for imprecision between calculated .scrollTop and .scrollHeight
   }
 
+  const debouncedStickToBottom = new Debounced(() => stickToBottom, 50)
+
   return {
-    action: ((_node, props?: { getContainerElement?: (node: HTMLElement) => HTMLElement }) => {
+    action: ((
+      _node,
+      props?: {
+        getContainerElement?: (node: HTMLElement) => HTMLElement
+      }
+    ) => {
       const node = props?.getContainerElement?.(_node) ?? _node
-      lastHeight = node.offsetHeight
+      lastSize = node.offsetHeight
       if (!(node instanceof HTMLDivElement)) {
         return
       }
       ref = node
-      resizeObserver.observe(node.firstElementChild!)
-      node.addEventListener('scroll', onscroll)
+
+      const resizeObserver =
+        'observeContentElement' in params
+          ? (() => {
+              const observer = new ResizeObserver((entries) => {
+                const entry = entries[0]
+                if (!entry) {
+                  return
+                }
+                const newHeight = entry.contentRect.height
+                if (newHeight !== lastSize) {
+                  onResize(newHeight > lastSize ? 'expanded' : 'contracted')
+                  lastSize = newHeight
+                }
+              })
+              observer.observe(params.observeContentElement(ref))
+              return observer
+            })()
+          : undefined
+
+      if ('observeContentSize' in params) {
+        $effect(() => {
+          const newSize = params.observeContentSize()
+          if (newSize !== lastSize) {
+            onResize(newSize > lastSize ? 'expanded' : 'contracted')
+            lastSize = newSize
+            onscroll()
+          }
+        })
+      }
+
+      ref.addEventListener('scroll', onscroll)
       scrollToBottom()
       return {
         destroy: () => {
-          resizeObserver.disconnect()
-          node.removeEventListener('scroll', onscroll)
+          resizeObserver?.disconnect()
+          ref.removeEventListener('scroll', onscroll)
         }
       }
-    }) satisfies Action,
+    }) satisfies Action<HTMLDivElement>,
     scrollToBottom,
     get stickToBottom() {
-      return stickToBottom
+      return debouncedStickToBottom.current
     },
     set stickToBottom(value: boolean) {
       stickToBottom = value
