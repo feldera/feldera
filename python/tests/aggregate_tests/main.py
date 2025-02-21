d825f08c8ce961da5c025fc5bda100959b2622d7
rivudhk <rivudhkr@gmail.com>
2025-02-22 02:38:44 +0545

    py: modify complex type tests
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/aggregate_tests/main.py
+++ python/tests/aggregate_tests/main.py
@@ -1,13 +1,7 @@
-import sys
-import inspect
-from types import ModuleType
-
-from tests.aggregate_tests.aggtst_base import DEBUG, TstAccumulator
-
-######################
 ## Add here import statements for all files with tests
 
 from tests.aggregate_tests.aggtst_base import *  # noqa: F403
+from tests.aggregate_tests.atest_loader import *  # noqa: F403
 from tests.aggregate_tests.test_array import *  # noqa: F403
 from tests.aggregate_tests.test_arg_max import *  # noqa: F403
 from tests.aggregate_tests.test_arg_min import *  # noqa: F403
@@ -164,38 +158,8 @@ from tests.aggregate_tests.test_float_tbl import *  # noqa: F403
 from tests.aggregate_tests.test_uuid import *  # noqa: F403
 
 
-def register_tests_in_module(module, ta: TstAccumulator):
-    """Registers all the tests in the specified module.
-    Tests are classes that start with aggtst_.
-    (As a consequence, a test may be disabled by renaming it
-    not to start with 'aggtst_'.)
-    They must all derive from TstView or TstTable"""
-    for name, obj in inspect.getmembers(module):
-        if name.startswith("aggtst_"):
-            if inspect.isclass(obj):
-                cls = getattr(module, name)
-                instance = cls()
-                instance.register(ta)
-                if DEBUG:
-                    print(f"Registering {name}")
-
-
-def run():
-    """Find all tests loaded by the current module and register them"""
-    ta = TstAccumulator()
-    loaded = []
-    for key, module in sys.modules.items():
-        if isinstance(module, ModuleType):
-            if not module.__name__.startswith("tests.aggregate_tests"):
-                continue
-            loaded.append(module)
-    for module in loaded:
-        register_tests_in_module(module, ta)
-    ta.run_tests()
-
-
 def main():
-    run()
+    run("aggtst_", "aggregate_tests")
 
 
 if __name__ == "__main__":
--- python/tests/arithmetic_tests/main.py
+++ python/tests/arithmetic_tests/main.py
@@ -1,13 +1,7 @@
-import sys
-import inspect
-from types import ModuleType
-
-from tests.aggregate_tests.aggtst_base import DEBUG, TstAccumulator
-
-######################
 ## Add here import statements for all files with tests
 
 from tests.aggregate_tests.aggtst_base import *  # noqa: F403
+from tests.aggregate_tests.atest_loader import *  # noqa: F403
 from tests.arithmetic_tests.test_tables import *  # noqa: F403
 from tests.arithmetic_tests.test_date import *  # noqa: F403
 from tests.arithmetic_tests.test_interval import *  # noqa: F403
@@ -25,38 +19,8 @@ from tests.arithmetic_tests.test_timestamp import *  # noqa: F403
 # mts = MINUTE to SECOND, mts_str = MINUTE to SECOND converted to string
 
 
-def register_tests_in_module(module, ta: TstAccumulator):
-    """Registers all the tests in the specified module.
-    Tests are classes that start with arithtst_.
-    (As a consequence, a test may be disabled by renaming it
-    not to start with 'arithtst_'.)
-    They must all derive from TstView or TstTable"""
-    for name, obj in inspect.getmembers(module):
-        if name.startswith("arithtst_"):
-            if inspect.isclass(obj):
-                cls = getattr(module, name)
-                instance = cls()
-                instance.register(ta)
-                if DEBUG:
-                    print(f"Registering {name}")
-
-
-def run():
-    """Find all tests loaded by the current module and register them"""
-    ta = TstAccumulator()
-    loaded = []
-    for key, module in sys.modules.items():
-        if isinstance(module, ModuleType):
-            if not module.__name__.startswith("tests.arithmetic_tests"):
-                continue
-            loaded.append(module)
-    for module in loaded:
-        register_tests_in_module(module, ta)
-    ta.run_tests()
-
-
 def main():
-    run()
+    run("arithtst_", "arithmetic_tests")
 
 
 if __name__ == "__main__":
