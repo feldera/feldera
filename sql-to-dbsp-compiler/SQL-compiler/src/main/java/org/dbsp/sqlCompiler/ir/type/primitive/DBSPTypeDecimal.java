fbe1b0f950ae85d959c59dbdc333c77ad9e47e46
Mihai Budiu <mbudiu@feldera.com>
2025-02-27 15:23:09 -0800

    [SQL] Max scale for decimal is 28
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/type/primitive/DBSPTypeDecimal.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/type/primitive/DBSPTypeDecimal.java
@@ -45,7 +45,7 @@ import java.util.Objects;
 public class DBSPTypeDecimal extends DBSPTypeBaseType
         implements IsNumericType {
     public static final int MAX_PRECISION = 28;   // Total digits. This limit comes from the Rust decimal library
-    public static final int MAX_SCALE = 10;       // Digits after decimal period.
+    public static final int MAX_SCALE = MAX_PRECISION; // Digits after decimal period.
 
     public final int precision;
     public final int scale;
