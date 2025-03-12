b2c86793d4bcff8852ed7c95d99d067db89648f8
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2025-03-13 02:48:28 +0545

    [docs] usecase: Accelerating Batch Analytics Part 3
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -95,7 +95,8 @@ const guides = {
             },
             items: [
                 'use_cases/batch/part1',
-                'use_cases/batch/part2'
+                'use_cases/batch/part2',
+                'use_cases/batch/part3'
             ]
         },
         {
