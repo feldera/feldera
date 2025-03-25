446ffb5dd8602b8a535516b427566fb3b5457b9b
Leonid Ryzhyk <leonid@feldera.com>
2025-03-25 12:27:11 -0700

    [adapters] Improve deserialize_table_record.
    The `deserialize_table_record` macro generated Rust code that compiled
    very slowly. This commit improves compilation times significantly by
    breaking the code up into smaller functions and shoving dynamic dispatch
    down compiler's throat.
    
    * Invoke `deserialize` for individual fields via dynamic dispatch, thus
      preventing these functions from getting inlined.
    
    * Convert generared `visit_map` and `visit_seq` into loops.
    
    * Use a static HashMap for field name lookups. This can potentially
      speed up the parsing of records with many fields, but I haven't run
      any benchmarks.
    
    I've been testing this on a program that consists of ~10 tables with ~90
    columns each. It reduces the number of generated LLVM code by ~30% and
    compilation time by approximately 2.5x (which shows that the compiler
    doesn't like large functions, so in the future we may want to focus our
    optimizations on those).
    
    TODOs:
    * We can do the same for serialization functions, which are less
      terrible that deserialization, but can still benefit from the same
      optimizations.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/feldera-types/src/serde_with_context/mod.rs
+++ crates/feldera-types/src/serde_with_context/mod.rs
@@ -2,6 +2,10 @@ pub mod deserialize;
 pub mod serde_config;
 pub mod serialize;
 
-pub use deserialize::{DeserializationContext, DeserializeWithContext, FieldParseError};
+pub use deserialize::{
+    field_parse_error, invalid_length_error, missing_field_error, DeserializationContext,
+    DeserializeWithContext, ErasedDeserializationContext, ErasedDeserializeWithContext,
+    FieldParseError,
+};
 pub use serde_config::{DateFormat, SqlSerdeConfig, TimeFormat, TimestampFormat};
 pub use serialize::{SerializationContext, SerializeWithContext};
