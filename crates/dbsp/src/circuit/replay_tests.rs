44748e1ca41b134ec10cc26e8e80c616d0bfda29
Leonid Ryzhyk <leonid@feldera.com>
2025-04-01 13:39:53 -0700

    wip

--- crates/dbsp/src/circuit/replay_tests.rs
+++ crates/dbsp/src/circuit/replay_tests.rs
@@ -1,9 +1,11 @@
+use feldera_types::config::StorageConfig;
+
 use crate::{
     utils::Tup2, DBData, OrdZSet, OutputHandle, RootCircuit, Runtime, ZSetHandle, ZWeight,
 };
-use std::{marker::PhantomData, sync::Arc};
+use std::{fmt::Debug, marker::PhantomData, sync::Arc};
 
-use super::{dbsp_handle::Mode, CircuitConfig};
+use super::{dbsp_handle::Mode, CircuitConfig, CircuitStorageConfig};
 
 trait TestDataType {
     type InputHandles: Send + 'static;
@@ -110,80 +112,162 @@ fn test_replay<I1, I2, I3, O1, O2, O3>(
     O2: TestDataType,
     O3: TestDataType,
 {
-    let circuit_config = CircuitConfig::with_workers(4).with_mode(Mode::Persistent);
+    let mut circuit_config = CircuitConfig::with_workers(4).with_mode(Mode::Persistent);
+    let path = tempfile::tempdir().unwrap().into_path();
 
-    // Create both reference circuits, feed data1 to circuit1; feed data1 and data2 to circuit2.
+    circuit_config.storage = Some(CircuitStorageConfig {
+        config: StorageConfig {
+            path: path.to_string_lossy().into_owned(),
+            cache: Default::default(),
+        },
+        options: Default::default(),
+        init_checkpoint: None,
+    });
 
-    let circuit_constructor1_clone = circuit_constructor1.clone();
-    let (mut circuit, (input_handles1, input_handles2, output_handles1, output_handles2)) =
-        Runtime::init_circuit(&circuit_config, move |circuit| {
-            Ok(circuit_constructor1_clone(circuit))
-        })
-        .unwrap();
-
+    // Create both reference circuits, feed I1 and I2 to circuit1; feed I2 and I3 to circuit2.
     let mut reference_output1 = Vec::new();
     let mut reference_output2 = Vec::new();
-
-    for (data1, data2) in inputs1.iter() {
-        I1::push_inputs(data1.clone(), &input_handles1);
-        I2::push_inputs(data2.clone(), &input_handles2);
-
-        circuit.step().unwrap();
-
-        reference_output1.push(O1::read_outputs(&output_handles1));
-        reference_output2.push(O2::read_outputs(&output_handles2));
-    }
-
-    for (data2, _data3) in inputs2.iter() {
-        I2::push_inputs(data2.clone(), &input_handles2);
-
-        circuit.step().unwrap();
-
-        reference_output2.push(O2::read_outputs(&output_handles2));
-    }
-
-    let circuit_constructor2_clone = circuit_constructor2.clone();
-    let (mut circuit, (input_handles2, input_handles3, output_handles2, output_handles3)) =
-        Runtime::init_circuit(&circuit_config, move |circuit| {
-            Ok(circuit_constructor2_clone(circuit))
-        })
-        .unwrap();
-
     let mut reference_output2_2 = Vec::new();
     let mut reference_output3 = Vec::new();
 
-    for (_data1, data2) in inputs1.iter() {
-        I2::push_inputs(data2.clone(), &input_handles2);
+    {
+        let circuit_constructor1_clone = circuit_constructor1.clone();
+        let (mut circuit, (input_handles1, input_handles2, output_handles1, output_handles2)) =
+            Runtime::init_circuit(&circuit_config, move |circuit| {
+                Ok(circuit_constructor1_clone(circuit))
+            })
+            .unwrap();
 
-        circuit.step().unwrap();
+        for (data1, data2) in inputs1.iter() {
+            I1::push_inputs(data1.clone(), &input_handles1);
+            I2::push_inputs(data2.clone(), &input_handles2);
 
-        reference_output2_2.push(O2::read_outputs(&output_handles2));
+            circuit.step().unwrap();
+
+            reference_output1.push(O1::read_outputs(&output_handles1));
+            reference_output2.push(O2::read_outputs(&output_handles2));
+        }
+
+        for (data2, _data3) in inputs2.iter() {
+            I2::push_inputs(data2.clone(), &input_handles2);
+
+            circuit.step().unwrap();
+
+            reference_output2.push(O2::read_outputs(&output_handles2));
+        }
+
+        circuit.kill().unwrap();
+
+        let circuit_constructor2_clone = circuit_constructor2.clone();
+        let (mut circuit, (input_handles2, input_handles3, output_handles2, output_handles3)) =
+            Runtime::init_circuit(&circuit_config, move |circuit| {
+                Ok(circuit_constructor2_clone(circuit))
+            })
+            .unwrap();
+
+        for (_data1, data2) in inputs1.iter() {
+            I2::push_inputs(data2.clone(), &input_handles2);
+
+            circuit.step().unwrap();
+
+            reference_output2_2.push(O2::read_outputs(&output_handles2));
+        }
+
+        for (data2, data3) in inputs2.iter() {
+            I2::push_inputs(data2.clone(), &input_handles2);
+            I3::push_inputs(data3.clone(), &input_handles3);
+
+            circuit.step().unwrap();
+
+            reference_output2_2.push(O2::read_outputs(&output_handles2));
+            reference_output3.push(O3::read_outputs(&output_handles3));
+        }
+
+        circuit.kill().unwrap();
+
+        // The common part of the two circuits must return identical results.
+        assert_eq!(reference_output2, reference_output2_2);
     }
 
-    for (data2, data3) in inputs2.iter() {
-        I2::push_inputs(data2.clone(), &input_handles2);
-        I3::push_inputs(data3.clone(), &input_handles3);
+    let mut actual_output1 = Vec::new();
+    let mut actual_output2 = Vec::new();
+    let mut actual_output3 = Vec::new();
 
-        circuit.step().unwrap();
+    let checkpoint = {
+        // Create the first circuit.
+        let circuit_constructor1_clone = circuit_constructor1.clone();
 
-        reference_output2_2.push(O2::read_outputs(&output_handles2));
-        reference_output3.push(O3::read_outputs(&output_handles3));
+        let (mut circuit, (input_handles1, input_handles2, output_handles1, output_handles2)) =
+            Runtime::init_circuit(&circuit_config, move |circuit| {
+                Ok(circuit_constructor1_clone(circuit))
+            })
+            .unwrap();
+
+        // Feed inputs1.
+        for (data1, data2) in inputs1.iter() {
+            I1::push_inputs(data1.clone(), &input_handles1);
+            I2::push_inputs(data2.clone(), &input_handles2);
+
+            circuit.step().unwrap();
+
+            actual_output1.push(O1::read_outputs(&output_handles1));
+            actual_output2.push(O2::read_outputs(&output_handles2));
+        }
+
+        // Checkpoint.
+        let checkpoint = circuit.commit().unwrap();
+        circuit.kill();
+        checkpoint
+    };
+
+    {
+        // Restart the second circuit from the checkpoint.
+        let mut circuit_config = circuit_config.clone();
+        circuit_config.storage.as_mut().unwrap().init_checkpoint = Some(checkpoint.uuid.clone());
+
+        let circuit_constructor2_clone = circuit_constructor2.clone();
+
+        let (mut circuit, (input_handles2, input_handles3, output_handles2, output_handles3)) =
+            Runtime::init_circuit(&circuit_config, move |circuit| {
+                Ok(circuit_constructor2_clone(circuit))
+            })
+            .unwrap();
+
+        // Feed inputs2.
+        for (data2, data3) in inputs2.iter() {
+            I2::push_inputs(data2.clone(), &input_handles2);
+            I3::push_inputs(data3.clone(), &input_handles3);
+
+            circuit.step().unwrap();
+
+            actual_output2.push(O2::read_outputs(&output_handles2));
+            actual_output3.push(O3::read_outputs(&output_handles3));
+        }
+
+        circuit.kill();
     }
 
-    // The common part of the two circuits must return identical results.
-    assert_eq!(reference_output2, reference_output2_2);
-
-    // Create the first circuit.
-
-    // Feed data1.
-
-    // Checkpoint
-
-    // Create the second circuit.
-
-    // Restart the second circuit from the checkpoint.
-
-    // Feed data2.
-
-    // Compare the outputs of both circuits.
+    // Compare the outputs.
 }
+
+// Linear circuit without integrals where the old and the new circuits are disjoint.
+// No state to checkpoint or replay.
+fn linear_circuit1(
+    circuit: &mut RootCircuit,
+) -> (ZSetHandle<u64>, (), OutputHandle<OrdZSet<u64>>, ()) {
+    let (input_stream, input_handle) = circuit.add_input_zset();
+}
+
+fn linear_circuit2(
+    circuit: &mut RootCircuit,
+) -> ((), ZSetHandle<u64>, (), OutputHandle<OrdZSet<u64>>) {
+    todo!()
+}
+
+// Linear circuit with materialized inputs.
+
+// Aggregate
+
+// Join
+
+// Recursion
