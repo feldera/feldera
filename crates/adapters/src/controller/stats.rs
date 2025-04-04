362506744ddf84030aafb5c6099a70c75502de22
Ben Pfaff <blp@feldera.com>
2025-04-04 16:24:33 -0700

    [adapters] Report memory and CPU time on Windows too.
    `psutil` doesn't support Windows but other crates do.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/adapters/src/controller/stats.rs
+++ crates/adapters/src/controller/stats.rs
@@ -35,15 +35,15 @@ use crate::PipelineState;
 use anyhow::Error as AnyError;
 use atomic::Atomic;
 use bytemuck::NoUninit;
+use cpu_time::ProcessTime;
 use crossbeam::sync::{ShardedLock, ShardedLockReadGuard, Unparker};
 use feldera_adapterlib::transport::InputReader;
 use feldera_types::config::PipelineConfig;
+use memory_stats::memory_stats;
 use metrics::{KeyName, SharedString as MetricString, Unit as MetricUnit};
 use metrics_util::{debugging::DebugValue, CompositeKey};
 use num_derive::FromPrimitive;
 use ordered_float::OrderedFloat;
-#[cfg(any(target_os = "macos", target_os = "linux"))]
-use psutil::process::{Process, ProcessError};
 use rand::{seq::index::sample, thread_rng};
 use rmpv::Value as RmpValue;
 use serde::{ser::SerializeStruct, Serialize, Serializer};
@@ -55,7 +55,6 @@ use std::{
         atomic::{AtomicBool, AtomicU64, Ordering},
         Mutex,
     },
-    time::Duration,
 };
 use tracing::{debug, error, info};
 
@@ -91,11 +90,11 @@ pub struct GlobalControllerMetrics {
 
     /// Resident set size of the pipeline process, in bytes.
     // This field is computed on-demand by calling `ControllerStatus::update`.
-    pub rss_bytes: Option<AtomicU64>,
+    pub rss_bytes: AtomicU64,
 
-    /// CPU time used by the pipeline process, in milliseconds.
+    /// CPU time used by the pipeline across all threads, in milliseconds.
     // This field is computed on-demand by calling `ControllerStatus::update`.
-    pub cpu_msecs: Option<AtomicU64>,
+    pub cpu_msecs: AtomicU64,
 
     /// Time elapsed while the pipeline is executing a step, multiplied by the
     /// number of foreground and background threads, in milliseconds.
@@ -146,18 +145,10 @@ where
 
 impl GlobalControllerMetrics {
     fn new(processed_records: u64) -> Self {
-        fn unix_only_stat() -> Option<AtomicU64> {
-            #[cfg(any(target_os = "macos", target_os = "linux"))]
-            return Some(AtomicU64::new(0));
-
-            #[cfg(not(any(target_os = "macos", target_os = "linux")))]
-            return None;
-        }
-
         Self {
             state: Atomic::new(PipelineState::Paused),
-            rss_bytes: unix_only_stat(),
-            cpu_msecs: unix_only_stat(),
+            rss_bytes: AtomicU64::new(0),
+            cpu_msecs: AtomicU64::new(0),
             runtime_elapsed_msecs: AtomicU64::new(0),
             buffered_input_records: AtomicU64::new(0),
             total_input_records: AtomicU64::new(processed_records),
@@ -187,6 +178,14 @@ impl GlobalControllerMetrics {
     pub(crate) fn processed_records(&self, num_records: u64) -> u64 {
         self.total_processed_records
             .fetch_add(num_records, Ordering::AcqRel)
+    }
+
+    pub fn rss_bytes(&self) -> u64 {
+        self.rss_bytes.load(Ordering::Relaxed)
+    }
+
+    pub fn cpu_msecs(&self) -> u64 {
+        self.cpu_msecs.load(Ordering::Relaxed)
     }
 
     pub fn num_buffered_input_records(&self) -> u64 {
@@ -810,14 +809,6 @@ impl ControllerStatus {
         true
     }
 
-    /// Returns this process's memory size in bytes and its total CPU
-    /// consumption.
-    #[cfg(any(target_os = "macos", target_os = "linux"))]
-    fn process_stats() -> Result<(u64, Duration), ProcessError> {
-        let process = Process::current()?;
-        Ok((process.memory_info()?.rss(), process.cpu_times()?.busy()))
-    }
-
     pub fn update(&self, can_suspend: CanSuspend) {
         self.global_metrics
             .pipeline_complete
@@ -827,24 +818,22 @@ impl ControllerStatus {
             .can_suspend
             .store(can_suspend, Ordering::Release);
 
-        #[cfg(any(target_os = "macos", target_os = "linux"))]
-        {
-            match Self::process_stats() {
-                Ok((rss, cpu)) => {
-                    self.global_metrics
-                        .rss_bytes
-                        .as_ref()
-                        .unwrap()
-                        .store(rss, Ordering::Release);
-                    self.global_metrics
-                        .cpu_msecs
-                        .as_ref()
-                        .unwrap()
-                        .store(cpu.as_millis() as u64, Ordering::Release);
-                }
-                Err(e) => {
-                    error!("Failed to fetch RSS or CPU time of the process: {e}");
-                }
+        if let Some(usage) = memory_stats() {
+            self.global_metrics
+                .rss_bytes
+                .store(usage.physical_mem as u64, Ordering::Relaxed);
+        } else {
+            error!("Failed to fetch process RSS");
+        }
+
+        match ProcessTime::try_now() {
+            Ok(time) => {
+                self.global_metrics
+                    .cpu_msecs
+                    .store(time.as_duration().as_millis() as u64, Ordering::Relaxed);
+            }
+            Err(e) => {
+                error!("Failed to fetch process times: {e}");
             }
         }
     }
