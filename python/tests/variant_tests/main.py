af6c84bb19643811038f41ac2d13f5ef3a432f21
rivudhk <rivudhkr@gmail.com>
2025-04-23 20:17:21 +0545

    py: add more complex variant tests
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/variant_tests/main.py
+++ python/tests/variant_tests/main.py
@@ -5,6 +5,8 @@ from tests.aggregate_tests.atest_run import run  # noqa: F403
 from tests.variant_tests.dtype_and_variant import *  # noqa: F403
 from tests.variant_tests.cpmx_variant import *  # noqa: F403
 from tests.variant_tests.arr_of_cmpx_type import *  # noqa: F403
+from tests.variant_tests.row_of_cmpx_type import *  # noqa: F403
+from tests.variant_tests.udt_of_cmpx_type import *  # noqa: F403
 
 
 def main():
