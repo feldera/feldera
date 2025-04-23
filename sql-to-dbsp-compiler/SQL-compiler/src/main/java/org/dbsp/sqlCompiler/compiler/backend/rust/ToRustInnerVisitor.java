mihaibudiu/dbsp

[SQL] Implement (partially) FIRST_VALUE and LAST_VALUE window aggregation functions
Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/backend/rust/ToRustInnerVisitor.java
@@ -23,6 +23,7 @@
 
 package org.dbsp.sqlCompiler.compiler.backend.rust;
 
+import org.apache.calcite.config.NullCollation;
 import org.apache.calcite.util.TimeString;
 import org.dbsp.sqlCompiler.compiler.CompilerOptions;
 import org.dbsp.sqlCompiler.compiler.DBSPCompiler;
@@ -223,19 +224,27 @@ public class ToRustInnerVisitor extends InnerVisitor {
     /**
      * Helper function for {@link ToRustInnerVisitor#generateComparator} and
      * {@link ToRustInnerVisitor#generateCmpFunc}.
+     * @param nullable True if the field is nullable (or the tuple itself).
      * @param fieldNo  Field index that is compared.
      * @param ascending Comparison direction.
+     * @param nullCollation How nulls are compared
      */
-    void emitCompareField(int fieldNo, boolean ascending) {
-        this.builder.append("let ord = left.")
+    void emitCompareField(boolean nullable, int fieldNo, boolean ascending, NullCollation nullCollation) {
+        String name = "compare" + (nullable ? "N" : "_");
+        this.builder.append("let ord = ")
+                .append(name)
+                .append("(&left.")
                 .append(fieldNo)
-                .append(".cmp(&right.")
+                .append(", ")
+                .append("&right.")
                 .append(fieldNo)
+                .append(", ")
+                .append(ascending)
+                .append(", NullCollation::")
+                .append(Utilities.capitalize(nullCollation.toString()))
                 .append(");")
                 .newline();
         this.builder.append("if ord != Ordering::Equal { return ord");
-        if (!ascending)
-            this.builder.append(".reverse()");
         this.builder.append(" };")
                 .newline();
     }
@@ -268,11 +277,15 @@ public class ToRustInnerVisitor extends InnerVisitor {
             return;
         if (comparator.is(DBSPFieldComparatorExpression.class)) {
             DBSPFieldComparatorExpression fieldComparator = comparator.to(DBSPFieldComparatorExpression.class);
+            boolean nullable = comparator.comparedValueType()
+                    .to(DBSPTypeTupleBase.class)
+                    .getFieldType(fieldComparator.fieldNo)
+                    .mayBeNull;
             this.generateComparator(fieldComparator.source, fieldsCompared);
             if (fieldsCompared.contains(fieldComparator.fieldNo))
                 throw new InternalCompilerError("Field " + fieldComparator.fieldNo + " used twice in sorting");
             fieldsCompared.add(fieldComparator.fieldNo);
-            this.emitCompareField(fieldComparator.fieldNo, fieldComparator.ascending);
+            this.emitCompareField(nullable, fieldComparator.fieldNo, fieldComparator.ascending, fieldComparator.nullCollation);
         } else {
             DBSPDirectComparatorExpression direct = comparator.to(DBSPDirectComparatorExpression.class);
             this.generateComparator(direct.source, fieldsCompared);
@@ -284,12 +297,12 @@ public class ToRustInnerVisitor extends InnerVisitor {
     void generateCmpFunc(DBSPComparatorExpression comparator) {
         // impl CmpFunc<(String, i32, i32)> for CmpXX {
         //     fn cmp(left: &(String, i32, i32), right: &(String, i32, i32)) -> std::cmp::Ordering {
-        //         let ord = left.1.cmp(&right.1);
+        //         let ord = compare(&left.0, &right.0, true); // last value is nullsLast
         //         if ord != Ordering::Equal { return ord; }
-        //         let ord = right.2.cmp(&left.2);
+        //         let ord = compare(&right.1, &left.1, true);
         //         if ord != Ordering::Equal { return ord; }
-        //         let ord = left.3.cmp(&right.3);
-        //         if ord != Ordering::Equal { return ord; }
+        //         let ord = compare(&left.2, &right.2, true);
+        //         if ord != Ordering::Equal { return ord.reverse(); }
         //         return Ordering::Equal;
         //     }
         // }
@@ -322,7 +335,7 @@ public class ToRustInnerVisitor extends InnerVisitor {
         if (type.is(DBSPTypeTuple.class)) {
             for (int i = 0; i < type.to(DBSPTypeTuple.class).size(); i++) {
                 if (fieldsCompared.contains(i)) continue;
-                this.emitCompareField(i, true);
+                this.emitCompareField(false, i, true, SqlToRelCompiler.NULL_COLLATION);
             }
         }
         this.builder.append("return Ordering::Equal;")

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/visitors/inner/ExpressionTranslator.java
@@ -217,7 +217,8 @@ public class ExpressionTranslator extends TranslateVisitor<IDBSPInnerNode> {
     public void postorder(DBSPFieldComparatorExpression node) {
         DBSPExpression source = this.getE(node.source);
         this.map(node, new DBSPFieldComparatorExpression(
-                node.getNode(), source.to(DBSPComparatorExpression.class), node.fieldNo, node.ascending));
+                node.getNode(), source.to(DBSPComparatorExpression.class),
+                node.fieldNo, node.ascending, node.nullCollation));
     }
 
     @Override

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/expression/DBSPComparatorExpression.java
@@ -23,16 +23,16 @@
 
 package org.dbsp.sqlCompiler.ir.expression;
 
-import org.dbsp.sqlCompiler.compiler.backend.MerkleInner;
+import org.apache.calcite.config.NullCollation;
 import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
 import org.dbsp.sqlCompiler.compiler.visitors.VisitDecision;
 import org.dbsp.sqlCompiler.compiler.visitors.inner.InnerVisitor;
 import org.dbsp.sqlCompiler.ir.type.DBSPType;
-import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeComparator;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPComparatorType;
 
 /** A base class representing a comparator used for sorting. */
 public abstract class DBSPComparatorExpression extends DBSPExpression {
-    protected DBSPComparatorExpression(CalciteObject node, DBSPTypeComparator type) {
+    protected DBSPComparatorExpression(CalciteObject node, DBSPComparatorType type) {
         super(node, type);
     }
 
@@ -45,18 +45,18 @@ public abstract class DBSPComparatorExpression extends DBSPExpression {
         visitor.postorder(this);
     }
 
-    public DBSPTypeComparator getComparatorType() {
-        return this.type.to(DBSPTypeComparator.class);
+    public DBSPComparatorType getComparatorType() {
+        return this.type.to(DBSPComparatorType.class);
     }
 
     /** Type of value that is being compared. */
     public abstract DBSPType comparedValueType();
 
-    public DBSPComparatorExpression field(int index, boolean ascending) {
-        return new DBSPFieldComparatorExpression(this.getNode(), this, index, ascending);
+    public DBSPComparatorExpression field(int index, boolean ascending, NullCollation nullCollation) {
+        return new DBSPFieldComparatorExpression(this.getNode(), this, index, ascending, nullCollation);
     }
 
     public String getComparatorStructName() {
-        return this.type.to(DBSPTypeComparator.class).name;
+        return this.type.to(DBSPComparatorType.class).name;
     }
 }

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/expression/DBSPFieldComparatorExpression.java
@@ -24,6 +24,7 @@
 package org.dbsp.sqlCompiler.ir.expression;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.calcite.config.NullCollation;
 import org.dbsp.sqlCompiler.compiler.backend.JsonDecoder;
 import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
 import org.dbsp.sqlCompiler.compiler.visitors.VisitDecision;
@@ -31,7 +32,7 @@ import org.dbsp.sqlCompiler.compiler.visitors.inner.EquivalenceContext;
 import org.dbsp.sqlCompiler.ir.IDBSPInnerNode;
 import org.dbsp.sqlCompiler.compiler.visitors.inner.InnerVisitor;
 import org.dbsp.sqlCompiler.ir.type.DBSPType;
-import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeComparator;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPComparatorType;
 import org.dbsp.util.IIndentStream;
 import org.dbsp.util.Utilities;
 
@@ -41,13 +42,17 @@ import org.dbsp.util.Utilities;
 public final class DBSPFieldComparatorExpression extends DBSPComparatorExpression {
     public final DBSPComparatorExpression source;
     public final boolean ascending;
+    public final NullCollation nullCollation;
     public final int fieldNo;
 
-    public DBSPFieldComparatorExpression(CalciteObject node, DBSPComparatorExpression source, int fieldNo, boolean ascending) {
-        super(node, DBSPTypeComparator.generateType(source.getComparatorType(), fieldNo, ascending));
+    public DBSPFieldComparatorExpression(
+            CalciteObject node, DBSPComparatorExpression source, int fieldNo,
+            boolean ascending, NullCollation nullCollation) {
+        super(node, DBSPComparatorType.generateType(source.getComparatorType(), fieldNo, ascending, nullCollation));
         this.source = source;
         this.fieldNo = fieldNo;
         this.ascending = ascending;
+        this.nullCollation = nullCollation;
     }
 
     @Override
@@ -72,7 +77,8 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
             return false;
         return this.source == o.source &&
                 this.ascending == o.ascending &&
-                this.fieldNo == o.fieldNo;
+                this.fieldNo == o.fieldNo &&
+                this.nullCollation == o.nullCollation;
     }
 
     @Override
@@ -88,7 +94,7 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
     @Override
     public DBSPExpression deepCopy() {
         return this.source.deepCopy().to(DBSPComparatorExpression.class)
-                .field(this.fieldNo, this.ascending);
+                .field(this.fieldNo, this.ascending, this.nullCollation);
     }
 
     @Override
@@ -98,6 +104,7 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
             return false;
         return this.ascending == otherExpression.ascending &&
                 this.fieldNo == otherExpression.fieldNo &&
+                this.nullCollation == otherExpression.nullCollation &&
                 this.source.equivalent(context, otherExpression.source);
     }
 
@@ -106,6 +113,8 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
         DBSPComparatorExpression source = fromJsonInner(node, "source", decoder, DBSPComparatorExpression.class);
         int fieldNo = Utilities.getIntProperty(node, "fieldNo");
         boolean ascending = Utilities.getBooleanProperty(node, "ascending");
-        return new DBSPFieldComparatorExpression(CalciteObject.EMPTY, source, fieldNo, ascending);
+        NullCollation nullCollation =
+                Enum.valueOf(NullCollation.class, Utilities.getStringProperty(node, "nullCollation"));
+        return new DBSPFieldComparatorExpression(CalciteObject.EMPTY, source, fieldNo, ascending, nullCollation);
     }
 }

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/type/user/DBSPComparatorType.java
@@ -1,6 +1,7 @@
 package org.dbsp.sqlCompiler.ir.type.user;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.calcite.config.NullCollation;
 import org.dbsp.sqlCompiler.compiler.backend.JsonDecoder;
 import org.dbsp.sqlCompiler.compiler.backend.MerkleInner;
 import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
@@ -10,12 +11,10 @@ import org.dbsp.sqlCompiler.ir.type.DBSPType;
 import org.dbsp.sqlCompiler.ir.type.DBSPTypeCode;
 import org.dbsp.util.Utilities;
 
-import java.util.List;
-
-/** The type of a comparator.  In Rust it's just a struct,
+/** The type of a comparator.  In Rust, it's just a struct,
  * but which implements a CmpFun trait. */
-public class DBSPTypeComparator extends DBSPTypeUser {
-    DBSPTypeComparator(String name) {
+public class DBSPComparatorType extends DBSPTypeUser {
+    DBSPComparatorType(String name) {
         super(CalciteObject.EMPTY, DBSPTypeCode.COMPARATOR, name, false);
     }
 
@@ -26,31 +25,31 @@ public class DBSPTypeComparator extends DBSPTypeUser {
         return MerkleInner.hash(tuple.toString()).makeIdentifier("CMP");
     }
 
-    public static DBSPTypeComparator
+    public static DBSPComparatorType
     generateType(DBSPType tuple) {
-        return new DBSPTypeComparator(generateName(tuple));
+        return new DBSPComparatorType(generateName(tuple));
     }
 
     /** The name of a comparator that compares an extra field in addition to
      * another comparator.  This is the name of a
      * {@link org.dbsp.sqlCompiler.ir.expression.DBSPFieldComparatorExpression}. */
     static String generateName(
-            DBSPTypeComparator comparator,
-            int field, boolean ascending) {
-        return MerkleInner.hash(comparator.name + "." + field + " " + ascending)
+            DBSPComparatorType comparator,
+            int field, boolean ascending, NullCollation nullCollation) {
+        return MerkleInner.hash(comparator.name + "." + field + " " + ascending + " " + nullCollation)
                 .makeIdentifier("CMP");
     }
 
-    public static DBSPTypeComparator
-    generateType(DBSPTypeComparator type, int field, boolean ascending) {
-        return new DBSPTypeComparator(generateName(type, field, ascending));
+    public static DBSPComparatorType
+    generateType(DBSPComparatorType type, int field, boolean ascending, NullCollation nullCollation) {
+        return new DBSPComparatorType(generateName(type, field, ascending, nullCollation));
     }
 
     /** The name of a comparator that compares an extra field in addition to
      * another comparator.  This is the name of a
      * {@link org.dbsp.sqlCompiler.ir.expression.DBSPDirectComparatorExpression}. */
     static String generateName(
-            DBSPTypeComparator comparator,
+            DBSPComparatorType comparator,
             boolean ascending) {
         return MerkleInner.hash(comparator.name + " " + ascending)
                 .makeIdentifier("CMP");
@@ -65,15 +64,15 @@ public class DBSPTypeComparator extends DBSPTypeUser {
         visitor.postorder(this);
     }
 
-    public static DBSPTypeComparator
-    generateType(DBSPTypeComparator type, boolean ascending) {
-        return new DBSPTypeComparator(generateName(type, ascending));
+    public static DBSPComparatorType
+    generateType(DBSPComparatorType type, boolean ascending) {
+        return new DBSPComparatorType(generateName(type, ascending));
     }
 
     @SuppressWarnings("unused")
-    public static DBSPTypeComparator fromJson(JsonNode node, JsonDecoder decoder) {
+    public static DBSPComparatorType fromJson(JsonNode node, JsonDecoder decoder) {
         String name = Utilities.getStringProperty(node, "name");
         DBSPTypeCode code = DBSPTypeCode.valueOf(Utilities.getStringProperty(node, "code"));
-        return new DBSPTypeComparator(name);
+        return new DBSPComparatorType(name);
     }
 }

feldera/feldera

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/backend/rust/ToRustInnerVisitor.java
@@ -223,19 +223,27 @@ public class ToRustInnerVisitor extends InnerVisitor {
     /**
      * Helper function for {@link ToRustInnerVisitor#generateComparator} and
      * {@link ToRustInnerVisitor#generateCmpFunc}.
+     * @param nullable True if the field is nullable (or the tuple itself).
      * @param fieldNo  Field index that is compared.
      * @param ascending Comparison direction.
+     * @param nullsFirst How nulls are compared
      */
-    void emitCompareField(int fieldNo, boolean ascending) {
-        this.builder.append("let ord = left.")
+    void emitCompareField(boolean nullable, int fieldNo, boolean ascending, boolean nullsFirst) {
+        String name = "compare" + (nullable ? "N" : "_");
+        this.builder.append("let ord = ")
+                .append(name)
+                .append("(&left.")
                 .append(fieldNo)
-                .append(".cmp(&right.")
+                .append(", ")
+                .append("&right.")
                 .append(fieldNo)
+                .append(", ")
+                .append(ascending)
+                .append(", ")
+                .append(nullsFirst)
                 .append(");")
                 .newline();
         this.builder.append("if ord != Ordering::Equal { return ord");
-        if (!ascending)
-            this.builder.append(".reverse()");
         this.builder.append(" };")
                 .newline();
     }
@@ -268,11 +276,15 @@ public class ToRustInnerVisitor extends InnerVisitor {
             return;
         if (comparator.is(DBSPFieldComparatorExpression.class)) {
             DBSPFieldComparatorExpression fieldComparator = comparator.to(DBSPFieldComparatorExpression.class);
+            boolean nullable = comparator.comparedValueType()
+                    .to(DBSPTypeTupleBase.class)
+                    .getFieldType(fieldComparator.fieldNo)
+                    .mayBeNull;
             this.generateComparator(fieldComparator.source, fieldsCompared);
             if (fieldsCompared.contains(fieldComparator.fieldNo))
                 throw new InternalCompilerError("Field " + fieldComparator.fieldNo + " used twice in sorting");
             fieldsCompared.add(fieldComparator.fieldNo);
-            this.emitCompareField(fieldComparator.fieldNo, fieldComparator.ascending);
+            this.emitCompareField(nullable, fieldComparator.fieldNo, fieldComparator.ascending, fieldComparator.nullsFirst);
         } else {
             DBSPDirectComparatorExpression direct = comparator.to(DBSPDirectComparatorExpression.class);
             this.generateComparator(direct.source, fieldsCompared);
@@ -284,12 +296,12 @@ public class ToRustInnerVisitor extends InnerVisitor {
     void generateCmpFunc(DBSPComparatorExpression comparator) {
         // impl CmpFunc<(String, i32, i32)> for CmpXX {
         //     fn cmp(left: &(String, i32, i32), right: &(String, i32, i32)) -> std::cmp::Ordering {
-        //         let ord = left.1.cmp(&right.1);
+        //         let ord = compare(&left.0, &right.0, true, true); // last value is nullsLast
         //         if ord != Ordering::Equal { return ord; }
-        //         let ord = right.2.cmp(&left.2);
+        //         let ord = compare(&right.1, &left.1, true, true);
         //         if ord != Ordering::Equal { return ord; }
-        //         let ord = left.3.cmp(&right.3);
-        //         if ord != Ordering::Equal { return ord; }
+        //         let ord = compare(&left.2, &right.2, true, false);
+        //         if ord != Ordering::Equal { return ord.reverse(); }
         //         return Ordering::Equal;
         //     }
         // }
@@ -322,7 +334,7 @@ public class ToRustInnerVisitor extends InnerVisitor {
         if (type.is(DBSPTypeTuple.class)) {
             for (int i = 0; i < type.to(DBSPTypeTuple.class).size(); i++) {
                 if (fieldsCompared.contains(i)) continue;
-                this.emitCompareField(i, true);
+                this.emitCompareField(false, i, true, true);
             }
         }
         this.builder.append("return Ordering::Equal;")

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/visitors/inner/ExpressionTranslator.java
@@ -217,7 +217,8 @@ public class ExpressionTranslator extends TranslateVisitor<IDBSPInnerNode> {
     public void postorder(DBSPFieldComparatorExpression node) {
         DBSPExpression source = this.getE(node.source);
         this.map(node, new DBSPFieldComparatorExpression(
-                node.getNode(), source.to(DBSPComparatorExpression.class), node.fieldNo, node.ascending));
+                node.getNode(), source.to(DBSPComparatorExpression.class),
+                node.fieldNo, node.ascending, node.nullsFirst));
     }
 
     @Override

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/expression/DBSPComparatorExpression.java
@@ -23,16 +23,15 @@
 
 package org.dbsp.sqlCompiler.ir.expression;
 
-import org.dbsp.sqlCompiler.compiler.backend.MerkleInner;
 import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
 import org.dbsp.sqlCompiler.compiler.visitors.VisitDecision;
 import org.dbsp.sqlCompiler.compiler.visitors.inner.InnerVisitor;
 import org.dbsp.sqlCompiler.ir.type.DBSPType;
-import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeComparator;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPComparatorType;
 
 /** A base class representing a comparator used for sorting. */
 public abstract class DBSPComparatorExpression extends DBSPExpression {
-    protected DBSPComparatorExpression(CalciteObject node, DBSPTypeComparator type) {
+    protected DBSPComparatorExpression(CalciteObject node, DBSPComparatorType type) {
         super(node, type);
     }
 
@@ -45,18 +44,22 @@ public abstract class DBSPComparatorExpression extends DBSPExpression {
         visitor.postorder(this);
     }
 
-    public DBSPTypeComparator getComparatorType() {
-        return this.type.to(DBSPTypeComparator.class);
+    public DBSPComparatorType getComparatorType() {
+        return this.type.to(DBSPComparatorType.class);
     }
 
     /** Type of value that is being compared. */
     public abstract DBSPType comparedValueType();
 
+    public DBSPComparatorExpression field(int index, boolean ascending, boolean nullsFirst) {
+        return new DBSPFieldComparatorExpression(this.getNode(), this, index, ascending, nullsFirst);
+    }
+
     public DBSPComparatorExpression field(int index, boolean ascending) {
-        return new DBSPFieldComparatorExpression(this.getNode(), this, index, ascending);
+        return new DBSPFieldComparatorExpression(this.getNode(), this, index, ascending, true);
     }
 
     public String getComparatorStructName() {
-        return this.type.to(DBSPTypeComparator.class).name;
+        return this.type.to(DBSPComparatorType.class).name;
     }
 }

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/expression/DBSPFieldComparatorExpression.java
@@ -31,7 +31,7 @@ import org.dbsp.sqlCompiler.compiler.visitors.inner.EquivalenceContext;
 import org.dbsp.sqlCompiler.ir.IDBSPInnerNode;
 import org.dbsp.sqlCompiler.compiler.visitors.inner.InnerVisitor;
 import org.dbsp.sqlCompiler.ir.type.DBSPType;
-import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeComparator;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPComparatorType;
 import org.dbsp.util.IIndentStream;
 import org.dbsp.util.Utilities;
 
@@ -41,13 +41,18 @@ import org.dbsp.util.Utilities;
 public final class DBSPFieldComparatorExpression extends DBSPComparatorExpression {
     public final DBSPComparatorExpression source;
     public final boolean ascending;
+    /** This is considered after ascending */
+    public final boolean nullsFirst;
     public final int fieldNo;
 
-    public DBSPFieldComparatorExpression(CalciteObject node, DBSPComparatorExpression source, int fieldNo, boolean ascending) {
-        super(node, DBSPTypeComparator.generateType(source.getComparatorType(), fieldNo, ascending));
+    public DBSPFieldComparatorExpression(
+            CalciteObject node, DBSPComparatorExpression source, int fieldNo,
+            boolean ascending, boolean nullsFirst) {
+        super(node, DBSPComparatorType.generateType(source.getComparatorType(), fieldNo, ascending, nullsFirst));
         this.source = source;
         this.fieldNo = fieldNo;
         this.ascending = ascending;
+        this.nullsFirst = nullsFirst;
     }
 
     @Override
@@ -72,7 +77,8 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
             return false;
         return this.source == o.source &&
                 this.ascending == o.ascending &&
-                this.fieldNo == o.fieldNo;
+                this.fieldNo == o.fieldNo &&
+                this.nullsFirst == o.nullsFirst;
     }
 
     @Override
@@ -88,7 +94,7 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
     @Override
     public DBSPExpression deepCopy() {
         return this.source.deepCopy().to(DBSPComparatorExpression.class)
-                .field(this.fieldNo, this.ascending);
+                .field(this.fieldNo, this.ascending, this.nullsFirst);
     }
 
     @Override
@@ -98,6 +104,7 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
             return false;
         return this.ascending == otherExpression.ascending &&
                 this.fieldNo == otherExpression.fieldNo &&
+                this.nullsFirst == otherExpression.nullsFirst &&
                 this.source.equivalent(context, otherExpression.source);
     }
 
@@ -106,6 +113,7 @@ public final class DBSPFieldComparatorExpression extends DBSPComparatorExpressio
         DBSPComparatorExpression source = fromJsonInner(node, "source", decoder, DBSPComparatorExpression.class);
         int fieldNo = Utilities.getIntProperty(node, "fieldNo");
         boolean ascending = Utilities.getBooleanProperty(node, "ascending");
-        return new DBSPFieldComparatorExpression(CalciteObject.EMPTY, source, fieldNo, ascending);
+        boolean nullsFirst = Utilities.getBooleanProperty(node, "nullsFirst");
+        return new DBSPFieldComparatorExpression(CalciteObject.EMPTY, source, fieldNo, ascending, nullsFirst);
     }
 }

sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/ir/type/user/DBSPComparatorType.java
@@ -10,12 +10,10 @@ import org.dbsp.sqlCompiler.ir.type.DBSPType;
 import org.dbsp.sqlCompiler.ir.type.DBSPTypeCode;
 import org.dbsp.util.Utilities;
 
-import java.util.List;
-
-/** The type of a comparator.  In Rust it's just a struct,
+/** The type of a comparator.  In Rust, it's just a struct,
  * but which implements a CmpFun trait. */
-public class DBSPTypeComparator extends DBSPTypeUser {
-    DBSPTypeComparator(String name) {
+public class DBSPComparatorType extends DBSPTypeUser {
+    DBSPComparatorType(String name) {
         super(CalciteObject.EMPTY, DBSPTypeCode.COMPARATOR, name, false);
     }
 
@@ -26,31 +24,31 @@ public class DBSPTypeComparator extends DBSPTypeUser {
         return MerkleInner.hash(tuple.toString()).makeIdentifier("CMP");
     }
 
-    public static DBSPTypeComparator
+    public static DBSPComparatorType
     generateType(DBSPType tuple) {
-        return new DBSPTypeComparator(generateName(tuple));
+        return new DBSPComparatorType(generateName(tuple));
     }
 
     /** The name of a comparator that compares an extra field in addition to
      * another comparator.  This is the name of a
      * {@link org.dbsp.sqlCompiler.ir.expression.DBSPFieldComparatorExpression}. */
     static String generateName(
-            DBSPTypeComparator comparator,
-            int field, boolean ascending) {
-        return MerkleInner.hash(comparator.name + "." + field + " " + ascending)
+            DBSPComparatorType comparator,
+            int field, boolean ascending, boolean nullsFirst) {
+        return MerkleInner.hash(comparator.name + "." + field + " " + ascending + " " + nullsFirst)
                 .makeIdentifier("CMP");
     }
 
-    public static DBSPTypeComparator
-    generateType(DBSPTypeComparator type, int field, boolean ascending) {
-        return new DBSPTypeComparator(generateName(type, field, ascending));
+    public static DBSPComparatorType
+    generateType(DBSPComparatorType type, int field, boolean ascending, boolean nullsFirst) {
+        return new DBSPComparatorType(generateName(type, field, ascending, nullsFirst));
     }
 
     /** The name of a comparator that compares an extra field in addition to
      * another comparator.  This is the name of a
      * {@link org.dbsp.sqlCompiler.ir.expression.DBSPDirectComparatorExpression}. */
     static String generateName(
-            DBSPTypeComparator comparator,
+            DBSPComparatorType comparator,
             boolean ascending) {
         return MerkleInner.hash(comparator.name + " " + ascending)
                 .makeIdentifier("CMP");
@@ -65,15 +63,15 @@ public class DBSPTypeComparator extends DBSPTypeUser {
         visitor.postorder(this);
     }
 
-    public static DBSPTypeComparator
-    generateType(DBSPTypeComparator type, boolean ascending) {
-        return new DBSPTypeComparator(generateName(type, ascending));
+    public static DBSPComparatorType
+    generateType(DBSPComparatorType type, boolean ascending) {
+        return new DBSPComparatorType(generateName(type, ascending));
     }
 
     @SuppressWarnings("unused")
-    public static DBSPTypeComparator fromJson(JsonNode node, JsonDecoder decoder) {
+    public static DBSPComparatorType fromJson(JsonNode node, JsonDecoder decoder) {
         String name = Utilities.getStringProperty(node, "name");
         DBSPTypeCode code = DBSPTypeCode.valueOf(Utilities.getStringProperty(node, "code"));
-        return new DBSPTypeComparator(name);
+        return new DBSPComparatorType(name);
     }
 }