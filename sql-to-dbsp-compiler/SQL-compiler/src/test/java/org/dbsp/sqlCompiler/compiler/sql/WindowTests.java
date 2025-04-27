90705abea82be1d6107f0a36f38cb58bf05259ee
Mihai Budiu <mbudiu@feldera.com>
2025-04-27 13:16:25 -0700

    [SQL] Generate correct code for LEAD/LAG window aggregates
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/WindowTests.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/WindowTests.java
@@ -1,16 +1,110 @@
 package org.dbsp.sqlCompiler.compiler.sql;
 
 import org.dbsp.sqlCompiler.compiler.sql.quidem.ScottBaseTests;
+import org.dbsp.util.Linq;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WindowTests extends ScottBaseTests {
-    static final String[] aggregates = {
+    record PermutationTest(
+            String[] aggregates,
+            String fixedColumn,
+            String[] headers,
+            String[] fixedColumnValues,
+            String[][] result) {
+        List<String> generateAll() {
+            List<List<Integer>> permutations = generatePermutations(this.headers.length);
+            return Linq.map(permutations, this::generateTest);
+        }
+
+        String generateTest(List<Integer> permutation) {
+            Assert.assertEquals(permutation.size(), aggregates.length);
+            final String line = "+---------";
+
+            StringBuilder test = new StringBuilder();
+            test.append("SELECT ");
+            for (int i : permutation) {
+                test.append(this.aggregates[i]).append(",\n");
+            }
+            test.append(this.fixedColumn)
+                    .append(" FROM emp;\n")
+                    .append(line.repeat(6))
+                    .append("+\n");
+            for (int i : permutation) {
+                test.append("| ")
+                        .append(headers[i])
+                        .append(" ");
+            }
+            test.append("| ").append(this.fixedColumn).append(" ");
+            test.append("|\n")
+                    .append(line.repeat(6))
+                    .append("+\n");
+            int rows = this.fixedColumnValues.length;
+            for (int row = 0; row < rows; row++) {
+                for (int i : permutation) {
+                    String res = result[row][i];
+                    test.append("| ").append(res).append(" ");
+                }
+                test.append("| ")
+                        .append(this.fixedColumnValues[row])
+                        .append(" |\n");
+            }
+            test.append(line.repeat(6))
+                    .append("+\n");
+            test.append("(").append(rows).append(" rows)\n");
+            return test.toString();
+        }
+    }
+
+    record QueriesAndOutput(
+            String[] aggregates,
+            String fixedColumn,
+            String output) {
+        PermutationTest createTest() {
+            String[] rows = output.split("\n");
+            Assert.assertTrue(rows.length >= 2);
+            final int dataRows = rows.length - 2;
+            String header = rows[0];
+            String[] columns = header.substring(1).split("\\|");
+            String[] headers = new String[columns.length - 1];
+
+            Assert.assertEquals(headers.length, aggregates.length);
+            int index = 0;
+            for (String col : columns) {
+                col = col.trim();
+                if (col.equals(fixedColumn))
+                    continue;
+                headers[index] = col;
+                index++;
+            }
+
+            String[][] result = new String[dataRows][headers.length];
+            String[] fixedColumnValues = new String[dataRows];
+            for (int i = 2; i < rows.length; i++) {
+                String row = rows[i];
+                String[] values = row.substring(1).split("\\|");
+                Assert.assertEquals(columns.length, values.length);
+                index = 0;
+                for (int j = 0; j < columns.length; j++) {
+                    String value = values[j].trim();
+                    if (columns[j].trim().equals(this.fixedColumn)) {
+                        fixedColumnValues[i-2] = value;
+                    } else {
+                        result[i-2][index] = value;
+                        index++;
+                    }
+                }
+            }
+            return new PermutationTest(aggregates, fixedColumn, headers, fixedColumnValues, result);
+        }
+    }
+
+    static QueriesAndOutput q0 = new QueriesAndOutput(new String[]{
             "LEAD(sal, 1) OVER (PARTITION BY deptno ORDER BY sal DESC) AS next_highest",
             "SUM(sal) OVER (PARTITION BY deptno) AS total_by_dept",
-            "AVG(sal) OVER (PARTITION BY deptno) AS average_by_dept",
             """
                 MAX(sal) OVER (
                      PARTITION BY deptno
@@ -19,186 +113,69 @@ public class WindowTests extends ScottBaseTests {
                 ) AS max_in_range""",
             "FIRST_VALUE(sal) OVER (PARTITION BY deptno ORDER BY sal DESC " +
                     "RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS high_in_dept"
-    };
+    }, "empno", """
+            | empno | next_highest | total_by_dept | max_in_range | high_in_dept |
+            | ----- | ------------ | ------------- | -------------| ------------ |
+            | 7369  |              | 10875.00      | 800.00       | 3000.00      |
+            | 7499  | 1500.00      | 9400.00       | 1600.00      | 2850.00      |
+            | 7521  | 1250.00      | 9400.00       | 1250.00      | 2850.00      |
+            | 7566  | 1100.00      | 10875.00      | 2975.00      | 3000.00      |
+            | 7654  | 950.00       | 9400.00       | 1250.00      | 2850.00      |
+            | 7698  | 1600.00      | 9400.00       | 2850.00      | 2850.00      |
+            | 7782  | 1300.00      | 8750.00       | 2450.00      | 5000.00      |
+            | 7788  | 3000.00      | 10875.00      | 3000.00      | 3000.00      |
+            | 7839  | 2450.00      | 8750.00       | 5000.00      | 5000.00      |
+            | 7844  | 1250.00      | 9400.00       | 1500.00      | 2850.00      |
+            | 7876  | 800.00       | 10875.00      | 1100.00      | 3000.00      |
+            | 7900  |              | 9400.00       | 950.00       | 2850.00      |
+            | 7902  | 2975.00      | 10875.00      | 3000.00      | 3000.00      |
+            | 7934  |              | 8750.00       | 1300.00      | 5000.00      |""");
 
-    static final String[] headers = {
-            "next_highest",
-            "total_by_dept",
-            "average_by_dept",
-            "max_in_range",
-            "high_in_dept",
-            "empno"
-    };
-
-    /*
-        MySQL output
-        | empno | total_by_dept | average_by_dept | next_highest | max_in_range | high_in_dept |
-        | ----- | ------------- | --------------- | ------------ | -------------| ------------ |
-        | 7369  | 10875.00      | 2175.000000     |              | 800.00       | 3000.00      |
-        | 7499  | 9400.00       | 1566.666667     | 1500.00      | 1600.00      | 2850.00      |
-        | 7521  | 9400.00       | 1566.666667     | 1250.00      | 1250.00      | 2850.00      |
-        | 7566  | 10875.00      | 2175.000000     | 1100.00      | 2975.00      | 3000.00      |
-        | 7654  | 9400.00       | 1566.666667     | 950.00       | 1250.00      | 2850.00      |
-        | 7698  | 9400.00       | 1566.666667     | 1600.00      | 2850.00      | 2850.00      |
-        | 7782  | 8750.00       | 2916.666667     | 1300.00      | 2450.00      | 5000.00      |
-        | 7788  | 10875.00      | 2175.000000     | 3000.00      | 3000.00      | 3000.00      |
-        | 7839  | 8750.00       | 2916.666667     | 2450.00      | 5000.00      | 5000.00      |
-        | 7844  | 9400.00       | 1566.666667     | 1250.00      | 1500.00      | 2850.00      |
-        | 7876  | 10875.00      | 2175.000000     | 800.00       | 1100.00      | 3000.00      |
-        | 7900  | 9400.00       | 1566.666667     |              | 950.00       | 2850.00      |
-        | 7902  | 10875.00      | 2175.000000     | 2975.00      | 3000.00      | 3000.00      |
-        | 7934  | 8750.00       | 2916.666667     |              | 1300.00      | 5000.00      |
-     */
-
-    static final String[] empno = {
-            "7369",
-            "7499",
-            "7521",
-            "7566",
-            "7654",
-            "7698",
-            "7782",
-            "7788",
-            "7839",
-            "7844",
-            "7876",
-            "7900",
-            "7902",
-            "7934"
-    };
-
-    // validated on MySQL
-    static final String[][] result = {{
-        // next_highest
-            "       ",
-            "1500.00",
-            "1250.00",
-            "1100.00",
-            "950.00 ",
-            "1600.00",
-            "1300.00",
-            "3000.00",
-            "2450.00",
-            "1250.00",
-            "800.00 ",
-            "       ",
-            "2975.00",
-            "       "
-    }, {
-        // total_by_dept
-            "10875.00",
-            "9400.00 ",
-            "9400.00 ",
-            "10875.00",
-            "9400.00 ",
-            "9400.00 ",
-            "8750.00 ",
-            "10875.00",
-            "8750.00 ",
-            "9400.00 ",
-            "10875.00",
-            "9400.00 ",
-            "10875.00",
-            "8750.00 "
-    }, {
-        // average_by_dept
-            "2175.000000",
-            "1566.666667",
-            "1566.666667",
-            "2175.000000",
-            "1566.666667",
-            "1566.666667",
-            "2916.666667",
-            "2175.000000",
-            "2916.666667",
-            "1566.666667",
-            "2175.000000",
-            "1566.666667",
-            "2175.000000",
-            "2916.666667"
-    }, {
-        // max_in_range
-            "800.00 ",
-            "1600.00",
-            "1250.00",
-            "2975.00",
-            "1250.00",
-            "2850.00",
-            "2450.00",
-            "3000.00",
-            "5000.00",
-            "1500.00",
-            "1100.00",
-            "950.00 ",
-            "3000.00",
-            "1300.00"
-    }, {
-        // high_in_dept
-            "3000.00",
-            "2850.00",
-            "2850.00",
-            "3000.00",
-            "2850.00",
-            "2850.00",
-            "5000.00",
-            "3000.00",
-            "5000.00",
-            "2850.00",
-            "3000.00",
-            "2850.00",
-            "3000.00",
-            "5000.00"
-    }
-    };
+    static QueriesAndOutput q1 = new QueriesAndOutput(new String[] {
+            "SUM(sal) OVER (PARTITION BY deptno) AS total_by_dept",
+            "LEAD(sal, 1) OVER (PARTITION BY deptno ORDER BY sal DESC) AS next_highest",
+            "AVG(sal) OVER (PARTITION BY deptno) AS avg_by_dept",
+            "LAG(sal, 1) OVER (PARTITION BY deptno ORDER BY sal DESC) AS prev_highest"
+    }, "empno", """
+            | empno | total_by_dept | next_highest | avg_by_dept | prev_highest |
+            | ----- | ------------- | ------------ | ----------- | ------------ |
+            | 7369  | 10875.00      |              | 2175.000000 | 1100.00      |
+            | 7499  | 9400.00       | 1500.00      | 1566.666667 | 2850.00      |
+            | 7521  | 9400.00       | 1250.00      | 1566.666667 | 1500.00      |
+            | 7566  | 10875.00      | 1100.00      | 2175.000000 | 3000.00      |
+            | 7654  | 9400.00       | 950.00       | 1566.666667 | 1250.00      |
+            | 7698  | 9400.00       | 1600.00      | 1566.666667 |              |
+            | 7782  | 8750.00       | 1300.00      | 2916.666667 | 5000.00      |
+            | 7788  | 10875.00      | 3000.00      | 2175.000000 |              |
+            | 7839  | 8750.00       | 2450.00      | 2916.666667 |              |
+            | 7844  | 9400.00       | 1250.00      | 1566.666667 | 1600.00      |
+            | 7876  | 10875.00      | 800.00       | 2175.000000 | 2975.00      |
+            | 7900  | 9400.00       |              | 1566.666667 | 1250.00      |
+            | 7902  | 10875.00      | 2975.00      | 2175.000000 | 3000.00      |
+            | 7934  | 8750.00       |              | 2916.666667 | 2450.00      |""");
 
     @Test
-    public void testPermutations() {
-        // Generate all 32 permutations of columns
-        List<Integer> nums = List.of(0, 1, 2, 3, 4);
-        List<List<Integer>> permutations = generatePermutations(nums);
-        for (var list: permutations) {
-            // TODO: this is currently broken
-            if (list.get(0) != 0) continue;
-            String test = this.generateTest(list);
-            this.qs(test, TestOptimizations.Optimized);
+    public void testQ0() {
+        PermutationTest test = q0.createTest();
+        List<String> permutations = test.generateAll();
+        for (var t : permutations) {
+            this.qs(t, TestOptimizations.Optimized);
         }
     }
 
-    String generateTest(List<Integer> permutation) {
-        StringBuilder test = new StringBuilder();
-        test.append("SELECT ");
-        for (int i: permutation) {
-            test.append(aggregates[i]).append(",");
+    @Test
+    public void testQ1() {
+        PermutationTest test = q1.createTest();
+        List<String> permutations = test.generateAll();
+        for (var t : permutations) {
+            this.qs(t, TestOptimizations.Optimized);
         }
-        String line = "+---------";
-        test.append("empno FROM emp;\n")
-                .append(line.repeat(6))
-                .append("+\n");
-        for (int i: permutation) {
-            test.append("| ")
-                    .append(headers[i])
-                    .append(" ");
-        }
-        test.append("|\n")
-                .append(line.repeat(6))
-                .append("+\n");
-        int rows = empno.length;
-        for (int row = 0; row < rows; row++) {
-            for (int i: permutation) {
-                String res = result[i][row];
-                test.append("| ").append(res).append(" ");
-            }
-            test.append("| ")
-                    .append(empno[row])
-                    .append(" |\n");
-        }
-        test.append(line.repeat(6))
-                .append("+\n");
-        test.append("(").append(rows).append(" rows)\n");
-        return test.toString();
     }
 
-    static List<List<Integer>> generatePermutations(List<Integer> nums) {
+    static List<List<Integer>> generatePermutations(int size) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+            nums.add(i);
         List<List<Integer>> result = new ArrayList<>();
         backtrack(nums, new ArrayList<>(), result);
         return result;
