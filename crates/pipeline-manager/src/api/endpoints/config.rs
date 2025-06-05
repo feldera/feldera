5596907882f6d3e6e88f2ee96133143e7c9fcb0f
Karakatiza666 <bulakh.96@gmail.com>
2025-06-05 17:40:37 +0300

    Make LicenseInformation pipeline_manager struct public
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- crates/pipeline-manager/src/api/endpoints/config.rs
+++ crates/pipeline-manager/src/api/endpoints/config.rs
@@ -1,44 +1,11 @@
 // Configuration API to retrieve the current authentication configuration and list of demos
 use actix_web::{get, web::Data as WebData, HttpRequest, HttpResponse};
-use chrono::{DateTime, Utc};
 use serde::Serialize;
 use utoipa::ToSchema;
 
 use crate::api::main::ServerState;
 use crate::error::ManagerError;
-
-#[derive(Serialize, ToSchema)]
-#[allow(dead_code)]
-pub enum DisplaySchedule {
-    /// Display it only once: after dismissal do not show it again
-    Once,
-    /// Display it again the next session if it is dismissed
-    Session,
-    /// Display it again after a certain period of time after it is dismissed
-    Every { seconds: u64 },
-    /// Always display it, do not allow it to be dismissed
-    Always,
-}
-
-#[derive(Serialize, ToSchema)]
-pub(crate) struct LicenseInformation {
-    /// Duration until the license expires
-    pub expires_in_seconds: u64,
-    /// Timestamp at which the license expires
-    pub expires_at: DateTime<Utc>,
-    /// Whether the license is expired
-    pub is_expired: bool,
-    /// Whether the license is a trial
-    pub is_trial: bool,
-    /// Optional description of the advantages of extending the license / upgrading from a trial
-    pub description_html: String,
-    /// URL that navigates the user to extend / upgrade their license
-    pub extension_url: String,
-    /// Timestamp from which the user should be reminded of the license expiring soon
-    pub remind_starting_at: DateTime<Utc>,
-    /// Suggested frequency of reminding the user about the license expiring soon
-    pub remind_schedule: DisplaySchedule,
-}
+use crate::license::{DisplaySchedule, LicenseInformation};
 
 #[derive(Serialize, ToSchema)]
 pub(crate) struct UpdateInformation {
---
+++ crates/pipeline-manager/src/license.rs
@@ -0,0 +1,36 @@
+use chrono::{DateTime, Utc};
+use serde::{Serialize, Deserialize};
+use utoipa::ToSchema;
+
+#[derive(Serialize, Deserialize, ToSchema, Clone, PartialEq)]
+#[allow(dead_code)]
+pub enum DisplaySchedule {
+    /// Display it only once: after dismissal do not show it again
+    Once,
+    /// Display it again the next session if it is dismissed
+    Session,
+    /// Display it again after a certain period of time after it is dismissed
+    Every { seconds: u64 },
+    /// Always display it, do not allow it to be dismissed
+    Always,
+}
+
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct LicenseInformation {
+    /// Duration until the license expires
+    pub expires_in_seconds: u64,
+    /// Timestamp at which the license expires
+    pub expires_at: DateTime<Utc>,
+    /// Whether the license is expired
+    pub is_expired: bool,
+    /// Whether the license is a trial
+    pub is_trial: bool,
+    /// Optional description of the advantages of extending the license / upgrading from a trial
+    pub description_html: String,
+    /// URL that navigates the user to extend / upgrade their license
+    pub extension_url: Option<String>,
+    /// Timestamp from which the user should be reminded of the license expiring soon
+    pub remind_starting_at: DateTime<Utc>,
+    /// Suggested frequency of reminding the user about the license expiring soon
+    pub remind_schedule: DisplaySchedule,
+}
