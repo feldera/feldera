2114cac06016eda2f03f5ec2a646d5e90f6efcbc
Ben Pfaff <blp@feldera.com>
2025-06-10 10:48:39 -0700

    [dbsp] Make ListMerger able to more granularly consume fuel.
    `ListMerger` only checked that it had run out of fuel between keys.  Each
    key can have an arbitrary number of values, so this makes merging lumpy
    when there are many.  This commit fixes the problem.  It fixes one
    source of pauses due to slow merger threads.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/trace/spine_async/list_merger.rs
+++ crates/dbsp/src/trace/spine_async/list_merger.rs
@@ -74,6 +74,7 @@ where
     B: Batch,
 {
     cursors: Vec<C>,
+    any_values: bool,
     has_mut: Vec<bool>,
     tmp_weight: Box<B::R>,
     time_diffs: Option<Box<DynWeightedPairs<DynDataTyped<B::Time>, B::R>>>,
@@ -102,6 +103,7 @@ where
 
         ListMerger {
             cursors,
+            any_values: false,
             has_mut,
             tmp_weight: factories.weight_factory().default_box(),
             time_diffs,
@@ -141,9 +143,20 @@ where
                     .map(|index| (index, self.cursors[index].key())),
             );
 
+            // If we're resuming after stopping due to running out of fuel in a
+            // previous call, then we might have exhausted the values in some of
+            // the keys, so drop them.  We still need them in `orig_min_keys` so
+            // we can advance the key for all of them later.
+            let mut min_keys = if self.any_values {
+                orig_min_keys
+                    .into_iter()
+                    .filter(|index| self.cursors[*index].val_valid())
+                    .collect::<IndexSet>()
+            } else {
+                orig_min_keys
+            };
+
             // As long as there is more than one cursor with minimum keys...
-            let mut any_values = false;
-            let mut min_keys = orig_min_keys;
             while min_keys.is_long() {
                 // ...Find the indexes of the cursors with minimum values, among
                 // those with minimum keys, and copy their time-diff pairs and
@@ -153,7 +166,8 @@ where
                         .into_iter()
                         .map(|index| (index, self.cursors[index].val())),
                 );
-                any_values = self.copy_times(builder, time_map_func, min_vals, fuel) || any_values;
+                self.any_values =
+                    self.copy_times(builder, time_map_func, min_vals, fuel) || self.any_values;
 
                 // Then go on to the next value in each cursor, dropping the keys
                 // for which we've exhausted the values.
@@ -163,15 +177,21 @@ where
                         min_keys.remove(index);
                     }
                 }
+                if *fuel <= 0 {
+                    return;
+                }
             }
 
             // If there's exactly one cursor left with minimum key, copy its
             // values into the output.
             if let Some(index) = min_keys.first() {
                 loop {
-                    any_values =
-                        self.copy_times(builder, time_map_func, min_keys, fuel) || any_values;
+                    self.any_values =
+                        self.copy_times(builder, time_map_func, min_keys, fuel) || self.any_values;
                     self.cursors[index].step_val();
+                    if *fuel <= 0 {
+                        return;
+                    }
                     if !self.cursors[index].val_valid() {
                         break;
                     }
@@ -179,13 +199,14 @@ where
             }
 
             // If we wrote any values for these minimum keys, write the key.
-            if any_values {
+            if self.any_values {
                 let index = orig_min_keys.first().unwrap();
                 if self.has_mut[index] {
                     builder.push_key_mut(self.cursors[index].key_mut());
                 } else {
                     builder.push_key(self.cursors[index].key());
                 }
+                self.any_values = false;
             }
 
             // Advance each minimum-key cursor, dropping the cursors for which
@@ -202,17 +223,21 @@ where
         // directly to the output.
         if let Some(index) = remaining_cursors.first() {
             while *fuel > 0 {
-                let mut any_values = false;
                 loop {
-                    any_values = self.copy_times(builder, time_map_func, remaining_cursors, fuel)
-                        || any_values;
+                    self.any_values =
+                        self.copy_times(builder, time_map_func, remaining_cursors, fuel)
+                            || self.any_values;
                     self.cursors[index].step_val();
                     if !self.cursors[index].val_valid() {
                         break;
                     }
+                    if *fuel <= 0 {
+                        return;
+                    }
                 }
-                debug_assert!(time_map_func.is_some() || any_values, "This assertion should fail only if B::Cursor is a spine or a CursorList, but we shouldn't be merging those");
-                if any_values {
+                debug_assert!(time_map_func.is_some() || self.any_values, "This assertion should fail only if B::Cursor is a spine or a CursorList, but we shouldn't be merging those");
+                if self.any_values {
+                    self.any_values = false;
                     if self.has_mut[index] {
                         builder.push_key_mut(self.cursors[index].key_mut());
                     } else {
