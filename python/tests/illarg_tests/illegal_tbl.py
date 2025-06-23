c36a19ef68824b27f9d4855979dc4ba947b3c6f2
rivudhk <rivudhkr@gmail.com>
2025-06-23 23:57:05 +0545

    py: add legal and illegal argument tests for CONCAT function
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

---
+++ python/tests/illarg_tests/illegal_tbl.py
@@ -0,0 +1,33 @@
+from tests.aggregate_tests.aggtst_base import TstTable
+
+
+class illarg_tbl(TstTable):
+    """Define the table used by illegal argument tests"""
+
+    def __init__(self):
+        self.sql = """CREATE TABLE illegal_tbl(
+                      id INT,
+                      intt INT,
+                      decimall DECIMAL(6, 2),
+                      reall REAL,
+                      dbl DOUBLE,
+                      booll BOOL,
+                      str VARCHAR,
+                      bin BINARY,
+                      tmestmp TIMESTAMP,
+                      uuidd UUID
+                      )"""
+        self.data = [
+            {
+                "id": 0,
+                "intt": -12,
+                "decimall": -1111.52,
+                "reall": -57681.18,
+                "dbl": -38.2711234601246,
+                "booll": True,
+                "str": "hello ",
+                "bin": [11, 22, 32],
+                "tmestmp": "2020-06-21 14:23:44",
+                "uuidd": "42b8fec7-c7a3-4531-9611-4bde80f9cb4c",
+            }
+        ]
---
+++ python/tests/illarg_tests/main.py
@@ -0,0 +1,14 @@
+## Add here import statements for all files with tests
+
+from tests.aggregate_tests.aggtst_base import *  # noqa: F403
+from tests.aggregate_tests.atest_run import run  # noqa: F403
+from tests.illarg_tests.str_type_fn import *  # noqa: F403
+from tests.illarg_tests.illegal_tbl import *  # noqa: F403
+
+
+def main():
+    run("illarg_", "illarg_tests")
+
+
+if __name__ == "__main__":
+    main()
