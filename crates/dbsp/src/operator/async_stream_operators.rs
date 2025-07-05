e55ca2787db59b25d3e91af076047c0fb2a4008c
Leonid Ryzhyk <leonid@feldera.com>
2025-07-05 14:47:58 -0700

    [dbsp] PartitionedRollingAggregate.
    Reimplement PartitionedRollingAggregate as a splitter.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/dbsp/src/operator/async_stream_operators.rs
+++ crates/dbsp/src/operator/async_stream_operators.rs
@@ -3,7 +3,7 @@ use std::{borrow::Cow, marker::PhantomData, pin::Pin, rc::Rc};
 use crate::{
     circuit::{
         metadata::{OperatorLocation, OperatorMeta},
-        operator_traits::{BinaryOperator, Operator, TernaryOperator},
+        operator_traits::{BinaryOperator, Operator, QuaternaryOperator, TernaryOperator},
         GlobalNodeId,
     },
     Error, Scope,
@@ -49,10 +49,6 @@ where
 
     fn init(&mut self, global_id: &GlobalNodeId) {
         Rc::get_mut(&mut self.operator).unwrap().init(global_id);
-    }
-
-    fn metrics(&self) {
-        self.operator.metrics();
     }
 
     fn metadata(&self, meta: &mut OperatorMeta) {
@@ -158,7 +154,6 @@ where
         }
     }
 }
-
 pub trait StreamingTernaryOperator<I1, I2, I3, O>: Operator
 where
     I1: Clone,
@@ -207,10 +202,6 @@ where
 
     fn init(&mut self, global_id: &GlobalNodeId) {
         Rc::get_mut(&mut self.operator).unwrap().init(global_id);
-    }
-
-    fn metrics(&self) {
-        self.operator.metrics();
     }
 
     fn metadata(&self, meta: &mut OperatorMeta) {
@@ -308,6 +299,172 @@ where
 
         let Some((output, complete)) = stream.next().await else {
             panic!("StreamingTernaryOperator unexpectedly reached end of stream");
+        };
+
+        if complete {
+            self.stream = None;
+            output
+        } else {
+            output
+        }
+    }
+}
+
+pub trait StreamingQuaternaryOperator<I1, I2, I3, I4, O>: Operator
+where
+    I1: Clone,
+    I2: Clone,
+    I3: Clone,
+    I4: Clone,
+{
+    fn eval(
+        self: Rc<Self>,
+        i1: Cow<'_, I1>,
+        i2: Cow<'_, I2>,
+        i3: Cow<'_, I3>,
+        i4: Cow<'_, I4>,
+    ) -> impl AsyncStream<Item = (O, bool)> + 'static;
+}
+
+pub struct StreamingQuaternaryWrapper<I1, I2, I3, I4, O, Op> {
+    operator: Rc<Op>,
+    stream: Option<Pin<Box<dyn AsyncStream<Item = (O, bool)>>>>,
+    phantom: PhantomData<fn(&I1, &I2, &I3, &I4, &O)>,
+}
+
+impl<I1, I2, I3, I4, O, Op> StreamingQuaternaryWrapper<I1, I2, I3, I4, O, Op> {
+    pub fn new(operator: Op) -> Self {
+        Self {
+            operator: Rc::new(operator),
+            stream: None,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<I1, I2, I3, I4, O, Op> Operator for StreamingQuaternaryWrapper<I1, I2, I3, I4, O, Op>
+where
+    I1: Clone + 'static,
+    I2: Clone + 'static,
+    I3: Clone + 'static,
+    I4: Clone + 'static,
+    O: 'static,
+    Op: StreamingQuaternaryOperator<I1, I2, I3, I4, O> + 'static,
+{
+    fn name(&self) -> Cow<'static, str> {
+        self.operator.name()
+    }
+
+    fn location(&self) -> OperatorLocation {
+        self.operator.location()
+    }
+
+    fn init(&mut self, global_id: &GlobalNodeId) {
+        Rc::get_mut(&mut self.operator).unwrap().init(global_id);
+    }
+
+    fn metadata(&self, meta: &mut OperatorMeta) {
+        self.operator.metadata(meta);
+    }
+
+    fn clock_start(&mut self, scope: Scope) {
+        Rc::get_mut(&mut self.operator).unwrap().clock_start(scope);
+    }
+
+    fn clock_end(&mut self, scope: Scope) {
+        Rc::get_mut(&mut self.operator).unwrap().clock_end(scope);
+    }
+
+    fn is_async(&self) -> bool {
+        self.operator.is_async()
+    }
+
+    fn is_input(&self) -> bool {
+        self.operator.is_input()
+    }
+
+    fn ready(&self) -> bool {
+        self.operator.ready()
+    }
+
+    fn register_ready_callback<F>(&mut self, cb: F)
+    where
+        F: Fn() + Send + Sync + 'static,
+    {
+        Rc::get_mut(&mut self.operator)
+            .unwrap()
+            .register_ready_callback(cb);
+    }
+
+    fn fixedpoint(&self, scope: Scope) -> bool {
+        self.operator.fixedpoint(scope)
+    }
+
+    #[allow(unused_variables)]
+    fn commit(&mut self, base: &StoragePath, persistent_id: Option<&str>) -> Result<(), Error> {
+        Rc::get_mut(&mut self.operator)
+            .unwrap()
+            .commit(base, persistent_id)
+    }
+
+    #[allow(unused_variables)]
+    fn restore(&mut self, base: &StoragePath, persistent_id: Option<&str>) -> Result<(), Error> {
+        Rc::get_mut(&mut self.operator)
+            .unwrap()
+            .restore(base, persistent_id)
+    }
+
+    fn clear_state(&mut self) -> Result<(), Error> {
+        Rc::get_mut(&mut self.operator).unwrap().clear_state()
+    }
+
+    fn start_replay(&mut self) -> Result<(), Error> {
+        Rc::get_mut(&mut self.operator).unwrap().start_replay()
+    }
+
+    fn is_replay_complete(&self) -> bool {
+        self.operator.is_replay_complete()
+    }
+
+    fn end_replay(&mut self) -> Result<(), Error> {
+        Rc::get_mut(&mut self.operator).unwrap().end_replay()
+    }
+
+    fn flush(&mut self) {
+        Rc::get_mut(&mut self.operator).unwrap().flush();
+    }
+
+    fn is_flush_complete(&self) -> bool {
+        self.stream.is_none()
+    }
+}
+
+impl<I1, I2, I3, I4, O, Op> QuaternaryOperator<I1, I2, I3, I4, O>
+    for StreamingQuaternaryWrapper<I1, I2, I3, I4, O, Op>
+where
+    I1: Clone + 'static,
+    I2: Clone + 'static,
+    I3: Clone + 'static,
+    I4: Clone + 'static,
+    O: 'static,
+    Op: StreamingQuaternaryOperator<I1, I2, I3, I4, O> + 'static,
+{
+    async fn eval(
+        &mut self,
+        i1: Cow<'_, I1>,
+        i2: Cow<'_, I2>,
+        i3: Cow<'_, I3>,
+        i4: Cow<'_, I4>,
+    ) -> O {
+        if self.stream.is_none() {
+            self.stream = Some(Box::pin(self.operator.clone().eval(i1, i2, i3, i4))
+                as Pin<Box<dyn AsyncStream<Item = (O, bool)>>>);
+        }
+
+        let stream = self.stream.as_mut().unwrap();
+
+        let Some((output, complete)) = stream.next().await else {
+            panic!("StreamingQuaternaryOperator unexpectedly reached end of stream");
         };
 
         if complete {
