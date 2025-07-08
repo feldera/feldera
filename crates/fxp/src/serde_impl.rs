ecc20ad7e3a65694333cf881de1faa7fcd1ea9db
Ben Pfaff <blp@feldera.com>
2025-07-08 16:06:23 -0700

    [fxp] Add DynamicDecimal for serializing and deserializing decimals.
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/fxp/src/serde_impl.rs
+++ crates/fxp/src/serde_impl.rs
@@ -1,15 +1,30 @@
-use serde::{de::Unexpected, ser::SerializeStruct, Serialize, Serializer};
+use serde::{
+    de::{Error, Unexpected},
+    ser::SerializeStruct,
+    Serialize, Serializer,
+};
 use smallstr::SmallString;
 
-use crate::Fixed;
+use crate::{DynamicDecimal, Fixed};
 use std::{
-    fmt::{self, Write},
+    fmt::{self, Display, Write},
     str::FromStr,
 };
 
 const FIXED_KEY_TOKEN: &str = "$serde_json::private::Number";
 
-struct FixedVisitor<const P: usize, const S: usize>;
+fn serialize_helper<T, S>(value: T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: Display,
+    S: Serializer,
+{
+    let mut string = SmallString::<[u8; 64]>::new();
+    write!(&mut string, "{}", value).unwrap();
+
+    let mut s = serializer.serialize_struct(FIXED_KEY_TOKEN, 1)?;
+    s.serialize_field(FIXED_KEY_TOKEN, string.as_str())?;
+    s.end()
+}
 
 impl<const P: usize, const S: usize> Serialize for Fixed<P, S> {
     /// Serializes this `Fixed` in the same form used by [serde_json::Number]
@@ -20,12 +35,21 @@ impl<const P: usize, const S: usize> Serialize for Fixed<P, S> {
     where
         Ser: Serializer,
     {
-        let mut string = SmallString::<[u8; 64]>::new();
-        write!(&mut string, "{}", self).unwrap();
+        serialize_helper(self, serializer)
+    }
+}
 
-        let mut s = serializer.serialize_struct(FIXED_KEY_TOKEN, 1)?;
-        s.serialize_field(FIXED_KEY_TOKEN, string.as_str())?;
-        s.end()
+impl Serialize for DynamicDecimal {
+    /// Serializes this `DynamicDecimal` in the same form used by
+    /// [serde_json::Number] when the `arbitrary_precision` feature is turned
+    /// on.
+    ///
+    /// [serde_json::Number]: https://docs.rs/serde_json/latest/serde_json/struct.Number.html
+    fn serialize<Ser>(&self, serializer: Ser) -> Result<Ser::Ok, Ser::Error>
+    where
+        Ser: Serializer,
+    {
+        serialize_helper(self, serializer)
     }
 }
 
@@ -34,48 +58,62 @@ impl<'de, const P: usize, const S: usize> serde::Deserialize<'de> for Fixed<P, S
     where
         D: serde::de::Deserializer<'de>,
     {
-        deserializer.deserialize_any(FixedVisitor)
+        let decimal = DynamicDecimal::deserialize(deserializer)?;
+        decimal.try_into().map_err(|_| {
+            D::Error::custom(format!(
+                "decimal number {decimal} is outside the value range {} to {} (inclusive)",
+                Fixed::<P, S>::MIN,
+                Fixed::<P, S>::MAX
+            ))
+        })
     }
 }
 
-impl<'de, const P: usize, const S: usize> serde::de::Visitor<'de> for FixedVisitor<P, S> {
-    type Value = Fixed<P, S>;
+impl<'de> serde::Deserialize<'de> for DynamicDecimal {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        deserializer.deserialize_any(DecimalVisitor)
+    }
+}
+
+struct DecimalVisitor;
+
+impl<'de> serde::de::Visitor<'de> for DecimalVisitor {
+    type Value = DynamicDecimal;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            formatter,
-            "a fixed-point number between {} and {}, inclusive",
-            Self::Value::MIN,
-            Self::Value::MAX
-        )
+        write!(formatter, "a decimal number")
     }
 
     fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Fixed::try_from(value).map_err(|_| E::invalid_value(Unexpected::Signed(value), &self))
+        Ok(DynamicDecimal::from(value))
     }
 
     fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Fixed::try_from(value).map_err(|_| E::invalid_value(Unexpected::Unsigned(value), &self))
+        Ok(DynamicDecimal::from(value))
     }
 
     fn visit_f64<E>(self, value: f64) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Fixed::try_from(value).map_err(|_| E::invalid_value(Unexpected::Float(value), &self))
+        DynamicDecimal::try_from(value)
+            .map_err(|_| E::invalid_value(Unexpected::Float(value), &self))
     }
 
     fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Self::Value::from_str(value).map_err(|_| E::invalid_value(Unexpected::Str(value), &self))
+        DynamicDecimal::from_str(value).map_err(|_| E::invalid_value(Unexpected::Str(value), &self))
     }
 
     fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
@@ -83,19 +121,19 @@ impl<'de, const P: usize, const S: usize> serde::de::Visitor<'de> for FixedVisit
         A: serde::de::MapAccess<'de>,
     {
         let mut map = map;
-        let value = map.next_key::<FixedKey>()?;
+        let value = map.next_key::<DecimalKey>()?;
         if value.is_none() {
             return Err(serde::de::Error::invalid_type(Unexpected::Map, &self));
         }
-        let v: FixedFromString<P, S> = map.next_value()?;
+        let v: DecimalFromString = map.next_value()?;
         Ok(v.value)
     }
 }
 
-struct FixedKey;
+struct DecimalKey;
 
-impl<'de> serde::de::Deserialize<'de> for FixedKey {
-    fn deserialize<D>(deserializer: D) -> Result<FixedKey, D::Error>
+impl<'de> serde::de::Deserialize<'de> for DecimalKey {
+    fn deserialize<D>(deserializer: D) -> Result<DecimalKey, D::Error>
     where
         D: serde::de::Deserializer<'de>,
     {
@@ -121,35 +159,35 @@ impl<'de> serde::de::Deserialize<'de> for FixedKey {
         }
 
         deserializer.deserialize_identifier(FieldVisitor)?;
-        Ok(FixedKey)
+        Ok(DecimalKey)
     }
 }
 
-pub struct FixedFromString<const P: usize, const S: usize> {
-    pub value: Fixed<P, S>,
+pub struct DecimalFromString {
+    pub value: DynamicDecimal,
 }
 
-impl<'de, const P: usize, const S: usize> serde::de::Deserialize<'de> for FixedFromString<P, S> {
+impl<'de> serde::de::Deserialize<'de> for DecimalFromString {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::de::Deserializer<'de>,
     {
-        struct Visitor<const P: usize, const S: usize>;
+        struct Visitor;
 
-        impl<const P: usize, const S: usize> serde::de::Visitor<'_> for Visitor<P, S> {
-            type Value = FixedFromString<P, S>;
+        impl serde::de::Visitor<'_> for Visitor {
+            type Value = DecimalFromString;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("string containing a FIXED")
+                formatter.write_str("string containing a decimal number")
             }
 
-            fn visit_str<E>(self, value: &str) -> Result<FixedFromString<P, S>, E>
+            fn visit_str<E>(self, value: &str) -> Result<DecimalFromString, E>
             where
                 E: serde::de::Error,
             {
-                let d = Fixed::from_str(value)
+                let d = DynamicDecimal::from_str(value)
                     .map_err(|_| serde::de::Error::invalid_value(Unexpected::Str(value), &self))?;
-                Ok(FixedFromString { value: d })
+                Ok(DecimalFromString { value: d })
             }
         }
 
@@ -159,12 +197,14 @@ impl<'de, const P: usize, const S: usize> serde::de::Deserialize<'de> for FixedF
 
 #[cfg(test)]
 mod test {
+    use std::str::FromStr;
+
     use serde_json::json;
 
-    use crate::Fixed;
+    use crate::{DynamicDecimal, Fixed};
 
     #[test]
-    fn serialize() {
+    fn serialize_fixed() {
         type F = Fixed<10, 2>;
 
         assert_eq!(
@@ -174,7 +214,15 @@ mod test {
     }
 
     #[test]
-    fn deserialize() {
+    fn serialize_dynamic() {
+        assert_eq!(
+            serde_json::to_string(&DynamicDecimal::from_str("1.23").unwrap()).unwrap(),
+            json!("1.23")
+        );
+    }
+
+    #[test]
+    fn deserialize_fixed() {
         type F = Fixed<10, 2>;
 
         assert_eq!(
@@ -197,5 +245,44 @@ mod test {
             serde_json::from_str::<F>("-123").unwrap(),
             F::try_from(-123).unwrap()
         );
+        assert_eq!(
+            serde_json::from_str::<F>("99999999").unwrap(),
+            F::try_from(99999999).unwrap()
+        );
+        assert!(serde_json::from_str::<F>("999999999").is_err());
+    }
+
+    #[test]
+    fn deserialize_dynamic() {
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("1.23").unwrap(),
+            DynamicDecimal::try_from(1.23).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("\"1.23\"").unwrap(),
+            DynamicDecimal::try_from(1.23).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>(r#"{"$serde_json::private::Number": "1.23"}"#)
+                .unwrap(),
+            DynamicDecimal::try_from(1.23).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("123").unwrap(),
+            DynamicDecimal::try_from(123).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("-123").unwrap(),
+            DynamicDecimal::try_from(-123).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("99999999").unwrap(),
+            DynamicDecimal::try_from(99999999).unwrap()
+        );
+        assert_eq!(
+            serde_json::from_str::<DynamicDecimal>("1e38").unwrap(),
+            DynamicDecimal::try_from(1e38).unwrap()
+        );
+        assert!(serde_json::from_str::<DynamicDecimal>("1e39").is_err());
     }
 }
