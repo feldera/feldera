89ce973c3982f46ac7dce986672c12287c2a9987
rivudhk <rivudhkr@gmail.com>
2025-07-10 03:27:58 +0545

    py: add suggested fix
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/illarg_tests/check_negative_tests.py
+++ python/tests/illarg_tests/check_negative_tests.py
@@ -1,12 +1,14 @@
 from tests.aggregate_tests.aggtst_base import TstView
 
 
-# Test different types of negative tests that are expected to fail but end up passing
+# Test different types of negative tests that are expected to fail
+# Case 1: expected to fail -> fails
+class ignore_check_negtest1(TstView):
+    """Program expects Error: "Unimplemented" but receives Error: "Not Yet Implemented"
+    what happens? ->
+        SQL Compilation doesn't pass,
+        Program Error"""
 
-
-# Case 1:
-# Program expects Error: "Unimplemented" but receives Error: "Not Yet Implemented"
-class illarg_check_negtest1(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW check_negtest1 AS SELECT
@@ -15,9 +17,14 @@ class illarg_check_negtest1(TstView):
         self.expected_error = "Unimplemented"
 
 
-# Case 2:
-# Despite the SQL being correct, test case is supplied 'expected_error_type' attribute(it is considered a negative test), and data attribute isn't supplied
-class illarg_check_negtest2(TstView):
+# Case 2: expected to fail -> fails
+class ignore_check_negtest2(TstView):
+    """Despite the SQL being correct, test case is supplied 'expected_error_type' attribute(it is considered a negative test), and data attribute isn't supplied
+    what happens? ->
+        SQL Compilation passes,
+        Rust Compilation passes,
+        but throws AssertionError, since expected_data != received_data"""
+
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW check_negtest2 AS SELECT
@@ -26,13 +33,20 @@ class illarg_check_negtest2(TstView):
         self.expected_error = "Not yet implemented"
 
 
-# Case 3:
-# Despite the SQL being correct, test case is supplied 'expected_error_type' attribute(it is considered a negative test), but data attribute is supplied
-class illarg_check_negtest3(TstView):
+# Case 3: program was expected to fail-> but passes
+class ignore_check_negtest3(TstView):
+    """Despite the SQL being correct, test case is supplied 'expected_error_type' attribute(it is considered a negative test), but data attribute is supplied
+    what happens?
+        SQL Passes,
+        Rust Compilation Passes,
+        Program Execution Passes,
+        but throws AssertionError, and Pipeline shuts down"""
+
     def __init__(self):
         # checked manually
         self.data = [{"str": "hello hello "}]
         self.sql = """CREATE MATERIALIZED VIEW check_negtest3 AS SELECT
                       CONCAT(str, str) AS str
-                      FROM illegal_tbl"""
+                      FROM illegal_tbl
+                      WHERE id = 0"""
         self.expected_error = "Not yet implemented"
