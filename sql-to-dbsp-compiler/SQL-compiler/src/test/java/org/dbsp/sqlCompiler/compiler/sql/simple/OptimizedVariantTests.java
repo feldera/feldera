49614c9ce249ae5a9fe2b190e4feab48c6df0d8e
Mihai Budiu <mbudiu@feldera.com>
2025-07-11 11:40:36 -0700

    Remove obsolete benchmarking script
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/OptimizedVariantTests.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/OptimizedVariantTests.java
@@ -1,7 +1,6 @@
 package org.dbsp.sqlCompiler.compiler.sql.simple;
 
 import org.dbsp.sqlCompiler.compiler.CompilerOptions;
-import org.dbsp.sqlCompiler.compiler.DBSPCompiler;
 
 // Runs the VariantTests with optimizations
 public class OptimizedVariantTests extends VariantTests {
