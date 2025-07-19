7fd1656b3812aa8ca22c0c170c0fe338e7f05589
Leonid Ryzhyk <leonid@feldera.com>
2025-07-18 17:26:00 -0700

    DevTweaks::splitter_chunk_size_records.
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- crates/dbsp/src/circuit/dbsp_handle.rs
+++ crates/dbsp/src/circuit/dbsp_handle.rs
@@ -267,7 +267,6 @@ pub struct CircuitConfig {
 
 #[derive(Debug, Clone, Eq, PartialEq, Deserialize)]
 #[serde(default)]
-#[derive(Default)]
 pub struct DevTweaks {
     /// Whether to asynchronously fetch keys needed for the join operator from
     /// storage.  Asynchronous fetching should be faster for high-latency
@@ -303,6 +302,28 @@ pub struct DevTweaks {
     /// Enable backfill avoidance feature.
     // This flag is only used by the pipeline manager.
     pub backfill_avoidance: bool,
+
+    /// Controls the maximal number of records output by splitter operators
+    /// (joins, distinct, aggregation, rolling window and group operators) at
+    /// each step.
+    ///
+    /// The default value is 10,000 records.
+    // TODO: splitter_chunk_size_bytes, per-operator chunk size.
+    pub splitter_chunk_size_records: u64,
+}
+
+impl Default for DevTweaks {
+    fn default() -> Self {
+        Self {
+            fetch_join: false,
+            fetch_distinct: false,
+            merger: MergerType::default(),
+            storage_mb_max: None,
+            stack_overflow_backtrace: false,
+            backfill_avoidance: false,
+            splitter_chunk_size_records: 10_000,
+        }
+    }
 }
 
 impl DevTweaks {
@@ -318,6 +339,10 @@ impl DevTweaks {
         }
         tweaks
     }
+}
+
+pub fn splitter_output_chunk_size() -> usize {
+    Runtime::with_dev_tweaks(|d| d.splitter_chunk_size_records as usize)
 }
 
 /// Configuration for storage in a [Runtime]-hosted circuit.
