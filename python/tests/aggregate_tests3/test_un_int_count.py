94a50045887086e88bcb5d3b0ecaa1552ee91335
rivudhk <rivudhkr@gmail.com>
2025-07-21 03:47:57 +0545

    py: unsigned int tests for ARRAY_AGG, COUNT, COUNTIF aggregates
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

---
+++ python/tests/aggregate_tests3/test_un_int_count.py
@@ -0,0 +1,39 @@
+from tests.aggregate_tests.aggtst_base import TstView
+
+
+class aggtst_un_int_count(TstView):
+    def __init__(self):
+        # Validated on MySQL
+        self.data = [{"count": 4}]
+        self.sql = """CREATE MATERIALIZED VIEW un_int_count AS SELECT
+                      COUNT(*) AS count
+                      FROM un_int_tbl"""
+
+
+class aggtst_un_int_count_groupby(TstView):
+    def __init__(self):
+        # Validated on MySQL
+        self.data = [{"id": 0, "count": 2}, {"id": 1, "count": 2}]
+        self.sql = """CREATE MATERIALIZED VIEW un_int_count_gby AS SELECT
+                      id, COUNT(*) AS count
+                      FROM un_int_tbl
+                      GROUP BY id"""
+
+
+class aggtst_un_int_count_where(TstView):
+    def __init__(self):
+        # Validated on MySQL
+        self.data = [{"count": 3}]
+        self.sql = """CREATE MATERIALIZED VIEW un_int_count_where AS SELECT
+                      COUNT(*) FILTER(WHERE c2 > 45) AS count
+                      FROM un_int_tbl"""
+
+
+class aggtst_un_int_count_where_groupby(TstView):
+    def __init__(self):
+        # Validated on MySQL
+        self.data = [{"id": 0, "count": 1}, {"id": 1, "count": 2}]
+        self.sql = """CREATE MATERIALIZED VIEW un_int_count_where_gby AS SELECT
+                      id, COUNT(*) FILTER(WHERE c2 > 45) AS count
+                      FROM un_int_tbl
+                      GROUP BY id"""
