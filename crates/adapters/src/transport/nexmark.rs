feldera/feldera

commit 6f345f03a925a2d627b041053b0f33d1e080b640

[adapters] Provide resume info on initialization.
Partially fixes #4348.

Previously, the input connector initialization protocol first invoked
the `open` method specifying connector config, but not giving any
indication on whether the connector is resuming from a checkpoint. It
would then send a Seek message telling the connector to seek to the
checkpointed offset.

This had two downsides:

- The main one is that in general connectors need checkpoint info during
  initialization. A case in point is the delta lake input connector: when
  recovering a long-running pipeline, the original table
  version may no longer be available due to the retention policy, however
  the latest version the connector was on before the checkpoint was made
  may still be available. In this case, we want to open the checkpointed
  version instead of the original version, which requires resume info to
  be available during initialization.

- Second, this design complicated connector implementation as
  connectors had to wait for the Seek command on startup.

The new design removes the Seek command adds a `resume_info` argument to
`InputEndpoint::open`.

Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

crates/adapters/src/transport/nexmark.rs
@@ -1,6 +1,6 @@
 //! An input adapter that generates Nexmark event input data.
 
-use feldera_adapterlib::transport::{InputReaderCommand, Resume};
+use feldera_adapterlib::transport::{parse_resume_info, InputReaderCommand, Resume};
 use feldera_types::config::FtModel;
 use std::cmp::min;
 use std::collections::VecDeque;
@@ -52,11 +52,13 @@ impl TransportInputEndpoint for NexmarkEndpoint {
         consumer: Box<dyn InputConsumer>,
         parser: Box<dyn Parser>,
         _schema: Relation,
+        resume_info: Option<serde_json::Value>,
     ) -> AnyResult<Box<dyn InputReader>> {
         Ok(Box::new(InputGenerator::new(
             &self.config,
             consumer,
             parser,
+            resume_info,
         )?))
     }
 }
@@ -72,7 +74,13 @@ impl InputGenerator {
         config: &NexmarkInputConfig,
         consumer: Box<dyn InputConsumer>,
         parser: Box<dyn Parser>,
+        resume_info: Option<serde_json::Value>,
     ) -> AnyResult<Self> {
+        let resume_info = if let Some(resume_info) = resume_info {
+            Some(parse_resume_info::<Metadata>(&resume_info)?)
+        } else {
+            None
+        };
         let mut guard = INNER.lock().unwrap();
         let inner = guard.upgrade().unwrap_or_else(|| {
             let inner = Arc::new(Inner::new());
@@ -81,7 +89,7 @@ impl InputGenerator {
         });
         drop(guard);
 
-        inner.configure(config, consumer.clone(), parser)?;
+        inner.configure(config, consumer.clone(), parser, resume_info)?;
 
         Ok(Self {
             table: config.table,
@@ -98,7 +106,6 @@ impl InputReader for InputGenerator {
                 let _ = self.inner.command_sender.send(command);
             }
             _ => match command {
-                InputReaderCommand::Seek(_) => (),
                 InputReaderCommand::Replay { .. } => self.consumer.replayed(0, 0),
                 InputReaderCommand::Extend => (),
                 InputReaderCommand::Pause => (),
@@ -179,7 +186,7 @@ impl InnerInit {
     pub fn fully_configured(&self) -> bool {
         self.parsers.values().all(|p| p.is_some())
     }
-    pub fn start_worker(self) {
+    pub fn start_worker(self, resume_info: Option<Metadata>) {
         assert!(self.fully_configured());
 
         let Self {
@@ -197,9 +204,13 @@ impl InnerInit {
             .name(String::from("nexmark"))
             .spawn({
                 move || {
-                    if let Err(error) =
-                        worker_thread(options, parsers, consumers.clone(), command_receiver)
-                    {
+                    if let Err(error) = worker_thread(
+                        options,
+                        parsers,
+                        consumers.clone(),
+                        command_receiver,
+                        resume_info,
+                    ) {
                         for consumer in consumers.values() {
                             consumer.error(true, anyhow!("{error}"));
                         }
@@ -233,6 +244,7 @@ impl Inner {
         config: &NexmarkInputConfig,
         consumer: Box<dyn InputConsumer>,
         parser: Box<dyn Parser>,
+        resume_info: Option<Metadata>,
     ) -> AnyResult<()> {
         let mut status = self.status.lock().unwrap();
         match &mut *status {
@@ -248,7 +260,7 @@ impl Inner {
                     else {
                         unreachable!()
                     };
-                    init.start_worker();
+                    init.start_worker(resume_info);
                 }
                 Ok(())
             }
@@ -269,6 +281,7 @@ fn worker_thread(
     parsers: EnumMap<NexmarkTable, Box<dyn Parser>>,
     consumers: EnumMap<NexmarkTable, Box<dyn InputConsumer>>,
     command_receiver: UnboundedReceiver<InputReaderCommand>,
+    resume_info: Option<Metadata>,
 ) -> AnyResult<()> {
     let mut command_receiver = InputCommandReceiver::<Metadata, ()>::new(command_receiver);
 
@@ -302,7 +315,7 @@ fn worker_thread(
         .collect::<Vec<_>>();
     drop(parsers);
 
-    let mut next_event_id = if let Some(metadata) = command_receiver.blocking_recv_seek()? {
+    let mut next_event_id = if let Some(metadata) = resume_info {
         metadata.event_ids.end
     } else {
         0
@@ -332,7 +345,7 @@ fn worker_thread(
             Some(command_receiver.blocking_recv()?)
         };
         match command {
-            Some(InputReaderCommand::Seek(_)) | Some(InputReaderCommand::Replay { .. }) => {
+            Some(InputReaderCommand::Replay { .. }) => {
                 unreachable!("{command:?} must be at the beginning of the command stream")
             }
             Some(InputReaderCommand::Extend) => running = true,

crates/adapterlib/src/transport.rs
@@ -9,6 +9,7 @@ use dyn_clone::DynClone;
 use feldera_types::config::FtModel;
 use feldera_types::program_schema::Relation;
 use rmpv::{ext::Error as RmpDecodeError, Value as RmpValue};
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde_json::{Error as JsonError, Value as JsonValue};
 use tokio::sync::mpsc::error::TryRecvError;
@@ -55,12 +56,19 @@ pub trait TransportInputEndpoint: InputEndpoint {
     /// data into records. Returns an [`InputReader`] for reading the endpoint's
     /// data.  The endpoint will use `consumer` to report its progress.
     ///
+    /// The `resume_info` parameter is used when resuming the pipeline from a
+    /// checkpoint. It contains resume metadata that the endpoint returned via the
+    /// [`InputConsumer::extended`] function before suspending. When specified,
+    /// it tells a fault-tolerant input reader to seek past the data already read
+    /// in the step whose metadata is given by the value.
+    ///
     /// The reader is initially paused.
     fn open(
         &self,
         consumer: Box<dyn InputConsumer>,
         parser: Box<dyn Parser>,
         schema: Relation,
+        resume_info: Option<JsonValue>,
     ) -> AnyResult<Box<dyn InputReader>>;
 }
 
@@ -68,6 +76,7 @@ pub trait IntegratedInputEndpoint: InputEndpoint {
     fn open(
         self: Box<Self>,
         input_handle: &InputCollectionHandle,
+        resume_info: Option<JsonValue>,
     ) -> AnyResult<Box<dyn InputReader>>;
 }
 
@@ -80,9 +89,6 @@ pub trait IntegratedInputEndpoint: InputEndpoint {
 ///
 /// ```text
 ///   ┌─⯇─ (start) ─⯈──┐
-///   │      │         │
-///   │      ▼         │
-///   ├─⯇─ Seek ─⯈─────│
 ///   │      │         │
 ///   │      │  ┌───┐  │
 ///   │      ▼  ▼   │  │
@@ -150,15 +156,6 @@ pub trait IntegratedInputEndpoint: InputEndpoint {
 /// record the step properly and fault tolerance replay will be incorrect.
 #[derive(Debug)]
 pub enum InputReaderCommand {
-    /// Tells a fault-tolerant input reader to seek past the data already read
-    /// in the step whose metadata is given by the value.
-    ///
-    /// # Constraints
-    ///
-    /// Only fault-tolerant input readers need to accept this. If it is given,
-    /// it will be issued only once and before any of the other commands.
-    Seek(JsonValue),
-
     /// Tells the input reader to replay the step described by `metadata` and
     /// `data` by reading and flushing buffers for the data in the step, and
     /// then [InputConsumer::replayed] to signal completion.
@@ -172,18 +169,13 @@ pub enum InputReaderCommand {
     /// # Constraints
     ///
     /// Only fault-tolerant input readers need to accept this. It will be issued
-    /// zero or more times, after [InputReaderCommand::Seek] but before any
-    /// other commands.
+    /// zero or more times, before any other command.
     Replay { metadata: JsonValue, data: RmpValue },
 
     /// Tells the input reader to accept further input. The first time it
-    /// receives this command, the reader should start from:
-    ///
-    /// - If [InputReaderCommand::Seek] or [InputReaderCommand::Replay] was
-    ///   previously issued, then just beyond the end of the data from the last
-    ///   call.
-    ///
-    /// - Otherwise, from the beginning of the input.
+    /// receives this command, the reader should start from the resume point
+    /// passed as `resume_info` when the endpoint was opened, if any, and
+    /// otherwise from the beginning of input.
     ///
     /// The input reader should report the data that it queues to
     /// [InputConsumer::buffered] as it queues it.
@@ -246,7 +238,7 @@ impl InputReaderCommand {
     /// fault-tolerant endpoints).
     pub fn as_nonft(&self) -> Option<NonFtInputReaderCommand> {
         match self {
-            InputReaderCommand::Seek(_) | InputReaderCommand::Replay { .. } => None,
+            InputReaderCommand::Replay { .. } => None,
             InputReaderCommand::Queue { .. } => Some(NonFtInputReaderCommand::Queue),
             InputReaderCommand::Extend => {
                 Some(NonFtInputReaderCommand::Transition(PipelineState::Running))
@@ -454,10 +446,6 @@ pub trait InputReader: Send + Sync {
     /// channel's sender.
     fn is_closed(&self) -> bool;
 
-    fn seek(&self, metadata: JsonValue) {
-        self.request(InputReaderCommand::Seek(metadata));
-    }
-
     fn replay(&self, metadata: JsonValue, data: RmpValue) {
         self.request(InputReaderCommand::Replay { metadata, data });
     }
@@ -595,7 +583,7 @@ pub enum Resume {
     /// the step exactly.
     Seek {
         /// Metadata needed for the controller to restart the input adapter from
-        /// just after this input step using [InputReaderCommand::Seek].
+        /// just after this input step.
         seek: JsonValue,
     },
 
@@ -603,7 +591,7 @@ pub enum Resume {
     /// step.
     Replay {
         /// Metadata needed for the controller to restart the input adapter from
-        /// just after this input step using [InputReaderCommand::Seek].
+        /// just after this input step.
         seek: JsonValue,
 
         /// The data needed for the controller to replay exactly this input step
@@ -689,6 +677,15 @@ impl Resume {
 }
 
 dyn_clone::clone_trait_object!(InputConsumer);
+
+/// Helper function to parse resume info passed to [`InputConsumer::extended`].
+pub fn parse_resume_info<M>(metadata: &JsonValue) -> AnyResult<M>
+where
+    M: DeserializeOwned,
+{
+    serde_json::from_value::<M>(metadata.clone())
+            .map_err(|e| anyhow::anyhow!("unable to parse checkpointed connector state (checkpointed state: {metadata}; parse error: {e})"))
+}
 
 pub type AsyncErrorCallback = Box<dyn Fn(bool, AnyError) + Send + Sync>;
 
@@ -777,8 +774,6 @@ pub trait OutputEndpoint: Send {
 ///
 /// A fault-tolerant connector wants to receive, in order:
 ///
-/// - Zero or one [InputReaderCommand::Seek]s.
-///
 /// - Zero or more [InputReaderCommand::Replay]s.
 ///
 /// - Zero or more other commands.
@@ -835,40 +830,6 @@ impl<M, D> InputCommandReceiver<M, D> {
         }
     }
 
-    pub fn blocking_recv_seek(&mut self) -> Result<Option<M>, InputCommandReceiverError>
-    where
-        M: for<'a> Deserialize<'a>,
-    {
-        let command = self.blocking_recv()?;
-        self.take_seek(command)
-    }
-
-    pub async fn recv_seek(&mut self) -> Result<Option<M>, InputCommandReceiverError>
-    where
-        M: for<'a> Deserialize<'a>,
-    {
-        let command = self.recv().await?;
-        self.take_seek(command)
-    }
-
-    fn take_seek(
-        &mut self,
-        command: InputReaderCommand,
-    ) -> Result<Option<M>, InputCommandReceiverError>
-    where
-        M: for<'a> Deserialize<'a>,
-    {
-        debug_assert!(self.buffer.is_none());
-        match command {
-            InputReaderCommand::Seek(metadata) => Ok(Some(serde_json::from_value::<M>(metadata)?)),
-            InputReaderCommand::Disconnect => Err(InputCommandReceiverError::Disconnected),
-            other => {
-                self.put_back(other);
-                Ok(None)
-            }
-        }
-    }
-
     pub fn blocking_recv_replay(&mut self) -> Result<Option<(M, D)>, InputCommandReceiverError>
     where
         M: for<'a> Deserialize<'a>,
@@ -896,7 +857,6 @@ impl<M, D> InputCommandReceiver<M, D> {
         D: for<'a> Deserialize<'a>,
     {
         match command {
-            InputReaderCommand::Seek(_) => unreachable!(),
             InputReaderCommand::Replay { metadata, data } => Ok(Some((
                 serde_json::from_value::<M>(metadata)?,
                 rmpv::ext::from_value::<D>(data)?,

crates/adapters/src/adhoc/table.rs
@@ -302,6 +302,7 @@ impl DataSink for AdHocTableSink {
                 &endpoint_name,
                 config,
                 Some(Box::new(endpoint.clone()) as Box<dyn TransportInputEndpoint>),
+                None,
             )
             .map_err(|e| DataFusionError::External(e.into()))?;
 

crates/adapters/src/controller/mod.rs
@@ -364,10 +364,11 @@ impl Controller {
         &self,
         endpoint_name: &str,
         config: &InputEndpointConfig,
+        resume_info: Option<JsonValue>,
     ) -> Result<EndpointId, ControllerError> {
         debug!("Connecting input endpoint '{endpoint_name}'; config: {config:?}");
         self.inner.fail_if_bootstrapping_or_restoring()?;
-        self.inner.connect_input(endpoint_name, config)
+        self.inner.connect_input(endpoint_name, config, resume_info)
     }
 
     /// Disconnect an existing input endpoint.
@@ -400,10 +401,11 @@ impl Controller {
         endpoint_name: &str,
         endpoint_config: InputEndpointConfig,
         endpoint: Box<dyn TransportInputEndpoint>,
+        resume_info: Option<JsonValue>,
     ) -> Result<EndpointId, ControllerError> {
         self.inner.fail_if_bootstrapping_or_restoring()?;
         self.inner
-            .add_input_endpoint(endpoint_name, endpoint_config, Some(endpoint))
+            .add_input_endpoint(endpoint_name, endpoint_config, Some(endpoint), resume_info)
     }
 
     /// Disconnect an existing output endpoint.
@@ -757,29 +759,22 @@ impl CircuitThread {
 
         let (mut circuit, catalog) = circuit_factory(circuit_config)?;
         let lir = circuit.lir()?;
-        let (parker, backpressure_thread, command_receiver, controller) =
-            ControllerInner::new(pipeline_config, catalog, lir, error_cb, processed_records)?;
-
-        controller
-            .status
-            .set_bootstrap_in_progress(circuit.bootstrap_in_progress());
 
         // Seek each input endpoint to its initial offset.
         //
         // If we're not restoring from a checkpoint, `input_metadata` will be empty so
         // this will do nothing.
-        let input_metadata = if let Some(input_metadata) = input_metadata {
-            for (endpoint_name, metadata) in &input_metadata {
-                let Ok(endpoint_id) = controller.input_endpoint_id_by_name(endpoint_name) else {
+        let resume_info = if let Some(input_metadata) = &input_metadata {
+            let mut resume_info = HashMap::new();
+
+            for (endpoint_name, seek) in input_metadata.iter() {
+                let Some(endpoint_config) = config.inputs.get(endpoint_name.as_str()) else {
                     info!("Found checkpointed state for input connector '{endpoint_name}', but the connector is not present in the new pipeline configuration; this connector will not be added to the pipeline");
                     continue;
                 };
 
-                let endpoint = &controller.status.input_status()[&endpoint_id];
-
-                let node_id = controller
-                    .catalog
-                    .input_collection_handle(&SqlIdentifier::from(&endpoint.config.stream))
+                let node_id = catalog
+                    .input_collection_handle(&SqlIdentifier::from(&endpoint_config.stream))
                     .unwrap()
                     .node_id;
 
@@ -790,17 +785,32 @@ impl CircuitThread {
                     }
                 }
 
-                endpoint.reader.seek(metadata.clone());
+                resume_info.insert(endpoint_name.clone(), seek.clone());
             }
-            Some(
-                input_metadata
-                    .into_iter()
-                    .map(|(name, seek)| (name, Some(Resume::Seek { seek })))
-                    .collect(),
-            )
+            resume_info
         } else {
-            None
+            HashMap::new()
         };
+
+        let (parker, backpressure_thread, command_receiver, controller) = ControllerInner::new(
+            pipeline_config,
+            catalog,
+            lir,
+            error_cb,
+            processed_records,
+            &resume_info,
+        )?;
+
+        controller
+            .status
+            .set_bootstrap_in_progress(circuit.bootstrap_in_progress());
+
+        let input_metadata = input_metadata.map(|input_metadata| {
+            input_metadata
+                .into_iter()
+                .map(|(name, seek)| (name, Some(Resume::Seek { seek })))
+                .collect()
+        });
 
         let ft = match ft_model {
             Some(FtModel::ExactlyOnce) => {
@@ -1666,7 +1676,7 @@ impl FtState {
             controller.disconnect_input(&endpoint_id);
         }
         for (endpoint_name, config) in &metadata.add_inputs {
-            controller.connect_input(endpoint_name, config)?;
+            controller.connect_input(endpoint_name, config, None)?;
         }
         for (endpoint_name, pause) in &metadata.changed_inputs {
             if *pause {
@@ -2556,6 +2566,7 @@ impl ControllerInner {
         lir: LirCircuit,
         error_cb: Box<dyn Fn(Arc<ControllerError>) + Send + Sync>,
         processed_records: u64,
+        resume_info: &HashMap<String, JsonValue>,
     ) -> Result<(Parker, BackpressureThread, Receiver<Command>, Arc<Self>), ControllerError> {
         let status = Arc::new(ControllerStatus::new(config.clone(), processed_records));
         let circuit_thread_parker = Parker::new();
@@ -2592,9 +2603,12 @@ impl ControllerInner {
                     let controller = controller.clone();
                     let input_name = input_name.clone();
                     let input_config = input_config.clone();
+
+                    let resume_info = resume_info.get(&*input_name).cloned();
+
                     Box::new(move || {
                         catch_unwind(AssertUnwindSafe(|| {
-                            controller.connect_input(&input_name, &input_config)
+                            controller.connect_input(&input_name, &input_config, resume_info)
                         }))
                         .unwrap_or_else(|_| Err(ControllerError::ControllerPanic))
                     }) as Box<dyn FnOnce() -> Result<_, ControllerError> + Send>
@@ -2625,7 +2639,7 @@ impl ControllerInner {
             source_tasks.chain(sink_tasks),
         )?;
 
-        let _ = controller.connect_input("now", &now_endpoint_config(&config));
+        let _ = controller.connect_input("now", &now_endpoint_config(&config), None);
 
         let backpressure_thread =
             BackpressureThread::new(controller.clone(), backpressure_thread_parker);
@@ -2682,6 +2696,7 @@ impl ControllerInner {
         self: &Arc<Self>,
         endpoint_name: &str,
         endpoint_config: &InputEndpointConfig,
+        resume_info: Option<JsonValue>,
     ) -> Result<EndpointId, ControllerError> {
         let endpoint = input_transport_config_to_endpoint(
             endpoint_config.connector_config.transport.clone(),
@@ -2691,7 +2706,12 @@ impl ControllerInner {
 
         // If `endpoint` is `None`, it means that the endpoint config specifies an integrated
         // input connector.  Such endpoints are instantiated inside `add_input_endpoint`.
-        self.add_input_endpoint(endpoint_name, endpoint_config.clone(), endpoint)
+        self.add_input_endpoint(
+            endpoint_name,
+            endpoint_config.clone(),
+            endpoint,
+            resume_info,
+        )
     }
 
     pub fn disconnect_input(self: &Arc<Self>, endpoint_id: &EndpointId) {
@@ -2709,6 +2729,7 @@ impl ControllerInner {
         endpoint_name: &str,
         endpoint_config: InputEndpointConfig,
         endpoint: Option<Box<dyn TransportInputEndpoint>>,
+        resume_info: Option<JsonValue>,
     ) -> Result<EndpointId, ControllerError> {
         debug!("Adding input endpoint '{endpoint_name}'; config: {endpoint_config:?}");
 
@@ -2787,7 +2808,7 @@ impl ControllerInner {
                     format.new_parser(endpoint_name, input_handle, &format_config.config)?;
 
                 let reader = endpoint
-                    .open(probe, parser, input_handle.schema.clone())
+                    .open(probe, parser, input_handle.schema.clone(), resume_info)
                     .map_err(|e| ControllerError::input_transport_error(endpoint_name, true, e))?;
                 (reader, endpoint.fault_tolerance())
             }
@@ -2797,7 +2818,7 @@ impl ControllerInner {
 
                 let fault_tolerance = endpoint.fault_tolerance();
                 let reader = endpoint
-                    .open(input_handle)
+                    .open(input_handle, resume_info)
                     .map_err(|e| ControllerError::input_transport_error(endpoint_name, true, e))?;
                 (reader, fault_tolerance)
             }

commit 7204f6504db3ff995a71e43fd86aa827dbedf9f3

[adapters] Fix nexmark input connector resume from checkpoint.
Commit 6f345f03a925 ("[adapters] Provide resume info on initialization.")
broke resume for the nexmark connector, because only one of the nexmark
tables (Bid) writes nonempty resume info, but the commit tried to parse it
from the other tables too.  This fixes the problem by ignoring resume
info except on Bid.

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/adapters/src/transport/nexmark.rs
@@ -76,7 +76,9 @@ impl InputGenerator {
         parser: Box<dyn Parser>,
         resume_info: Option<serde_json::Value>,
     ) -> AnyResult<Self> {
-        let resume_info = if let Some(resume_info) = resume_info {
+        let resume_info = if config.table != NexmarkTable::Bid {
+            None
+        } else if let Some(resume_info) = resume_info {
             Some(parse_resume_info::<Metadata>(&resume_info)?)
         } else {
             None