3bc7e97726ca6cc9c595a26d37f616381e16bb7a
Gerd Zellweger <mail@gerdzellweger.com>
2025-07-24 20:18:06 -0700

    [py] Add ability to specify API key for test client
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -2,13 +2,14 @@ from feldera.rest import FelderaClient
 import os
 import unittest
 
+API_KEY = os.environ.get("FELDERA_API_KEY")
 BASE_URL = os.environ.get("FELDERA_BASE_URL", "http://localhost:8080")
 KAFKA_SERVER = os.environ.get("FELDERA_KAFKA_SERVER", "localhost:19092")
 PIPELINE_TO_KAFKA_SERVER = os.environ.get(
     "FELDERA_PIPELINE_TO_KAFKA_SERVER", "redpanda:9092"
 )
 
-TEST_CLIENT = FelderaClient(BASE_URL)
+TEST_CLIENT = FelderaClient(BASE_URL, api_key=API_KEY)
 
 
 def enterprise_only(fn):
