2d00aca45de26b91e3afe51d54bf7f45517e6d4c
Jørn Lode <jorn.lode@eviny.no>
2025-07-28 14:07:57 +0200

    [adapters] Support configuration of NATS input
    Signed-off-by: Jørn Lode <jorn.lode@eviny.no>

---
+++ crates/adapters/src/transport/nats/input/atomic_option.rs
@@ -0,0 +1,25 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::num::NonZeroU64;
+
+#[derive(Debug)]
+pub struct AtomicOptionNonZeroU64 {
+    inner: AtomicU64,
+}
+
+impl AtomicOptionNonZeroU64 {
+    pub fn new(val: Option<NonZeroU64>) -> Self {
+        let raw = val.map_or(0, NonZeroU64::get);
+        Self {
+            inner: AtomicU64::new(raw),
+        }
+    }
+
+    pub fn load(&self, order: Ordering) -> Option<NonZeroU64> {
+        NonZeroU64::new(self.inner.load(order))
+    }
+
+    pub fn store(&self, val: Option<NonZeroU64>, order: Ordering) {
+        let raw = val.map_or(0, NonZeroU64::get);
+        self.inner.store(raw, order);
+    }
+}
