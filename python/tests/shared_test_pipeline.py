7188c2d425db6dff8edc63c3136a92470d5757b8
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2025-08-01 21:20:15 +0545

    py: refactor python tests to run in parallel
    * python tests should no longer run for hours in CI, seems like it was
    a bug in the stop implementation, where we did for x in
    collection.pop(), which instead of popping all the items, only pops the
    first one and iterates on it.
    
    * adds support for waiting until all data represented by a completion
    token has been processed
    
    * refactor the tests to run in parallel
    
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- python/tests/shared_test_pipeline.py
+++ python/tests/shared_test_pipeline.py
@@ -1,8 +1,6 @@
 import unittest
 from tests import TEST_CLIENT
 from feldera import PipelineBuilder, Pipeline
-from feldera.runtime_config import RuntimeConfig
-from feldera.enums import PipelineStatus
 
 
 class SharedTestPipeline(unittest.TestCase):
@@ -34,25 +32,16 @@ class SharedTestPipeline(unittest.TestCase):
                 cls.ddl,
             ).create_or_replace()
 
-    def set_runtime_config(self, runtime_config: RuntimeConfig):
-        self._pipeline = PipelineBuilder(
-            self.client, self.pipeline_name, self.ddl, runtime_config=runtime_config
+    def setUp(self):
+        p = PipelineBuilder(
+            self.client, self._testMethodName, sql=self.ddl
         ).create_or_replace()
+        self.p = p
 
-    def reset_runtime_config(self):
-        self.set_runtime_config(RuntimeConfig.default())
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls._pipeline is not None:
-            if cls._pipeline.status() != PipelineStatus.STOPPED:
-                cls._pipeline.stop(force=True)
-            cls._pipeline.clear_storage()
-            cls._pipeline = None
-            cls._ddls = []
+    def tearDown(self):
+        self.p.stop(force=True)
+        self.p.clear_storage()
 
     @property
     def pipeline(self) -> Pipeline:
-        p = type(self)._pipeline
-        assert p is not None, "Shared pipeline was not created successfully."
-        return p
+        return self.p
