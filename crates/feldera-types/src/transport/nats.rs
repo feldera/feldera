a9a70ffd1c8c178c74316c49765caaccf69ebf6b
Jørn Lode <jorn.lode@eviny.no>
2025-08-05 13:01:51 +0200

    [adapters] NATS: Restructure config type
    Signed-off-by: Jørn Lode <jorn.lode@eviny.no>

--- crates/feldera-types/src/transport/nats.rs
+++ crates/feldera-types/src/transport/nats.rs
@@ -1,24 +1,86 @@
-use async_nats::jetstream::consumer::pull::OrderedConfig;
+use std::path::PathBuf;
+use time::OffsetDateTime;
+
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use utoipa::ToSchema;
 
+fn is_default<T: Default + Eq>(t: &T) -> bool {
+    t == &T::default()
+}
+
+// TODO How does the user choose? Think about what "UI" you would prefer.
 #[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
-// TODO Expose more/all of async_nats::ConnectOptions here
-pub struct NatsInputConfig {
+pub enum Credentials {
+    FromString(String),
+    FromFile(PathBuf),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
+pub struct UserAndPassword {
+    pub user: String,
+    pub password: String,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema, Default)]
+pub struct Auth {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub credentials: Option<Credentials>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub jwt: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub nkey: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub token: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub user_and_password: Option<UserAndPassword>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
+pub struct ConnectOptions {
     pub server_url: String,
-    pub credentials: Option<String>,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub auth: Auth,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
+pub enum DeliverPolicy {
+    All,
+    Last,
+    New,
+    ByStartSequence { start_sequence: u64 },
+    ByStartTime { start_time: OffsetDateTime },
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
+pub struct ConsumerConfig {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub filter_subjects: Vec<String>,
+    //pub replay_policy: ReplayPolicy,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub rate_limit: u64,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub sample_frequency: u8,
+    pub deliver_policy: DeliverPolicy,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub max_waiting: i64,
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub metadata: HashMap<String, String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub max_batch: Option<i64>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub max_bytes: Option<i64>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub max_expires: Option<std::time::Duration>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
+pub struct NatsInputConfig {
+    pub connection_config: ConnectOptions,
     pub stream_name: String,
-    // TODO: Having feldera-types depend on async_nats is probably undesirable.
-    //
-    // TODO: Currently we have to supply all those settings which are not tagged with
-    // `serde(default)`, which seems a bit arbitrary.
-    //
-    // TODO: For the `max_*` settings, judging by the nats-server source code, it
-    // seems like -1 represents an "unset" value. I'd prefer to use strong types instead of
-    // sentinel values here, and not have this API be tainted by Golang's ways.
-    // Also, any of these settings which are set to their `Default::default()`
-    // value will not be serialized (by async_nats), so `0` is also
-    // interpreted as "unset". For some reason though, the deserializer for `OrderedConfig`
-    // does not tag them with `serde(default)`, so they can't be omitted in the user config.
-    pub consumer_config: OrderedConfig,
+    pub consumer_config: ConsumerConfig,
 }
