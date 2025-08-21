a4ca99ae0f1490a8fc4d4f023b6d124a242f8b19
rivudhk <rivudhkr@gmail.com>
2025-08-21 19:01:40 +0545

    py: add suggested fix
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/negative_tests/neg_arithmetic.py
+++ python/tests/negative_tests/neg_arithmetic.py
@@ -9,34 +9,28 @@ class neg_add_tiny_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_tiny_int AS SELECT
-                      a.tiny_int + b.tiny_int AS tiny_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '120 + 100' causes overflow"
+                      tiny_int + tiny_int2 AS tiny_int
+                      FROM numeric_tbl"""
+        self.expected_error = "'120 + 100' causes overflow"
 
 
 class neg_add_small_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_small_int AS SELECT
-                      a.small_int + b.small_int AS small_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '32750 + 32700' causes overflow"
+                      small_int + small_int2 AS small_int
+                      FROM numeric_tbl"""
+        self.expected_error = "'32750 + 32700' causes overflow"
 
 
 class neg_add_big_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_big_int AS SELECT
-                      a.big_int + b.big_int AS big_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
+                      big_int + big_int2 AS big_int
+                      FROM numeric_tbl"""
         self.expected_error = (
-            "panic message: '8123302036854775807 + 9223372036854775807' causes overflow"
+            "'8123302036854775807 + 9223372036854775807' causes overflow"
         )
 
 
@@ -44,55 +38,47 @@ class neg_add_intt(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_intt AS SELECT
-                      a.intt + b.intt AS intt
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '2147483647 + 2147483000' causes overflow"
+                      intt + intt2 AS intt
+                      FROM numeric_tbl"""
+        self.expected_error = "'2147483647 + 2147483000' causes overflow"
 
 
 class neg_add_tiny_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_tiny_un_int AS SELECT
-                      a.tiny_int + b.tiny_int AS tiny_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '250 + 200' causes overflow"
+                      tiny_int + tiny_int2 AS tiny_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'250 + 200' causes overflow"
 
 
 class neg_add_small_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_small_int AS SELECT
-                      a.small_int + b.small_int AS small_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '65430 + 65435' causes overflow"
+                      small_int + small_int2 AS small_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'65430 + 65435' causes overflow"
 
 
 class neg_add_big_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_big_un_int AS SELECT
-                      a.big_int + b.big_int AS big_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '12446742073709541615 + 18446742073709541615' causes overflow"
+                      big_int + big_int2 AS big_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = (
+            "'12446742073709541615 + 18446742073709541615' causes overflow"
+        )
 
 
 class neg_add_un_intt(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW add_un_intt AS SELECT
-                      a.intt + b.intt AS un_intt
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '4294966290 + 4292966290' causes overflow"
+                      intt + intt2 AS un_intt
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'4294966290 + 4292966290' causes overflow"
 
 
 # Multiplication
@@ -100,34 +86,28 @@ class neg_mul_tiny_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_tiny_int AS SELECT
-                      a.tiny_int * b.tiny_int AS tiny_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '120 * 100' causes overflow"
+                      tiny_int * tiny_int2 AS tiny_int
+                      FROM numeric_tbl"""
+        self.expected_error = "'120 * 100' causes overflow"
 
 
 class neg_mul_small_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_small_int AS SELECT
-                      a.small_int * b.small_int AS small_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '32750 * 32700' causes overflow"
+                      small_int * small_int2 AS small_int
+                      FROM numeric_tbl"""
+        self.expected_error = "'32750 * 32700' causes overflow"
 
 
 class neg_mul_big_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_big_int AS SELECT
-                      a.big_int * b.big_int AS big_int
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
+                      big_int * big_int2 AS big_int
+                      FROM numeric_tbl"""
         self.expected_error = (
-            "panic message: '8123302036854775807 * 9223372036854775807' causes overflow"
+            "'8123302036854775807 * 9223372036854775807' causes overflow"
         )
 
 
@@ -135,52 +115,44 @@ class neg_mul_intt(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_intt AS SELECT
-                      a.intt * b.intt AS intt
-                      FROM numeric_tbl a
-                      JOIN numeric_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '2147483647 * 2147483000' causes overflow"
+                      intt * intt2 AS intt
+                      FROM numeric_tbl"""
+        self.expected_error = "'2147483647 * 2147483000' causes overflow"
 
 
 class neg_mul_tiny_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_tiny_un_int AS SELECT
-                      a.tiny_int * b.tiny_int AS tiny_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '250 * 200' causes overflow"
+                      tiny_int * tiny_int2 AS tiny_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'250 * 200' causes overflow"
 
 
 class neg_mul_small_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_small_int AS SELECT
-                      a.small_int * b.small_int AS small_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '65430 * 65435' causes overflow"
+                      small_int * small_int2 AS small_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'65430 * 65435' causes overflow"
 
 
 class neg_mul_big_un_int(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_big_un_int AS SELECT
-                      a.big_int * b.big_int AS big_un_int
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '12446742073709541615 * 18446742073709541615' causes overflow"
+                      big_int * big_int2 AS big_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = (
+            "'12446742073709541615 * 18446742073709541615' causes overflow"
+        )
 
 
 class neg_mul_un_intt(TstView):
     def __init__(self):
         # checked manually
         self.sql = """CREATE MATERIALIZED VIEW mul_un_intt AS SELECT
-                      a.intt * b.intt AS un_intt
-                      FROM numeric_un_tbl a
-                      JOIN numeric_un_tbl b
-                      ON a.id = 0 AND b.id = 1"""
-        self.expected_error = "panic message: '4294966290 * 4292966290' causes overflow"
+                      intt * intt2 AS un_intt
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'4294966290 * 4292966290' causes overflow"
--- python/tests/negative_tests/neg_table.py
+++ python/tests/negative_tests/neg_table.py
@@ -8,25 +8,26 @@ class neg_numeric_tbl(TstTable):
         self.sql = """CREATE TABLE numeric_tbl(
                       id INT,
                       tiny_int TINYINT,
+                      tiny_int2 TINYINT,
                       small_int SMALLINT,
+                      small_int2 SMALLINT,
                       intt INTEGER,
-                      big_int BIGINT
+                      intt2 INTEGER,
+                      big_int BIGINT,
+                      big_int2 BIGINT
                       )"""
         self.data = [
             {
                 "id": 0,
                 "tiny_int": 120,
+                "tiny_int2": 100,
                 "small_int": 32750,
+                "small_int2": 32700,
                 "intt": 2147483647,
+                "intt2": 2147483000,
                 "big_int": 8123302036854775807,
-            },
-            {
-                "id": 1,
-                "tiny_int": 100,
-                "small_int": 32700,
-                "intt": 2147483000,
-                "big_int": 9223372036854775807,
-            },
+                "big_int2": 9223372036854775807,
+            }
         ]
 
 
@@ -37,23 +38,24 @@ class neg_numeric_unsigned_tbl(TstTable):
         self.sql = """CREATE TABLE numeric_un_tbl(
                       id INT,
                       tiny_int TINYINT UNSIGNED,
+                      tiny_int2 TINYINT UNSIGNED,
                       small_int SMALLINT UNSIGNED,
+                      small_int2 SMALLINT UNSIGNED,
                       intt INTEGER UNSIGNED,
-                      big_int BIGINT UNSIGNED
+                      intt2 INTEGER UNSIGNED,
+                      big_int BIGINT UNSIGNED,
+                      big_int2 BIGINT UNSIGNED
                       )"""
         self.data = [
             {
                 "id": 0,
                 "tiny_int": 250,
+                "tiny_int2": 200,
                 "small_int": 65430,
+                "small_int2": 65435,
                 "intt": 4294966290,
+                "intt2": 4292966290,
                 "big_int": 12446742073709541615,
-            },
-            {
-                "id": 1,
-                "tiny_int": 200,
-                "small_int": 65435,
-                "intt": 4292966290,
-                "big_int": 18446742073709541615,
+                "big_int2": 18446742073709541615,
             },
         ]
