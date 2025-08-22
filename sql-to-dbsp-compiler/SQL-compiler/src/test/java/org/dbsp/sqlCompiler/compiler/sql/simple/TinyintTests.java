966c0c5728642ffe52d797cb6588e8f199be37f8
Mihai Budiu <mbudiu@feldera.com>
2025-08-22 00:57:22 -0500

    [SQLLIB] Include type in error message about overflow
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/TinyintTests.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/TinyintTests.java
@@ -325,7 +325,7 @@ public class TinyintTests extends SqlIoTest {
 
     @Test
     public void testSelectOverflow() {
-        this.qf("SELECT i.f1, i.f1 * 2::TINYINT AS x FROM INT_TBL i", "causes overflow");
+        this.qf("SELECT i.f1, i.f1 * 2::TINYINT AS x FROM INT_TBL i", "causes overflow for type TINYINT");
         this.qf( "SELECT i.f1, i.f1 + '2'::TINYINT AS x FROM INT_TBL i", "causes overflow");
         this.qf("SELECT i.f1, i.f1 - '2'::TINYINT AS x FROM INT_TBL i", "causes overflow");
     }
