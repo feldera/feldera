d85d89e598c42a5c6015bc891982354e94a561b2
Leonid Ryzhyk <leonid@feldera.com>
2025-08-28 16:25:40 -0700

    [py] Fix checkpoint().
    The `Pipeline.checkpoint()` method returned checkpoint sequence number in
    one path and request status in the other. This commit fixes it to return
    sequence number consistently.
    
    Also add `is_complete()` method to check if the pipeline is complete
    without blocking.
    
    Signed-off-by: Leonid Ryzhyk <leonid@feldera.com>

--- python/feldera/pipeline.py
+++ python/feldera/pipeline.py
@@ -326,7 +326,7 @@ class Pipeline:
                     f" time {elapsed}s, timeout: {timeout_s}s"
                 )
 
-            pipeline_complete: bool = self.stats().global_metrics.pipeline_complete
+            pipeline_complete: bool = self.is_complete()
             if pipeline_complete is None:
                 raise RuntimeError(
                     "received unknown metrics from the pipeline, pipeline_complete is None"
@@ -338,6 +338,19 @@ class Pipeline:
 
         if force_stop:
             self.stop(force=True)
+
+    def is_complete(self) -> bool:
+        """
+        Check if the pipeline has completed processing all input records.
+
+        Returns True if (1) all input connectors attached to the
+        pipeline have finished reading their input data sources and issued
+        end-of-input notifications to the pipeline, and (2) all inputs received
+        from these connectors have been fully processed and corresponding
+        outputs have been sent out through the output connectors.
+        """
+
+        return self.stats().global_metrics.pipeline_complete
 
     def start(self, wait: bool = True, timeout_s: Optional[float] = None):
         """
@@ -625,6 +638,8 @@ metrics"""
         :param timeout_s: The maximum time (in seconds) to wait for the
             checkpoint to complete.
 
+        :return: The checkpoint sequence number.
+
         :raises FelderaAPIError: If enterprise features are not enabled.
         """
 
@@ -647,9 +662,7 @@ pipeline '{self.name}' to make checkpoint '{seq}'"""
                 time.sleep(0.1)
                 continue
 
-            return status
-
-        return seq
+            return seq
 
     def checkpoint_status(self, seq: int) -> CheckpointStatus:
         """
