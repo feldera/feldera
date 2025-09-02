5c0e2437a45eae3c551f87d4af741e9cabc87dcb
rivudhk <rivudhkr@gmail.com>
2025-09-02 21:16:57 +0545

    py: add more negative tests
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

---
+++ python/tests/runtime_aggtest/negative_tests/test_neg_arithmetic.py
@@ -0,0 +1,261 @@
+from tests.runtime_aggtest.aggtst_base import TstView
+
+
+# Overflow in Arithmetic Operations
+
+
+# Addition
+class neg_add_tiny_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_tiny_int AS SELECT
+                      tiny_int + tiny_int2 AS tiny_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'120 + 100' causes overflow for type TINYINT"
+
+
+class neg_add_small_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_small_int AS SELECT
+                      small_int + small_int2 AS small_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'32750 + 32700' causes overflow for type SMALLINT"
+
+
+class neg_add_big_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_big_int AS SELECT
+                      big_int + big_int2 AS big_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'8123302036854775807 + 9223372036854775807' causes overflow for type BIGINT"
+
+
+class neg_add_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_intt AS SELECT
+                      intt + intt2 AS intt
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'2147483647 + 2147483000' causes overflow for type INTEGER"
+        )
+
+
+class neg_add_tiny_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_tiny_un_int AS SELECT
+                      tiny_int + tiny_int2 AS tiny_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'250 + 200' causes overflow for type TINYINT UNSIGNED"
+
+
+class neg_add_small_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_small_int AS SELECT
+                      small_int + small_int2 AS small_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'65430 + 65435' causes overflow for type SMALLINT UNSIGNED"
+        )
+
+
+class neg_add_big_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_big_un_int AS SELECT
+                      big_int + big_int2 AS big_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'12446742073709541615 + 18446742073709541615' causes overflow for type BIGINT UNSIGNED"
+
+
+class neg_add_un_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW add_un_intt AS SELECT
+                      intt + intt2 AS un_intt
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'4294966290 + 4292966290' causes overflow for type INTEGER UNSIGNED"
+        )
+
+
+# Multiplication
+class neg_mul_tiny_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_tiny_int AS SELECT
+                      tiny_int * tiny_int2 AS tiny_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'120 * 100' causes overflow for type TINYINT"
+
+
+class neg_mul_small_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_small_int AS SELECT
+                      small_int * small_int2 AS small_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'32750 * 32700' causes overflow for type SMALLINT"
+
+
+class neg_mul_big_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_big_int AS SELECT
+                      big_int * big_int2 AS big_int
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'8123302036854775807 * 9223372036854775807' causes overflow for type BIGINT"
+
+
+class neg_mul_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_intt AS SELECT
+                      intt * intt2 AS intt
+                      FROM numeric_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'2147483647 * 2147483000' causes overflow for type INTEGER"
+        )
+
+
+class neg_mul_tiny_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_tiny_un_int AS SELECT
+                      tiny_int * tiny_int2 AS tiny_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'250 * 200' causes overflow for type TINYINT UNSIGNED"
+
+
+class neg_mul_small_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_small_int AS SELECT
+                      small_int * small_int2 AS small_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'65430 * 65435' causes overflow for type SMALLINT UNSIGNED"
+        )
+
+
+class neg_mul_big_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_big_un_int AS SELECT
+                      big_int * big_int2 AS big_un_int
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = "'12446742073709541615 * 18446742073709541615' causes overflow for type BIGINT UNSIGNED"
+
+
+class neg_mul_un_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW mul_un_intt AS SELECT
+                      intt * intt2 AS un_intt
+                      FROM numeric_un_tbl
+                      WHERE id = 0"""
+        self.expected_error = (
+            "'4294966290 * 4292966290' causes overflow for type INTEGER UNSIGNED"
+        )
+
+
+# Division
+class neg_div_tiny_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_tiny_int AS SELECT
+                      tiny_int / tiny_int2 AS tiny_int
+                      FROM numeric_tbl
+                      WHERE id = 1"""
+        self.expected_error = "'-128 / -1' causes overflow for type TINYINT"
+
+
+class neg_div_small_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_small_int AS SELECT
+                      small_int / small_int2 AS small_int
+                      FROM numeric_tbl
+                      WHERE id = 1"""
+        self.expected_error = "'-32768 / -1' causes overflow for type SMALLINT"
+
+
+class neg_div_big_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_big_int AS SELECT
+                      big_int / big_int2 AS big_int
+                      FROM numeric_tbl
+                      WHERE id = 1"""
+        self.expected_error = (
+            "'-9223372036854775808 / -1' causes overflow for type BIGINT"
+        )
+
+
+class neg_div_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_intt AS SELECT
+                      intt / intt2 AS intt
+                      FROM numeric_tbl
+                      WHERE id = 1"""
+        self.expected_error = "'-2147483648 / -1' causes overflow for type INTEGER"
+
+
+class neg_div_tiny_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_tiny_un_int AS SELECT
+                      tiny_int / 0::TINYINT UNSIGNED AS tiny_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'250 / 0' causes overflow for type TINYINT UNSIGNED"
+
+
+class neg_div_small_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_small_un_int AS SELECT
+                      small_int / 0::SMALLINT UNSIGNED AS small_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = "'65430 / 0' causes overflow for type SMALLINT UNSIGNED"
+
+
+class neg_div_big_un_int(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_big_un_int AS SELECT
+                      big_int / 0::BIGINT UNSIGNED AS big_un_int
+                      FROM numeric_un_tbl"""
+        self.expected_error = (
+            "'12446742073709541615 / 0' causes overflow for type BIGINT UNSIGNED"
+        )
+
+
+class neg_div_un_intt(TstView):
+    def __init__(self):
+        # checked manually
+        self.sql = """CREATE MATERIALIZED VIEW div_un_intt AS SELECT
+                      intt / 0::INTEGER UNSIGNED AS un_intt
+                      FROM numeric_un_tbl"""
+        self.expected_error = (
+            "'4294966290 / 0' causes overflow for type INTEGER UNSIGNED"
+        )
--- python/tests/runtime_aggtest/negative_tests/test_neg_table.py
+++ python/tests/runtime_aggtest/negative_tests/test_neg_table.py
@@ -27,7 +27,41 @@ class neg_numeric_tbl(TstTable):
                 "intt2": 2147483000,
                 "big_int": 8123302036854775807,
                 "big_int2": 9223372036854775807,
-            }
+            },
+            {
+                "id": 1,
+                "tiny_int": -128,
+                "tiny_int2": -1,
+                "small_int": -32768,
+                "small_int2": -1,
+                "intt": -2147483648,
+                "intt2": -1,
+                "big_int": -9223372036854775808,
+                "big_int2": -1,
+            },
+        ]
+
+
+class neg_numeric_tbl1(TstTable):
+    """Define the table used by negative numeric tests"""
+
+    def __init__(self):
+        self.sql = """CREATE TABLE numeric_tbl1(
+                      id INT,
+                      tiny_int TINYINT,
+                      small_int SMALLINT,
+                      intt INTEGER,
+                      big_int BIGINT
+                      )"""
+        self.data = [
+            {
+                "id": 0,
+                "tiny_int": 127,
+                "small_int": 32767,
+                "intt": 2147483647,
+                "big_int": 9223372036854775807,
+            },
+            {"id": 1, "tiny_int": 1, "small_int": 1, "intt": 1, "big_int": 1},
         ]
 
 
@@ -58,4 +92,27 @@ class neg_numeric_unsigned_tbl(TstTable):
                 "big_int": 12446742073709541615,
                 "big_int2": 18446742073709541615,
             },
+        ]
+
+
+class neg_numeric_unsigned_tbl1(TstTable):
+    """Define the table used by negative integer unsigned tests"""
+
+    def __init__(self):
+        self.sql = """CREATE TABLE numeric_un_tbl1(
+                      id INT,
+                      tiny_int TINYINT UNSIGNED,
+                      small_int SMALLINT UNSIGNED,
+                      intt INTEGER UNSIGNED,
+                      big_int BIGINT UNSIGNED
+                      )"""
+        self.data = [
+            {
+                "id": 0,
+                "tiny_int": 255,
+                "small_int": 65535,
+                "intt": 4294967295,
+                "big_int": 18446744073709551615,
+            },
+            {"id": 1, "tiny_int": 1, "small_int": 1, "intt": 1, "big_int": 1},
         ]
