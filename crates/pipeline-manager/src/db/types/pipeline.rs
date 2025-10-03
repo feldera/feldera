c7765c4fe7a4be793b72fdae215d498751a192a9
Leonid Ryzhyk <ryzhyk@gmail.com>
2025-10-02 23:09:48 -0700

    [manager] Include program config when selector=status.
    The config is small enough to include in the abbreviated version of the
    descriptor. We will use it to implement stale pipeline platform version
    in a subsequent commit.
    
    Signed-off-by: Leonid Ryzhyk <ryzhyk@gmail.com>

--- crates/pipeline-manager/src/db/types/pipeline.rs
+++ crates/pipeline-manager/src/db/types/pipeline.rs
@@ -263,6 +263,7 @@ pub struct ExtendedPipelineDescrMonitoring {
     pub created_at: DateTime<Utc>,
     pub version: Version,
     pub platform_version: String,
+    pub program_config: serde_json::Value,
     pub program_version: Version,
     pub program_status: ProgramStatus,
     pub program_status_since: DateTime<Utc>,
