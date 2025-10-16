1dc737d8e9905a66f8163101c25cf621f3da643c
Ben Pfaff <blp@feldera.com>
2025-10-16 14:28:08 -0700

    [dbsp] Rename Trace::commit to Trace::save.
    It didn't commit the files it wrote, so the name was confusing.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/operator/dynamic/accumulate_trace.rs
+++ crates/dbsp/src/operator/dynamic/accumulate_trace.rs
@@ -808,7 +808,7 @@ where
         let pid = require_persistent_id(pid, &self.global_id)?;
         self.trace
             .as_mut()
-            .map(|trace| trace.commit(base, pid, files))
+            .map(|trace| trace.save(base, pid, files))
             .unwrap_or(Ok(()))
     }
 
