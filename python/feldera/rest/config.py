2a787c655057a66f6ea5e7600c0b8d37249b7192
Gerd Zellweger <mail@gerdzellweger.com>
2025-11-13 21:28:23 -0800

    [py] more robust retry logic in presence of upgrades
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- python/feldera/rest/config.py
+++ python/feldera/rest/config.py
@@ -1,7 +1,8 @@
-from typing import Optional
-import os
-from feldera.rest._helpers import requests_verify_from_env
 import logging
+import os
+from typing import Optional
+
+from feldera.rest._helpers import requests_verify_from_env
 
 
 class Config:
@@ -18,18 +19,22 @@ class Config:
         timeout: Optional[float] = None,
         connection_timeout: Optional[float] = None,
         requests_verify: Optional[bool | str] = None,
+        health_recovery_timeout: Optional[int] = None,
     ) -> None:
         """
         See documentation of the `FelderaClient` constructor for the other arguments.
 
         :param version: (Optional) Version of the API to use.
             Default: `v0`.
+        :param health_recovery_timeout: (Optional) Maximum time in seconds to wait for cluster health recovery after a 502 error.
+            Default: `300` (5 minutes).
         """
         self.url: str = url or os.environ.get("FELDERA_HOST") or "http://localhost:8080"
         self.api_key: Optional[str] = api_key or os.environ.get("FELDERA_API_KEY")
         self.version: str = version or "v0"
         self.timeout: Optional[float] = timeout
         self.connection_timeout: Optional[float] = connection_timeout
+        self.health_recovery_timeout: int = health_recovery_timeout or 300
         env_verify = requests_verify_from_env()
         self.requests_verify: bool | str = (
             requests_verify if requests_verify is not None else env_verify
--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -1,11 +1,17 @@
+import logging
+import os
+
 from feldera.testutils import (
-    TEST_CLIENT,
-    unique_pipeline_name,
-    enterprise_only,
     API_KEY,
     BASE_URL,
     FELDERA_REQUESTS_VERIFY,
+    TEST_CLIENT,
+    enterprise_only,
+    unique_pipeline_name,
 )
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logging.basicConfig(level=LOGLEVEL)
 
 __all__ = [
     "TEST_CLIENT",
