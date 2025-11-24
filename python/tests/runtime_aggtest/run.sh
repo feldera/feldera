873606dbf89cfba1c851064c6d90d73be5a6374c
Swanand Mulay <73115739+swanandx@users.noreply.github.com>
2025-11-24 17:01:10 +0530

    ci: run aggtests in parallel using xargs
    Our ci has parallel compilation setup, so multiple pipelines can be
    compiled at once. We use xargs -P to run runtime_aggtests in parallel
    so we can take advantage of it and speed up our ci.
    
    Signed-off-by: Swanand Mulay <73115739+swanandx@users.noreply.github.com>

--- python/tests/runtime_aggtest/run.sh
+++ python/tests/runtime_aggtest/run.sh
@@ -2,19 +2,29 @@
 
 set -ex
 
-function runtest() {
+TESTS=(
+    "aggregate_tests/main.py"
+    "aggregate_tests2/main.py"
+    "aggregate_tests3/main.py"
+    "aggregate_tests4/main.py"
+    "aggregate_tests5/main.py"
+    "aggregate_tests6/main.py"
+    "arithmetic_tests/main.py"
+    "asof_tests/main.py"
+    "complex_type_tests/main.py"
+    "illarg_tests/main.py"
+    "negative_tests/main.py"
+    "orderby_tests/main.py"
+    "variant_tests/main.py"
+)
+
+function run_one() {
     uv run --locked $PYTHONPATH/tests/runtime_aggtest/$1
 }
 
-runtest aggregate_tests/main.py
-runtest aggregate_tests2/main.py
-runtest aggregate_tests3/main.py
-runtest aggregate_tests4/main.py
-runtest aggregate_tests5/main.py
-runtest aggregate_tests6/main.py
-runtest arithmetic_tests/main.py
-runtest complex_type_tests/main.py
-runtest orderby_tests/main.py
-runtest variant_tests/main.py
-runtest illarg_tests/main.py
-runtest negative_tests/main.py
+if [ "${RUNTIME_AGGTEST_JOBS:-1}" -le 1 ]; then
+  for t in "${TESTS[@]}"; do run_one "$t"; done
+else
+  echo "Running tests in parallel: ${RUNTIME_AGGTEST_JOBS} jobs"
+  printf '%s\n' "${TESTS[@]}" | xargs -P "${RUNTIME_AGGTEST_JOBS}" -I{} bash -e -c 'echo "Running: {}"; uv run --locked "$PYTHONPATH/tests/runtime_aggtest/{}"'
+fi
