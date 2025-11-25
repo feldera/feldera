b7165beefb6c13cd21da21d6a112cb6c0299b0c3
rivudhk <rivudhkr@gmail.com>
2025-11-25 17:08:39 +0545

    py: add multi join tests for AS-Of Joins
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/runtime_aggtest/asof_tests/main.py
+++ python/tests/runtime_aggtest/asof_tests/main.py
@@ -4,6 +4,7 @@ from tests.runtime_aggtest.aggtst_base import *  # noqa: F403
 from tests.runtime_aggtest.atest_run import run  # noqa: F403
 from test_asof_tbl import *  # noqa: F403
 from test_asof import *  # noqa: F403
+from test_asof_multijoins import *  # noqa: F403
 
 
 def main():
