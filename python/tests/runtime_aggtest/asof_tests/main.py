8a46cd5cc10ec3d24734826f9188f8e6b8cb7c8a
rivudhk <rivudhkr@gmail.com>
2025-12-03 01:07:53 +0545

    py: added as-of tests with sub queries on left input
    Signed-off-by: rivudhk <rivudhkr@gmail.com>

--- python/tests/runtime_aggtest/asof_tests/main.py
+++ python/tests/runtime_aggtest/asof_tests/main.py
@@ -2,10 +2,12 @@
 
 from tests.runtime_aggtest.aggtst_base import *  # noqa: F403
 from tests.runtime_aggtest.atest_run import run  # noqa: F403
+
 from test_asof_tbl import *  # noqa: F403
 from test_asof import *  # noqa: F403
 from test_asof_multijoins import *  # noqa: F403
 from test_asof_illarg import *  # noqa: F403
+from test_asof_subq import *  # noqa: F403
 
 
 def main():
