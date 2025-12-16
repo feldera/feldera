08a2a812b16977fe276b7c69df2f32dbe47ad5d7
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2025-12-16 08:22:31 +0000

    update samply url to feldera/samply
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- crates/adapters/src/controller.rs
+++ crates/adapters/src/controller.rs
@@ -822,7 +822,7 @@ impl Controller {
             )
         }
 
-        let err_msg = "is Samply installed and in `$PATH`? try: `curl --proto '=https' --tlsv1.2 -LsSf https://github.com/abhizer/samply/releases/download/v0.13.2/samply-installer.sh | sh`".to_string();
+        let err_msg = "is Samply installed and in `$PATH`? try: `curl --proto '=https' --tlsv1.2 -LsSf https://github.com/feldera/samply/releases/download/v0.13.2/samply-installer.sh | sh`".to_string();
 
         let version = tokio::process::Command::new("samply")
             .arg("--version")
