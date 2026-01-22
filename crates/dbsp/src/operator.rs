dd03e18698f7b191e44666dbda8c1953f368f866
Karakatiza666 <bulakh.96@gmail.com>
2026-01-22 08:50:23 +0000

    [dbsp] Re-implement OrderStatisticsMultiset for O(log(n)) complexity for insert, update, select and rank
    Signed-off-by: Karakatiza666 <bulakh.96@gmail.com>

--- crates/dbsp/src/operator.rs
+++ crates/dbsp/src/operator.rs
@@ -66,8 +66,8 @@ pub use apply::Apply;
 pub use condition::Condition;
 pub use delta0::Delta0;
 pub use dynamic::aggregate::{
-    Aggregator, Avg, Fold, Max, MaxSemigroup, Min, MinSemigroup, OrderStatisticTree,
-    OrderStatisticTreeSemigroup, PercentileCont, PercentileDisc, Postprocess,
+    Aggregator, Avg, Fold, Max, MaxSemigroup, Min, MinSemigroup, OrderStatisticsMultiset,
+    OrderStatisticsMultisetSemigroup, PercentileCont, PercentileDisc, Postprocess,
 };
 pub use dynamic::neighborhood::DynNeighborhood;
 pub use generator::{ConstantGenerator, Generator, GeneratorNested, TransactionGenerator};
