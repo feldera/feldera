2420056d60db5890fe41317a3135dbb74a157846
Ben Pfaff <blp@feldera.com>
2026-02-04 15:03:25 -0800

    [adapters] Fix multihost connector-initiated transactions in controller.
    In multihost, controller requests for transactions are decoupled from
    the coordinator starting and committing transactions as a whole.  As a
    result, it's common for a connector to request to start a transaction
    while the coordinator is still committing one, which caused an error to
    be logged.  This also caused later error logging when the connectors tried
    to commit the transaction that it wasn't able to start.  The result was
    that connector-initiated transactions were mostly ineffective.
    
    This commit changes the model for connector-initiated transactions in
    multihost.  Instead of tying them to the overall transaction status, with
    this commit, they are simply added and removed when the connector requests.
    There is no longer any need for the coordinator to advance them from
    requesting a commit to finally committed.  This was previously included
    to match the single-host model, but it should not be necessary because
    even if a connector quickly commits and starts a transaction, the
    coordinator will still be notified of the toggle and can (and does)
    treat the connector as requesting a commit on its side.
    
    This worked fine for me over a multiple-minute multihost run with
    connector-initiated transactions.
    
    Issue: https://github.com/feldera/cloud/issues/1436
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/adapters/src/controller.rs
+++ crates/adapters/src/controller.rs
@@ -4570,10 +4570,12 @@ impl TransactionInitiators {
     /// Clear the transaction requested state.
     ///
     /// Must be invoked when a transaction is committed.
-    fn clear(&mut self) {
+    fn clear(&mut self, is_multihost: bool) {
         self.transaction_id = None;
         self.initiated_by_api = None;
-        self.initiated_by_connectors.clear();
+        if !is_multihost {
+            self.initiated_by_connectors.clear();
+        }
     }
 
     /// Returns true if at least one participant has started a transaction and
@@ -4704,14 +4706,34 @@ impl TransactionInfo {
         endpoint_name: &str,
         label: Option<&str>,
     ) -> Result<(), ControllerError> {
+        if self.is_multihost {
+            match self
+                .initiators
+                .initiated_by_connectors
+                .entry(endpoint_name.to_string())
+            {
+                Entry::Vacant(entry) => {
+                    entry.insert(ConnectorTransactionPhase {
+                        phase: TransactionPhase::Started,
+                        label: label.map(String::from),
+                    });
+                    debug!("Connector {endpoint_name} initiated request for transaction");
+                    return Ok(());
+                }
+                Entry::Occupied(_entry) => {
+                    error!("Connector {endpoint_name} had already requested transaction");
+                    return Err(ControllerError::TransactionInProgress);
+                }
+            }
+        }
+
         let mut initiated = false;
 
         if self.transaction_state.is_committing() {
             return Err(ControllerError::TransactionInProgress);
         }
 
-        if self.initiators.transaction_id.is_none() && !self.is_multihost {
-            assert!(!self.initiators.is_ongoing(self.is_multihost));
+        if self.initiators.transaction_id.is_none() {
             self.last_transaction_id += 1;
             self.initiators.transaction_id = Some(self.last_transaction_id);
             initiated = true;
@@ -4754,6 +4776,20 @@ impl TransactionInfo {
         &mut self,
         endpoint_name: &str,
     ) -> Result<(), ControllerError> {
+        if self.is_multihost {
+            debug!("Connector {endpoint_name} requests transaction commit");
+            if self
+                .initiators
+                .initiated_by_connectors
+                .remove(endpoint_name)
+                .is_some()
+            {
+                return Ok(());
+            } else {
+                return Err(ControllerError::NoTransactionInProgress);
+            }
+        }
+
         let num_active_participants = self.initiators.num_active_participants();
 
         match self
@@ -4765,7 +4801,7 @@ impl TransactionInfo {
                 return Err(ControllerError::NoTransactionInProgress);
             }
             Entry::Occupied(mut entry) => {
-                assert!(self.is_multihost || self.initiators.transaction_id.is_some());
+                assert!(self.initiators.transaction_id.is_some());
                 if entry.get().phase != TransactionPhase::Started {
                     return Err(ControllerError::NoTransactionInProgress);
                 }
@@ -4799,6 +4835,10 @@ impl TransactionInfo {
                 }
             })
             .collect()
+    }
+
+    fn clear_initiators(&mut self) {
+        self.initiators.clear(self.is_multihost);
     }
 }
 
@@ -6120,7 +6160,7 @@ impl ControllerInner {
             .is_ready_to_commit(transaction_info.is_multihost)
             && transaction_info.transaction_state == TransactionState::None
         {
-            transaction_info.initiators.clear();
+            transaction_info.clear_initiators();
         }
 
         self.update_transaction_status(transaction_info);
@@ -6157,7 +6197,7 @@ impl ControllerInner {
             .is_ready_to_commit(transaction_info.is_multihost)
             && transaction_info.transaction_state == TransactionState::None
         {
-            transaction_info.initiators.clear();
+            transaction_info.clear_initiators();
         }
 
         self.update_transaction_status(transaction_info);
@@ -6242,7 +6282,7 @@ impl ControllerInner {
                     // Commit the running transaction.
                     transaction_info.transaction_state =
                         TransactionState::Committing(transaction_id);
-                    transaction_info.initiators.clear();
+                    transaction_info.clear_initiators();
                     Some(transaction_info.transaction_state)
                 }
                 TransactionState::Committing(_) => {
