9abbc51415e8258498907b40cfee4d8904a14fd4
Gerd Zellweger <mail@gerdzellweger.com>
2026-02-12 16:00:41 -0800

    [adapters] add check for runtime version feature
    We did not double check that we're actually running a
    binary that is compiled with a custom runtime.
    
    This would lead to running binaries of the platform
    version when we added a bug to how we generate the cargo files.
    
    This check hardens against this case:
    
    If a runtime version was set but we end up running with a binary
    with the wrong commit sha of the runtime we panic.
    
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- crates/adapters/build.rs
+++ crates/adapters/build.rs
@@ -1,4 +1,9 @@
+use vergen_gitcl::*;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let gitcl = GitclBuilder::default().sha(false).dirty(false).build()?;
+    Emitter::default().add_instructions(&gitcl)?.emit()?;
+
     println!("cargo:rerun-if-env-changed=FELDERA_RUNTIME_OVERRIDE");
     Ok(())
 }
