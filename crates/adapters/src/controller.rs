2dc1d9ed728512895ce71729dfd10ebed39e72c3
Leonid Ryzhyk <ryzhyk@gmail.com>
2026-03-06 13:30:10 -0800

    [adapters] Report recent connector errors in the API.
    Partially fixes #1108.
    
    Today, connector errors are only reported in the log where they can be
    difficult to find or get lost completely if the log is noisy.
    
    This commit exposes connector errors via the REST API:
    - We store up to a fixed number (100) most recent errors of each kind
      (transport/encoder/parser) and for each tag (we reuse the same tags that were
      recently introduced for log throttling).
    
    - Old errors are evicted in FIFO order
    
    - The error list is returned via
      /tables/table_name/connectors/connector_name/stats endpoint, but not via
      global /stats to avoid bloating the output of that endpoint.
    
    Signed-off-by: Leonid Ryzhyk <ryzhyk@gmail.com>

--- crates/adapters/src/controller.rs
+++ crates/adapters/src/controller.rs
@@ -6027,7 +6027,7 @@ impl ControllerInner {
         endpoint_name: &str,
     ) -> Result<ExternalInputEndpointStatus, ControllerError> {
         let endpoint_id = self.input_endpoint_id_by_name(endpoint_name)?;
-        Ok(self.status.input_status()[&endpoint_id].to_api_type())
+        Ok(self.status.input_status()[&endpoint_id].to_api_type(true))
     }
 
     fn output_endpoint_status(
@@ -6035,7 +6035,7 @@ impl ControllerInner {
         endpoint_name: &str,
     ) -> Result<ExternalOutputEndpointStatus, ControllerError> {
         let endpoint_id = self.output_endpoint_id_by_name(endpoint_name)?;
-        Ok(self.status.output_status()[&endpoint_id].to_api_type())
+        Ok(self.status.output_status()[&endpoint_id].to_api_type(true))
     }
 
     fn send_command(&self, command: Command) {
@@ -6061,7 +6061,7 @@ impl ControllerInner {
         tag: Option<&'static str>,
     ) {
         self.status
-            .input_transport_error(endpoint_id, fatal, &error);
+            .input_transport_error(endpoint_id, fatal, tag, &error);
         let tag = tag.map(|tag| format!("{endpoint_name}-{tag}"));
         self.error(
             Arc::new(ControllerError::input_transport_error(
@@ -6074,7 +6074,7 @@ impl ControllerInner {
     }
 
     pub fn parse_error(&self, endpoint_id: EndpointId, endpoint_name: &str, error: ParseError) {
-        self.status.parse_error(endpoint_id);
+        self.status.parse_error(endpoint_id, None, &error);
 
         let tag = error
             .get_error_tag()
@@ -6093,7 +6093,7 @@ impl ControllerInner {
         error: AnyError,
         tag: Option<&'static str>,
     ) {
-        self.status.encode_error(endpoint_id);
+        self.status.encode_error(endpoint_id, tag, &error);
         let tag = tag.map(|tag| format!("{endpoint_name}-{tag}"));
         self.error(
             Arc::new(ControllerError::encode_error(endpoint_name, error)),
@@ -6113,7 +6113,7 @@ impl ControllerInner {
         tag: Option<&'static str>,
     ) {
         self.status
-            .output_transport_error(endpoint_id, fatal, &error);
+            .output_transport_error(endpoint_id, fatal, tag, &error);
         let tag = tag.map(|tag| format!("{endpoint_name}-{tag}"));
         self.error(
             Arc::new(ControllerError::output_transport_error(
