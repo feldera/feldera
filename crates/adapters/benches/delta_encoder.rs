e20b7835b037b25d87869f4bfb4f36cd756697a1
Leonid Ryzhyk <ryzhyk@gmail.com>
2026-04-07 15:10:21 -0700

    [adapters] Delta output: enable checkpoints.
    The Delta output connector did not create periodic checkpoints.
    
    While this is in itself problematic, it also meant that the connector
    became slow over time, due to this delta-rs bug, which causes the
    `update_incremental` function to scan the entire transaction log on
    every commit:
    https://github.com/delta-io/delta-kernel-rs/issues/2103.
    
    This commit:
    - Introduces the `checkpoint_interval` option, which tells
      the connector to configure checkpoint interval when creating
      the table.
    - Creates a CommitBuilder that is actually setup to create
      checkpoints.
    
    Without this fix the time to create a trivial delta commit increases from
    1.5s to 6s after ~1000 commits. With the fix it remains constant at
    ~2s.
    
    Signed-off-by: Leonid Ryzhyk <ryzhyk@gmail.com>

--- crates/adapters/benches/delta_encoder.rs
+++ crates/adapters/benches/delta_encoder.rs
@@ -19,6 +19,7 @@ fn create_indexed_writer(threads: usize, table_uri: &str) -> DeltaTableWriter {
         max_retries: Some(0),
         threads: Some(threads),
         object_store_config: Default::default(),
+        checkpoint_interval: None,
     };
     let key_schema = Some(BenchKeyStruct::relation_schema());
     let mut value_schema = BenchTestStruct::relation_schema();
