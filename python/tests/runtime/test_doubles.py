ce03b37cd439858f71561813cb1e35ce1ed3995d
Mihai Budiu <mbudiu@feldera.com>
2026-05-15 15:53:05 -0700

    [python] Test double serialization
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

---
+++ python/tests/runtime/test_doubles.py
@@ -0,0 +1,58 @@
+import unittest
+
+import math
+from feldera import PipelineBuilder
+from tests import TEST_CLIENT, unique_pipeline_name
+from feldera.runtime_config import RuntimeConfig
+from feldera.testutils import FELDERA_TEST_NUM_WORKERS, FELDERA_TEST_NUM_HOSTS
+
+# Test serialization of doubles
+class TestDouble(unittest.TestCase):
+    def test_local(self):
+        sql = """
+CREATE TABLE t (
+    d DOUBLE
+) WITH ('connectors' = '[{
+   "name": "t",
+   "transport": {
+      "name": "datagen",
+      "config": {
+         "seed": 1,
+         "plan": [{
+            "limit": 1,
+            "fields": {
+               "d": { "values": [0] }
+            }
+         }]
+      }
+   }
+}]');
+
+CREATE MATERIALIZED VIEW v AS
+SELECT 1/d as one, 0/d as zero FROM t;
+        """
+
+        pipeline = PipelineBuilder(
+            TEST_CLIENT,
+            name=unique_pipeline_name("test_doubles"),
+            sql=sql,
+            runtime_config=RuntimeConfig(
+                workers=FELDERA_TEST_NUM_WORKERS,
+                hosts=FELDERA_TEST_NUM_HOSTS,
+            ),
+        ).create_or_replace()
+
+        pipeline.start_paused()
+        pipeline.resume()
+
+        pipeline.wait_for_completion()
+        data = list(pipeline.query_arrow_dicts("SELECT * FROM v"))
+        print(data)
+        assert len(data) == 1
+        assert data[0]["one"] == float('inf')
+        assert math.isnan(data[0]["zero"])
+        pipeline.stop(force=True)
+        pipeline.delete(True)
+
+if __name__ == "__main__":
+    unittest.main()
