0e8e08d2d83bf6377fd335d8f346eed0653f7d3e
Ben Pfaff <blp@feldera.com>
2026-06-12 13:30:20 -0700

    [dbsp] Segregate inter-host exchange channels by message size and type.
    Until now, when one host in a DBSP pipeline sends another one a message
    as part of synchronous or streaming exchange, it sent it over a single
    host-to-host TCP connection.  This means that synchronous exchange has to
    wait for streaming exchange messages, which increases latency.  This
    commit uses different TCP connections for streaming and synchronous
    exchange, which helps.  In addition, it uses separate TCP connections for
    small and large synchronous exchange; we speculate that giving small
    synchronous messages priority can help with broadcast and consensus
    messages (this is unproven).
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/operator/communication.rs
+++ crates/dbsp/src/operator/communication.rs
@@ -3,7 +3,8 @@ mod gather;
 mod shard;
 
 pub(crate) use exchange::{
-    Exchange, ExchangeClients, ExchangeDelivery, ExchangeDirectory, ExchangeId, pop_flushed,
+    Exchange, ExchangeClients, ExchangeDelivery, ExchangeDirectory, ExchangeId, MessageType,
+    pop_flushed,
 };
 pub use exchange::{
     ExchangeActivity, ExchangeReceiver, ExchangeSender, Mailbox, new_exchange_operators,
