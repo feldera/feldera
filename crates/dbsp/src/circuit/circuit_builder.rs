47fba227c856c9fed335e4a3cf57ab6878efc8f9
Ben Pfaff <blp@feldera.com>
2026-06-12 09:42:30 -0700

    [dbsp] Add meaningful names to Broadcast and Consensus operators.
    This makes it easier to identify them in CPU profiles.
    
    Signed-off-by: Ben Pfaff <blp@feldera.com>

--- crates/dbsp/src/circuit/circuit_builder.rs
+++ crates/dbsp/src/circuit/circuit_builder.rs
@@ -4536,7 +4536,7 @@ where
             let res = constructor(child)?;
             let child_clone = child.clone();
 
-            let consensus = Consensus::new();
+            let consensus = Consensus::new("fixed point");
 
             let termination_check = async move || {
                 // Send local fixed point status to all peers.
