feldera/feldera

commit 17e0d13be32baf9f5dbca46767b53631d1447fc1

[dbsp] Introduce `EnableCount` type.
This is a wrapper around `Arc<AtomicUsize>` that makes it easier to see
what code deals with accumulator enable counts.

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/dbsp/src/operator/dynamic/accumulator.rs
@@ -26,7 +26,7 @@ use crate::{
     trace::{Batch, BatchReader, Spine, Trace},
 };
 
-circuit_cache_key!(AccumulatorId<C, B: Batch>(StreamId => (Stream<C, Option<Spine<B>>>, Arc<AtomicUsize>)));
+circuit_cache_key!(AccumulatorId<C, B: Batch>(StreamId => (Stream<C, Option<Spine<B>>>, EnableCount)));
 circuit_cache_key!(ShardedAccumulatorId<C, B: Batch>(StreamId => Stream<C, Option<Spine<B>>>));
 
 /// `TypedMapKey` entry used to share `enable_count` across instances of the same accumulator in multiple workers.
@@ -41,8 +41,52 @@ impl EnableCountId {
     }
 }
 
+/// Used to enable/disable an accumulator during a transaction.
+///
+/// Most accumulators (created with dyn_accumulate()) are always enabled.
+/// One special case is when the accumulator is used as part of an output handle
+/// to collect updates to the output stream within a transaction. In this case,
+/// if there is no output connector attached to the stream, there is no need to
+/// store the updates (which during backfill can amount to storing a complete copy
+/// of the table or view).
+///
+/// This flag enables this optimization by keeping track of the number of consumers
+/// of the accumulator's output. It is equal to the number of attached output connectors
+/// plus the number of times the same accumulator was instantiated as part of a regular
+/// (non-output) operator with dyn_accumulate().
+#[derive(Clone, Debug, Default)]
+pub struct EnableCount(Arc<AtomicUsize>);
+
+impl EnableCount {
+    /// Creates a new `EnableCount` that is initially disabled.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns true if this `EnableCount` is enabled.
+    pub fn is_enabled(&self) -> bool {
+        self.0.load(Ordering::Acquire) > 0
+    }
+
+    /// Enable the accumulator for this output stream.
+    ///
+    /// This may be paired with a later call to [EnableCount::disable], if the
+    /// stream should eventually be disabled.
+    pub fn enable(&self) {
+        self.0.fetch_add(1, Ordering::AcqRel);
+    }
+
+    /// Disable the accumulator for this output stream.
+    ///
+    /// This must be paired with a prior call to [EnableCount::enable].
+    pub fn disable(&self) {
+        let old = self.0.fetch_add(1, Ordering::AcqRel);
+        assert!(old > 0);
+    }
+}
+
 impl TypedMapKey<LocalStoreMarker> for EnableCountId {
-    type Value = Arc<AtomicUsize>;
+    type Value = EnableCount;
 }
 
 impl<C, B> Stream<C, B>
@@ -53,7 +97,7 @@ where
     /// See [`Stream::accumulate`].
     pub fn dyn_accumulate(&self, factories: &B::Factories) -> Stream<C, Option<Spine<B>>> {
         let (stream, enable_count) = self.dyn_accumulate_with_enable_count(factories);
-        enable_count.fetch_add(1, Ordering::AcqRel);
+        enable_count.enable();
 
         stream
     }
@@ -62,7 +106,7 @@ where
     pub fn dyn_accumulate_with_enable_count(
         &self,
         factories: &B::Factories,
-    ) -> (Stream<C, Option<Spine<B>>>, Arc<AtomicUsize>) {
+    ) -> (Stream<C, Option<Spine<B>>>, EnableCount) {
         self.circuit()
             .cache_get_or_insert_with(AccumulatorId::new(self.stream_id()), || {
                 let accumulator = Accumulator::<B>::new(factories, Location::caller());
@@ -94,20 +138,8 @@ where
     // Output batch sizes.
     output_batch_stats: BatchSizeStats,
 
-    /// Used to enable/disable the accumulator during a transaction.
-    ///
-    /// Most accumulators (created with dyn_accumulate()) are always enabled.
-    /// One special case is when the accumulator is used as part of an output handle
-    /// to collect updates to the output stream within a transaction. In this case,
-    /// if there is no output connector attached to the stream, there is no need to
-    /// store the updates (which during backfill can amount to storing a complete copy
-    /// of the table or view).
-    ///
-    /// This flag enables this optimization by keeping track of the number of consumers
-    /// of the accumulator's output. It is equal to the number of attached output connectors
-    /// plus the number of times the same accumulator was instantiated as part of a regular
-    /// (non-output) operator with dyn_accumulate().
-    enable_count: Arc<AtomicUsize>,
+    /// Used to enable/disable an accumulator during a transaction.
+    enable_count: EnableCount,
 
     /// Whether the accumulator is enabled during the current transaction.
     ///
@@ -124,13 +156,13 @@ where
 {
     pub fn new(factories: &B::Factories, location: &'static Location<'static>) -> Self {
         let enable_count = match Runtime::runtime() {
-            None => Arc::new(AtomicUsize::new(0)),
+            None => EnableCount::default(),
             Some(runtime) => {
                 let accumulator_id = runtime.sequence_next();
                 runtime
                     .local_store()
                     .entry(EnableCountId::new(accumulator_id))
-                    .or_insert_with(|| Arc::new(AtomicUsize::new(0)))
+                    .or_insert_with(EnableCount::default)
                     .value()
                     .clone()
             }
@@ -233,8 +265,7 @@ where
 
         if len > 0 {
             if self.enabled_during_current_transaction.is_none() {
-                self.enabled_during_current_transaction =
-                    Some(self.enable_count.load(Ordering::Acquire) > 0);
+                self.enabled_during_current_transaction = Some(self.enable_count.is_enabled());
             }
 
             if self.enabled_during_current_transaction == Some(true) {

crates/dbsp/src/operator/accumulator.rs
@@ -1,9 +1,4 @@
-use std::{
-    borrow::Cow,
-    marker::PhantomData,
-    panic::Location,
-    sync::{Arc, atomic::AtomicUsize},
-};
+use std::{borrow::Cow, marker::PhantomData, panic::Location};
 
 use crate::{
     Batch, BatchReader, Circuit, Scope, Stream,
@@ -11,6 +6,7 @@ use crate::{
         metadata::OperatorLocation,
         operator_traits::{BinaryOperator, Operator},
     },
+    operator::dynamic::accumulator::EnableCount,
     trace::{
         Batch as DynBatch, BatchReaderFactories, Spine as DynSpine, SpineSnapshot, WithSnapshot,
     },
@@ -46,7 +42,7 @@ where
     ///
     /// Used to instantiate accumulators for output connectors. See `Accumulator::enable_count` documentation.
     #[track_caller]
-    pub fn accumulate_with_enable_count(&self) -> (Stream<C, Option<Spine<B>>>, Arc<AtomicUsize>) {
+    pub fn accumulate_with_enable_count(&self) -> (Stream<C, Option<Spine<B>>>, EnableCount) {
         let factories = BatchReaderFactories::new::<B::Key, B::Val, B::R>();
 
         let (result, enable_count) = self.inner().dyn_accumulate_with_enable_count(&factories);

crates/adapterlib/src/catalog.rs
@@ -3,7 +3,6 @@ use std::any::Any;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt::{Debug, Formatter};
-use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
 
 use anyhow::Result as AnyResult;
@@ -19,6 +18,7 @@ use aws_sdk_dynamodb::types::AttributeValue;
 use dbsp::circuit::NodeId;
 use dbsp::dynamic::{ClonableTrait, DynData, DynVec, Erase, Factory};
 use dbsp::operator::StagedBuffers;
+use dbsp::operator::dynamic::accumulator::EnableCount;
 use dyn_clone::DynClone;
 use feldera_sqllib::Variant;
 use feldera_types::format::csv::CsvFormatConfig;
@@ -1224,7 +1224,7 @@ pub struct OutputCollectionHandles {
     /// Reference to the enable count of the accumulator used to collect updates to this stream.
     /// Incremented every time an output connector is attached to this stream; decremented when
     /// the output connector is detached.
-    pub enable_count: Arc<AtomicUsize>,
+    pub enable_count: EnableCount,
 }
 
 impl OutputCollectionHandles {

crates/adapters/src/controller.rs
@@ -5313,9 +5313,7 @@ impl OutputEndpoints {
         handles: OutputCollectionHandles,
         endpoint_descr: OutputEndpointDescr,
     ) {
-        // Enable the accumulator for this output stream.
-        // See `struct Accumulator::enable_count` for more details.
-        handles.enable_count.fetch_add(1, Ordering::Relaxed);
+        handles.enable_count.enable();
         self.by_stream
             .entry(endpoint_descr.stream_name.clone())
             .or_insert_with(|| (handles, BTreeSet::new()))
@@ -5329,9 +5327,7 @@ impl OutputEndpoints {
             self.by_stream
                 .get_mut(&descr.stream_name)
                 .map(|(handles, endpoints)| {
-                    // Disable the accumulator for this output stream.
-                    let count = handles.enable_count.fetch_sub(1, Ordering::Relaxed);
-                    assert!(count > 0);
+                    handles.enable_count.disable();
                     endpoints.remove(endpoint_id)
                 });
         })

crates/adapters/src/static_compile/catalog.rs
@@ -4,6 +4,7 @@ use crate::{Catalog, ControllerError, catalog::OutputCollectionHandles};
 use dbsp::circuit::Layout;
 use dbsp::circuit::circuit_builder::CircuitBase;
 use dbsp::dynamic::DynData;
+use dbsp::operator::dynamic::accumulator::EnableCount;
 use dbsp::trace::spine_async::WithSnapshot;
 use dbsp::typed_batch::{Spine, TypedBatch};
 use dbsp::utils::Tup1;
@@ -23,7 +24,6 @@ use std::any::TypeId;
 use std::collections::BTreeMap;
 use std::fmt::Debug;
 use std::mem::transmute;
-use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
 
 const INTERNED_STRING_RELATION_NAME: &str = "feldera_interned_strings";
@@ -79,7 +79,7 @@ impl Catalog {
         integrate: bool,
     ) -> (
         Stream<RootCircuit, Option<Spine<Z>>>,
-        Arc<AtomicUsize>,
+        EnableCount,
         Option<Stream<RootCircuit, Spine<Z>>>,
     )
     where
@@ -110,7 +110,7 @@ impl Catalog {
 
             // TODO: we currently don't support a form of shard_workers_accumulate that can be enabled at
             // runtime, so we return a bogus enabled flag that controls nothing.
-            (accumulated_stream, Arc::new(AtomicUsize::new(0)), integral)
+            (accumulated_stream, EnableCount::default(), integral)
         } else if shard {
             let accumulated_stream = stream.shard_accumulate();
 
@@ -122,7 +122,7 @@ impl Catalog {
 
             // TODO: we currently don't support a form of shard_accumulate that can be enabled at
             // runtime, so we return a bogus enabled flag that controls nothing.
-            (accumulated_stream, Arc::new(AtomicUsize::new(0)), integral)
+            (accumulated_stream, EnableCount::default(), integral)
         } else {
             let (accumulated_stream, enabled_count) = stream.accumulate_with_enable_count();
             let integral = if integrate {
@@ -1039,7 +1039,7 @@ fn index_schema(
 
 #[cfg(test)]
 mod test {
-    use std::{io::Write, ops::Deref, sync::atomic::Ordering};
+    use std::{io::Write, ops::Deref};
 
     use crate::{Catalog, CircuitCatalog, catalog::RecordFormat, test::TestStruct};
     use dbsp::Runtime;
@@ -1101,9 +1101,7 @@ mod test {
             .unwrap();
 
         let output_stream_handles = catalog.output_handles(&("Input_map".into())).unwrap();
-        output_stream_handles
-            .enable_count
-            .fetch_add(1, Ordering::AcqRel);
+        output_stream_handles.enable_count.enable();
 
         // Step 1: insert a couple of values.
 

crates/dbsp/src/operator/output.rs
@@ -7,6 +7,7 @@ use crate::{
         metadata::{BatchSizeStats, OUTPUT_BATCHES_STATS, OperatorMeta},
         operator_traits::{BinarySinkOperator, Operator, SinkOperator},
     },
+    operator::dynamic::accumulator::EnableCount,
     storage::file::to_bytes,
     trace::{
         BatchReader as DynBatchReader, BatchReaderFactories, SpineSnapshot as DynSpineSnapshot,
@@ -20,10 +21,7 @@ use std::{
     hash::{Hash, Hasher},
     marker::PhantomData,
     mem::transmute,
-    sync::{
-        Arc,
-        atomic::{AtomicUsize, Ordering},
-    },
+    sync::Arc,
 };
 use typedmap::TypedMapKey;
 
@@ -93,7 +91,7 @@ where
         persistent_id: Option<&str>,
     ) -> OutputHandle<SpineSnapshot<B>> {
         let (handle, enable_count, _) = self.accumulate_output_persistent_with_gid(persistent_id);
-        enable_count.fetch_add(1, Ordering::AcqRel);
+        enable_count.enable();
         handle
     }
 
@@ -107,11 +105,7 @@ where
     pub fn accumulate_output_persistent_with_gid(
         &self,
         persistent_id: Option<&str>,
-    ) -> (
-        OutputHandle<SpineSnapshot<B>>,
-        Arc<AtomicUsize>,
-        GlobalNodeId,
-    ) {
+    ) -> (OutputHandle<SpineSnapshot<B>>, EnableCount, GlobalNodeId) {
         let (accumulated, enable_count) = self.accumulate_with_enable_count();
         let (output_handle, gid) = self
             .circuit()

commit 7127033ecc0fa2955ad23ed92512cb01825c8fca

[dbsp] Fix EnableCount::disable().
This fixes a bug introduced in commit 17e0d13be32b ("[dbsp] Introduce
`EnableCount` type.").

Signed-off-by: Ben Pfaff <blp@feldera.com>

crates/dbsp/src/operator/dynamic/accumulator.rs
@@ -125,7 +125,7 @@ impl EnableCount {
     ///
     /// This must be paired with a prior call to [EnableCount::enable].
     pub fn disable(&self) {
-        let old = self.0.fetch_add(1, Ordering::AcqRel);
+        let old = self.0.fetch_sub(1, Ordering::AcqRel);
         assert!(old > 0);
     }
 }