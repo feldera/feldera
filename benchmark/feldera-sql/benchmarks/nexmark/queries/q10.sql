6147198e3e455b07ce4f458916fd634c9b04ebd8
Mihai Budiu <mbudiu@feldera.com>
2026-07-30 15:23:36 -0700

    [nexmark] Port Nexmark unit test data from Rust to SQL/Java
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- benchmark/feldera-sql/benchmarks/nexmark/queries/q10.sql
+++ benchmark/feldera-sql/benchmarks/nexmark/queries/q10.sql
@@ -1,3 +1,3 @@
 CREATE VIEW q10 AS -- PARTITIONED BY (dt, hm) AS
-SELECT auction, bidder, price, date_time, extra, FORMAT_DATE('yyyy-MM-dd', date_time), FORMAT_DATE('HH:mm', date_time)
+SELECT auction, bidder, price, date_time, extra, FORMAT_TIMESTAMP('%Y-%m-%d', date_time), FORMAT_TIMESTAMP('%H:%M', date_time)
 FROM bid;
