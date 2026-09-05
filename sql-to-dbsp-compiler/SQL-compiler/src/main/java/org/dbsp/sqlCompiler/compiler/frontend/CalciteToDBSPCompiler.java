2f2ec499effdc6b385d1583839e01c81b232e2a7
Mihai Budiu <mbudiu@feldera.com>
2026-09-04 21:48:58 -0700

    [SQL] Do not build TopK operators that carry duplicate columns
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/frontend/CalciteToDBSPCompiler.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/compiler/frontend/CalciteToDBSPCompiler.java
@@ -175,7 +175,6 @@ import org.dbsp.sqlCompiler.ir.expression.DBSPDirectComparatorExpression;
 import org.dbsp.sqlCompiler.ir.expression.DBSPEqualityComparatorExpression;
 import org.dbsp.sqlCompiler.ir.expression.DBSPExpression;
 import org.dbsp.sqlCompiler.ir.expression.DBSPFlatmap;
-import org.dbsp.sqlCompiler.ir.expression.DBSPIfExpression;
 import org.dbsp.sqlCompiler.ir.expression.DBSPNoComparatorExpression;
 import org.dbsp.sqlCompiler.ir.expression.DBSPOpcode;
 import org.dbsp.sqlCompiler.ir.expression.DBSPRawTupleExpression;
@@ -1328,173 +1327,6 @@ public class CalciteToDBSPCompiler extends RelVisitor
         return filter;
     }
 
-    /** Keeps track of the fields of a tuple that are used as key fields.
-     * Note that key fields may be duplicated.
-     * E.g., a key may contain fields 1, 3, and 1 of a tuple of size 5, in this order.
-     * [_, 0 and 2, _, 1, _]. */
-    static class KeyFields {
-        /** Indexes of fields that belong to the key, in the order they appear in the key: [1, 3, 1] */
-        private final List<Integer> keyFields;
-        private final Set<Integer> keyFieldSet;
-
-        public KeyFields() {
-            this.keyFieldSet = new HashSet<>();
-            this.keyFields = new ArrayList<>();
-        }
-
-        public KeyFields(KeyFields other) {
-            this.keyFieldSet = new HashSet<>(other.keyFieldSet);
-            this.keyFields = new ArrayList<>(other.keyFields);
-        }
-
-        public boolean isKeyField(int i) {
-            return this.keyFieldSet.contains(i);
-        }
-
-        /** Call this function to add key fields in the order they appear in the key */
-        public void add(int index) {
-            this.keyFieldSet.add(index);
-            this.keyFields.add(index);
-        }
-
-        public void addAll(List<Integer> indexes) {
-            for (int i: indexes)
-                this.add(i);
-        }
-
-        public void addAllUsed(@Nullable FieldUseMap map) {
-            if (map == null)
-                return;
-            if (map.isRef())
-                map = map.deref();
-            this.addAll(map.getUsedFields());
-        }
-
-        /** Extract the non-key fields from a value into a tuple, in the order they appear in the tuple.
-         * For our example, the result will be [e.0, e.2, e.4]
-         *
-         * @param e Expression that is a reference to a tuple.
-         * @return  A tuple containing just the non-key fields, in order.
-         */
-        DBSPTupleExpression nonKeyFields(DBSPExpression e) {
-            // Almost like nonKeyFields with 3 arguments, except we don't insert casts ever
-            // return this.nonKeyFields(var, e.getType().to(DBSPTypeTupleBase.class), 0);
-            List<DBSPExpression> fields = new ArrayList<>();
-            DBSPTypeTupleBase tuple = e.getType().to(DBSPTypeTupleBase.class);
-            for (int i = 0; i < tuple.size(); i++) {
-                if (this.isKeyField(i))
-                    continue;
-                fields.add(e.field(i).applyCloneIfNeeded());
-            }
-            return new DBSPTupleExpression(fields, false);
-        }
-
-        /** Extract the non-key fields from a value into a tuple, and insert casts if needed.
-         * For our example, the result will be [(desiredType[0])e.0, (desiredType[1])e.2, (desiredType[2])e.4]
-         *
-         * @param e        Expression that is a reference to a tuple.
-         * @param desiredType Type to cast resulting tuple to.
-         * @return          A tuple containing just the non-key fields, in order.
-         */
-        DBSPTupleExpression nonKeyFields(DBSPExpression e, DBSPTypeTupleBase desiredType) {
-            List<DBSPExpression> fields = new ArrayList<>();
-            Utilities.enforce(e.getType().getToplevelFieldCount() >= desiredType.size());
-            for (int i = 0; i < desiredType.size(); i++) {
-                if (this.isKeyField(i))
-                    continue;
-                fields.add(e.field(i)
-                        .applyCloneIfNeeded()
-                        .nullabilityCast(desiredType.getFieldType(i), DBSPCastExpression.CastType.SqlUnsafe));
-            }
-            return new DBSPTupleExpression(fields, false);
-        }
-
-        /** Extract the key fields from a value (e), in the order they have to appear in the key.
-         * For our example the result will be [(keyType[0])e.1, (keyType[1])e.3, (keyType[2])e.1]
-         *
-         * @param e       Expression that is a reference to a tuple.
-         * @param keyType Type for the key fields in the result; may introduce nullability casts.
-         * @return A tuple containing just the key fields, in order.
-         */
-        DBSPTupleExpression keyFields(DBSPExpression e, DBSPTypeTupleBase keyType) {
-            List<DBSPExpression> fields = new ArrayList<>();
-            Utilities.enforce(keyType.size() == this.size());
-            int index = 0;
-            for (int kf: this.keyFields) {
-                fields.add(e.field(kf)
-                        .applyCloneIfNeeded()
-                        .nullabilityCast(keyType.getFieldType(index), DBSPCastExpression.CastType.SqlUnsafe));
-                index++;
-            }
-            return new DBSPTupleExpression(fields, false);
-        }
-
-        /** Given a type for the row, return the type for the key */
-        DBSPTypeTuple keyType(DBSPTypeTupleBase row) {
-            List<DBSPType> fields = new ArrayList<>();
-            for (int kf: this.keyFields) {
-                fields.add(row.getFieldType(kf));
-            }
-            return new DBSPTypeTuple(fields);
-        }
-
-        /**
-         * Produce a list with original fields from two values: the key value, and the data value.
-         *
-         * @param key       Expression containing the key fields, in order.
-         * @param data      Expression containing the data fields, in order.
-         * @param result    A list of the unshuffled fields.  The result is
-         *                  [data.0, key.0, data.1, key.1, data.2]
-         *                  (In this case we expect key.0 == key.2 always)
-         */
-        void unshuffleKeyAndDataFields(DBSPExpression key, DBSPExpression data, List<DBSPExpression> result) {
-            Utilities.enforce(key.getType().getToplevelFieldCount() == this.size());
-            int size = this.keyFieldSet.size() + data.getType().getToplevelFieldCount();
-            int skipped = 0;
-            boolean nullableData = data.getType().deref().mayBeNull;
-            for (int i = 0; i < size; i++) {
-                @Nullable DBSPExpression checkNull = nullableData ? data.deref().is_null() : null;
-                if (this.isKeyField(i)) {
-                    int index = 0;
-                    for (int ki: this.keyFields) {
-                        // There could be multiple positions, stop at the first one
-                        if (ki == i)
-                            break;
-                        index++;
-                    }
-                    DBSPExpression keyField = key.deref().field(index).applyCloneIfNeeded();
-                    if (nullableData) {
-                        if (!keyField.getType().mayBeNull)
-                            keyField = keyField.some();
-                        keyField = new DBSPIfExpression(key.getNode(),
-                                checkNull, keyField.getType().none(), keyField);
-                    }
-                    result.add(keyField);
-                    skipped++;
-                } else {
-                    DBSPExpression dataField;
-                    dataField = data.deref().field(i - skipped).applyCloneIfNeeded();
-                    if (nullableData) {
-                        if (!dataField.getType().mayBeNull)
-                            dataField = dataField.some();
-                        dataField = new DBSPIfExpression(key.getNode(),
-                                checkNull, dataField.getType().none(), dataField);
-                    }
-                    result.add(dataField);
-                }
-            }
-        }
-
-        private int size() {
-            return this.keyFields.size();
-        }
-
-        @Override
-        public String toString() {
-            return "KeyFields: " + this.keyFields;
-        }
-    }
-
     /** Create the AND of a list of boolean expressions */
     DBSPExpression makeAnd(List<DBSPExpression> predicates) {
         DBSPExpression result = predicates.get(0);
@@ -2029,8 +1861,7 @@ public class CalciteToDBSPCompiler extends RelVisitor
                 // The following loop is an adapted version of extendedRight.keyFields
                 final DBSPVariablePath joinRightVar = joinVar.getType().var();
                 final List<DBSPExpression> rightKeyFields = new ArrayList<>();
-                for (int i = 0; i < extendedRight.size(); i++) {
-                    int index = extendedRight.keyFields.get(i);
+                for (int index : extendedRight.keyFieldIndexes()) {
                     rightKeyFields.add(joinRightVar.deref().field(leftColumns + index)
                             .applyCloneIfNeeded()
                             .cast(node, rightResultType.getFieldType(index), DBSPCastExpression.CastType.SqlUnsafe));
@@ -2768,7 +2599,7 @@ public class CalciteToDBSPCompiler extends RelVisitor
             shuffle.add(inputRowType.size() + elementIndex);
             elementIndex++;
             index++;
-            previousPartitionKeys = ga.partitionKeys;
+            previousPartitionKeys = ga.partitionKeyIndexes();
         }
 
         shuffle.addAll(later);
--- sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/TopKTests.java
+++ sql-to-dbsp-compiler/SQL-compiler/src/test/java/org/dbsp/sqlCompiler/compiler/sql/simple/TopKTests.java
@@ -1,7 +1,11 @@
 package org.dbsp.sqlCompiler.compiler.sql.simple;
 
+import org.dbsp.sqlCompiler.circuit.operator.DBSPIndexedTopKOperator;
 import org.dbsp.sqlCompiler.compiler.DBSPCompiler;
 import org.dbsp.sqlCompiler.compiler.sql.tools.SqlIoTest;
+import org.dbsp.sqlCompiler.compiler.visitors.outer.CircuitVisitor;
+import org.dbsp.sqlCompiler.ir.type.derived.DBSPTypeTuple;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeIndexedZSet;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -274,5 +278,116 @@ public class TopKTests extends SqlIoTest {
                  2          | S2     | 2011-07-30  | 2
                  3          | S1     | 2011-08-02  | 1
                 (5 rows)""");
+    }
+
+    /** Compile a query with one TopK operator and check the arities of its output key and value. */
+    void checkTopKShape(String query, int keyArity, int valueArity) {
+        var cc = this.getCC("CREATE VIEW V AS " + query);
+        cc.visit(new CircuitVisitor(cc.compiler) {
+            int topKs = 0;
+
+            @Override
+            public void postorder(DBSPIndexedTopKOperator node) {
+                DBSPTypeIndexedZSet output = node.getOutputIndexedZSetType();
+                Assert.assertEquals(keyArity, output.keyType.to(DBSPTypeTuple.class).size());
+                Assert.assertEquals(valueArity, output.elementType.to(DBSPTypeTuple.class).size());
+                this.topKs++;
+            }
+
+            @Override
+            public void endVisit() {
+                Assert.assertEquals(1, this.topKs);
+                super.endVisit();
+            }
+        });
+    }
+
+    /** The partition fields live in the TopK key, so the value holds only the other fields and the rank.
+     * Every query selects all columns, so the optimizer trims nothing. */
+    @Test
+    public void topKValueOmitsPartitionFields() {
+        String select = "SELECT ID, DocumentId, Status, DateCreated, rn FROM (SELECT *, ROW_NUMBER() OVER (";
+        String from = ") AS rn FROM DocumentStatusLog) WHERE rn <= 2;";
+        // Partition fields in the middle of the row
+        this.checkTopKShape(select + "PARTITION BY Status, DocumentId ORDER BY DateCreated DESC" + from, 2, 3);
+        // No partition: the whole row and the rank are in the value
+        this.checkTopKShape(select + "ORDER BY DateCreated" + from, 0, 5);
+        // Partition on the first field
+        this.checkTopKShape(select + "PARTITION BY ID ORDER BY DateCreated" + from, 1, 4);
+        // Partition on every field: only the rank is in the value
+        this.checkTopKShape(select + "PARTITION BY ID, DocumentId, Status, DateCreated ORDER BY ID" + from, 4, 1);
+        // Partition field that is also an ORDER BY field
+        this.checkTopKShape(select + "PARTITION BY DocumentId ORDER BY DocumentId, DateCreated" + from, 1, 4);
+        // TopK after another window aggregate over the same partition; its result is one more input field
+        this.checkTopKShape("""
+                SELECT ID, DocumentId, Status, DateCreated, r, rn FROM (
+                    SELECT *, RANK() OVER (PARTITION BY DocumentId ORDER BY ID) AS r,
+                              ROW_NUMBER() OVER (PARTITION BY DocumentId ORDER BY DateCreated) AS rn
+                    FROM DocumentStatusLog)
+                WHERE rn <= 1;""", 1, 5);
+        // TopK after a window aggregate over a different partition, so the TopK builds its own index
+        this.checkTopKShape("""
+                SELECT ID, DocumentId, Status, DateCreated, r, rn FROM (
+                    SELECT *, RANK() OVER (PARTITION BY Status ORDER BY ID) AS r,
+                              ROW_NUMBER() OVER (PARTITION BY DocumentId ORDER BY DateCreated) AS rn
+                    FROM DocumentStatusLog)
+                WHERE rn <= 1;""", 1, 5);
+    }
+
+    /** NULL partition keys form one partition and come back as NULL once the fields are reassembled. */
+    @Test
+    public void nullPartitionKey() {
+        var ccs = this.getCCS("""
+                CREATE TABLE T(a INT, b INT NOT NULL);
+                CREATE VIEW V AS
+                SELECT a, b, rn FROM (
+                    SELECT *, ROW_NUMBER() OVER (PARTITION BY a ORDER BY b) AS rn FROM T)
+                WHERE rn <= 1;""");
+        // Validated on Postgres
+        ccs.step("INSERT INTO T VALUES (NULL, 1), (NULL, 2), (1, 3), (1, 4), (2, 5);", """
+                 a | b | rn | weight
+                --------------------
+                   | 1 | 1  | 1
+                 1 | 3 | 1  | 1
+                 2 | 5 | 1  | 1""");
+    }
+
+    /** Partition fields in the middle and at the end of the row, with every column and the rank selected. */
+    @Test
+    public void partitionFieldsInsideRow() {
+        // Validated on Postgres
+        String paramQuery = """
+                SELECT ID, DocumentId, Status, DateCreated, rn FROM (
+                    SELECT *, ?() OVER (PARTITION BY Status, DocumentId ORDER BY DateCreated DESC) AS rn
+                    FROM DocumentStatusLog)
+                WHERE rn <= 2;
+                 ID | DocumentId | Status | DateCreated | rn
+                --------------------------------------------
+                 6  | 1          | S1     | 2011-09-02  | 1
+                 2  | 1          | S1     | 2011-07-29  | 2
+                 3  | 1          | S2     | 2011-07-30  | 1
+                 1  | 2          | S1     | 2011-07-28  | 1
+                 4  | 2          | S2     | 2011-07-30  | 1
+                 5  | 2          | S3     | 2011-08-01  | 1
+                 6  | 3          | S1     | 2011-08-02  | 1
+                (7 rows)
+
+                SELECT ID, DocumentId, Status, DateCreated, rn FROM (
+                    SELECT *, ?() OVER (PARTITION BY DateCreated ORDER BY ID) AS rn
+                    FROM DocumentStatusLog)
+                WHERE rn <= 1;
+                 ID | DocumentId | Status | DateCreated | rn
+                --------------------------------------------
+                 1  | 2          | S1     | 2011-07-28  | 1
+                 2  | 1          | S1     | 2011-07-29  | 1
+                 3  | 1          | S2     | 2011-07-30  | 1
+                 5  | 2          | S3     | 2011-08-01  | 1
+                 6  | 3          | S1     | 2011-08-02  | 1
+                 6  | 1          | S1     | 2011-09-02  | 1
+                (6 rows)""";
+        for (String function : new String[]{"RANK", "DENSE_RANK", "ROW_NUMBER"}) {
+            // No ties within any partition, so the three functions agree
+            this.qst(paramQuery.replace("?", function));
+        }
     }
 }
